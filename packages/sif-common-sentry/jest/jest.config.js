10eca39e729839e5d0ef2f09d515dfba62799fcc
Frode Hansen <frode.hansen@bekk.no>
2022-06-02 09:09:59 +0200

    sif-common-sentry

---
+++ packages/sif-common-sentry/jest/jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.ts?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test))\\.(jsx?|tsx?)$',
+    moduleNameMapper: {
+        '\\.(css|jpg|png|svg|less)$': '<rootDir>/node_modules/jest-css-modules',
+    },
+    testEnvironment: 'jsdom',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    globals: {
+        'ts-jest': {
+            tsconfig: './tsconfig.json',
+            babelConfig: {
+                plugins: ['@babel/plugin-proposal-object-rest-spread'],
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                env: {
+                    test: {
+                        plugins: ['@babel/plugin-transform-modules-commonjs'],
+                    },
+                },
+            },
+        },
+    },
+    moduleDirectories: ['../node_modules'],
+    rootDir: '../src',
+};
