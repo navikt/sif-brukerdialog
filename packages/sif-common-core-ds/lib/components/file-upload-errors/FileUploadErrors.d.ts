67f649aa2cf7feff39ed6d39dfe49465022b8ed4
Frode Hansen <frode.hansen@bekk.no>
2022-06-03 14:24:26 +0200

    start ettersending

--- packages/sif-common-core-ds/lib/components/file-upload-errors/FileUploadErrors.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-interface Props {
-    filesThatDidntGetUploaded: File[];
-}
-declare const FileUploadErrors: ({ filesThatDidntGetUploaded }: Props) => JSX.Element | null;
-export default FileUploadErrors;
--- packages/sif-common-core-ds/lib/content/picture-scanning-guide/status-icon/StatusIcon.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export declare type StatusIconStatusKey = 'suksess' | 'advarsel' | 'feil';
-export interface Props {
-    status: StatusIconStatusKey;
-    title?: string;
-    size?: number;
-}
-declare const StatusIkon: (props: Props) => JSX.Element;
-export default StatusIkon;
