24d02d28441db8d01816205b108116cbfe948f08
Frode Hansen <frode.hansen@bekk.no>
2022-06-03 12:56:19 +0200

    Slette filer

--- packages/appstatus-react-ds/lib/types/sanityObjects.d.ts
+++
@@ -1,7 +0,0 @@
-import { LocaleRichTextObject, SanityMessageType } from '.';
-export interface SanityStatusMessage {
-    _type: 'statusMessage';
-    message: LocaleRichTextObject;
-    messageType?: SanityMessageType;
-}
-//# sourceMappingURL=sanityObjects.d.ts.map
