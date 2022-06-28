ad05dc364e0e338170bc16ea636a139e79d808e6
Frode Hansen <frode.hansen@bekk.no>
2022-06-28 10:27:20 +0200

    Flytte kode ut av render

--- apps/deling-omsorgsdager/src/app/pages/intro-page/IntroForm.tsx
+++ apps/deling-omsorgsdager/src/app/pages/intro-page/IntroForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useIntl } from 'react-intl';
+import { IntlShape, useIntl } from 'react-intl';
 import ExpandableInfo from '@navikt/sif-common-core-ds/lib/components/expandable-info/ExpandableInfo';
 import { YesOrNo } from '@navikt/sif-common-core/lib/types/YesOrNo';
 import intlHelper from '@navikt/sif-common-core-ds/lib/utils/intlUtils';
@@ -18,147 +18,135 @@ interface Props {
 
 const IntroFormComponents = getTypedFormComponents<IntroFormField, IntroFormData, ValidationError>();
 
+const erStoppet = (values: IntroFormData): boolean => {
+    if (values.korona === YesOrNo.YES) {
+        if (values.mottakersArbeidssituasjonErOk === YesOrNo.NO) {
+            return true;
+        }
+        if (values.erArbeidstakerSnEllerFrilanser === YesOrNo.NO) {
+            return true;
+        }
+    } else {
+        if (values.mottakerErEktefelleEllerSamboer === YesOrNo.NO) {
+            if (values.mottakerSamværsforelder === YesOrNo.NO) {
+                return true;
+            }
+        }
+        if (values.harAleneomsorg === YesOrNo.NO) {
+            return true;
+        }
+        if (values.mottakersArbeidssituasjonErOk === YesOrNo.NO) {
+            return true;
+        }
+        if (values.erArbeidstakerSnEllerFrilanser === YesOrNo.NO) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const getArbeidstakerSnEllerFrilanserSpm = (intl: IntlShape, values: IntroFormData) => {
+    return (
+        <FormQuestion
+            legend={intlHelper(intl, `introForm.form.${IntroFormField.erArbeidstakerSnEllerFrilanser}.spm`)}
+            name={IntroFormField.erArbeidstakerSnEllerFrilanser}
+            validate={getYesOrNoValidator()}
+            showStop={values.erArbeidstakerSnEllerFrilanser === YesOrNo.NO}
+            stopMessage={intlHelper(intl, 'introForm.form.erArbeidstakerSnEllerFrilanser.stopMessage')}
+        />
+    );
+};
+
+const getMottakersArbeidssituasjonErOk = (intl: IntlShape, values: IntroFormData) => {
+    return (
+        <FormQuestion
+            legend={intlHelper(intl, `introForm.form.${IntroFormField.mottakersArbeidssituasjonErOk}.spm`)}
+            name={IntroFormField.mottakersArbeidssituasjonErOk}
+            validate={getYesOrNoValidator()}
+            showStop={values.mottakersArbeidssituasjonErOk === YesOrNo.NO}
+            stopMessage={intlHelper(intl, 'introForm.form.mottakersArbeidssituasjonErOk.stopMessage')}
+        />
+    );
+};
+
+const getCommonQuestions = (intl: IntlShape, values: IntroFormData, addAleneomssorgQuestion?: boolean) => {
+    return (
+        <>
+            {addAleneomssorgQuestion && (
+                <>
+                    <FormQuestion
+                        legend={intlHelper(intl, `introForm.form.${IntroFormField.harAleneomsorg}.spm`)}
+                        name={IntroFormField.harAleneomsorg}
+                        validate={getYesOrNoValidator()}
+                        showStop={values.harAleneomsorg === YesOrNo.NO}
+                        stopMessage={intlHelper(intl, 'introForm.form.harAleneomsorg.stopMessage')}
+                        description={
+                            <ExpandableInfo
+                                title={intlHelper(intl, 'introForm.form.harAleneomsorg.spm.nedtrekk.titel')}>
+                                {intlHelper(intl, 'introForm.form.harAleneomsorg.spm.nedtrekk.1')}
+                                <p>{intlHelper(intl, 'introForm.form.harAleneomsorg.spm.nedtrekk.2')}</p>
+                                <p>
+                                    <Lenke href={getLenker().merOmFastBostedOgSamvær}>
+                                        {intlHelper(intl, 'introForm.form.harAleneomsorg.spm.nedtrekk.link')}
+                                    </Lenke>
+                                </p>
+                            </ExpandableInfo>
+                        }
+                    />
+                    {values.harAleneomsorg === YesOrNo.YES && getArbeidstakerSnEllerFrilanserSpm(intl, values)}
+                    {values.harAleneomsorg === YesOrNo.YES &&
+                        values.erArbeidstakerSnEllerFrilanser === YesOrNo.YES &&
+                        getMottakersArbeidssituasjonErOk(intl, values)}
+                </>
+            )}
+            {addAleneomssorgQuestion === undefined && getArbeidstakerSnEllerFrilanserSpm(intl, values)}
+            {addAleneomssorgQuestion === undefined &&
+                values.erArbeidstakerSnEllerFrilanser === YesOrNo.YES &&
+                getMottakersArbeidssituasjonErOk(intl, values)}
+        </>
+    );
+};
+
+const getQuestions = (intl: IntlShape, values: IntroFormData) => {
+    switch (values.korona) {
+        case YesOrNo.YES:
+            return getCommonQuestions(intl, values);
+        case YesOrNo.NO:
+            return (
+                <>
+                    <FormQuestion
+                        legend={intlHelper(
+                            intl,
+                            `introForm.form.${IntroFormField.mottakerErEktefelleEllerSamboer}.spm`
+                        )}
+                        name={IntroFormField.mottakerErEktefelleEllerSamboer}
+                        validate={getYesOrNoValidator()}
+                    />
+                    {values.mottakerErEktefelleEllerSamboer === YesOrNo.YES && getCommonQuestions(intl, values, true)}
+                    {values.mottakerErEktefelleEllerSamboer === YesOrNo.NO && (
+                        <>
+                            <FormQuestion
+                                legend={intlHelper(
+                                    intl,
+                                    `introForm.form.${IntroFormField.mottakerSamværsforelder}.spm`
+                                )}
+                                name={IntroFormField.mottakerSamværsforelder}
+                                validate={getYesOrNoValidator()}
+                                showStop={values.mottakerSamværsforelder === YesOrNo.NO}
+                                stopMessage={intlHelper(intl, 'introForm.form.mottakerSamværsforelder.stopMessage')}
+                            />
+                            {values.mottakerSamværsforelder === YesOrNo.YES && getCommonQuestions(intl, values, true)}
+                        </>
+                    )}
+                </>
+            );
+        default:
+            return null;
+    }
+};
+
 const IntroForm: React.FunctionComponent<Props> = ({ onValidSubmit }) => {
     const intl = useIntl();
-
-    const getArbeidstakerSnEllerFrilanserSpm = (values: IntroFormData) => {
-        return (
-            <FormQuestion
-                legend={intlHelper(intl, `introForm.form.${IntroFormField.erArbeidstakerSnEllerFrilanser}.spm`)}
-                name={IntroFormField.erArbeidstakerSnEllerFrilanser}
-                validate={getYesOrNoValidator()}
-                showStop={values.erArbeidstakerSnEllerFrilanser === YesOrNo.NO}
-                stopMessage={intlHelper(intl, 'introForm.form.erArbeidstakerSnEllerFrilanser.stopMessage')}
-            />
-        );
-    };
-
-    const getMottakersArbeidssituasjonErOk = (values: IntroFormData) => {
-        return (
-            <FormQuestion
-                legend={intlHelper(intl, `introForm.form.${IntroFormField.mottakersArbeidssituasjonErOk}.spm`)}
-                name={IntroFormField.mottakersArbeidssituasjonErOk}
-                validate={getYesOrNoValidator()}
-                showStop={values.mottakersArbeidssituasjonErOk === YesOrNo.NO}
-                stopMessage={intlHelper(intl, 'introForm.form.mottakersArbeidssituasjonErOk.stopMessage')}
-            />
-        );
-    };
-
-    const getCommonQuestions = (values: IntroFormData, addAleneomssorgQuestion?: boolean) => {
-        return (
-            <>
-                {addAleneomssorgQuestion && (
-                    <>
-                        <FormQuestion
-                            legend={intlHelper(intl, `introForm.form.${IntroFormField.harAleneomsorg}.spm`)}
-                            name={IntroFormField.harAleneomsorg}
-                            validate={getYesOrNoValidator()}
-                            showStop={values.harAleneomsorg === YesOrNo.NO}
-                            stopMessage={intlHelper(intl, 'introForm.form.harAleneomsorg.stopMessage')}
-                            description={
-                                <ExpandableInfo
-                                    title={intlHelper(intl, 'introForm.form.harAleneomsorg.spm.nedtrekk.titel')}>
-                                    {intlHelper(intl, 'introForm.form.harAleneomsorg.spm.nedtrekk.1')}
-                                    <p>{intlHelper(intl, 'introForm.form.harAleneomsorg.spm.nedtrekk.2')}</p>
-                                    <p>
-                                        <Lenke href={getLenker().merOmFastBostedOgSamvær}>
-                                            {intlHelper(intl, 'introForm.form.harAleneomsorg.spm.nedtrekk.link')}
-                                        </Lenke>
-                                    </p>
-                                </ExpandableInfo>
-                            }
-                        />
-                        {values.harAleneomsorg === YesOrNo.YES && getArbeidstakerSnEllerFrilanserSpm(values)}
-                        {values.harAleneomsorg === YesOrNo.YES &&
-                            values.erArbeidstakerSnEllerFrilanser === YesOrNo.YES &&
-                            getMottakersArbeidssituasjonErOk(values)}
-                    </>
-                )}
-                {addAleneomssorgQuestion === undefined && getArbeidstakerSnEllerFrilanserSpm(values)}
-                {addAleneomssorgQuestion === undefined &&
-                    values.erArbeidstakerSnEllerFrilanser === YesOrNo.YES &&
-                    getMottakersArbeidssituasjonErOk(values)}
-            </>
-        );
-    };
-
-    const getQuestions = (values: IntroFormData) => {
-        switch (values.korona) {
-            case YesOrNo.YES:
-                return getCommonQuestions(values);
-            case YesOrNo.NO:
-                return (
-                    <>
-                        <FormQuestion
-                            legend={intlHelper(
-                                intl,
-                                `introForm.form.${IntroFormField.mottakerErEktefelleEllerSamboer}.spm`
-                            )}
-                            name={IntroFormField.mottakerErEktefelleEllerSamboer}
-                            validate={getYesOrNoValidator()}
-                        />
-                        {values.mottakerErEktefelleEllerSamboer === YesOrNo.YES && getCommonQuestions(values, true)}
-                        {values.mottakerErEktefelleEllerSamboer === YesOrNo.NO && (
-                            <>
-                                <FormQuestion
-                                    legend={intlHelper(
-                                        intl,
-                                        `introForm.form.${IntroFormField.mottakerSamværsforelder}.spm`
-                                    )}
-                                    name={IntroFormField.mottakerSamværsforelder}
-                                    validate={getYesOrNoValidator()}
-                                    showStop={values.mottakerSamværsforelder === YesOrNo.NO}
-                                    stopMessage={intlHelper(intl, 'introForm.form.mottakerSamværsforelder.stopMessage')}
-                                />
-                                {values.mottakerSamværsforelder === YesOrNo.YES && getCommonQuestions(values, true)}
-                            </>
-                        )}
-                    </>
-                );
-            default:
-                return null;
-        }
-    };
-
-    const erStoppet = (values: IntroFormData): boolean => {
-        if (values.korona === YesOrNo.YES) {
-            if (values.erArbeidstakerSnEllerFrilanser === YesOrNo.NO) {
-                return true;
-            }
-            if (values.mottakersArbeidssituasjonErOk === YesOrNo.NO) {
-                return true;
-            }
-        } else {
-            if (values.mottakerErEktefelleEllerSamboer === YesOrNo.NO) {
-                if (values.mottakerSamværsforelder === YesOrNo.NO) {
-                    return true;
-                }
-                if (values.harAleneomsorg === YesOrNo.NO) {
-                    return true;
-                }
-                if (values.mottakersArbeidssituasjonErOk === YesOrNo.NO) {
-                    return true;
-                }
-                if (values.erArbeidstakerSnEllerFrilanser === YesOrNo.NO) {
-                    return true;
-                }
-            }
-            if (values.mottakerErEktefelleEllerSamboer === YesOrNo.YES) {
-                if (values.harAleneomsorg === YesOrNo.NO) {
-                    return true;
-                }
-                if (values.mottakersArbeidssituasjonErOk === YesOrNo.NO) {
-                    return true;
-                }
-                if (values.erArbeidstakerSnEllerFrilanser === YesOrNo.NO) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    };
-
     return (
         <IntroFormComponents.FormikWrapper
             initialValues={introFormInitialValues}
@@ -183,7 +171,7 @@ const IntroForm: React.FunctionComponent<Props> = ({ onValidSubmit }) => {
                                     </ExpandableInfo>
                                 }
                             />
-                            {getQuestions(values)}
+                            {getQuestions(intl, values)}
                         </IntroFormComponents.Form>
                     </section>
                 );
