ac03e9e800738481ac8c40e2117e0ad1baa1364c
Frode Hansen <frode.hansen@bekk.no>
2022-08-09 09:34:40 +0200

    Legge til hooks + level for SoknadHeader

---
+++ packages/sif-common-core-ds/src/hooks/useDecoratorLanguageSelector.ts
@@ -0,0 +1,20 @@
+import useEffectOnce from './useEffectOnce';
+import { setAvailableLanguages, onLanguageSelect, Locale } from '@navikt/nav-dekoratoren-moduler';
+
+function useDecoratorLanguageSelector(languages: Locale[], onChangeLocale: (locale: Locale) => void) {
+    useEffectOnce(() => {
+        if (languages.length > 1) {
+            setAvailableLanguages(
+                languages.map((locale) => ({
+                    locale,
+                    handleInApp: true,
+                }))
+            );
+        }
+        onLanguageSelect((language) => {
+            onChangeLocale(language.locale as Locale);
+        });
+    });
+}
+
+export default useDecoratorLanguageSelector;
