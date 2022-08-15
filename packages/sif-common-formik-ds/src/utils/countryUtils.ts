5a568f36f6e26a99565b9a69da861684a952475d
Frode Hansen <frode.hansen@bekk.no>
2022-08-15 18:12:31 +0200

    Fallback på landnavn

--- packages/sif-common-formik-ds/src/utils/countryUtils.ts
+++ packages/sif-common-formik-ds/src/utils/countryUtils.ts
@@ -3,8 +3,18 @@ import * as countries from 'i18n-iso-countries';
 countries.registerLocale(require('i18n-iso-countries/langs/nb.json'));
 countries.registerLocale(require('i18n-iso-countries/langs/nn.json'));
 
+export const getLocaleKey = (locale: string): string => {
+    switch (locale) {
+        case 'nn':
+        case 'no-NN':
+            return 'nn';
+        default:
+            return 'nb';
+    }
+};
+
 export const getCountryName = (alphaCode: string, locale: string): string => {
-    return countries.getName(alphaCode, locale);
+    return countries.getName(alphaCode, getLocaleKey(locale));
 };
 
 export const getCountries = () => countries;
