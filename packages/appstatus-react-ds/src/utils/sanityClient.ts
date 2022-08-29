97540118accd1ab4f077491f2e8d15ed521361ae
Frode Hansen <frode.hansen@bekk.no>
2022-08-29 14:40:45 +0200

    Legge til API_VERSION i sanityConfig

--- packages/appstatus-react-ds/src/utils/sanityClient.ts
+++ packages/appstatus-react-ds/src/utils/sanityClient.ts
@@ -1,11 +1,14 @@
 import sanityClient from '@sanity/client';
 import { SanityConfig } from '../types';
 
+const API_VERSION = '2022-03-07';
+
 export const getAppSanityClient = ({ projectId, dataset, token = '' }: SanityConfig) => {
     return sanityClient({
         projectId,
         dataset,
         token,
         useCdn: false,
+        apiVersion: API_VERSION,
     });
 };
