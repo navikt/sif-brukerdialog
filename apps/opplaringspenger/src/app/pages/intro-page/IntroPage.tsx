c71473241379a18bd5366a2a84e0003b21812719
Frode Hansen <frode.hansen@bekk.no>
2022-10-26 13:48:38 +0200

    Page på intro, velkommen og sendt

--- apps/opplaringspenger/src/app/pages/intro-page/IntroPage.tsx
+++ apps/opplaringspenger/src/app/pages/intro-page/IntroPage.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Page from '@navikt/sif-common-core-ds/lib/components/page/Page';
 import { SøknadRoutes } from '../../types/SøknadRoutes';
+import { Heading, Ingress } from '@navikt/ds-react';
 
 const IntroPage = () => {
     return (
-        <>
-            Intro
-            <p>
-                <Link to={SøknadRoutes.INNLOGGET_ROOT}>Gå til søknad</Link>
-            </p>
-        </>
+        <Page title="Introside">
+            <Heading level="1" size="large">
+                Søknad om opplæringspenger
+            </Heading>
+            <>
+                <Ingress>Introduksjonsside</Ingress>
+                <p>
+                    <Link to={SøknadRoutes.VELKOMMEN}>Gå til søknad</Link>
+                </p>
+            </>
+        </Page>
     );
 };
 
