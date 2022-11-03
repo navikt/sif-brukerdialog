97ea0714e1295528dd034f2f7e6f7ec6ade47b0b
Frode Hansen <frode.hansen@bekk.no>
2022-11-03 09:19:57 +0100

    Skjermdeling-fiks

--- apps/deling-omsorgsdager/server.js
+++ apps/deling-omsorgsdager/server.js
@@ -12,8 +12,15 @@ const server = express();
 server.use(
     helmet({
         contentSecurityPolicy: false,
+        crossOriginEmbedderPolicy: false,
     })
 );
+
+server.use((req, res, next) => {
+    res.set('X-XSS-Protection', '1; mode=block');
+    res.set('Feature-Policy', "geolocation 'none'; microphone 'none'; camera 'none'");
+    next();
+});
 
 server.use(compression());
 server.set('views', path.resolve(`${__dirname}/dist`));
@@ -42,6 +49,13 @@ const renderApp = (decoratorFragments) =>
 
 const startServer = (html) => {
     server.use(`${process.env.PUBLIC_PATH}/dist/js`, express.static(path.resolve(__dirname, 'dist/js')));
+    server.use(`${process.env.PUBLIC_PATH}/dist/css`, (req, res, next) => {
+        const requestReferer = req.headers.referer;
+        if (requestReferer !== undefined && requestReferer === 'https://nav.psplugin.com/') {
+            res.set('cross-origin-resource-policy', 'cross-origin');
+        }
+        next();
+    });
     server.use(`${process.env.PUBLIC_PATH}/dist/css`, express.static(path.resolve(__dirname, 'dist/css')));
     server.get(`${process.env.PUBLIC_PATH}/health/isAlive`, (req, res) => res.sendStatus(200));
     server.get(`${process.env.PUBLIC_PATH}/health/isReady`, (req, res) => res.sendStatus(200));
