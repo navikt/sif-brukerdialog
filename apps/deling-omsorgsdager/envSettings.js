440b8980f44869095c48067031cf79eefa4f6c4c
Frode Hansen <frode.hansen@bekk.no>
2022-11-04 11:14:37 +0100

    Oppdatere env.settings

--- apps/deling-omsorgsdager/envSettings.js
+++ apps/deling-omsorgsdager/envSettings.js
@@ -1,25 +1,18 @@
-/* eslint-disable no-console */
 const process = require('process');
 require('dotenv').config();
 
 const envSettings = () => {
-    const API_URL = process.env.API_URL;
-    const PUBLIC_PATH = process.env.PUBLIC_PATH;
-    const APP_VERSION = process.env.APP_VERSION;
-    const LOGIN_URL = process.env.LOGIN_URL;
-    const APPSTATUS_PROJECT_ID = process.env.APPSTATUS_PROJECT_ID;
-    const APPSTATUS_DATASET = process.env.APPSTATUS_DATASET;
-    const USE_AMPLITUDE = process.env.USE_AMPLITUDE;
-
     const appSettings = `
      window.appSettings = {
-         API_URL: '${API_URL}',
-         PUBLIC_PATH: '${PUBLIC_PATH}',
-         APP_VERSION: '${APP_VERSION}',
-         LOGIN_URL: '${LOGIN_URL}',
-         APPSTATUS_PROJECT_ID: '${APPSTATUS_PROJECT_ID}',
-         APPSTATUS_DATASET: '${APPSTATUS_DATASET}',
-         USE_AMPLITUDE: '${USE_AMPLITUDE}',
+         API_URL: '${process.env.API_URL}',
+         FRONTEND_API_PATH: '${process.env.FRONTEND_API_PATH}',
+         FRONTEND_VEDLEGG_URL:' ${process.env.FRONTEND_VEDLEGG_URL}',
+         PUBLIC_PATH: '${process.env.PUBLIC_PATH}',
+         APP_VERSION: '${process.env.APP_VERSION}',
+         LOGIN_URL: '${process.env.LOGIN_URL}',
+         APPSTATUS_PROJECT_ID: '${process.env.APPSTATUS_PROJECT_ID}',
+         APPSTATUS_DATASET: '${process.env.APPSTATUS_DATASET}',
+         USE_AMPLITUDE: '${process.env.USE_AMPLITUDE}',
      };`
         .trim()
         .replace(/ /g, '');
