6b61db31f83bade67596f1e220b4ffa2f5a13bbc
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-11-16 08:55:24 +0100

    Fix

--- apps/sif-ettersending/src/app/components/pages/confirmation-page/ConfirmationPage.tsx
+++ apps/sif-ettersending/src/app/components/pages/confirmation-page/ConfirmationPage.tsx
@@ -69,7 +69,6 @@ const ConfirmationPage = ({ søknadstype }: Props) => {
                                         target="_blank">
                                         <FormattedMessage id="page.confirmation.check.3.2" />
                                     </Link>
-                                    <FormattedMessage id="page.confirmation.check.3.3" />
                                 </li>
                             </ul>
                         </>
