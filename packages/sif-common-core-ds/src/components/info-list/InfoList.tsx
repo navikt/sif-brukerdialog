486bb5d7f390917dd6d56c887794979a04d58898
Frode Hansen <frode.hansen@bekk.no>
2022-12-04 16:47:42 +0100

    Cleanup infolist

--- packages/sif-common-core-ds/src/components/info-list/InfoList.tsx
+++ packages/sif-common-core-ds/src/components/info-list/InfoList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './infolist.css';
 
 const InfoList: React.FunctionComponent<React.HTMLAttributes<HTMLUListElement> & { className?: string }> = (props) => (
     <ul {...props} className={`infoList ${props.className}`} />
