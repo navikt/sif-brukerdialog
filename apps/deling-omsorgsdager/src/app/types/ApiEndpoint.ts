11e33442d9a623f71336023a1e91638b4e714c51
Frode Hansen <frode.hansen@bekk.no>
2022-12-19 07:56:26 +0100

    Slette deling av omsorgsdager app

--- apps/deling-omsorgsdager/src/app/types/ApiEndpoint.ts
+++
@@ -1,9 +0,0 @@
-export enum ApiEndpoint {
-    'soker' = 'oppslag/soker',
-    'barn' = 'oppslag/barn',
-    'mellomlagring' = 'mellomlagring/OMSORGSDAGER_MELDING',
-    'sendMeldingOverføring' = 'omsorgsdager-melding-fordeling/innsending',
-    'sendMeldingFordeling' = 'omsorgsdager-melding-fordeling/innsending',
-    'sendMeldingKoronaoverføring' = 'omsorgsdager-melding-koronaoverforing/innsending',
-    'samværsavtale' = 'vedlegg',
-}
