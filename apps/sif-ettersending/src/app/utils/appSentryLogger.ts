180aed7747aed0e139a6782783c628441bb4dd10
frodehansen2 <frode.hansen@bekk.no>
2023-01-31 09:46:54 +0100

    Ignorer 401 feil i ettersending (#500)

--- apps/sif-ettersending/src/app/utils/appSentryLogger.ts
+++ apps/sif-ettersending/src/app/utils/appSentryLogger.ts
@@ -1,5 +1,5 @@
 import getSentryLoggerForApp from '@navikt/sif-common-sentry';
 
-const appSentryLogger = getSentryLoggerForApp('ettersending', ['sykdom-i-familien']);
+const appSentryLogger = getSentryLoggerForApp('ettersending', ['sykdom-i-familien'], [/401/]);
 
 export default appSentryLogger;
