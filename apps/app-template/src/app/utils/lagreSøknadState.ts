1132cbabee48fc9f848e9cccf55e75c6057d5813
frodehansen2 <frode.hansen@bekk.no>
2023-02-24 13:42:35 +0100

    Slett template app (#566)

--- apps/app-template/src/app/utils/lagreSøknadState.ts
+++
@@ -1,6 +0,0 @@
-import søknadStateEndpoint from '../api/endpoints/søknadStateEndpoint';
-import { SøknadContextState } from '../types/SøknadContextState';
-
-export const lagreSøknadState = (state: SøknadContextState) => {
-    return søknadStateEndpoint.update(state);
-};
--- apps/app-template/src/storybook/mock-data/index.ts
+++
@@ -1,8 +0,0 @@
-import { Søker } from '../../app/types/Søker';
-
-export const SøkerMock: Søker = {
-    fødselsnummer: '30086421581',
-    fornavn: 'GODSLIG',
-    kjønn: 'M',
-    etternavn: 'KRONJUVEL',
-};
