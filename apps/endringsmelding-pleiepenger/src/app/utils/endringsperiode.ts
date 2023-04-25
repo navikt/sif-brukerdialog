e044ef1757e62758dc54e29e384fee11f4e3f784
frodehansen2 <frode.hansen@bekk.no>
2023-04-25 13:28:52 +0200

    Utvide tillatt endringsperiode (#773)
    * Utvide tillatt endringsperiode til 6 mnd bak i tid

--- apps/endringsmelding-pleiepenger/src/app/utils/endringsperiode.ts
+++ apps/endringsmelding-pleiepenger/src/app/utils/endringsperiode.ts
@@ -6,8 +6,8 @@ dayjs.extend(minMax);
 
 export const getEndringsdato = (): Date => new Date();
 
-/** Start av inneværende måned og tre måneder bakover og 12 måneder fremover */
+/** Start av inneværende måned og seks måneder bakover og 12 måneder fremover */
 export const getTillattEndringsperiode = (endringsdato: Date): DateRange => ({
-    from: dayjs(endringsdato).startOf('month').subtract(3, 'months').startOf('day').toDate(),
+    from: dayjs(endringsdato).startOf('month').subtract(6, 'months').startOf('day').toDate(),
     to: dayjs(endringsdato).endOf('month').add(12, 'months').startOf('day').toDate(),
 });
