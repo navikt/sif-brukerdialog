da89e269fed21fbcac14d47fe893a541e1753ecb
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-05-03 13:56:57 +0200

    fix5

--- apps/omsorgsdager-aleneomsorg-dialog/e2e/cypress/e2e/contextConfig.ts
+++ apps/omsorgsdager-aleneomsorg-dialog/e2e/cypress/e2e/contextConfig.ts
@@ -17,11 +17,11 @@ export const contextConfig = (props?: ConfigProps) => {
     const { mellomlagring, step } = props || {};
 
     beforeEach('intercept mellomlagring og levere tomt objekt', () => {
-        cy.intercept(`GET`, `${API}/mellomlagring/OMSORGSPENGER_MIDLERTIDIG_ALENE`, mellomlagring || { noData: 1 });
-        cy.intercept(`DELETE`, `${API}/mellomlagring/OMSORGSPENGER_MIDLERTIDIG_ALENE`, mellomlagring || {});
-        cy.intercept(`PUT`, `${API}/mellomlagring/OMSORGSPENGER_MIDLERTIDIG_ALENE`, {});
-        cy.intercept(`POST`, `${API}/mellomlagring/OMSORGSPENGER_MIDLERTIDIG_ALENE`, {});
-        cy.intercept(`POST`, `${API}/omsorgspenger-midlertidig-alene/innsending`, {});
+        cy.intercept(`GET`, `${API}/mellomlagring/OMSORGSDAGER_ALENEOMSORG`, mellomlagring || { noData: 1 });
+        cy.intercept(`DELETE`, `${API}/mellomlagring/OMSORGSDAGER_ALENEOMSORG`, mellomlagring || {});
+        cy.intercept(`PUT`, `${API}/mellomlagring/OMSORGSDAGER_ALENEOMSORG`, {});
+        cy.intercept(`POST`, `${API}/mellomlagring/OMSORGSDAGER_ALENEOMSORG`, {});
+        cy.intercept(`POST`, `${API}/omsorgsdager-aleneomsorg/innsending`, {});
         cy.intercept('GET', `${API}/oppslag/soker`, cyApiMockData.søkerMock);
         cy.intercept('GET', `${API}/oppslag/barn`, cyApiMockData.barnMock);
     });
