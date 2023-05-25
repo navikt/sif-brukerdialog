1c805f6aa9302f594a0d9396aa844effe9084995
Frode Hansen <frode.hansen@bekk.no>
2023-05-25 08:12:16 +0200

    Kommentar

--- apps/endringsmelding-pleiepenger/src/app/modules/persist-step-form-values/PersistStepFormValues.tsx
+++ apps/endringsmelding-pleiepenger/src/app/modules/persist-step-form-values/PersistStepFormValues.tsx
@@ -9,7 +9,10 @@ interface Props {
 }
 
 /**
- * Oppdaterer StepFormValuesContext når formik values endrer seg
+ * Oppdaterer StepFormValuesContext når formik values endrer seg. Brukes til å kontrollere om
+ * det er mismatch mellom søknadsdata og formValues, som kan skje hvis bruker navigerer med
+ * browser back/forward i stedet for submit, og for å mellomlagre skjemadata for et steg
+ * når bruker velger "Tilbake"-knappen.
  */
 const PersistStepFormValues: React.FunctionComponent<Props> = ({ stepId, onChange }) => {
     const { setStepFormValues } = useStepFormValuesContext();
