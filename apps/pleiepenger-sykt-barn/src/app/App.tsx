6f8bddb582de968c59a49d67405070531bc22982
Frode Hansen <frode.hansen@bekk.no>
2023-05-31 15:33:23 +0200

    App

--- apps/pleiepenger-sykt-barn/src/app/App.tsx
+++ apps/pleiepenger-sykt-barn/src/app/App.tsx
@@ -1,24 +1,17 @@
-import { SanityConfig } from '@navikt/appstatus-react-ds';
 import { Modal } from '@navikt/ds-react';
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route } from 'react-router-dom';
 import { AmplitudeProvider } from '@navikt/sif-common-amplitude/lib';
-import AppStatusWrapper from '@navikt/sif-common-core-ds/lib/components/app-status-wrapper/AppStatusWrapper';
 import SifAppWrapper from '@navikt/sif-common-core-ds/lib/components/sif-app-wrapper/SifAppWrapper';
-import { Locale } from '@navikt/sif-common-core-ds/lib/types/Locale';
-import dayjs from 'dayjs';
-import ApplicationWrapper from './components/application-wrapper/ApplicationWrapper';
+import { SoknadApplication, SoknadApplicationCommonRoutes } from '@navikt/sif-common-soknad-ds';
 import RouteConfig from './config/routeConfig';
+import { applicationIntlMessages } from './i18n';
 import GeneralErrorPage from './pages/general-error-page/GeneralErrorPage';
-import UnavailablePage from './pages/unavailable-page/UnavailablePage';
 import Søknad from './søknad/Søknad';
 import appSentryLogger from './utils/appSentryLogger';
 import { getEnvironmentVariable } from './utils/envUtils';
-import { getLocaleFromSessionStorage, setLocaleInSessionStorage } from './utils/localeUtils';
 import '@navikt/ds-css';
 import '@navikt/sif-common-core-ds/lib/styles/sif-ds-theme.css';
-// import './app.less';
 
 export const APPLICATION_KEY = 'pleiepengesoknad';
 export const SKJEMANAVN = 'Søknad om pleiepenger';
@@ -34,50 +27,40 @@ const ensureBaseNameForReactRouter = (routerBaseUrl: string) => {
 
 appSentryLogger.init();
 
-const localeFromSessionStorage = getLocaleFromSessionStorage();
-dayjs.locale(localeFromSessionStorage);
-
-const getAppStatusSanityConfig = (): SanityConfig | undefined => {
-    const projectId = getEnvironmentVariable('APPSTATUS_PROJECT_ID');
-    const dataset = getEnvironmentVariable('APPSTATUS_DATASET');
-    return !projectId || !dataset ? undefined : { projectId, dataset };
-};
-
 const App = () => {
-    const [locale, setLocale] = React.useState<Locale>(localeFromSessionStorage);
-
-    const appStatusSanityConfig = getAppStatusSanityConfig();
     const publicPath = getEnvironmentVariable('PUBLIC_PATH');
-
-    const content = (
-        <Routes>
-            <Route path="/" element={<Navigate to={RouteConfig.SØKNAD_ROUTE_PREFIX} replace={true} />} />
-            <Route path={`${RouteConfig.SØKNAD_ROUTE_PREFIX}/*`} element={<Søknad />} />
-            <Route path={RouteConfig.ERROR_PAGE_ROUTE} element={<GeneralErrorPage />} />
-        </Routes>
-    );
 
     return (
         <SifAppWrapper>
             <AmplitudeProvider applicationKey={APPLICATION_KEY}>
-                <ApplicationWrapper
-                    locale={locale}
-                    publicPath={publicPath}
-                    onChangeLocale={(activeLocale: Locale) => {
-                        setLocaleInSessionStorage(activeLocale);
-                        setLocale(activeLocale);
-                    }}>
-                    {appStatusSanityConfig ? (
-                        <AppStatusWrapper
-                            applicationKey={APPLICATION_KEY}
-                            unavailableContentRenderer={() => <UnavailablePage />}
-                            sanityConfig={appStatusSanityConfig}
-                            contentRenderer={() => content}
-                        />
-                    ) : (
-                        content
-                    )}
-                </ApplicationWrapper>
+                <SoknadApplication
+                    appName={SKJEMANAVN}
+                    intlMessages={applicationIntlMessages}
+                    sentryKey={APPLICATION_KEY}
+                    appStatus={{
+                        applicationKey: APPLICATION_KEY,
+                        sanityConfig: {
+                            projectId: getEnvironmentVariable('APPSTATUS_PROJECT_ID'),
+                            dataset: getEnvironmentVariable('APPSTATUS_DATASET'),
+                        },
+                    }}
+                    publicPath={publicPath}>
+                    <SoknadApplicationCommonRoutes
+                        contentRoutes={[
+                            <Route
+                                key="index"
+                                path="/"
+                                element={<Navigate to={RouteConfig.SØKNAD_ROUTE_PREFIX} replace={true} />}
+                            />,
+                            <Route key="søknad" path={`${RouteConfig.SØKNAD_ROUTE_PREFIX}/*`} element={<Søknad />} />,
+                            <Route
+                                key="errorpage"
+                                path={RouteConfig.ERROR_PAGE_ROUTE}
+                                element={<GeneralErrorPage />}
+                            />,
+                        ]}
+                    />
+                </SoknadApplication>
             </AmplitudeProvider>
         </SifAppWrapper>
     );
---
+++ apps/pleiepenger-sykt-barn/src/app/i18n/index.ts
@@ -0,0 +1,33 @@
+import { allCommonMessages } from '@navikt/sif-common-core-ds/lib/i18n/allCommonMessages';
+import { MessageFileFormat } from '@navikt/sif-common-core-ds/lib/types/MessageFileFormat';
+import bostedMessages from '@navikt/sif-common-forms-ds/lib/forms/bosted-utland/bostedUtlandMessages';
+import ferieuttakMessages from '@navikt/sif-common-forms-ds/lib/forms/ferieuttak/ferieuttakMessages';
+import opptjeningUtlandMessages from '@navikt/sif-common-forms-ds/lib/forms/opptjening-utland/opptjeningUtlandMessages';
+import tidsperiodeMessages from '@navikt/sif-common-forms-ds/lib/forms/tidsperiode/tidsperiodeMessages';
+import utenlandskNæringMessages from '@navikt/sif-common-forms-ds/lib/forms/utenlandsk-næring/utenlandskNæringMessages';
+import utenlandsoppholdMessages from '@navikt/sif-common-forms-ds/lib/forms/utenlandsopphold/utenlandsoppholdMessages';
+import virksomhetMessages from '@navikt/sif-common-forms-ds/lib/forms/virksomhet/virksomhetMessages';
+import { sifCommonPleiepengerMessages } from '../local-sif-common-pleiepenger/i18n';
+import { velkommenPageMessages } from '../pages/welcoming-page/velkommenPageMessages';
+import omsorgstilbudMessages from './omsorgstilbudMessages';
+
+export const appBokmålstekster = require('./nb.json');
+
+const bokmålstekster = {
+    ...allCommonMessages.nb,
+    ...utenlandsoppholdMessages.nb,
+    ...bostedMessages.nb,
+    ...virksomhetMessages.nb,
+    ...tidsperiodeMessages.nb,
+    ...ferieuttakMessages.nb,
+    ...omsorgstilbudMessages.nb,
+    ...sifCommonPleiepengerMessages.nb,
+    ...opptjeningUtlandMessages.nb,
+    ...utenlandskNæringMessages.nb,
+    ...appBokmålstekster,
+    ...velkommenPageMessages.nb,
+};
+
+export const applicationIntlMessages: MessageFileFormat = {
+    nb: bokmålstekster,
+};
