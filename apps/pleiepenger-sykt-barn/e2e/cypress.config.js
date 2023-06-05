e9457971d8e7f5cc2a91e3916541a9bc3082545a
Frode Hansen <frode.hansen@bekk.no>
2023-06-05 14:20:21 +0200

    log til cypres

--- apps/pleiepenger-sykt-barn/e2e/cypress.config.js
+++ apps/pleiepenger-sykt-barn/e2e/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
     e2e: {
         baseUrl: 'http://localhost:8080',
         testIsolation: false,
+        projectId: 'fnodxc',
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
         setupNodeEvents(on, config) {
