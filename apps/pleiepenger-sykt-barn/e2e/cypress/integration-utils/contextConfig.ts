63bfb8d29c451445f7887a5e1e4af4a4261e0578
Frode Hansen <frode.hansen@bekk.no>
2023-06-05 15:09:03 +0200

    red

--- apps/pleiepenger-sykt-barn/e2e/cypress/integration-utils/contextConfig.ts
+++ apps/pleiepenger-sykt-barn/e2e/cypress/integration-utils/contextConfig.ts
@@ -1,7 +1,7 @@
 import { cyApiMockData } from './cyApiMockData';
 
 const PUBLIC_PATH = '/familie/sykdom-i-familien/soknad/pleiepenger';
-const API = 'http://localhost:8082';
+const mockApiBaseUrl = 'http://localhost:8082';
 
 const getUrlForStep = (step?) => {
     const url = `${PUBLIC_PATH}/soknad${step ? `/${step}` : '/velkommen'}`;
@@ -16,18 +16,20 @@ interface ConfigProps {
 export const contextConfig = (props?: ConfigProps) => {
     const { mellomlagring, step, arbeidsgivere } = props || {};
     beforeEach('intercept api-kall', () => {
-        cy.intercept(`DELETE`, `${API}/mellomlagring/PLEIEPENGER_SYKT_BARN*`, mellomlagring || {});
-        cy.intercept(`PUT`, `${API}/mellomlagring/PLEIEPENGER_SYKT_BARN*`, mellomlagring || {});
-        cy.intercept(`POST`, `${API}/mellomlagring/PLEIEPENGER_SYKT_BARN*`, mellomlagring || {});
-        cy.intercept(`GET`, `${API}/mellomlagring/PLEIEPENGER_SYKT_BARN*`, mellomlagring || {});
-        cy.intercept('GET', `${API}/oppslag/arbeidsgiver*`, arbeidsgivere || cyApiMockData.arbeidsgivereMock);
-        cy.intercept('GET', `${API}/oppslag/soker*`, cyApiMockData.søkerMock);
-        cy.intercept('GET', `${API}/oppslag/barn*`, cyApiMockData.barnMock);
-        cy.intercept('POST', `${API}/pleiepenger-sykt-barn/innsending`, {});
-        cy.intercept(
-            { method: 'GET', hostname: 'localhost', url: '/oppslag/arbeidsgiver' },
-            arbeidsgivere || cyApiMockData.arbeidsgivereMock
+        cy.intercept(`DELETE`, `${mockApiBaseUrl}/mellomlagring/PLEIEPENGER_SYKT_BARN*`, mellomlagring || {});
+        cy.intercept(`PUT`, `${mockApiBaseUrl}/mellomlagring/PLEIEPENGER_SYKT_BARN*`, mellomlagring || {});
+        cy.intercept(`POST`, `${mockApiBaseUrl}/mellomlagring/PLEIEPENGER_SYKT_BARN*`, mellomlagring || {});
+        cy.intercept(`GET`, `${mockApiBaseUrl}/mellomlagring/PLEIEPENGER_SYKT_BARN*`, mellomlagring || {});
+        cy.intercept('GET', `${mockApiBaseUrl}/oppslag/arbeidsgiver*`, cyApiMockData.arbeidsgivereMock).as(
+            'getArbeidsgiver'
         );
+        cy.intercept('GET', `${mockApiBaseUrl}/oppslag/soker*`, cyApiMockData.søkerMock);
+        cy.intercept('GET', `${mockApiBaseUrl}/oppslag/barn*`, cyApiMockData.barnMock);
+        cy.intercept('POST', `${mockApiBaseUrl}/pleiepenger-sykt-barn/innsending`, {});
+        // cy.intercept(
+        //     { method: 'GET', hostname: 'localhost', url: '/oppslag/arbeidsgiver' },
+        //     arbeidsgivere || cyApiMockData.arbeidsgivereMock
+        // );
         cy.intercept(`https://ryujtq87.api.sanity.io*`, {});
     });
 
