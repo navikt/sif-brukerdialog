055489564937485421ac63c81c89c196c46da6fa
Frode Hansen <frode.hansen@bekk.no>
2023-06-12 10:53:16 +0200

    Ikke bruke mellomlagring

--- apps/pleiepenger-sykt-barn/e2e/cypress/e2e/2-test-arbeidssituasjoner.cy.ts
+++ apps/pleiepenger-sykt-barn/e2e/cypress/e2e/2-test-arbeidssituasjoner.cy.ts
@@ -1,16 +1,25 @@
 import { contextConfig } from '../integration-utils/contextConfig';
 import { testArbeidssituasjonAnsatt } from '../integration-utils/steps/arbeidssituasjon/ansatt';
 import { testArbeidssituasjonFrilanser } from '../integration-utils/steps/arbeidssituasjon/frilanser';
-import { mellomlagring } from '../integration-utils/mocks/mellomlagring';
+// import { mellomlagring } from '../integration-utils/mocks/mellomlagring';
 import { testArbeidssituasjonSN } from '../integration-utils/steps/arbeidssituasjon/selvstendigNæringsdrivende';
 import { testArbeidssituasjonUtenlandskNæring } from '../integration-utils/steps/arbeidssituasjon/utenlandskNæring';
 import { testArbeidssituasjonOpptjeningUtland } from '../integration-utils/steps/arbeidssituasjon/opptjeningUtland';
+import { fyllUtVelkommenSide } from '../integration-utils/steps/velkommenside';
+import { fyllUtOmBarnSteg } from '../integration-utils/steps/opplysningerOmBarnet';
+import { fyllUtPeriodeSteg } from '../integration-utils/steps/periode';
 
 describe('Arbeidssituasjoner', () => {
-    contextConfig({ mellomlagring, step: 'arbeidssituasjon' });
+    contextConfig();
 
     it('henter mellomlagring og annen info', () => {
         cy.wait(['@getMellomlagring', '@getSøker', '@getBarn']);
+    });
+
+    beforeEach(() => {
+        fyllUtVelkommenSide();
+        fyllUtOmBarnSteg();
+        fyllUtPeriodeSteg();
     });
 
     testArbeidssituasjonAnsatt();
