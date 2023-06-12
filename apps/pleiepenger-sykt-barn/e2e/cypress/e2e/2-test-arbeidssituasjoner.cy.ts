f02ac81c6d85aa2210d6d0c364c1976f932bab4a
Frode Hansen <frode.hansen@bekk.no>
2023-06-12 18:08:30 +0200

    arbsit

--- apps/pleiepenger-sykt-barn/e2e/cypress/e2e/2-test-arbeidssituasjoner.cy.ts
+++ apps/pleiepenger-sykt-barn/e2e/cypress/e2e/2-test-arbeidssituasjoner.cy.ts
@@ -1,24 +1,19 @@
-/** Denne vil rett og slett ikke kjøre stabilt på github
- * Utkommenteres til et annet testoppsett som virker settes opp.
- */
+import { contextConfig, gotoArbeidssituasjonStep } from '../integration-utils/contextConfig';
+import { mellomlagring } from '../integration-utils/mocks/mellomlagring';
+import { testArbeidssituasjonAnsatt } from '../integration-utils/steps/arbeidssituasjon/ansatt';
+import { testArbeidssituasjonFrilanser } from '../integration-utils/steps/arbeidssituasjon/frilanser';
+import { testArbeidssituasjonOpptjeningUtland } from '../integration-utils/steps/arbeidssituasjon/opptjeningUtland';
+import { testArbeidssituasjonSN } from '../integration-utils/steps/arbeidssituasjon/selvstendigNæringsdrivende';
+import { testArbeidssituasjonUtenlandskNæring } from '../integration-utils/steps/arbeidssituasjon/utenlandskNæring';
 
-// import { contextConfig, gotoArbeidssituasjonStep } from '../integration-utils/contextConfig';
-// import { mellomlagring } from '../integration-utils/mocks/mellomlagring';
-// import { testArbeidssituasjonAnsatt } from '../integration-utils/steps/arbeidssituasjon/ansatt';
-// import { testArbeidssituasjonFrilanser } from '../integration-utils/steps/arbeidssituasjon/frilanser';
-// import { testArbeidssituasjonOpptjeningUtland } from '../integration-utils/steps/arbeidssituasjon/opptjeningUtland';
-// import { testArbeidssituasjonSN } from '../integration-utils/steps/arbeidssituasjon/selvstendigNæringsdrivende';
-// import { testArbeidssituasjonUtenlandskNæring } from '../integration-utils/steps/arbeidssituasjon/utenlandskNæring';
-
-// describe('Arbeidssituasjoner', () => {
-//     contextConfig({ mellomlagring });
-//     beforeEach(() => {
-//         gotoArbeidssituasjonStep();
-//     });
-
-//     testArbeidssituasjonAnsatt();
-//     testArbeidssituasjonFrilanser();
-//     testArbeidssituasjonSN();
-//     testArbeidssituasjonUtenlandskNæring();
-//     testArbeidssituasjonOpptjeningUtland();
-// });
+describe('Arbeidssituasjoner', () => {
+    contextConfig({ mellomlagring });
+    beforeEach(() => {
+        gotoArbeidssituasjonStep();
+    });
+    testArbeidssituasjonAnsatt();
+    testArbeidssituasjonFrilanser();
+    testArbeidssituasjonSN();
+    testArbeidssituasjonUtenlandskNæring();
+    testArbeidssituasjonOpptjeningUtland();
+});
