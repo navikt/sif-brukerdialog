303e3358145aba0ad970e13728ac4e0c7d837c5e
Frode Hansen <frode.hansen@bekk.no>
2023-06-13 12:24:37 +0200

    Fjerne record

--- apps/pleiepenger-sykt-barn/e2e/cypress.config.js
+++ apps/pleiepenger-sykt-barn/e2e/cypress.config.js
@@ -2,11 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
+        // projectId: 'fnodxc',
         baseUrl: 'http://localhost:8080',
         testIsolation: false,
-        projectId: 'fnodxc',
-        // We've imported your old cypress plugins here.
-        // You may want to clean this up later by importing these.
         requestTimeout: 10000,
         setupNodeEvents(on, config) {
             return require('./cypress/plugins/index.js')(on, config);
