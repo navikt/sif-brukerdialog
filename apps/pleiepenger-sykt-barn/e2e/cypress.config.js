ebbc7bca5021a3849cd24e28e33b86732ece201f
Frode Hansen <frode.hansen@bekk.no>
2023-06-13 12:09:36 +0200

    record

--- apps/pleiepenger-sykt-barn/e2e/cypress.config.js
+++ apps/pleiepenger-sykt-barn/e2e/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
     e2e: {
         baseUrl: 'http://localhost:8080',
         testIsolation: false,
-        // projectId: 'fnodxc',
+        projectId: 'fnodxc',
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
         requestTimeout: 10000,
