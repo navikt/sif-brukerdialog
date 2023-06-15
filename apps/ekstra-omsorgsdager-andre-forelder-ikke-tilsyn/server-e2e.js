c0a0848931e35df11d9edd321740e65783dc84f5
Frode Hansen <frode.hansen@bekk.no>
2023-06-15 14:40:11 +0200

    distjs

--- apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/server-e2e.js
+++ apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/server-e2e.js
@@ -33,6 +33,7 @@ const renderApp = (decoratorFragments) =>
 
 const startServer = (html) => {
     server.use(`${process.env.PUBLIC_PATH}/dist/js`, express.static(path.resolve(__dirname, 'dist/js')));
+    server.use(`${process.env.PUBLIC_PATH}/distjs`, express.static(path.resolve(__dirname, 'dist/js')));
     server.use(`${process.env.PUBLIC_PATH}/dist/css`, express.static(path.resolve(__dirname, 'dist/css')));
     server.get(`${process.env.PUBLIC_PATH}/health/isAlive`, (req, res) => res.sendStatus(200));
     server.get(`${process.env.PUBLIC_PATH}/health/isReady`, (req, res) => res.sendStatus(200));
