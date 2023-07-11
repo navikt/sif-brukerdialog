f7b96ce88932bfad4c9926cf7929ed9b8484035b
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-07-11 16:05:36 +0200

    init arbeidstaker soknad

---
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/README.md
@@ -0,0 +1,21 @@
+# Søknad om utbetaling av omsorgsdager for arbeidstakere
+
+Kildekoden til frontend-appen for selvbetjeningsløsning for
+søknad om utbetaling av omsorgsdager for arbeidstakere.
+
+## Komme i gang
+
+For å kjøre opp app'en i dev:
+
+1. `yarn dev-api` i en terminal.
+2. `yarn snd` i en annen terminal.
+
+For å kjøre enhetstester:
+
+1.  `yarn test`.
+
+For å kjøre cypress tester:
+
+1. `yarn dev-api` i en terminal.
+2. `yarn snd` i en annen terminal.
+3. `yarn cypress-open` i en tredje terminal.
---
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/envSettings.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const process = require('process');
+
+const envSettings = () => {
+    const appSettings = `
+     window.appSettings = {
+        API_URL: '${process.env.API_URL}',
+        VEDLEGG_API_URL: '${process.env.VEDLEGG_API_URL}',
+        FRONTEND_API_PATH: '${process.env.FRONTEND_API_PATH}',
+        FRONTEND_VEDLEGG_URL:'${process.env.FRONTEND_VEDLEGG_URL}',
+        PUBLIC_PATH: '${process.env.PUBLIC_PATH}',
+        LOGIN_URL: '${process.env.LOGIN_URL}',
+        APPSTATUS_PROJECT_ID: '${process.env.APPSTATUS_PROJECT_ID}',
+        APPSTATUS_DATASET: '${process.env.APPSTATUS_DATASET}',
+        USE_AMPLITUDE: '${process.env.USE_AMPLITUDE}',
+        APP_VERSION: '${process.env.APP_VERSION}',
+        IMAGE: '${process.env.IMAGE}',
+     };`
+        .trim()
+        .replace(/ /g, '');
+
+    try {
+        return appSettings;
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+    }
+};
+
+module.exports = envSettings;
