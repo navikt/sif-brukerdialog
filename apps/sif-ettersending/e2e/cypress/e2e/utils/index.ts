25eff117310e19dd1bbf19f9c183df8d6805c2ae
Frode Hansen <frode.hansen@bekk.no>
2023-08-16 08:05:28 +0200

    cypress-axe i ettersending

--- apps/sif-ettersending/e2e/cypress/e2e/utils/index.ts
+++ apps/sif-ettersending/e2e/cypress/e2e/utils/index.ts
@@ -67,27 +67,3 @@ export const getTestElementByType = (type) => {
 export const getElement = (type) => {
     return cy.get(`${type}`);
 };
-
-export const gåTilOppsummeringFraArbeidssituasjon = () => {
-    /** cy.visit reloads, og da må en blir mellomlagring feil - fake click fortsett */
-    clickFortsett();
-    cy.wait(100);
-    clickFortsett();
-    cy.wait(100);
-    clickFortsett();
-    cy.wait(100);
-    clickFortsett();
-    cy.wait(100);
-    clickFortsett();
-};
-
-export const gåTilOppsummeringFraArbeidIPerioden = () => {
-    /** cy.visit reloads, og da må en blir mellomlagring feil - fake click fortsett */
-    clickFortsett();
-    cy.wait(100);
-    clickFortsett();
-    cy.wait(100);
-    clickFortsett();
-    cy.wait(100);
-    clickFortsett();
-};
