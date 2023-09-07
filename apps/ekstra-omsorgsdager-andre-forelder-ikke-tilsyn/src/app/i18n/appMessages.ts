93c1dbfcea9eb636d4c73d4b9279bff75cd96adc
Frode Hansen <frode.hansen@bekk.no>
2023-09-07 12:38:30 +0200

    Oppdatere ekstra omsorgsdager ved aleneomsorg og ikke tilsyn dialoger

--- apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/src/app/i18n/appMessages.ts
+++ apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/src/app/i18n/appMessages.ts
@@ -2,21 +2,25 @@ import { MessageFileFormat } from '@navikt/sif-common-core-ds/lib/types/MessageF
 
 export const appMessages: MessageFileFormat = {
     nb: {
-        'application.title': 'Søknad om ekstra omsorgsdager',
+        'application.title': 'Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
         'application.bannerTitle': 'Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
 
         'page.form.ubesvarteSpørsmålInfo': 'For å komme videre, må du svare på alle spørsmålene ovenfor',
 
-        'step.omAnnenForelder.pageTitle': 'Om den andre forelderen',
+        'step.omAnnenForelder.pageTitle':
+            'Om den andre forelderen - Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
         'step.omAnnenForelder.stepTitle': 'Om den andre forelderen',
 
-        'step.annenForelderSituasjon.pageTitle': 'Den andre forelderens situasjon',
+        'step.annenForelderSituasjon.pageTitle':
+            'Den andre forelderens situasjon - Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
         'step.annenForelderSituasjon.stepTitle': 'Den andre forelderens situasjon',
 
-        'step.omBarna.pageTitle': 'Om barn',
+        'step.omBarna.pageTitle':
+            'Om barn - Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
         'step.omBarna.stepTitle': 'Om barn',
 
-        'step.oppsummering.pageTitle': 'Oppsummering',
+        'step.oppsummering.pageTitle':
+            'Oppsummering - Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
         'step.oppsummering.stepTitle': 'Oppsummering',
 
         'steg.footer.avbryt': 'Avbryt og slett søknad',
