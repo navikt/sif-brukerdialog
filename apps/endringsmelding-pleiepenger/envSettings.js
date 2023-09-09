abf2124152e1f8a81a3c291a9405297a88efa784
Frode Hansen <frode.hansen@bekk.no>
2023-09-09 15:29:52 +0200

    Legge til env-variabler

--- apps/endringsmelding-pleiepenger/envSettings.js
+++ apps/endringsmelding-pleiepenger/envSettings.js
@@ -5,23 +5,23 @@ const envSettings = (MSW) => {
     const appSettings = `
      window.appSettings = {
         API_URL_INNSYN: '${process.env.API_URL_INNSYN}',
-        FRONTEND_INNSYN_API_PATH: '${process.env.FRONTEND_INNSYN_API_PATH}',
         API_URL: '${process.env.API_URL}',
-        FRONTEND_API_PATH: '${process.env.FRONTEND_API_PATH}',
         APP_VERSION: '${process.env.APP_VERSION}',
         APPSTATUS_DATASET: '${process.env.APPSTATUS_DATASET}',
         APPSTATUS_PROJECT_ID: '${process.env.APPSTATUS_PROJECT_ID}',
-        INNSYN_URL: '${process.env.INNSYN_URL}',
         DOMAIN_URL: '${process.env.DOMAIN_URL}',
+        FRONTEND_API_PATH: '${process.env.FRONTEND_API_PATH}',
+        FRONTEND_INNSYN_API_PATH: '${process.env.FRONTEND_INNSYN_API_PATH}',
+        GITHUB_REF_NAME: '${process.env.GITHUB_REF_NAME}',
+        IMAGE: '${process.env.IMAGE}',
+        INNSYN_URL: '${process.env.INNSYN_URL}',
         LOGIN_URL: '${process.env.LOGIN_URL}',
-        MINSIDE_URL: '${process.env.MINSIDE_URL}',
         MELLOMLAGRING: '${process.env.MELLOMLAGRING}',
+        MINSIDE_URL: '${process.env.MINSIDE_URL}',
         MSW: '${MSW || process.env.MSW}',
         PUBLIC_PATH: '${process.env.PUBLIC_PATH}',
-        USE_AMPLITUDE: '${process.env.USE_AMPLITUDE}',
         UKJENT_ARBEIDSFOHOLD: '${process.env.UKJENT_ARBEIDSFOHOLD}',
-        IMAGE: '${process.env.IMAGE}',
-        GITHUB_REF_NAME: '${process.env.GITHUB_REF_NAME}',
+        USE_AMPLITUDE: '${process.env.USE_AMPLITUDE}',
     };`
         .trim()
         .replace(/ /g, '');
--- apps/sif-ettersending/envSettings.js
+++ apps/sif-ettersending/envSettings.js
@@ -4,23 +4,24 @@ require('dotenv').config();
 const envSettings = () => {
     const appSettings = `
      window.appSettings = {
-        DEKORATOR_URL: '${process.env.DEKORATOR_URL}',
         API_URL: '${process.env.API_URL}',
-        VEDLEGG_API_URL: '${process.env.VEDLEGG_API_URL}',
+        APP_VERSION: '${process.env.APP_VERSION}',
+        APPSTATUS_DATASET: '${process.env.APPSTATUS_DATASET}',
+        APPSTATUS_PROJECT_ID: '${process.env.APPSTATUS_PROJECT_ID}',
+        DEKORATOR_URL: '${process.env.DEKORATOR_URL}',
+        DITT_NAV_URL: '${process.env.DITT_NAV_URL}',
         FRONTEND_API_PATH: '${process.env.FRONTEND_API_PATH}',
         FRONTEND_VEDLEGG_URL: '${process.env.FRONTEND_VEDLEGG_URL}',
-        PUBLIC_PATH: '${process.env.PUBLIC_PATH}',
-        LOGIN_URL: '${process.env.LOGIN_URL}',
-        NYNORSK: '${process.env.NYNORSK}',
-        INNSYN_PP: '${process.env.INNSYN_PP}',
-        DITT_NAV_URL: '${process.env.DITT_NAV_URL}',
-        MIN_SIDE_URL: '${process.env.MIN_SIDE_URL}',
-        APPSTATUS_PROJECT_ID: '${process.env.APPSTATUS_PROJECT_ID}',
-        APPSTATUS_DATASET: '${process.env.APPSTATUS_DATASET}',
-        USE_AMPLITUDE: '${process.env.USE_AMPLITUDE}',
-        LIVETS_SLUTTFASE: '${process.env.LIVETS_SLUTTFASE}',
-        IMAGE: '${process.env.IMAGE}',
         GITHUB_REF_NAME: '${process.env.GITHUB_REF_NAME}',
+        IMAGE: '${process.env.IMAGE}',
+        INNSYN_PP: '${process.env.INNSYN_PP}',
+        LIVETS_SLUTTFASE: '${process.env.LIVETS_SLUTTFASE}',
+        LOGIN_URL: '${process.env.LOGIN_URL}',
+        MIN_SIDE_URL: '${process.env.MIN_SIDE_URL}',
+        NYNORSK: '${process.env.NYNORSK}',
+        PUBLIC_PATH: '${process.env.PUBLIC_PATH}',
+        USE_AMPLITUDE: '${process.env.USE_AMPLITUDE}',
+        VEDLEGG_API_URL: '${process.env.VEDLEGG_API_URL}',
      };`
         .trim()
         .replace(/ /g, '');
