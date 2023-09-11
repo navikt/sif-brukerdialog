5426a69bf421963d6b7197f968a00b58e91699eb
Frode Hansen <frode.hansen@bekk.no>
2023-09-11 18:12:38 +0200

    YtelseKey

--- packages/sif-common-core-ds/src/types/Ytelser.ts
+++ packages/sif-common-core-ds/src/types/Ytelser.ts
@@ -1,6 +1,6 @@
-export type YtelseKeyMap = {
+type YtelserMap = {
     [key: string]: {
-        /** Brukes i ettersending */
+        /** Brukes ved ettersending av dokumenter */
         ettersendelseYtelseApiKey: string;
         /** Internt navn på ytelse */
         navn: string;
@@ -12,50 +12,60 @@ export type YtelseKeyMap = {
     };
 };
 
-export const Ytelser: YtelseKeyMap = {
-    pleiepengerForSyktBarn: {
+export enum YtelseKey {
+    'pleiepengerForSyktBarn' = 'pleiepengerForSyktBarn',
+    'pleiepengerLivetsSluttfase' = 'pleiepengerLivetsSluttfase',
+    'omsorgsdagerKroniskSyk' = 'omsorgsdagerKroniskSyk',
+    'omsorgsdagerAleneomsorg' = 'omsorgsdagerAleneomsorg',
+    'omsorgsdagerAnnenForelderIkkeTilsyn' = 'omsorgsdagerAnnenForelderIkkeTilsyn',
+    'omsorgspengerutbetalingSNFri' = 'omsorgspengerutbetalingSNFri',
+    'omsorgspengerutbetalingArbeidstaker' = 'omsorgspengerutbetalingArbeidstaker',
+}
+
+export const Ytelser: YtelserMap = {
+    [YtelseKey.pleiepengerForSyktBarn]: {
         ettersendelseYtelseApiKey: 'PLEIEPENGER_SYKT_BARN',
         navn: 'Pleiepenger for sykt barn',
         applikasjonstittel: {
             nb: 'Søknad om pleiepenger for sykt barn',
         },
     },
-    pleiepengerLivetsSluttfase: {
+    [YtelseKey.pleiepengerLivetsSluttfase]: {
         ettersendelseYtelseApiKey: 'PLEIEPENGER_LIVETS_SLUTTFASE',
         navn: 'Pleiepenger i livets sluttfase',
         applikasjonstittel: {
             nb: 'Søknad om pleiepenger i livets sluttfase',
         },
     },
-    omsorgsdagerKroniskSyk: {
+    [YtelseKey.omsorgsdagerKroniskSyk]: {
         ettersendelseYtelseApiKey: 'OMP_UTV_KS',
         navn: 'Ekstra omsorgsdager for barn som har kronisk/langvarig sykdom eller funksjonshemning',
         applikasjonstittel: {
             nb: 'Søknad om ekstra omsorgsdager for barn som har kronisk/langvarig sykdom eller funksjonshemning',
         },
     },
-    omsorgsdagerAleneomsorg: {
+    [YtelseKey.omsorgsdagerAleneomsorg]: {
         ettersendelseYtelseApiKey: 'OMP_ALENEOMSORG',
         navn: 'Ekstra omsorgsdager ved aleneomsorg',
         applikasjonstittel: {
             nb: 'Søknad om ekstra omsorgsdager ved aleneomsorg',
         },
     },
-    omsorgsdagerAnnenForelderIkkeTilsyn: {
+    [YtelseKey.omsorgsdagerAnnenForelderIkkeTilsyn]: {
         ettersendelseYtelseApiKey: 'OMP_UTV_MA',
         navn: 'Ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
         applikasjonstittel: {
             nb: 'Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
         },
     },
-    omsorgspengerutbetalingSNFri: {
+    [YtelseKey.omsorgspengerutbetalingSNFri]: {
         ettersendelseYtelseApiKey: 'OMP_UT_SNF',
         navn: 'Utbetaling av omsorgspenger for selvstendig næringsdrivende og frilansere',
         applikasjonstittel: {
             nb: 'Søknad om utbetaling av omsorgspenger for selvstendig næringsdrivende og frilansere',
         },
     },
-    omsorgspengerutbetalingArbeidstaker: {
+    [YtelseKey.omsorgspengerutbetalingArbeidstaker]: {
         ettersendelseYtelseApiKey: 'OMP_UT_ARBEIDSTAKER',
         navn: 'Utbetaling av omsorgspenger for arbeidstaker',
         applikasjonstittel: {
