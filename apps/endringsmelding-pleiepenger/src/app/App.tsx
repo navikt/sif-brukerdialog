5ece934eea6294f9917479325abb7f9378f84fde
Frode Hansen <frode.hansen@bekk.no>
2023-09-14 09:30:24 +0200

    Pakkeoppdatering + cypress justering i endring

--- apps/endringsmelding-pleiepenger/src/app/App.tsx
+++ apps/endringsmelding-pleiepenger/src/app/App.tsx
@@ -28,7 +28,7 @@ ensureBaseNameForReactRouter(publicPath);
 
 function prepare() {
     if (getEnvironmentVariable('APP_VERSION') !== 'production') {
-        if (getEnvironmentVariable('MSW') === 'on') {
+        if (getEnvironmentVariable('MSW') === 'o-n') {
             return import('../mocks/msw/browser').then(({ worker }) => {
                 worker.start({
                     onUnhandledRequest: 'bypass',
