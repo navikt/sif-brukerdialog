f8d28199fa2ef645fc13bedc21e217d006942f2f
Frode Hansen <frode.hansen@bekk.no>
2023-09-18 15:36:10 +0200

    Fjerne StateInfo fra dev/q

--- apps/omsorgspengerutbetaling-soknad/src/app/søknad/SøknadStep.tsx
+++ apps/omsorgspengerutbetaling-soknad/src/app/søknad/SøknadStep.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
 import { useLogSidevisning } from '@navikt/sif-common-amplitude/lib';
-import { getEnvironmentVariable } from '@navikt/sif-common-core-ds/lib/utils/envUtils';
 import intlHelper from '@navikt/sif-common-core-ds/lib/utils/intlUtils';
 import { soknadStepUtils, Step } from '@navikt/sif-common-soknad-ds';
 import InvalidStepSøknadsdataInfo from '../components/invalid-step-søknadsdata-info/InvalidStepSøknadsdataInfo';
-import StateInfo from '../components/state-info/StateInfo';
 import useAvbrytEllerFortsettSenere from '../hooks/useAvbrytSøknad';
 import { StepId } from '../types/StepId';
 import { useSøknadContext } from './context/hooks/useSøknadContext';
@@ -31,7 +29,6 @@ const SøknadStep: React.FunctionComponent<Props> = ({ stepId, children }) => {
     const { index } = stepConfig[stepId];
 
     const steps = soknadStepUtils.getProgressStepsFromConfig(stepConfig, index, intl);
-    const isDevMode = getEnvironmentVariable('APP_VERSION') === 'dev';
 
     return (
         <Step
@@ -42,7 +39,6 @@ const SøknadStep: React.FunctionComponent<Props> = ({ stepId, children }) => {
             onContinueLater={fortsettSøknadSenere}>
             <InvalidStepSøknadsdataInfo stepId={stepId} stepConfig={stepConfig} />
             {children}
-            {isDevMode ? <StateInfo /> : null}
         </Step>
     );
 };
