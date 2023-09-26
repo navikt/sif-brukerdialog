561a08bcf616c44e5bfc061395f48a815739eb3c
Frode Hansen <frode.hansen@bekk.no>
2023-09-26 13:11:16 +0200

    Fjerne createPortal

--- apps/pleiepenger-sykt-barn/src/app/local-sif-common-pleiepenger/components/tid-enkeltdag-dialog/TidEnkeltdagDialog.tsx
+++ apps/pleiepenger-sykt-barn/src/app/local-sif-common-pleiepenger/components/tid-enkeltdag-dialog/TidEnkeltdagDialog.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { dateFormatter } from '@navikt/sif-common-utils/lib';
 import TidEnkeltdagForm, { TidEnkeltdagFormProps } from './TidEnkeltdagForm';
 import './styles/tidEnkeltdagDialog.less';
-import { createPortal } from 'react-dom';
 
 export interface TidEnkeltdagDialogProps {
     open?: boolean;
@@ -19,23 +18,21 @@ const TidEnkeltdagDialog: React.FunctionComponent<TidEnkeltdagDialogProps> = ({
     if (!open) {
         return null;
     }
-    return open
-        ? createPortal(
-              <Modal
-                  open={open}
-                  onClose={formProps.onCancel}
-                  className="tidEnkeltdagDialog"
-                  header={{
-                      heading: `${dialogTitle} ${dateFormatter.dayDateMonthYear(formProps.dato)}`,
-                      closeButton: true,
-                  }}>
-                  <Modal.Body>
-                      <TidEnkeltdagForm {...formProps} />
-                  </Modal.Body>
-              </Modal>,
-              document.body,
-          )
-        : null;
+    return open ? (
+        <Modal
+            open={open}
+            onClose={formProps.onCancel}
+            className="tidEnkeltdagDialog"
+            portal={true}
+            header={{
+                heading: `${dialogTitle} ${dateFormatter.dayDateMonthYear(formProps.dato)}`,
+                closeButton: true,
+            }}>
+            <Modal.Body>
+                <TidEnkeltdagForm {...formProps} />
+            </Modal.Body>
+        </Modal>
+    ) : null;
 };
 
 export default TidEnkeltdagDialog;
