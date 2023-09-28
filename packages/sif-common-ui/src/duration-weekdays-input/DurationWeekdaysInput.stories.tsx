28d1178635b85436910b6ab55eaf00cf19fcbfcf
Frode Hansen <frode.hansen@bekk.no>
2023-09-28 09:55:26 +0200

    Justere cypress-test

--- packages/sif-common-ui/src/duration-weekdays-input/DurationWeekdaysInput.stories.tsx
+++ packages/sif-common-ui/src/duration-weekdays-input/DurationWeekdaysInput.stories.tsx
@@ -19,8 +19,14 @@ const dateRange = ISODateRangeToDateRange('2023-05-25/2023-08-08');
 const disabledDates = getDatesInDateRange(dateRange).filter((d) => [1, 3, 4].includes(dayjs(d).day()));
 
 export const Default = Template.bind({});
-const args: DurationWeekdaysInputProps = {
+
+Default.args = {
+    dateRange,
+    disabledDates: [],
+};
+
+export const DisabledDates = Template.bind({});
+DisabledDates.args = {
     dateRange,
     disabledDates,
 };
-Default.args = args;
