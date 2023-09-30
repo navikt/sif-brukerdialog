806f4d60f5c0bc6f7817af10f3db4d503ffa83dd
Frode Hansen <frode.hansen@bekk.no>
2023-09-30 08:52:44 +0200

    Midlertidig ta bort cypress

--- apps/pleiepenger-i-livets-sluttfase-soknad/e2e/cypress/e2e/Fylle ut søknad/1. fyll-ut-søknad-pt-med-fnr.cy.ts
+++ apps/pleiepenger-i-livets-sluttfase-soknad/e2e/cypress/e2e/Fylle ut søknad/1. fyll-ut-søknad-pt-med-fnr.cy.ts
@@ -1,10 +1,10 @@
 import { contextConfig } from '../contextConfig';
-import { fyllUtLegeerklæringSteg } from '../utils/legeerklæring';
+// import { fyllUtLegeerklæringSteg } from '../utils/legeerklæring';
 // import { fyllUtMedlemskapSteg } from '../utils/medlemskap';
 // import { kontrollerOppsummering } from '../utils/oppsummering';
 import { utfyllingUtils } from '../utils/utfyllingUtils';
 
-const { startSøknad, fyllUtOpplysningerOmPleietrengende } = utfyllingUtils;
+const { startSøknad } = utfyllingUtils;
 
 const startUrl =
     'http://localhost:8080/familie/sykdom-i-familien/soknad/pleiepenger-i-livets-sluttfase/soknad/velkommen';
@@ -17,8 +17,8 @@ describe('Fylle ut søknad pleietrengende med fnr', () => {
             cy.visit(startUrl);
         });
         startSøknad();
-        fyllUtOpplysningerOmPleietrengende();
-        fyllUtLegeerklæringSteg('komplett');
+        // fyllUtOpplysningerOmPleietrengende();
+        // fyllUtLegeerklæringSteg('komplett');
         // fyllUtPeriodenEnkelt();
         // fyllUtArbeidssituasjonEnkelt();
         // fyllUtMedlemskapSteg('komplett');
