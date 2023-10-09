60659050cc0354737ffe309a652c584fb43a578a
Frode Hansen <frode.hansen@bekk.no>
2023-10-09 14:25:39 +0200

    Bruke vite i alle /packages

--- packages/sif-common-formik-ds/vite.config.ts
+++ packages/sif-common-formik-ds/vite.config.ts
@@ -1,15 +1,8 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 import Checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            common: path.resolve(__dirname, './src/common'),
-            dev: path.resolve(__dirname, './src/dev'),
-        },
-    },
     plugins: [react(), Checker({ typescript: true })],
 });
