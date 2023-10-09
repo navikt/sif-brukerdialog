3afcc3da6d60c300c5e2c976551c3b4aca2057be
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-10-09 11:49:01 +0200

    fix

--- apps/omsorgsdager-kalkulator/components/result/ResultatArea.tsx
+++ apps/omsorgsdager-kalkulator/components/result/ResultatArea.tsx
@@ -1,93 +1,79 @@
-import { FormattedMessage } from "react-intl";
-import { caseResultViewOf, ResultView } from "./ResultView";
-import ResultBox from "./ResultBox";
-import { BodyLong, Heading } from "@navikt/ds-react";
-import Link from "next/link";
-import { getYear } from "@/utils/utils";
-import { lenker } from "@/utils/lenker";
+import { FormattedMessage } from 'react-intl';
+import { caseResultViewOf, ResultView } from './ResultView';
+import ResultBox from './ResultBox';
+import { BodyLong, Heading } from '@navikt/ds-react';
+import Link from 'next/link';
+import { getYear } from '../../utils/utils';
+import { lenker } from '../../utils/lenker';
 
 interface Props {
-  resultView: ResultView<number>;
+    resultView: ResultView<number>;
 }
 
 const ResultatArea: React.FC<Props> = ({ resultView }: Props) => {
-  const year = getYear();
-  return caseResultViewOf(
-    () => null,
-    () => (
-      <ResultBox type={"WARNING"}>
-        <Heading level="2" size="medium" className="pb-3">
-          <FormattedMessage id={"resultat-area.title"} />
-        </Heading>
-        <BodyLong className="pb-3 text-justify" size="small">
-          <FormattedMessage id={"resultat-area.orange.1"} />
-        </BodyLong>
+    const year = getYear();
+    return caseResultViewOf(
+        () => null,
+        () => (
+            <ResultBox type={'WARNING'}>
+                <Heading level="2" size="medium" className="pb-3">
+                    <FormattedMessage id={'resultat-area.title'} />
+                </Heading>
+                <BodyLong className="pb-3 text-justify" size="small">
+                    <FormattedMessage id={'resultat-area.orange.1'} />
+                </BodyLong>
 
-        <BodyLong size="large" weight="semibold">
-          <FormattedMessage id={"resultat-area.orange.2.1.a"} />
-        </BodyLong>
+                <BodyLong size="large" weight="semibold">
+                    <FormattedMessage id={'resultat-area.orange.2.1.a'} />
+                </BodyLong>
 
-        <BodyLong className="pb-3 text-justify" size="small">
-          <FormattedMessage
-            id={"resultat-area.orange.2.1.b"}
-            values={{ year }}
-          />
-        </BodyLong>
+                <BodyLong className="pb-3 text-justify" size="small">
+                    <FormattedMessage id={'resultat-area.orange.2.1.b'} values={{ year }} />
+                </BodyLong>
 
-        <BodyLong className="pb-3 text-justify" size="small">
-          <FormattedMessage id={"resultat-area.orange.3"} />
-        </BodyLong>
-        <BodyLong size="small">
-          <Link href={lenker.omsorgspengerNavno}>
-            <FormattedMessage id={"resultat-area.tilbake-til-omsorgspenger"} />
-          </Link>
-        </BodyLong>
-      </ResultBox>
-    ),
-    (result: number) => {
-      return (
-        <>
-          <ResultBox type={"SUCCESS"}>
-            <Heading level="2" size="medium" className="pb-3">
-              <FormattedMessage id={"resultat-area.title"} />
-            </Heading>
+                <BodyLong className="pb-3 text-justify" size="small">
+                    <FormattedMessage id={'resultat-area.orange.3'} />
+                </BodyLong>
+                <BodyLong size="small">
+                    <Link href={lenker.omsorgspengerNavno}>
+                        <FormattedMessage id={'resultat-area.tilbake-til-omsorgspenger'} />
+                    </Link>
+                </BodyLong>
+            </ResultBox>
+        ),
+        (result: number) => {
+            return (
+                <>
+                    <ResultBox type={'SUCCESS'}>
+                        <Heading level="2" size="medium" className="pb-3">
+                            <FormattedMessage id={'resultat-area.title'} />
+                        </Heading>
 
-            <BodyLong className="pb-3 text-justify" size="small">
-              <FormattedMessage id={"resultat-area.green.1"} />
-            </BodyLong>
+                        <BodyLong className="pb-3 text-justify" size="small">
+                            <FormattedMessage id={'resultat-area.green.1'} />
+                        </BodyLong>
 
-            <BodyLong size="large" weight="semibold">
-              <FormattedMessage
-                id={"resultat-area.green.2.1.a"}
-                values={{ result }}
-              />
-            </BodyLong>
+                        <BodyLong size="large" weight="semibold">
+                            <FormattedMessage id={'resultat-area.green.2.1.a'} values={{ result }} />
+                        </BodyLong>
 
-            <BodyLong className="pb-3 text-justify" size="small">
-              <FormattedMessage
-                id={"resultat-area.green.2.1.b"}
-                values={{ year }}
-              />
-            </BodyLong>
+                        <BodyLong className="pb-3 text-justify" size="small">
+                            <FormattedMessage id={'resultat-area.green.2.1.b'} values={{ year }} />
+                        </BodyLong>
 
-            <BodyLong className="pb-3 text-justify" size="small">
-              <FormattedMessage
-                id={"resultat-area.green.3.1"}
-                values={{ year }}
-              />
-            </BodyLong>
-            <BodyLong size="small">
-              <Link href={lenker.omsorgspengerNavno}>
-                <FormattedMessage
-                  id={"resultat-area.tilbake-til-omsorgspenger"}
-                />
-              </Link>
-            </BodyLong>
-          </ResultBox>
-        </>
-      );
-    }
-  )(resultView);
+                        <BodyLong className="pb-3 text-justify" size="small">
+                            <FormattedMessage id={'resultat-area.green.3.1'} values={{ year }} />
+                        </BodyLong>
+                        <BodyLong size="small">
+                            <Link href={lenker.omsorgspengerNavno}>
+                                <FormattedMessage id={'resultat-area.tilbake-til-omsorgspenger'} />
+                            </Link>
+                        </BodyLong>
+                    </ResultBox>
+                </>
+            );
+        },
+    )(resultView);
 };
 
 export default ResultatArea;
--- apps/omsorgsdager-kalkulator/pages/kalkulator/form-parts/AntallBarnFormPart.tsx
+++ apps/omsorgsdager-kalkulator/pages/kalkulator/form-parts/AntallBarnFormPart.tsx
@@ -1,54 +1,43 @@
-import { ValidationError } from "@/components/sif-formik/validation/types";
-import { KlakulatorFormFields, KlakulatorFormValues } from "../Kalkulator";
-import { getTypedFormComponents } from "@/components/sif-formik/getTypedFormComponents";
-import { useFeatureToggleIntl } from "@/hooks/useFeatureToggleIntl";
-import getRequiredFieldValidator from "@/components/sif-formik/validation/getRequiredFieldValidator";
-import { ReadMore } from "@navikt/ds-react";
-import { FormattedMessage } from "react-intl";
+import { ValidationError } from '../../../components/sif-formik/validation/types';
+import { KlakulatorFormFields, KlakulatorFormValues } from '../Kalkulator';
+import { getTypedFormComponents } from '../../../components/sif-formik/getTypedFormComponents';
+import { useFeatureToggleIntl } from '../../../hooks/useFeatureToggleIntl';
+import getRequiredFieldValidator from '../../../components/sif-formik/validation/getRequiredFieldValidator';
+import { ReadMore } from '@navikt/ds-react';
+import { FormattedMessage } from 'react-intl';
 
 interface Props {
-  setBarn: (value: string) => void;
+    setBarn: (value: string) => void;
 }
 
-const { Select } = getTypedFormComponents<
-  KlakulatorFormFields,
-  KlakulatorFormValues,
-  ValidationError
->();
+const { Select } = getTypedFormComponents<KlakulatorFormFields, KlakulatorFormValues, ValidationError>();
 
 const AntallBarnFormPart: React.FC<Props> = ({ setBarn }: Props) => {
-  const { formatMessage } = useFeatureToggleIntl();
-  const nBarnMaks = 20;
+    const { formatMessage } = useFeatureToggleIntl();
+    const nBarnMaks = 20;
 
-  return (
-    <>
-      <Select
-        label={formatMessage("kalkulator.antallBarn")}
-        name={KlakulatorFormFields.antallBarn}
-        validate={getRequiredFieldValidator()}
-        afterOnChange={(value) => setBarn(value)}
-        description={
-          <ReadMore
-            header={formatMessage("kalkulator.antallBarn.readMore.title")}
-          >
-            <FormattedMessage id={"kalkulator.antallBarn.readMore"} />
-          </ReadMore>
-        }
-      >
-        {Array.from({ length: nBarnMaks }, (_, i) => i).map((value: number) => {
-          return (
-            <option
-              id={`n_barn_i_husstanden${value}`}
-              value={value}
-              key={value}
-            >
-              {value}
-            </option>
-          );
-        })}
-      </Select>
-    </>
-  );
+    return (
+        <>
+            <Select
+                label={formatMessage('kalkulator.antallBarn')}
+                name={KlakulatorFormFields.antallBarn}
+                validate={getRequiredFieldValidator()}
+                afterOnChange={(value) => setBarn(value)}
+                description={
+                    <ReadMore header={formatMessage('kalkulator.antallBarn.readMore.title')}>
+                        <FormattedMessage id={'kalkulator.antallBarn.readMore'} />
+                    </ReadMore>
+                }>
+                {Array.from({ length: nBarnMaks }, (_, i) => i).map((value: number) => {
+                    return (
+                        <option id={`n_barn_i_husstanden${value}`} value={value} key={value}>
+                            {value}
+                        </option>
+                    );
+                })}
+            </Select>
+        </>
+    );
 };
 
 export default AntallBarnFormPart;
