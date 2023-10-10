d681a6ea36e14288097f7b69585f6a379094f4bb
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-10-10 08:43:16 +0200

    fix

--- apps/omsorgsdager-kalkulator/next.config.js
+++ apps/omsorgsdager-kalkulator/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/omsorgspenger/kalkulator-antall-omsorgsdager",
-  basePath: "",
-  reactStrictMode: true,
-  output: "standalone",
-  i18n: {
-    locales: ["nb"],
-    defaultLocale: "nb",
-  },
+    // basePath: "/omsorgspenger/kalkulator-antall-omsorgsdager",
+    basePath: '',
+    reactStrictMode: true,
+    output: 'standalone',
+    i18n: {
+        locales: ['nb'],
+        defaultLocale: 'nb',
+    },
 };
 
 module.exports = nextConfig;
