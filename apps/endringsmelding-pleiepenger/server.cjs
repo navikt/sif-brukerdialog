5c5d0384d59f87cba1dd7ce670d824648a1b733f
Frode Hansen <frode.hansen@bekk.no>
2023-10-13 14:06:21 +0200

    headers

--- apps/endringsmelding-pleiepenger/server.cjs
+++ apps/endringsmelding-pleiepenger/server.cjs
@@ -44,11 +44,7 @@ server.set('view engine', 'mustache');
 server.engine('html', mustacheExpress());
 
 server.use((_req, res, next) => {
-    res.removeHeader('X-Powered-By');
-    res.set('X-Frame-Options', 'SAMEORIGIN');
     res.set('X-XSS-Protection', '1; mode=block');
-    res.set('X-Content-Type-Options', 'nosniff');
-    res.set('Referrer-Policy', 'no-referrer');
     res.set('Feature-Policy', "geolocation 'none'; microphone 'none'; camera 'none'");
     next();
 });
