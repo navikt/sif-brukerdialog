26e72b1efecf0318ac85c289e85f639cba0ee857
Frode Hansen <frode.hansen@bekk.no>
2023-10-17 16:07:25 +0200

    Fallback hardkodet path

--- apps/sif-ettersending/vite.config.ts
+++ apps/sif-ettersending/vite.config.ts
@@ -1,24 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import 'dotenv/config';
+import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 
-import 'dotenv/config';
-
-export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        base: env.PUBLIC_PATH,
-        plugins: [
-            react({
-                include: '**/*.{jsx,tsx}',
-            }),
-            checker({ typescript: true }),
-        ],
-        resolve: {},
-        build: {
-            sourcemap: true,
-        },
-    };
+export default defineConfig({
+    base: process.env.PUBLIC_PATH || '/familie/sykdom-i-familien/soknad/ettersending',
+    plugins: [
+        react({
+            include: '**/*.{jsx,tsx}',
+        }),
+        checker({ typescript: true }),
+    ],
+    resolve: {},
+    build: {
+        sourcemap: true,
+    },
 });
