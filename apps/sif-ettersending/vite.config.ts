87d9a2e8a795ae5b34f4e0867690602190a2169a
Frode Hansen <frode.hansen@bekk.no>
2023-10-17 15:45:48 +0200

    loadEnv

--- apps/sif-ettersending/vite.config.ts
+++ apps/sif-ettersending/vite.config.ts
@@ -1,19 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
+
 import 'dotenv/config';
 
-export default defineConfig({
-    base: `${process.env.VITE_PUBLIC_PATH}`,
-    plugins: [
-        react({
-            include: '**/*.{jsx,tsx}',
-        }),
-        checker({ typescript: true }),
-    ],
-    resolve: {},
-    build: {
-        sourcemap: true,
-    },
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        base: env.PUBLIC_PATH,
+        plugins: [
+            react({
+                include: '**/*.{jsx,tsx}',
+            }),
+            checker({ typescript: true }),
+        ],
+        resolve: {},
+        build: {
+            sourcemap: true,
+        },
+    };
 });
