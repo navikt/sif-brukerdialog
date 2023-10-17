86e5c8967ea0ae9cdf43c815945e4f7234876afc
Frode Hansen <frode.hansen@bekk.no>
2023-10-17 19:21:48 +0200

    Sette base i build script

--- apps/endringsmelding-pleiepenger/vite.config.ts
+++ apps/endringsmelding-pleiepenger/vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
-import 'dotenv/config';
 
 export default defineConfig({
-    base: `${process.env.PUBLIC_PATH}`,
     plugins: [
         react({
             include: '**/*.{jsx,tsx}',
--- apps/sif-ettersending/vite.config.ts
+++ apps/sif-ettersending/vite.config.ts
@@ -1,12 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import 'dotenv/config';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-    base: process.env.PUBLIC_PATH || '/familie/sykdom-i-familien/soknad/ettersending',
     plugins: [
         react({
             include: '**/*.{jsx,tsx}',
