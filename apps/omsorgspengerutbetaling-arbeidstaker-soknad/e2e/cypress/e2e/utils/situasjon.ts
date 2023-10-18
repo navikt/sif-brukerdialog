e75b0f7d1fbbe6a3c7dd6b3339662037e4ea09f0
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-10-18 11:59:12 +0200

    fix5

--- apps/omsorgspengerutbetaling-arbeidstaker-soknad/e2e/cypress/e2e/utils/situasjon.ts
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/e2e/cypress/e2e/utils/situasjon.ts
@@ -2,6 +2,7 @@ import { getTestElement, selectRadio, selectRadioNo, selectRadioYes, submitSkjem
 
 export const fyllUtSituasjonSteg = () => {
     it('Fyll ut situasjon steg', () => {
+        cy.wait(['@getArbeidsgiver']);
         getTestElement('arbeidsforhold-liste').each(($element, $index) => {
             cy.wrap($element).within(() => {
                 selectRadioYes('arbeidsforhold-harHattFravær');
--- apps/omsorgspengerutbetaling-arbeidstaker-soknad/e2e/cypress/e2e/utils/utfyllingUtils.ts
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/e2e/cypress/e2e/utils/utfyllingUtils.ts
@@ -13,9 +13,6 @@ import {
     submitModal,
     submitSkjema,
     getElement,
-    selectRadioByNameAndValue,
-    getRadioButtons,
-    selectRadioByValue,
 } from '.';
 
 const fileName = 'navlogopng.png';
@@ -25,21 +22,6 @@ const tilDato = dayjs().format('YYYY-MM-DD');
 const datoDelvisFravær = dayjs().startOf('week').subtract(4, 'weeks').format('YYYY-MM-DD');
 const fomDatoIUtlandet = dayjs().startOf('week').subtract(3, 'weeks').add(2, 'day').format('YYYY-MM-DD');
 const tomDatoIUtlandet = dayjs().startOf('week').subtract(3, 'weeks').add(4, 'day').format('YYYY-MM-DD');
-const frilansStartDato = dayjs().startOf('week').subtract(10, 'weeks').format('YYYY-MM-DD');
-
-const virksomhet = {
-    næringstype: 'JORDBRUK_SKOGBRUK',
-    registrertINorge: 'yes',
-    navn: 'Abc',
-    organisasjonsnummer: '999263550' /** Navs orgnur */,
-    fraOgMed: '01.01.2010',
-    tilOgMed: dayjs().subtract(1, 'day').format('DD.MM.YYYY'),
-    hattVarigEndringAvNæringsinntektSiste4Kalenderår: 'yes',
-    varigEndringINæringsinntekt_dato: dayjs().subtract(1, 'year').format('DD.MM.YYYY'),
-    varigEndringINæringsinntekt_inntektEtterEndring: '100',
-    varigEndringINæringsinntekt_forklaring: 'Lorem ipsum',
-    harRegnskapsfører: 'no',
-};
 
 const startSøknad = () => {
     it('Starter søknad', () => {
@@ -85,66 +67,6 @@ const fyllUtFraværSteg = () => {
     });
 };
 
-const fyllUtVirksomhetDialog = () => {
-    selectRadioByNameAndValue('næringstype', virksomhet.næringstype);
-    selectRadioByNameAndValue('registrertINorge', virksomhet.registrertINorge);
-    getInputByName('navnPåVirksomheten').click().type(virksomhet.navn).blur();
-    getInputByName('organisasjonsnummer').click().type(virksomhet.organisasjonsnummer).blur();
-    getInputByName('fom').click().type(virksomhet.fraOgMed).blur();
-    getInputByName('tom').click().type(virksomhet.tilOgMed).blur();
-    selectRadioByNameAndValue(
-        'hattVarigEndringAvNæringsinntektSiste4Kalenderår',
-        virksomhet.hattVarigEndringAvNæringsinntektSiste4Kalenderår,
-    );
-    getInputByName('varigEndringINæringsinntekt_dato').click().type(virksomhet.varigEndringINæringsinntekt_dato).blur();
-    getInputByName('varigEndringINæringsinntekt_inntektEtterEndring')
-        .click()
-        .type(virksomhet.varigEndringINæringsinntekt_inntektEtterEndring)
-        .blur();
-    getInputByName('varigEndringINæringsinntekt_forklaring')
-        .click()
-        .type(virksomhet.varigEndringINæringsinntekt_forklaring)
-        .blur();
-    selectRadioByNameAndValue('harRegnskapsfører', virksomhet.harRegnskapsfører);
-    submitModal();
-};
-
-const fyllerUtArbeidssituasjonSteg = () => {
-    it('Fyller ut Arbeidssituasjon steg', () => {
-        selectRadioYesOrNo('frilans_erFrilanser', true);
-        getInputByName('frilans_startdato').click().type(frilansStartDato).blur();
-        selectRadioYesOrNo('frilans_jobberFortsattSomFrilans', true);
-
-        selectRadioYesOrNo('selvstendig_erSelvstendigNæringsdrivende', true);
-        selectRadioYesOrNo('selvstendig_harFlereVirksomheter', true);
-        getElement('button').contains('Registrer virksomhet').click();
-        fyllUtVirksomhetDialog();
-        submitSkjema();
-        cy.wait('@putMellomlagring');
-    });
-};
-
-const fyllerUtFraværFraSteg = () => {
-    it('Fyller ut Fravær Fra steg', () => {
-        getRadioButtons().each(($element, $index) => {
-            if ($index % 2 === 0) {
-                cy.wrap($element).within(() => {
-                    selectRadioByValue('SELVSTENDIG_VIRKSOMHET');
-                });
-            } else if ($index % 3 === 0) {
-                cy.wrap($element).within(() => {
-                    selectRadioByValue('BEGGE');
-                });
-            } else
-                cy.wrap($element).within(() => {
-                    selectRadioByValue('FRILANSER');
-                });
-        });
-        submitSkjema();
-        cy.wait('@putMellomlagring');
-    });
-};
-
 const lastOppLegeerklæring = () => {
     it('laster opp legeerklæring', () => {
         cy.fixture(fileName, 'binary')
@@ -182,8 +104,6 @@ export const utfyllingUtils = {
     fyllUtOmBarn,
     fyllUtFraværSteg,
     lastOppLegeerklæring,
-    fyllerUtArbeidssituasjonSteg,
-    fyllerUtFraværFraSteg,
     sendInnSøknad,
     kontrollerKvittering,
 };
