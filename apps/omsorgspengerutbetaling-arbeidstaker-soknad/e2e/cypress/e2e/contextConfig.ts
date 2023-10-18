c995368f57c22e833e6b52829e11a7801d7e9277
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-10-18 13:39:42 +0200

    fix7

--- apps/omsorgspengerutbetaling-arbeidstaker-soknad/e2e/cypress/e2e/contextConfig.ts
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/e2e/cypress/e2e/contextConfig.ts
@@ -1,4 +1,7 @@
 import { cyApiMockData } from './data/cyApiMockData';
+
+const INTERCEPT_PATH = `**`;
+
 const PUBLIC_PATH = '/soknad';
 
 const getUrlForStep = (step?) => {
@@ -16,17 +19,29 @@ export const contextConfig = (props?: ConfigProps) => {
     const { mellomlagring, step } = props || {};
 
     beforeEach('intercept mellomlagring og levere tomt objekt', () => {
-        cy.intercept(`GET`, `**/mellomlagring/OMSORGSPENGER_UTBETALING_ARBEIDSTAKER`, mellomlagring || { noData: 1 });
-        cy.intercept(`DELETE`, `**/mellomlagring/OMSORGSPENGER_UTBETALING_ARBEIDSTAKER`, mellomlagring || {});
-        cy.intercept(`PUT`, `**/mellomlagring/OMSORGSPENGER_UTBETALING_ARBEIDSTAKER`, {}).as('putMellomlagring');
-        cy.intercept(`POST`, `**/mellomlagring/OMSORGSPENGER_UTBETALING_ARBEIDSTAKER`, {});
-        cy.intercept(`POST`, `**/omsorgspenger-utbetaling-arbeidstaker/innsending`, {}).as('innsending');
-        cy.intercept('POST', `**/vedlegg`, {
+        cy.intercept(
+            `GET`,
+            `${INTERCEPT_PATH}/mellomlagring/OMSORGSPENGER_UTBETALING_ARBEIDSTAKER`,
+            mellomlagring || { noData: 1 },
+        );
+        cy.intercept(
+            `DELETE`,
+            `${INTERCEPT_PATH}/mellomlagring/OMSORGSPENGER_UTBETALING_ARBEIDSTAKER`,
+            mellomlagring || {},
+        );
+        cy.intercept(`PUT`, `${INTERCEPT_PATH}/mellomlagring/OMSORGSPENGER_UTBETALING_ARBEIDSTAKER`, {}).as(
+            'putMellomlagring',
+        );
+        cy.intercept(`POST`, `${INTERCEPT_PATH}/mellomlagring/OMSORGSPENGER_UTBETALING_ARBEIDSTAKER`, {});
+        cy.intercept(`POST`, `${INTERCEPT_PATH}/omsorgspenger-utbetaling-arbeidstaker/innsending`, {}).as('innsending');
+        cy.intercept('POST', `${INTERCEPT_PATH}/vedlegg`, {
             location: '/vedlegg',
             headers: { Location: '/vedlegg', 'access-control-expose-headers': 'Location' },
         });
-        cy.intercept('GET', `**/oppslag/soker*`, cyApiMockData.søkerMock).as('getSøker');
-        cy.intercept('GET', `**/oppslag/arbeidsgiver*`, cyApiMockData.arbeidsgiverMock).as('getArbeidsgiver');
+        cy.intercept('GET', `${INTERCEPT_PATH}/oppslag/soker*`, cyApiMockData.søkerMock).as('getSøker');
+        cy.intercept('GET', `${INTERCEPT_PATH}/oppslag/arbeidsgiver*`, cyApiMockData.arbeidsgiverMock).as(
+            'getArbeidsgiver',
+        );
         cy.intercept('*.api.sanity.io', {});
     });
 
