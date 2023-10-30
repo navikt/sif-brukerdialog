4f01ddd9e2718ea4c99d37e94ea208624a78da8b
Frode Hansen <frode.hansen@bekk.no>
2023-10-30 10:38:42 +0100

    endre path til report

--- apps/sif-ettersending/e2e/playwright/utils/utfyllingUtils.ts
+++ apps/sif-ettersending/e2e/playwright/utils/utfyllingUtils.ts
@@ -5,21 +5,21 @@ const startUrl = 'http://localhost:8080/familie/sykdom-i-familien/soknad/etterse
 
 const velgYtelsePleiepenger = async (page: Page) => {
     await page.goto(startUrl);
-    await page.getByText('Pleiepenger for sykt barn').click();
-    await page.getByTestId('typedFormikForm-submitButton').click();
+    await page.getByLabel('Pleiepenger for sykt barn').click();
+    await page.getByRole('button').getByText('Gå videre').click();
 };
 
 const startSøknad = async (page: Page) => {
     // const intro = page.locator('.navds-guide-panel__content');
     // await expect(intro).toContainText('Hei');
     await page.getByTestId('bekreft-label').click();
-    await page.getByTestId('typedFormikForm-submitButton').click();
+    await page.getByRole('button').getByText('Start ettersendelse').click();
 };
 
 const fyllUtBeskrivelseSteg = async (page: Page) => {
     await page.getByRole('heading', { name: 'Ettersendelse av dokumentasjon' });
     await page.getByTestId('beskrivelse').fill('Her er en kommentar');
-    await page.getByTestId('typedFormikForm-submitButton').click();
+    await page.getByRole('button').getByText('Neste').click();
 };
 
 const fyllUtDokumenterSteg = async (page: Page) => {
@@ -32,7 +32,8 @@ const fyllUtDokumenterSteg = async (page: Page) => {
     await fileChooser.setFiles('./e2e/playwright/files/navlogopng.png');
     const listItems = await page.locator('.attachmentListElement');
     expect(listItems).toHaveCount(1);
-    await page.getByTestId('typedFormikForm-submitButton').click();
+    await page.getByRole('button').getByText('Neste').click();
+    // const feilmelding = await page.getByText('Feil i skjema');
 };
 
 const kontrollerOppsummering = async (page: Page) => {
@@ -43,7 +44,7 @@ const kontrollerOppsummering = async (page: Page) => {
 };
 const sendInnDokumenter = async (page: Page) => {
     await page.getByTestId('bekreft-label').click();
-    await page.getByTestId('typedFormikForm-submitButton').click();
+    await page.getByRole('button').getByText('Send inn').click();
 };
 
 const kontrollerKvittering = async (page: Page) => {
