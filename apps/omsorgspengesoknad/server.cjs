b306b3640d21be3bcbdd0e7c333c50f3723ca477
albrektsson <emil.albrektsson@nav.no>
2023-11-10 08:21:18 +0100

    Emil tester ting han ikke kan

--- apps/omsorgspengesoknad/server.cjs
+++ apps/omsorgspengesoknad/server.cjs
@@ -6,10 +6,7 @@ const getAppSettings = require('./src/build/AppSettings.cjs');
 const getDecorator = require('./src/build/decorator.cjs');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
-const jose = require('jose');
 const { v4: uuidv4 } = require('uuid');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const { initTokenX, exchangeToken } = require('./tokenx.cjs');
 const RateLimit = require('express-rate-limit');
 
 const isDev = process.env.NODE_ENV === 'development';
@@ -26,8 +23,13 @@ server.use((_req, res, next) => {
     res.set('X-Content-Type-Options', 'nosniff');
     res.set('Referrer-Policy', 'no-referrer');
     res.set('Feature-Policy', "geolocation 'none'; microphone 'none'; camera 'none'");
+    res.set('X-Correlation-ID', uuidv4());
+    if (process.env.NAIS_CLIENT_ID !== undefined) {
+        res.set('X-K9-Brukerdialog', process.env.NAIS_CLIENT_ID);
+    }
     next();
 });
+
 server.use(compression());
 server.use(cookieParser());
 
@@ -42,42 +44,6 @@ server.engine('html', mustacheExpress());
 
 const logError = (errorMessage, details) => console.log(errorMessage, details);
 
-const isExpiredOrNotAuthorized = (token) => {
-    if (token) {
-        try {
-            const exp = jose.decodeJwt(token).exp;
-            return Date.now() >= exp * 1000;
-        } catch (err) {
-            // eslint-disable-next-line no-console
-            console.error('Feilet med dekoding av token: ', err);
-            return true;
-        }
-    }
-    return true;
-};
-
-const getRouterConfig = async (req, audienceInnsyn) => {
-    {
-        req.headers['X-Correlation-ID'] = uuidv4();
-
-        if (process.env.NAIS_CLIENT_ID !== undefined) {
-            req.headers['X-K9-Brukerdialog'] = process.env.NAIS_CLIENT_ID;
-        }
-
-        if (req.headers['authorization'] !== undefined) {
-            const token = req.headers['authorization'].replace('Bearer ', '');
-            if (isExpiredOrNotAuthorized(token)) {
-                return undefined;
-            }
-            const exchangedToken = await exchangeToken(token, audienceInnsyn);
-            if (exchangedToken != null && !exchangedToken.expired() && exchangedToken.access_token) {
-                req.headers['authorization'] = `Bearer ${exchangedToken.access_token}`;
-            }
-        }
-
-        return undefined;
-    }
-};
 
 const renderApp = (decoratorFragments) =>
     new Promise((resolve, reject) => {
@@ -90,32 +56,12 @@ const renderApp = (decoratorFragments) =>
         });
     });
 
-const setupTokenX = async () => {
-    if (isDev) {
-        return Promise.resolve();
-    }
-    return Promise.all([initTokenX()]);
-};
-
 const startServer = async (html) => {
-    await setupTokenX();
-
     server.get(`${process.env.PUBLIC_PATH}/health/isAlive`, (_req, res) => res.sendStatus(200));
     server.get(`${process.env.PUBLIC_PATH}/health/isReady`, (_req, res) => res.sendStatus(200));
 
     server.use(
         process.env.FRONTEND_API_PATH,
-        createProxyMiddleware({
-            target: process.env.API_URL,
-            changeOrigin: true,
-            pathRewrite: (path) => {
-                return path.replace(process.env.FRONTEND_API_PATH, '');
-            },
-            router: async (req) => getRouterConfig(req, false),
-            secure: true,
-            xfwd: true,
-            logLevel: 'info',
-        }),
     );
 
     if (isDev) {
