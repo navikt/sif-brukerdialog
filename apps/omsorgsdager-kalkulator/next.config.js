218868afe163883596fa4cfbf492d51192c4be96
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-11-13 13:21:43 +0100

    la til redirect

--- apps/omsorgsdager-kalkulator/next.config.js
+++ apps/omsorgsdager-kalkulator/next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
         locales: ['nb'],
         defaultLocale: 'nb',
     },
+    async redirects() {
+        return [
+            {
+                source: '/beregne',
+                destination: '/',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
