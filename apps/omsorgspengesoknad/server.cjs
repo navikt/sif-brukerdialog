5a3f2313d7748b553be1135a501c52c4dcb2ea5f
albrektsson <emil.albrektsson@nav.no>
2023-11-21 12:54:03 +0100

    Testet o fjerne tokenx

--- apps/omsorgspengesoknad/server.cjs
+++ apps/omsorgspengesoknad/server.cjs
@@ -6,17 +6,7 @@ const getAppSettings = require('./src/build/AppSettings.cjs');
 const getDecorator = require('./src/build/decorator.cjs');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
-const jose = require('jose');
-const { v4: uuidv4 } = require('uuid');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const { initTokenX, exchangeToken } = require('./tokenx.cjs');
-const RateLimit = require('express-rate-limit');
-
-const isDev = process.env.NODE_ENV === 'development';
-
-if (isDev) {
-    require('dotenv').config();
-}
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 const server = express();
 server.use((_req, res, next) => {
@@ -31,53 +21,11 @@ server.use((_req, res, next) => {
 server.use(compression());
 server.use(cookieParser());
 
-if (isDev) {
-    require('dotenv').config();
-    server.set('views', `${__dirname}`);
-} else {
-    server.set('views', `${__dirname}/dist`);
-}
+server.set('views', `${__dirname}/dist`);
 server.set('view engine', 'mustache');
 server.engine('html', mustacheExpress());
 
 const logError = (errorMessage, details) => console.log(errorMessage, details);
-
-const isExpiredOrNotAuthorized = (token) => {
-    if (token) {
-        try {
-            const exp = jose.decodeJwt(token).exp;
-            return Date.now() >= exp * 1000;
-        } catch (err) {
-            // eslint-disable-next-line no-console
-            console.error('Feilet med dekoding av token: ', err);
-            return true;
-        }
-    }
-    return true;
-};
-
-const getRouterConfig = async (req, audienceInnsyn) => {
-    {
-        req.headers['X-Correlation-ID'] = uuidv4();
-
-        if (process.env.NAIS_CLIENT_ID !== undefined) {
-            req.headers['X-K9-Brukerdialog'] = process.env.NAIS_CLIENT_ID;
-        }
-
-        if (req.headers['authorization'] !== undefined) {
-            const token = req.headers['authorization'].replace('Bearer ', '');
-            if (isExpiredOrNotAuthorized(token)) {
-                return undefined;
-            }
-            const exchangedToken = await exchangeToken(token, audienceInnsyn);
-            if (exchangedToken != null && !exchangedToken.expired() && exchangedToken.access_token) {
-                req.headers['authorization'] = `Bearer ${exchangedToken.access_token}`;
-            }
-        }
-
-        return undefined;
-    }
-};
 
 const renderApp = (decoratorFragments) =>
     new Promise((resolve, reject) => {
@@ -90,16 +38,7 @@ const renderApp = (decoratorFragments) =>
         });
     });
 
-const setupTokenX = async () => {
-    if (isDev) {
-        return Promise.resolve();
-    }
-    return Promise.all([initTokenX()]);
-};
-
 const startServer = async (html) => {
-    await setupTokenX();
-
     server.get(`${process.env.PUBLIC_PATH}/health/isAlive`, (_req, res) => res.sendStatus(200));
     server.get(`${process.env.PUBLIC_PATH}/health/isReady`, (_req, res) => res.sendStatus(200));
 
@@ -111,58 +50,16 @@ const startServer = async (html) => {
             pathRewrite: (path) => {
                 return path.replace(process.env.FRONTEND_API_PATH, '');
             },
-            router: async (req) => getRouterConfig(req, false),
             secure: true,
             xfwd: true,
             logLevel: 'info',
         }),
     );
 
-    if (isDev) {
-        const fs = require('fs');
-        fs.writeFileSync(path.resolve(__dirname, 'index-decorated.html'), html);
-        const vedleggMockStore = './dist/vedlegg';
-
-        if (!fs.existsSync(vedleggMockStore)) {
-            fs.mkdirSync(vedleggMockStore);
-        }
-
-        const vite = await require('vite').createServer({
-            root: __dirname,
-            server: {
-                middlewareMode: true,
-                port: 8080,
-                open: './index-decorated.html',
-            },
-        });
-
-        server.use(
-            '/api',
-            RateLimit({
-                windowMs: 15 * 60 * 1000, // 15 minutes
-                max: 500, // max 100 requests per windowMs
-                standardHeaders: 'draft-7',
-                legacyHeaders: false,
-            }),
-        );
-
-        server.get(/^\/(?!.*dist).*$/, (req, _res, next) => {
-            const fullPath = path.resolve(__dirname, decodeURIComponent(req.path.substring(1)));
-            const fileExists = fs.existsSync(fullPath);
-
-            if ((!fileExists && !req.url.startsWith('/@')) || req.url === '/') {
-                req.url = '/index-decorated.html';
-            }
-            next();
-        });
-
-        server.use(vite.middlewares);
-    } else {
-        server.use(`${process.env.PUBLIC_PATH}/assets`, express.static(path.resolve(__dirname, 'dist/assets')));
-        server.get(/^\/(?!.*api)(?!.*dist).*$/, (_req, res) => {
-            res.send(html);
-        });
-    }
+    server.use(`${process.env.PUBLIC_PATH}/assets`, express.static(path.resolve(__dirname, 'dist/assets')));
+    server.get(/^\/(?!.*api)(?!.*dist).*$/, (_req, res) => {
+        res.send(html);
+    });
 
     const port = process.env.PORT || 8080;
     server.listen(port, () => {
