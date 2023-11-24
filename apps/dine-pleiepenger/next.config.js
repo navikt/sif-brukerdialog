d3ad463f66156728ddd41a78fe0b1642f7205f5e
Frode Hansen <frode.hansen@bekk.no>
2023-11-24 08:52:39 +0100

    ret

--- apps/dine-pleiepenger/next.config.js
+++ apps/dine-pleiepenger/next.config.js
@@ -35,15 +35,6 @@ const nextConfig = {
         dirs: ['src'],
         ignoreDuringBuilds: true,
     },
-    env: {
-        INNSYN_BACKEND_SCOPE: 'dummy-value',
-        BRUKERDIALOG_BACKEND_SCOPE: 'dummy-value',
-        IDPORTEN_CLIENT_ID: 'dummy-value',
-        IDPORTEN_WELL_KNOWN_URL: 'dummy-value',
-        TOKEN_X_WELL_KNOWN_URL: 'dummy-value',
-        TOKEN_X_PRIVATE_JWK: 'dummy-value',
-        TOKEN_X_CLIENT_ID: 'dummy-value',
-    },
 
     redirects: async () => [{ source: '/', destination: '/dine-pleiepenger', permanent: false, basePath: false }],
 
