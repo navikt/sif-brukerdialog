e98bc68a62b42386c28daa15893cfd5dcff00700
Frode Hansen <frode.hansen@bekk.no>
2023-11-24 07:53:13 +0100

    next-logger

--- apps/dine-pleiepenger/src/pages/api/soker.api.ts
+++ apps/dine-pleiepenger/src/pages/api/soker.api.ts
@@ -5,8 +5,11 @@ import { Søker } from '../../types/Søker';
 import { isForbidden } from '../../utils/apiUtils';
 import { getSøker } from '../../server/innsynService';
 import { isLocal } from '../../utils/env';
+import { createChildLogger } from '@navikt/next-logger';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const childLogger = createChildLogger(req.headers['x-request-id'] as string);
+
     try {
         const context = !isLocal
             ? createRequestContext(req.headers['x-request-id'] as string | undefined, req.headers['authorization'])
@@ -16,9 +19,12 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
             res.status(401).json({ error: 'Access denied - context is undefined' });
             return;
         }
+        childLogger.debug(context);
         const response = await getSøker(context);
         res.send(response);
     } catch (err) {
+        childLogger.error(`Fetching søker failed: ${err}`);
+        childLogger.debug(err);
         if (isForbidden(err)) {
             res.status(403).json({ error: 'Bruker har ikke tilgang' });
         }
