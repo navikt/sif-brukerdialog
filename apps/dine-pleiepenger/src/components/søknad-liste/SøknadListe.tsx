c6cb8854ad87dc3233ea07f182966b91d9014f1d
Frode Hansen <frode.hansen@bekk.no>
2023-12-04 16:38:27 +0100

    UU - sette fokus etter "Vis flere"

--- apps/dine-pleiepenger/src/components/søknad-liste/SøknadListe.tsx
+++ apps/dine-pleiepenger/src/components/søknad-liste/SøknadListe.tsx
@@ -3,7 +3,7 @@ import { Søknad } from '../../types/Søknad';
 import SøknadTitle from './SøknadTitle';
 import SøknadContent from './SøknadContent';
 import Skeleton from 'react-loading-skeleton';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { Add } from '@navikt/ds-icons';
 
 interface Props {
@@ -12,23 +12,38 @@ interface Props {
 
 const SøknadListe: React.FunctionComponent<Props> = ({ søknader = [] }) => {
     const [antall, setAntall] = useState(3);
+    const [focusIndex, setFocusIndex] = useState<number | undefined>();
 
     const totalt = useMemo(() => søknader.length, [søknader]);
+
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (focusIndex && ref.current) {
+            ref.current.focus();
+            setFocusIndex(undefined);
+        }
+    }, [antall, focusIndex]);
 
     if (søknader.length === 0) {
         return <Alert variant="info">Ingen søknader funnet</Alert>;
     }
 
     const visFlereSøknader = () => {
+        setFocusIndex(antall);
         setAntall(Math.min(søknader.length, antall + 5));
     };
 
     return (
         <VStack gap="2">
-            {søknader.slice(0, antall).map((søknad) => {
+            {søknader.slice(0, antall).map((søknad, index) => {
                 const labelId = `søknad-${søknad.journalpostId}`;
                 return (
-                    <ExpansionCard key={søknad.journalpostId} aria-labelledby={labelId}>
+                    <ExpansionCard
+                        tabIndex={-1}
+                        ref={index === focusIndex ? ref : undefined}
+                        key={søknad.journalpostId}
+                        aria-labelledby={labelId}>
                         <ExpansionCard.Header>
                             <ExpansionCard.Title id={labelId}>
                                 <SøknadTitle søknad={søknad} />
