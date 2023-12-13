ef7329d1e29c963e72264045a01603b9c3b503e7
Frode Hansen <frode.hansen@bekk.no>
2023-12-13 19:21:22 +0100

    Retry

--- apps/dine-pleiepenger/src/pages/api/dokument/[...info].api.ts
+++ apps/dine-pleiepenger/src/pages/api/dokument/[...info].api.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { withAuthenticatedApi } from '../../../auth/withAuthentication';
-import { fetchDocumentStream } from '../../../server/fetchDocumentStream';
 import { ApiService } from '../../../server/apiService';
+import { fetchDocument } from '../../../server/fetchDocument';
 import { getContextForApiHandler } from '../../../utils/apiUtils';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -15,8 +15,14 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     try {
         const path = `dokument/${info.join('/')}?dokumentTittel=${dokumentTittel}`;
-        const data = await fetchDocumentStream(path, getContextForApiHandler(req), ApiService.sifInnsyn);
-        data.pipe(res);
+        const blob = await fetchDocument(path, getContextForApiHandler(req), ApiService.sifInnsyn);
+        const resBufferArray = await blob.arrayBuffer();
+        const resBuffer = Buffer.from(resBufferArray);
+        res.setHeader('Content-Type', 'application/PDF');
+        res.setHeader('Content-Length', (blob as Blob).size.toString());
+        res.setHeader('Content-Disposition', `filename=${dokumentTittel}`);
+        res.write(resBuffer, 'binary');
+        res.end();
     } catch (err) {
         res.status(500).json({ error: 'Kunne ikke hente dokument', err });
     }
--- apps/dine-pleiepenger/src/server/fetchDocument.ts
+++ apps/dine-pleiepenger/src/server/fetchDocument.ts
@@ -1,20 +1,18 @@
 import { createChildLogger } from '@navikt/next-logger';
-import axios from 'axios';
 import { RequestContext } from '../types/RequestContext';
 import { ApiService } from './apiService';
 import { exchangeTokenAndPrepRequest } from './utils/exchangeTokenPrepRequest';
 
-export async function fetchDocument(path: string, context: RequestContext, service: ApiService): Promise<any> {
+export async function fetchDocument(path: string, context: RequestContext, service: ApiService): Promise<Blob> {
     const childLogger = createChildLogger(context.requestId);
 
     const { url, headers } = await exchangeTokenAndPrepRequest(service, context, path);
 
     childLogger.info(`Fetching document from ${url}`);
 
-    const response = await axios.get(url, {
-        responseType: 'document',
-        headers: { ...headers },
-    });
-
-    return response.data;
+    const response = await fetch(url, { headers });
+    if (!response.ok) {
+        throw new Error(`Unexpected response fetching PDF ${response.statusText}`);
+    }
+    return await response.blob();
 }
