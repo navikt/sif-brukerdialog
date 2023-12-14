795d48676ca8b5b919192d3a6d74521dd5fb88a5
Frode Hansen <frode.hansen@bekk.no>
2023-12-14 10:06:59 +0100

    Fjerne sentryvite playwright plugin

--- apps/pleiepenger-sykt-barn/vite.config.ts
+++ apps/pleiepenger-sykt-barn/vite.config.ts
@@ -3,7 +3,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
-import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -11,11 +10,6 @@ export default defineConfig({
             include: '**/*.{jsx,tsx}',
         }),
         checker({ typescript: true }),
-        sentryVitePlugin({
-            org: 'nav',
-            project: 'sykdom-i-familien',
-            authToken: process.env.SENTRY_AUTH_TOKEN,
-        }),
     ],
     resolve: {},
     build: {
