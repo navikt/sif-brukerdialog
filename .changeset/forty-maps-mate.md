5b106ca3dbc02736e32578d4d9376bc4be5d19a3
frodehansen2 <frode.hansen@bekk.no>
2023-12-15 15:48:09 +0100

    Forenkle zod schema (#1480)
    * Forenkle zod schema
    
    * Changeset

---
+++ .changeset/forty-maps-mate.md
@@ -0,0 +1,5 @@
+---
+"@navikt/dine-pleiepenger": patch
+---
+
+Forenkle zod-schema for organisasjon. Unngå feil typing i data som ikke brukes
--- apps/dine-pleiepenger/src/server/api-models/OrganisasjonSchema.ts
+++ apps/dine-pleiepenger/src/server/api-models/OrganisasjonSchema.ts
@@ -2,9 +2,5 @@ import { z } from 'zod';
 
 export const OrganisasjonSchema = z.object({
     navn: z.string(),
-    skalJobbe: z.boolean(),
-    skalJobbeProsent: z.number(),
-    vetIkkeEkstrainfo: z.union([z.string(), z.null(), z.undefined()]),
-    jobberNormaltTimer: z.number(),
     organisasjonsnummer: z.string(),
 });
