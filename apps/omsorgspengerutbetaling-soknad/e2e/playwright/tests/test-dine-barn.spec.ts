9ce02bd81742e1b00bb38db2fbb53c63ba7b3b18
Frode Hansen <frode.hansen@bekk.no>
2024-01-05 13:22:02 +0100

    Test barn over 13

--- apps/omsorgspengerutbetaling-soknad/e2e/playwright/tests/test-dine-barn.spec.ts
+++ apps/omsorgspengerutbetaling-soknad/e2e/playwright/tests/test-dine-barn.spec.ts
@@ -1,56 +1,85 @@
-import { Page, expect, test } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { playwrightApiMockData } from '../mock-data/playwrightApiMockData';
 import { utfyllingUtils } from '../utils/utfyllingUtils';
 
-const fyllUtOmBarnfyllUtOmBarnToUnder13år = async (page: Page) => {
-    await page.getByRole('heading', { name: 'Om barn' });
-    await page.getByRole('group', { name: 'Har du søkt om eller fått ekstra' }).getByLabel('Ja').click();
-    await page.getByRole('group', { name: 'Har du dekket de 10 første omsorgsdagene i år?' }).getByLabel('Ja').click();
-    await page.getByTestId('typedFormikForm-submitButton').click();
-};
-const fyllUtOmBarnfyllUtOmBarnTreUnder13år = async (page: Page) => {
-    await page.getByRole('heading', { name: 'Om barn' });
-    await page.getByRole('group', { name: 'Har du dekket de 10 første omsorgsdagene i år?' }).getByLabel('Ja').click();
-    await page.getByTestId('typedFormikForm-submitButton').click();
-};
-
-test.describe('Tester varianter av Dine barn steg', () => {
-    test.beforeEach(async ({ page }) => {
-        await page.route('https://**.nav.no/**', async (route) => {
-            await route.fulfill({ status: 200 });
-        });
-        await page.route('**/mellomlagring/OMSORGSPENGER_UTBETALING_SNF', async (route) => {
-            await route.fulfill({ status: 200, body: '{}' });
-        });
-        await page.route('**/oppslag/soker', async (route) => {
-            await route.fulfill({ status: 200, body: JSON.stringify(playwrightApiMockData.søkerMock) });
-        });
-        await page.route('**/oppslag/barn', async (route) => {
-            await route.fulfill({ status: 200, body: JSON.stringify(playwrightApiMockData.barnMock.toBarnUnder13) });
-        });
-        await page.route('**/vedlegg', async (route) => {
-            await route.fulfill({
-                status: 200,
-                headers: { Location: '/vedlegg', 'access-control-expose-headers': 'Location' },
-            });
-        });
-        await page.route('**/innsending', async (route) => {
-            await route.fulfill({ status: 200 });
+const startScenario = async (page: Page, barnMockData: any) => {
+    await page.route('https://**.nav.no/**', async (route) => {
+        await route.fulfill({ status: 200 });
+    });
+    await page.route('**/mellomlagring/OMSORGSPENGER_UTBETALING_SNF', async (route) => {
+        await route.fulfill({ status: 200, body: '{}' });
+    });
+    await page.route('**/oppslag/soker', async (route) => {
+        await route.fulfill({ status: 200, body: JSON.stringify(playwrightApiMockData.søkerMock) });
+    });
+    await page.route('**/vedlegg', async (route) => {
+        await route.fulfill({
+            status: 200,
+            headers: { Location: '/vedlegg', 'access-control-expose-headers': 'Location' },
         });
     });
+    await page.route('**/innsending', async (route) => {
+        await route.fulfill({ status: 200 });
+    });
+    await page.route('**/oppslag/barn', async (route) => {
+        await route.fulfill({ status: 200, body: JSON.stringify(barnMockData) });
+    });
+};
 
-    test('Fyller ut søknad med to barn under 13 år', async ({ page }) => {
+const svarUtils = (page: Page) => ({
+    harSøktOmEkstraDager: async (harSøkt: boolean) => {
+        await page
+            .getByRole('group', { name: 'Har du søkt om eller fått ekstra' })
+            .getByLabel(harSøkt ? 'Ja' : 'Nei')
+            .click();
+    },
+    harAleneomsorg: async (harSøkt: boolean) => {
+        await page
+            .getByRole('group', { name: 'Har du aleneomsorg' })
+            .getByLabel(harSøkt ? 'Ja' : 'Nei')
+            .click();
+    },
+    harDekketDeTiFørsteDagene: async (harDekket: boolean) => {
+        await page
+            .getByRole('group', { name: 'Har du dekket de 10 første omsorgsdagene i år?' })
+            .getByLabel(harDekket ? 'Ja' : 'Nei')
+            .click();
+    },
+    submitIsEnabled: async () => {
+        return page.getByRole('button', { name: 'Neste' }).isEnabled();
+    },
+});
+
+test.describe('Tester varianter av Dine barn steg', () => {
+    test('To barn under 13 år', async ({ page }) => {
+        await startScenario(page, playwrightApiMockData.barnMock.toBarnUnder13);
         await utfyllingUtils.startSøknad(page);
-        await fyllUtOmBarnfyllUtOmBarnToUnder13år(page);
-        await expect(page.getByRole('heading', { level: 1, name: 'Dager du søker om utbetaling for' })).toBeVisible();
+
+        await svarUtils(page).harSøktOmEkstraDager(true);
+        await svarUtils(page).harDekketDeTiFørsteDagene(true);
+
+        const melding = await page.locator('.navds-alert', { hasText: 'må du dekke de 10 første omsorgsdagene' });
+        await expect(melding).toBeVisible();
     });
 
     test('Fyller ut søknad med tre barn under 13 år', async ({ page }) => {
-        await page.route('**/oppslag/barn', async (route) => {
-            await route.fulfill({ status: 200, body: JSON.stringify(playwrightApiMockData.barnMock.treBarnUnder13) });
-        });
+        await startScenario(page, playwrightApiMockData.barnMock.treBarnUnder13);
         await utfyllingUtils.startSøknad(page);
-        await fyllUtOmBarnfyllUtOmBarnTreUnder13år(page);
-        await expect(page.getByRole('heading', { level: 1, name: 'Dager du søker om utbetaling for' })).toBeVisible();
+
+        await svarUtils(page).harDekketDeTiFørsteDagene(true);
+    });
+    test('Fyller ut søknad med kun barn over 13 år', async ({ page }) => {
+        await startScenario(page, playwrightApiMockData.barnMock.ettBarnOver13);
+        await utfyllingUtils.startSøknad(page);
+
+        /** Har søkt utvidet */
+        await svarUtils(page).harSøktOmEkstraDager(true);
+        expect(await page.getByText('Fordi du har kronisk sykt barn over 13')).toBeVisible();
+        expect(await svarUtils(page).submitIsEnabled()).toBe(true);
+
+        /** Har ikke søkt utvidet */
+        await svarUtils(page).harSøktOmEkstraDager(false);
+        expect(await page.getByText('For å ha rett på omsorgsdager for barn som er 13 år eller eldre')).toBeVisible();
+        expect(await svarUtils(page).submitIsEnabled()).toBe(false);
     });
 });
