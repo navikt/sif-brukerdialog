552cd4cd08dd357d9c01b168678c62984cca97a5
Frode Hansen <frode.hansen@bekk.no>
2024-01-25 13:39:42 +0100

    Storybook for ikke tilgang i endringsmelding

--- apps/endringsmelding-pleiepenger/src/storybook/stories/pages/IngenTilgangPage.stories.tsx
+++ apps/endringsmelding-pleiepenger/src/storybook/stories/pages/IngenTilgangPage.stories.tsx
@@ -1,40 +1,73 @@
-import { Meta, StoryFn } from '@storybook/react';
-import * as React from 'react';
-import IngenTilgangPage, { IngenTilgangPageProps } from '../../../app/pages/ingen-tilgang/IngenTilgangPage';
-import { IngenTilgangÅrsak } from '../../../app/types/IngenTilgangÅrsak';
-import { søkerMock } from '../../data/søkerMock';
+import type { Meta, StoryObj } from '@storybook/react';
+import IngenTilgangPage from '../../../app/pages/ingen-tilgang/IngenTilgangPage';
 import { withAmplitudeProvider } from '../../decorators/withAmplitudeProvider';
 import { withIntl } from '../../decorators/withIntl';
 import { withRouterProvider } from '../../decorators/withRouter';
+import { søkerMock } from '../../data/søkerMock';
+import { IngenTilgangÅrsak } from '../../../app/types/IngenTilgangÅrsak';
 
 const meta: Meta<typeof IngenTilgangPage> = {
     title: 'Pages/IngenTilgangPage',
     component: IngenTilgangPage,
     decorators: [withAmplitudeProvider, withIntl, withRouterProvider],
-    argTypes: {
-        årsak: {
-            options: Object.keys(IngenTilgangÅrsak).map((årsak) => [årsak]),
-            control: {
-                type: 'select',
-            },
-            defaultValue: IngenTilgangÅrsak.harUgyldigK9FormatSak,
-        },
+    parameters: {
+        layout: 'centered',
+    },
+};
+export default meta;
+
+type Story = StoryObj<typeof IngenTilgangPage>;
+
+export const Default: Story = {
+    name: 'harArbeidsgiverUtenArbeidsaktivitet',
+    args: {
+        søker: søkerMock,
+        årsak: [IngenTilgangÅrsak.harArbeidsgiverUtenArbeidsaktivitet],
     },
 };
 
-export default meta;
-
-const Template: StoryFn<typeof IngenTilgangPage> = (props) => {
-    const propsToUse: IngenTilgangPageProps = {
-        ...props,
-        // eslint-disable-next-line react/prop-types
-        årsak: props.årsak,
-    };
-    return <IngenTilgangPage {...propsToUse} søker={søkerMock} />;
+export const ArbeidstidSomSelvstendigNæringsdrivende: Story = {
+    name: 'harArbeidstidSomSelvstendigNæringsdrivende',
+    args: {
+        søker: søkerMock,
+        årsak: [IngenTilgangÅrsak.harArbeidstidSomSelvstendigNæringsdrivende],
+    },
 };
 
-export const Default = Template.bind({});
+export const IngenPerioder: Story = {
+    name: 'harIngenPerioder',
+    args: {
+        søker: søkerMock,
+        årsak: [IngenTilgangÅrsak.harIngenPerioder],
+    },
+};
 
-Default.args = {};
+export const IngenSak: Story = {
+    name: 'harIngenSak',
+    args: {
+        søker: søkerMock,
+        årsak: [IngenTilgangÅrsak.harIngenSak],
+    },
+};
 
-Default.parameters = {};
+export const MerEnnEnSak: Story = {
+    name: 'harMerEnnEnSak',
+    args: {
+        søker: søkerMock,
+        årsak: [IngenTilgangÅrsak.harMerEnnEnSak],
+    },
+};
+export const UgyldigK9FormatSak: Story = {
+    name: 'harUgyldigK9FormatSak',
+    args: {
+        søker: søkerMock,
+        årsak: [IngenTilgangÅrsak.harUgyldigK9FormatSak],
+    },
+};
+export const SøknadsperioderUtenforTillattEndringsperiode: Story = {
+    name: 'søknadsperioderUtenforTillattEndringsperiode',
+    args: {
+        søker: søkerMock,
+        årsak: [IngenTilgangÅrsak.søknadsperioderUtenforTillattEndringsperiode],
+    },
+};
