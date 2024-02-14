12dd789387d378c228a17c83ca86bb566602b233
Frode Hansen <frode.hansen@bekk.no>
2024-02-14 13:14:49 +0100

    På vent

--- apps/dine-pleiepenger/src/server/api-models/Behandlingsstatus.ts
+++ apps/dine-pleiepenger/src/server/api-models/Behandlingsstatus.ts
@@ -1,6 +1,6 @@
 export enum Behandlingsstatus {
     OPPRETTET = 'OPPRETTET',
     UNDER_BEHANDLING = 'UNDER_BEHANDLING',
-    PÅ_VENT = 'PA_VENT',
+    PÅ_VENT = 'PÅ_VENT',
     AVSLUTTET = 'AVSLUTTET',
 }
