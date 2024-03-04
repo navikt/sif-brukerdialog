b3ce5e8881db948cf7a3640df77aa63b2cdfaed9
Ramin Esfandiari <ramin.esfandiari@nav.no>
2024-03-04 15:18:15 +0000

    Revert changes

--- apps/dine-pleiepenger/src/server/api-models/SakSchema.ts
+++ apps/dine-pleiepenger/src/server/api-models/SakSchema.ts
@@ -6,22 +6,7 @@ export type Sak = z.infer<typeof SakSchema>;
 
 export const SakSchema = z.object({
   saksnummer: z.string(),
-  saksbehandlingsFrist: z.union([
-    z.preprocess((val) => {
-      // Ensure only non-null string values are processed
-      if (val === null || typeof val !== 'string') return undefined;
-
-      // Attempt to parse the date string, handling any errors gracefully
-      try {
-        const date = parseMaybeDateStringToDate(val);
-        return date;
-      } catch (error) {
-        console.error(`Error parsing date: ${error}`);
-        return undefined;
-      }
-    }, z.date()),
-    z.undefined(),
-  ]),
+  saksbehandlingsFrist: z.union([z.preprocess((val) => parseMaybeDateStringToDate(val), z.date()), z.undefined()]),
   behandlinger: z.array(BehandlingSchema),
 });
 
