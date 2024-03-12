bb32772f378b7c9a18d4d04fac6f5b9314d7ee7f
frodehansen2 <frode.hansen@bekk.no>
2024-03-12 20:31:43 +0100

    Log til sentry når bruker ikke har saker (#1667)
    * Legge på log
    
    * Endre log env

--- apps/endringsmelding-pleiepenger/src/app/utils/appSentryLogger.ts
+++ apps/endringsmelding-pleiepenger/src/app/utils/appSentryLogger.ts
@@ -1,3 +1,3 @@
 import getSentryLoggerForApp from '@navikt/sif-common-sentry';
 
-export const appSentryLogger = getSentryLoggerForApp('pleiepengesoknad', ['pleiepengesoknad.nav.no/dist/js']);
+export const appSentryLogger = getSentryLoggerForApp('endringsmelding-pleiepenger', ['endringsmelding-pleiepenger']);
