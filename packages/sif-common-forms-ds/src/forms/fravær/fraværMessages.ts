a5134536886b62ce52e33e5bfdc5e0f276aa0ea9
Frode Hansen <frode.hansen@bekk.no>
2024-03-25 08:59:46 +0100

    Legge til @forms i tekstnøkler

--- packages/sif-common-forms-ds/src/forms/fravær/fraværMessages.ts
+++ packages/sif-common-forms-ds/src/forms/fravær/fraværMessages.ts
@@ -2,79 +2,71 @@ import { useIntl } from 'react-intl';
 import { typedIntlHelper } from '@navikt/sif-common-core-ds/src/utils/intlUtils';
 
 const nb = {
-    'fravær.form.validation.dateOutsideRange': 'Første gyldige dato er {fom}, og siste gyldige dato er {tom}',
-    'fravær.form.validation.fromDateAfterToDate': 'Fra-dato må være lik eller før til-dato',
-    'fravær.form.validation.toDateBeforeFromDate': 'Til-dato må være lik eller etter fra-dato',
-    'fravær.form.validation.fra_og_til_er_ulike_år': 'Fra og til dato må være i samme år',
-    'fravær.form.validation.timer_mer_enn_arbeidstimer':
+    '@forms.fravær.form.validation.dateOutsideRange': 'Første gyldige dato er {fom}, og siste gyldige dato er {tom}',
+    '@forms.fravær.form.validation.fromDateAfterToDate': 'Fra-dato må være lik eller før til-dato',
+    '@forms.fravær.form.validation.toDateBeforeFromDate': 'Til-dato må være lik eller etter fra-dato',
+    '@forms.fravær.form.validation.fra_og_til_er_ulike_år': 'Fra og til dato må være i samme år',
+    '@forms.fravær.form.validation.timer_mer_enn_arbeidstimer':
         'Antall timer med fravær kan ikke være mer enn antall timer du skulle ha jobbet denne dagen',
-    'fravær.form.validation.er_helg': 'Lørdag og søndag er ikke gyldig.',
-    'fieldvalidation.dager_overlapper_med_andre_dager':
-        'Én eller flere dager med fravær overlapper med andre dager med fravær.',
-    'fieldvalidation.fra_dato_kolliderer_med_annet_fravær':
-        'Fra og med datoen overlapper med allerede registrert fraværsdag',
-    'fieldvalidation.til_dato_kolliderer_med_annet_fravær':
-        'Til og med datoen overlapper med allerede registrert fraværsdag',
-    'fieldvalidation.dato_kolliderer_med_annet_fravær': 'Datoen overlapper med allerede registrert fraværsdag',
+    '@forms.fravær.form.validation.er_helg': 'Lørdag og søndag er ikke gyldig.',
+    '@forms.fravær.form.felles.ok': 'Ok',
+    '@forms.fravær.form.felles.avbryt': 'Avbryt',
+    '@forms.fravær.form.periode.tittel': 'Periode med fravær fra jobb',
+    '@forms.fravær.form.periode.tidsrom': 'Velg tidsrom',
+    '@forms.fravær.form.periode.fom': 'Fra og med',
+    '@forms.fravær.form.periode.tom': 'Til og med',
+    '@forms.fravær.form.dag.tittel': 'Dag med delvis fravær fra jobb',
+    '@forms.fravær.form.dag.dato': 'Dato',
+    '@forms.fravær.form.dag.antallArbeidstimer': 'Antall timer du skulle ha jobbet denne dagen',
+    '@forms.fravær.form.dag.timerFravær': 'Antall timer du var borte fra jobb denne dagen',
 
-    'fravær.form.felles.ok': 'Ok',
-    'fravær.form.felles.avbryt': 'Avbryt',
-
-    'fravær.form.periode.tittel': 'Periode med fravær fra jobb',
-
-    'fravær.form.periode.tidsrom': 'Velg tidsrom',
-    'fravær.form.periode.fom': 'Fra og med',
-    'fravær.form.periode.tom': 'Til og med',
-    'fravær.form.dag.tittel': 'Dag med delvis fravær fra jobb',
-    'fravær.form.dag.dato': 'Dato',
-    'fravær.form.dag.antallArbeidstimer': 'Antall timer du skulle ha jobbet denne dagen',
-    'fravær.form.dag.timerFravær': 'Antall timer du var borte fra jobb denne dagen',
-
-    'fraværDagForm.dato.dateHasNoValue': 'Du må oppgi dato for når du hadde delvis fravær.',
-    'fraværDagForm.dato.dateHasInvalidFormat':
+    '@forms.fraværDagForm.dato.dateHasNoValue': 'Du må oppgi dato for når du hadde delvis fravær.',
+    '@forms.fraværDagForm.dato.dateHasInvalidFormat':
         'Du må oppgi dato for når du hadde delvis fravær i et gyldig format. Gyldig format er dd.mm.åååå.',
-    'fraværDagForm.dato.dateIsAfterMax':
+    '@forms.fraværDagForm.dato.dateIsAfterMax':
         'Dato for når du hadde delvis fravær kan ikke være etter dagens dato. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværDagForm.dato.dateIsBeforeMin':
+    '@forms.fraværDagForm.dato.dateIsBeforeMin':
         'Datoen for når du hadde delvis fravær kan ikke være før {dato}. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværDagForm.dato.er_helg':
+    '@forms.fraværDagForm.dato.er_helg':
         'Dato for når du hadde delvis fravær må være en ukedag, det kan ikke være lørdag eller søndag.',
-    'fraværDagForm.dato.dato_kolliderer_med_annet_fravær': 'Datoen overlapper med allerede registrert fraværsdag',
-    'fraværDagForm.timerArbeidsdag.noValue': 'Du må velge antall timer du skulle ha jobbet denne dagen.',
-    'fraværDagForm.timerFravær.noValue': 'Du må velge antall timer du var borte fra jobb denne dagen',
-    'fraværDagForm.timerFravær.fravær_timer_mer_enn_arbeidstimer': 'Fravær kan ikke være høyere enn arbeidstimer',
+    '@forms.fraværDagForm.dato.dato_kolliderer_med_annet_fravær':
+        'Datoen overlapper med allerede registrert fraværsdag',
+    '@forms.fraværDagForm.timerArbeidsdag.noValue': 'Du må velge antall timer du skulle ha jobbet denne dagen.',
+    '@forms.fraværDagForm.timerFravær.noValue': 'Du må velge antall timer du var borte fra jobb denne dagen',
+    '@forms.fraværDagForm.timerFravær.fravær_timer_mer_enn_arbeidstimer':
+        'Fravær kan ikke være høyere enn arbeidstimer',
 
-    'fraværPeriodeForm.fraOgMed.dateHasNoValue':
+    '@forms.fraværPeriodeForm.fraOgMed.dateHasNoValue':
         'Du må oppgi når perioden startet. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværPeriodeForm.fraOgMed.dateIsAfterMax':
+    '@forms.fraværPeriodeForm.fraOgMed.dateIsAfterMax':
         'Datoen for når perioden startet kan ikke være etter dagens dato. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværPeriodeForm.fraOgMed.dateIsBeforeMin':
+    '@forms.fraværPeriodeForm.fraOgMed.dateIsBeforeMin':
         'Datoen for når perioden startet kan ikke være før {dato}. Skriv inn eller velg sluttdato fra datovelgeren.',
-    'fraværPeriodeForm.fraOgMed.dateHasInvalidFormat':
+    '@forms.fraværPeriodeForm.fraOgMed.dateHasInvalidFormat':
         'Du må oppgi dato for når perioden startet i et gyldig format. Gyldig format er dd.mm.åååå.',
-    'fraværPeriodeForm.fraOgMed.fromDateIsAfterToDate':
+    '@forms.fraværPeriodeForm.fraOgMed.fromDateIsAfterToDate':
         'Startdatoen for perioden må være før sluttdatoen, eller på samme dag som sluttdatoen. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværPeriodeForm.tilOgMed.dateHasNoValue':
+    '@forms.fraværPeriodeForm.tilOgMed.dateHasNoValue':
         'Du må oppgi når perioden sluttet. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværPeriodeForm.tilOgMed.dateIsAfterMax':
+    '@forms.fraværPeriodeForm.tilOgMed.dateIsAfterMax':
         'Datoen for når perioden sluttet kan ikke være etter dagens dato. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværPeriodeForm.tilOgMed.dateIsBeforeMin':
+    '@forms.fraværPeriodeForm.tilOgMed.dateIsBeforeMin':
         'Datoen for når perioden sluttet kan ikke være før {dato}. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværPeriodeForm.tilOgMed.dateHasInvalidFormat':
+    '@forms.fraværPeriodeForm.tilOgMed.dateHasInvalidFormat':
         'Du må oppgi dato for når perioden sluttet i et gyldig format. Gyldig format er dd.mm.åååå.',
-    'fraværPeriodeForm.tilOgMed.toDateIsBeforeFromDate':
+    '@forms.fraværPeriodeForm.tilOgMed.toDateIsBeforeFromDate':
         'Sluttdatoen for perioden kan ikke være før startdatoen. Skriv inn eller velg dato fra datovelgeren.',
-    'fraværPeriodeForm.fraOgMed.er_helg':
+    '@forms.fraværPeriodeForm.fraOgMed.er_helg':
         'Periodens fra-dato må være en ukedag, det kan ikke være lørdag eller søndag. Hvis perioden startet en lørdag eller søndag må du velge mandagen etter som startdato.',
-    'fraværPeriodeForm.fraOgMed.fra_og_til_er_ulike_år': 'Fra og til dato må være i samme år',
-    'fraværPeriodeForm.fraOgMed.fra_dato_kolliderer_med_annet_fravær':
+    '@forms.fraværPeriodeForm.fraOgMed.fra_og_til_er_ulike_år': 'Fra og til dato må være i samme år',
+    '@forms.fraværPeriodeForm.fraOgMed.fra_dato_kolliderer_med_annet_fravær':
         'Fra og med datoen overlapper med allerede registrert fraværsdag.',
-    'fraværPeriodeForm.tilOgMed.er_helg':
+    '@forms.fraværPeriodeForm.tilOgMed.er_helg':
         'Periodens til-dato må være en ukedag, det kan ikke være lørdag eller søndag. Hvis perioden sluttet en lørdag eller søndag må du velge fredagen før som sluttdato.',
-    'fraværPeriodeForm.tilOgMed.fra_og_til_er_ulike_år': 'Fra og til dato må være i samme år',
-    'fraværPeriodeForm.tilOgMed.til_dato_kolliderer_med_annet_fravær':
+    '@forms.fraværPeriodeForm.tilOgMed.fra_og_til_er_ulike_år': 'Fra og til dato må være i samme år',
+    '@forms.fraværPeriodeForm.tilOgMed.til_dato_kolliderer_med_annet_fravær':
         'Til og med datoen overlapper med allerede registrert fraværsdag',
-    'fraværPeriodeForm.periode.dager_overlapper_med_andre_dager':
+    '@forms.fraværPeriodeForm.periode.dager_overlapper_med_andre_dager':
         'Én eller flere dager med fravær overlapper med andre dager med fravær.',
 };
 
