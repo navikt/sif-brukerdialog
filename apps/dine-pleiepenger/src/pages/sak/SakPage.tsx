75f740a367cc125c9886f1f7319a0e04c5e2c4bb
Frode Hansen <frode.hansen@bekk.no>
2024-04-10 10:50:34 +0200

    Saksnummer i title

--- apps/dine-pleiepenger/src/pages/sak/SakPage.tsx
+++ apps/dine-pleiepenger/src/pages/sak/SakPage.tsx
@@ -56,7 +56,7 @@ const SakPage: React.FunctionComponent<Props> = ({ sak, pleietrengende, saksbeha
                 />
             }>
             <Head>
-                <title>Din pleiepengesak - {navn}</title>
+                <title>Din pleiepengesak - {sak.saksnummer}</title>
             </Head>
             <VStack gap="12">
                 {statusISak?.venteårsak && statusISak.status !== Behandlingsstatus.AVSLUTTET ? (
