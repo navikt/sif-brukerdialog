da09cbe1fb0aab61ee0627b61923e5146f6dd784
Frode Hansen <frode.hansen@bekk.no>
2024-04-16 07:52:29 +0200

    Endre tekst ved ugyldig fnr (allerede brukt)

--- packages/sif-common-forms-ds/src/forms/fosterbarn/fosterbarnMessages.ts
+++ packages/sif-common-forms-ds/src/forms/fosterbarn/fosterbarnMessages.ts
@@ -13,7 +13,7 @@ const nb = {
     '@forms.fosterbarnForm.fødselsnummer.fødselsnummerIsNot11Chars':
         'Du har oppgitt et ugyldig fødselsnummer. Et gyldig fødselsnummer består av 11 siffer.',
     '@forms.fosterbarnForm.fødselsnummer.fødselsnummerIsNotAllowed':
-        'Du har oppgitt ditt eget fødselsnummer som barnets fødselsnummer. Skriv inn barnets fødselsnummer.',
+        'Du har oppgitt ditt eget fødselsnummer, eller et fødselsnummer for et fosterbarn du allerede har lagt til, som barnets fødselsnummer. Skriv inn barnets fødselsnummer.',
     '@forms.fosterbarnForm.navn.stringHasNoValue': 'Skriv inn barnets navn',
 };
 const nn: Record<keyof typeof nb, string> = {
