befa22e8dfac915fd35d0663b7f34c73be22a1aa
Frode Hansen <frode.hansen@bekk.no>
2024-04-23 12:26:27 +0200

    Bugfix - sort søknadshendelser FORVENTET_SVAR

--- apps/dine-pleiepenger/src/utils/__tests__/sakUtils.test.ts
+++ apps/dine-pleiepenger/src/utils/__tests__/sakUtils.test.ts
@@ -1,8 +1,10 @@
 import { ISODateToDate } from '@navikt/sif-common-utils';
 import { Behandling } from '../../server/api-models/BehandlingSchema';
 import { Behandlingsstatus } from '../../server/api-models/Behandlingsstatus';
-import { getSisteBehandlingISak, sortBehandlingerNyesteFørst } from '../sakUtils';
+import { getSisteBehandlingISak, sortBehandlingerNyesteFørst, sortSøknadshendelse } from '../sakUtils';
 import { Sak } from '../../server/api-models/SakSchema';
+import { Søknadshendelse, SøknadshendelseType } from '../../types/Søknadshendelse';
+import { Søknadstype } from '../../server/api-models/Søknadstype';
 
 const behandling1: Behandling = {
     status: Behandlingsstatus.UNDER_BEHANDLING,
@@ -42,6 +44,37 @@ describe('sakUtils', () => {
             };
             const sisteBehandling = getSisteBehandlingISak(sak);
             expect(sisteBehandling).toEqual(behandling2);
+        });
+    });
+
+    describe('sortSøknadshendelse', () => {
+        const hendelse1: Søknadshendelse = {
+            dato: ISODateToDate('2020-01-01'),
+            type: SøknadshendelseType.FERDIG_BEHANDLET,
+        };
+        const hendelse2: Søknadshendelse = {
+            dato: ISODateToDate('2020-01-03'),
+            type: SøknadshendelseType.MOTTATT_SØKNAD,
+            søknad: {} as any,
+        };
+        const hendelse3: Søknadshendelse = {
+            dato: ISODateToDate('2020-01-03'),
+            type: SøknadshendelseType.FERDIG_BEHANDLET,
+        };
+
+        const hendelseForventetSvar: Søknadshendelse = {
+            dato: ISODateToDate('2020-01-02'),
+            type: SøknadshendelseType.FORVENTET_SVAR,
+            søknadstyperIBehandling: [Søknadstype.SØKNAD],
+        };
+
+        it('sorterer riktig på hendelser som ikke er FORVENTET_SVAR', () => {
+            const result = [hendelse1, hendelse2, hendelse3].sort(sortSøknadshendelse);
+            expect(result).toEqual([hendelse1, hendelse3, hendelse2]);
+        });
+        it('sorterer alltid FORVENTET_SVAR sist', () => {
+            const result = [hendelseForventetSvar, hendelse1, hendelse2, hendelse3].sort(sortSøknadshendelse);
+            expect(result).toEqual([hendelse1, hendelse3, hendelse2, hendelseForventetSvar]);
         });
     });
 });
