2336a83222d3c03f2e30cc1fd87a81cd3fd89aac
Frode Hansen <frode.hansen@bekk.no>
2024-04-24 11:00:18 +0200

    Kvittering

--- apps/omsorgsdager-aleneomsorg-dialog/src/app/pages/kvittering/kvitteringMesssages.ts
+++ apps/omsorgsdager-aleneomsorg-dialog/src/app/pages/kvittering/kvitteringMesssages.ts
@@ -3,8 +3,8 @@ const nb = {
     'kvittering.info.tittel': 'Hva skjer videre nå?',
     'kvittering.info.1': 'Vi behandler søknaden din.',
     'kvittering.info.2': 'Vi kontakter deg hvis vi trenger flere opplysninger.',
-    'kvittering.info.3.1': 'Når søknaden er ferdigbehandlet, får du et svar fra oss på «Min side».',
-    'kvittering.info.3.2.lenke': 'Du kan sjekke saksbehandlingstiden her.',
+    'kvittering.info.3':
+        'Når søknaden er ferdigbehandlet, får du et svar fra oss på «Min side». <Lenke>Du kan sjekke saksbehandlingstiden her</Lenke>.',
 };
 
 const nn: Record<keyof typeof nb, string> = { ...nb };
