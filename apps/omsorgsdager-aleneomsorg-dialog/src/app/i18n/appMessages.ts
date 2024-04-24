6d47634ef1be21d390e3833e76cb4270653fdfcf
Frode Hansen <frode.hansen@bekk.no>
2024-04-24 10:43:24 +0200

    Velkommen-page

--- apps/omsorgsdager-aleneomsorg-dialog/src/app/i18n/appMessages.ts
+++ apps/omsorgsdager-aleneomsorg-dialog/src/app/i18n/appMessages.ts
@@ -1,24 +1,40 @@
-import { MessageFileFormat } from '@navikt/sif-common-core-ds/src/types/MessageFileFormat';
+import { annetBarnMessages } from '@navikt/sif-common-forms-ds';
+import { kvitteringMessages } from '../pages/kvittering/kvitteringMesssages';
+import { velkommenPageMessages } from '../pages/velkommen/velkommenPageMessages';
+import { omOmsorgenForBarnMessages } from '../søknad/steps/om-omsorgen-for-barn/omOmsorgenForBarnMessages';
+import { oppsummeringMessages } from '../søknad/steps/oppsummering/oppsummeringMessages';
+import { tidspunktForAleneomsorgMessages } from '../søknad/steps/tidspunkt-for-aleneomsorg/tidspunktForAleneomsorgMessages';
+import { validateApiDataMessages } from '../utils/søknadsdataToApiData/validateApiData';
 
-export const appMessages: MessageFileFormat = {
-    nb: {
-        'application.title': 'Søknad om ekstra omsorgsdager ved aleneomsorg',
-        'page.form.ubesvarteSpørsmålInfo': 'For å komme videre, må du svare på alle spørsmålene ovenfor',
+const nb = {
+    ...velkommenPageMessages.nb,
+    ...omOmsorgenForBarnMessages.nb,
+    ...annetBarnMessages.nb,
+    ...tidspunktForAleneomsorgMessages.nb,
+    ...oppsummeringMessages.nb,
+    ...kvitteringMessages.nb,
+    ...validateApiDataMessages.nb,
+    'application.title': 'Søknad om ekstra omsorgsdager ved aleneomsorg',
+    'page.form.ubesvarteSpørsmålInfo': 'For å komme videre, må du svare på alle spørsmålene ovenfor',
 
-        'step.omOmsorgenForBarn.stepTitle': 'Om aleneomsorg for barn',
-        'step.tidspunktForAleneomsorg.stepTitle': 'Tidspunkt for aleneomsorg',
-        'step.oppsummering.stepTitle': 'Oppsummering',
+    'step.omOmsorgenForBarn.stepTitle': 'Om aleneomsorg for barn',
+    'step.tidspunktForAleneomsorg.stepTitle': 'Tidspunkt for aleneomsorg',
+    'step.oppsummering.stepTitle': 'Oppsummering',
 
-        'steg.footer.avbryt': 'Avbryt og slett søknad',
-        'steg.footer.fortsettSenere': 'Avslutt og fortsett senere',
+    'steg.footer.avbryt': 'Avbryt og slett søknad',
+    'steg.footer.fortsettSenere': 'Avslutt og fortsett senere',
 
-        'initialLoadError.pageTitle': 'Det oppstod en feil',
-        'initialLoadError.text.1': 'Det oppstod en feil under oppstarten av søknaden. Vennligst prøv igjen senere.',
+    'initialLoadError.pageTitle': 'Det oppstod en feil',
+    'initialLoadError.text.1': 'Det oppstod en feil under oppstarten av søknaden. Vennligst prøv igjen senere.',
 
-        'resetMellomlagring.text.1': 'Dersom feilen vedvarer, kan du prøve å starte på nytt med et tom skjema.',
-        'resetMellomlagring.startPåNytt': 'Start på nytt',
+    'resetMellomlagring.text.1': 'Dersom feilen vedvarer, kan du prøve å starte på nytt med et tom skjema.',
+    'resetMellomlagring.startPåNytt': 'Start på nytt',
 
-        'validation.harForståttRettigheterOgPlikter.notChecked':
-            'Du må bekrefte at du har lest og forstått dine plikter.',
-    },
+    'validation.harForståttRettigheterOgPlikter.notChecked': 'Du må bekrefte at du har lest og forstått dine plikter.',
 };
+
+const nn: Record<keyof typeof nb, string> = {
+    ...nb,
+};
+
+export const appMessages = { nb, nn };
