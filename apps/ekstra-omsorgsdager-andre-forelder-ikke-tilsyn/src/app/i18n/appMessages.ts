3424d3f87e4475ea4bfe7cb1d5950b85d88cad11
Frode Hansen <frode.hansen@bekk.no>
2024-04-24 15:01:40 +0200

    Konvertere messages filer

--- apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/src/app/i18n/appMessages.ts
+++ apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/src/app/i18n/appMessages.ts
@@ -1,26 +1,41 @@
-import { MessageFileFormat } from '@navikt/sif-common-core-ds/src/types/MessageFileFormat';
+import { barnMessages } from '../pre-common/forms/barn/barnMessages';
+import { kvitteringMessages } from '../pages/kvittering/kvitteringMesssages';
+import { personalOpplysningerMessages } from '../pages/velkommen/personalopplysninger/personalopplysninger.messages';
+import { velkommenPageMessages } from '../pages/velkommen/velkommenPageMessages';
+import { annenForelderenSituasjonMessages } from '../søknad/steps/annen-forelderens-situasjon/annenForelderenSituasjonMessages';
+import { omAnnenForelderMessages } from '../søknad/steps/om-annen-forelder/omAnnenForelderMessages';
+import { omBarnaMessages } from '../søknad/steps/om-barna/omBarnaMessages';
+import { oppsummeringMessages } from '../søknad/steps/oppsummering/oppsummeringMessages';
+import { validateApiDataMessages } from '../utils/søknadsdataToApiData/validateApiData';
 
-export const appMessages: MessageFileFormat = {
-    nb: {
-        'application.title': 'Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
+const nb = {
+    ...personalOpplysningerMessages.nb,
+    ...velkommenPageMessages.nb,
+    ...omAnnenForelderMessages.nb,
+    ...annenForelderenSituasjonMessages.nb,
+    ...omBarnaMessages.nb,
+    ...barnMessages.nb,
+    ...oppsummeringMessages.nb,
+    ...kvitteringMessages.nb,
+    ...validateApiDataMessages.nb,
 
-        'page.form.ubesvarteSpørsmålInfo': 'For å komme videre, må du svare på alle spørsmålene ovenfor',
+    'application.title': 'Søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn',
 
-        'step.omAnnenForelder.stepTitle': 'Om den andre forelderen',
+    'step.omAnnenForelder.stepTitle': 'Om den andre forelderen',
+    'step.annenForelderSituasjon.stepTitle': 'Den andre forelderens situasjon',
+    'step.omBarna.stepTitle': 'Om barn',
+    'step.oppsummering.stepTitle': 'Oppsummering',
 
-        'step.annenForelderSituasjon.stepTitle': 'Den andre forelderens situasjon',
+    'initialLoadError.pageTitle': 'Det oppstod en feil',
+    'initialLoadError.text.1': 'Det oppstod en feil under oppstarten av søknaden. Vennligst prøv igjen senere.',
 
-        'step.omBarna.stepTitle': 'Om barn',
+    'resetMellomlagring.text.1': 'Dersom feilen vedvarer, kan du prøve å starte på nytt med et tom skjema.',
+    'resetMellomlagring.startPåNytt': 'Start på nytt',
+};
 
-        'step.oppsummering.stepTitle': 'Oppsummering',
+const nn: Record<keyof typeof nb, string> = { ...nb };
 
-        'steg.footer.avbryt': 'Avbryt og slett søknad',
-        'steg.footer.fortsettSenere': 'Avslutt og fortsett senere',
-
-        'initialLoadError.pageTitle': 'Det oppstod en feil',
-        'initialLoadError.text.1': 'Det oppstod en feil under oppstarten av søknaden. Vennligst prøv igjen senere.',
-
-        'resetMellomlagring.text.1': 'Dersom feilen vedvarer, kan du prøve å starte på nytt med et tom skjema.',
-        'resetMellomlagring.startPåNytt': 'Start på nytt',
-    },
+export const appMessages = {
+    nb,
+    nn,
 };
--- apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/src/app/pages/velkommen/velkommenPageMessages.ts
+++ apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/src/app/pages/velkommen/velkommenPageMessages.ts
@@ -1,30 +1,29 @@
-import { MessageFileFormat } from '@navikt/sif-common-core-ds/src/types/MessageFileFormat';
+const nb = {
+    'page.velkommen.sidetittel': 'Søknad om ekstra omsorgsdager',
+    'page.velkommen.guide.tittel': 'Hei {navn}',
+    'page.velkommen.guide.ingress':
+        'Velkommen til søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn.',
+    'page.velkommen.guide.tekst.1':
+        'Denne søknaden er for deg som skal søke om ekstra omsorgsdager fordi den andre forelderen ikke kan ha tilsyn med barn i en periode som varer i minst 6 måneder. Dette er i situasjoner hvor den andre forelderen for eksempel',
+    'page.velkommen.guide.tekst.1.1': 'er fysisk eller psykisk syk',
+    'page.velkommen.guide.tekst.1.2': 'er innlagt i helseinstitusjon',
+    'page.velkommen.guide.tekst.1.3': 'har en funksjonshemning',
+    'page.velkommen.guide.tekst.1.4': 'er i fengsel',
+    'page.velkommen.guide.tekst.1.5': 'utøver verneplikt',
 
-export const velkommenPageMessages: MessageFileFormat = {
-    nb: {
-        'page.velkommen.sidetittel': 'Søknad om ekstra omsorgsdager',
-        'page.velkommen.guide.tittel': 'Hei {navn}',
-        'page.velkommen.guide.ingress':
-            'Velkommen til søknad om ekstra omsorgsdager når den andre forelderen ikke kan ha tilsyn med barn.',
-        'page.velkommen.guide.tekst.1':
-            'Denne søknaden er for deg som skal søke om ekstra omsorgsdager fordi den andre forelderen ikke kan ha tilsyn med barn i en periode som varer i minst 6 måneder. Dette er i situasjoner hvor den andre forelderen for eksempel',
-        'page.velkommen.guide.tekst.1.1': 'er fysisk eller psykisk syk',
-        'page.velkommen.guide.tekst.1.2': 'er innlagt i helseinstitusjon',
-        'page.velkommen.guide.tekst.1.3': 'har en funksjonshemning',
-        'page.velkommen.guide.tekst.1.4': 'er i fengsel',
-        'page.velkommen.guide.tekst.1.5': 'utøver verneplikt',
+    'page.velkommen.guide.tekst.2':
+        'Hvis du får innvilget søknaden får du doblet antall omsorgsdager. For å ha rett til å bruke omsorgsdager må du være yrkesaktiv. Det betyr at du må være arbeidstaker, selvstendig næringsdrivende, frilanser eller en kombinasjon av disse.',
 
-        'page.velkommen.guide.tekst.2':
-            'Hvis du får innvilget søknaden får du doblet antall omsorgsdager. For å ha rett til å bruke omsorgsdager må du være yrkesaktiv. Det betyr at du må være arbeidstaker, selvstendig næringsdrivende, frilanser eller en kombinasjon av disse.',
-
-        'page.velkommen.omSøknaden.tittel': 'Om søknaden',
-        'page.velkommen.omSøknaden.1': 'Du får veiledning underveis i søknaden om hva du skal fylle ut, og hvordan.',
-        'page.velkommen.omSøknaden.2':
-            'Vi tar vare på svarene dine i 72 timer. Hvis du innenfor den tiden for eksempel vil ta en pause eller blir automatisk logget ut, fortsetter du der du var når du kommer tilbake.',
-        'page.velkommen.omSøknaden.3':
-            'Du må svare på alle spørsmålene for å kunne gå videre. Hvis du mangler etterspurt dokumentasjon, kan du ettersende det så snart du kan.',
-        'page.velkommen.omSøknaden.4': 'Om hvordan vi innhenter opplysninger om deg',
-        'page.velkommen.harForståttRettigheterOgPlikter.notChecked':
-            'Du må velge at du har forstått ditt ansvar som søker',
-    },
+    'page.velkommen.omSøknaden.tittel': 'Om søknaden',
+    'page.velkommen.omSøknaden.1': 'Du får veiledning underveis i søknaden om hva du skal fylle ut, og hvordan.',
+    'page.velkommen.omSøknaden.2':
+        'Vi tar vare på svarene dine i 72 timer. Hvis du innenfor den tiden for eksempel vil ta en pause eller blir automatisk logget ut, fortsetter du der du var når du kommer tilbake.',
+    'page.velkommen.omSøknaden.3':
+        'Du må svare på alle spørsmålene for å kunne gå videre. Hvis du mangler etterspurt dokumentasjon, kan du ettersende det så snart du kan.',
+    'page.velkommen.omSøknaden.4': 'Om hvordan vi innhenter opplysninger om deg',
+    'page.velkommen.harForståttRettigheterOgPlikter.notChecked': 'Du må velge at du har forstått ditt ansvar som søker',
 };
+
+const nn: Record<keyof typeof nb, string> = { ...nb };
+
+export const velkommenPageMessages = { nb, nn };
