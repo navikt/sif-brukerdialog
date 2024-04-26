475dc4cf7d6af7a75aa9ac5edd53bac872ca755d
frodehansen2 <frode.hansen@bekk.no>
2024-04-26 08:37:10 +0200

    Ikke sende søknader hvis bruker har sak (#1793)
    * Ikke returnerer søknader når bruker har sak
    
    * Samle getBrukerprofil serverside
    
    * Fikse mock-data i test
    
    * Changeset

---
+++ .changeset/six-hotels-happen.md
@@ -0,0 +1,5 @@
+---
+"@navikt/dine-pleiepenger": minor
+---
+
+Endre til å hente ut brukerprofil på serversiden, og ikke returnere innsendinger til bruker dersom det finnes sak.
--- apps/dine-pleiepenger/src/types/InnsynData.ts
+++ apps/dine-pleiepenger/src/types/InnsynData.ts
@@ -1,6 +1,7 @@
 import { ApplicationState } from '@navikt/appstatus-react-ds/src/hooks/useGetApplicationStatus';
 import { PleietrengendeMedSak } from '../server/api-models/PleietrengendeMedSakSchema';
 import { Søker } from '../server/api-models/SøkerSchema';
+import { Brukerprofil } from './Brukerprofil';
 import { Mellomlagringer } from './Mellomlagring';
 import { InnsendtSøknad } from './Søknad';
 
@@ -8,6 +9,7 @@ export interface Innsynsdata {
     søker: Søker;
     appStatus?: ApplicationState;
     innsendteSøknader: InnsendtSøknad[];
+    brukerprofil: Brukerprofil;
     mellomlagring: Mellomlagringer;
     saker: PleietrengendeMedSak[];
     saksbehandlingstidUker?: number;
