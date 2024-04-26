13a5ca021ef4dfdb475153633f5dd0bfcf967190
Frode Hansen <frode.hansen@bekk.no>
2024-04-26 08:49:36 +0200

    VelkommenPage

--- apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/src/app/pages/velkommen/VelkommenGuide.tsx
+++ apps/ekstra-omsorgsdager-andre-forelder-ikke-tilsyn/src/app/pages/velkommen/VelkommenGuide.tsx
@@ -1,7 +1,7 @@
+import { BodyLong, GuidePanel, Heading } from '@navikt/ds-react';
 import React from 'react';
-import { GuidePanel, Heading, Ingress } from '@navikt/ds-react';
-import { FormattedMessage } from 'react-intl';
 import InfoList from '@navikt/sif-common-core-ds/src/components/lists/info-list/InfoList';
+import { AppText } from '../../i18n';
 
 interface Props {
     navn: string;
@@ -10,35 +10,35 @@ interface Props {
 const VelkommenGuide: React.FunctionComponent<Props> = ({ navn }) => (
     <GuidePanel>
         <Heading level="1" size="large" spacing={true}>
-            <FormattedMessage id="page.velkommen.guide.tittel" values={{ navn }} />
+            <AppText id="page.velkommen.guide.tittel" values={{ navn }} />
         </Heading>
 
-        <Ingress>
-            <FormattedMessage id="page.velkommen.guide.ingress" />
-        </Ingress>
+        <BodyLong size="large">
+            <AppText id="page.velkommen.guide.ingress" />
+        </BodyLong>
 
         <p>
-            <FormattedMessage id="page.velkommen.guide.tekst.1" />
+            <AppText id="page.velkommen.guide.tekst.1" />
         </p>
         <InfoList>
             <li>
-                <FormattedMessage id="page.velkommen.guide.tekst.1.1" />
+                <AppText id="page.velkommen.guide.tekst.1.1" />
             </li>
             <li>
-                <FormattedMessage id="page.velkommen.guide.tekst.1.2" />
+                <AppText id="page.velkommen.guide.tekst.1.2" />
             </li>
             <li>
-                <FormattedMessage id="page.velkommen.guide.tekst.1.3" />
+                <AppText id="page.velkommen.guide.tekst.1.3" />
             </li>
             <li>
-                <FormattedMessage id="page.velkommen.guide.tekst.1.4" />
+                <AppText id="page.velkommen.guide.tekst.1.4" />
             </li>
             <li>
-                <FormattedMessage id="page.velkommen.guide.tekst.1.5" />
+                <AppText id="page.velkommen.guide.tekst.1.5" />
             </li>
         </InfoList>
         <p>
-            <FormattedMessage id="page.velkommen.guide.tekst.2" />
+            <AppText id="page.velkommen.guide.tekst.2" />
         </p>
     </GuidePanel>
 );
