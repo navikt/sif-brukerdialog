5572d011c7977c45a218ff9c8ea7a1f0f17dd6ec
Frode Hansen <frode.hansen@bekk.no>
2024-04-26 19:30:16 +0200

    Arbeidssituasjon

--- apps/omsorgspengerutbetaling-soknad/src/app/søknad/steps/arbeidssituasjon/arbeidssituasjonMessages.ts
+++ apps/omsorgspengerutbetaling-soknad/src/app/søknad/steps/arbeidssituasjon/arbeidssituasjonMessages.ts
@@ -1,45 +1,47 @@
-export const arbeidssituasjonMessages = {
-    nb: {
-        'step.arbeidssituasjon.info.1':
-            'Nå trenger vi å vite hva som var arbeidssituasjonen din i dagene du søker utbetaling for.',
-        'step.arbeidssituasjon.advarsel.ingenSituasjonValgt': 'Du må velge minst én av situasjonene over.',
+const nb = {
+    'step.arbeidssituasjon.info.1':
+        'Nå trenger vi å vite hva som var arbeidssituasjonen din i dagene du søker utbetaling for.',
+    'step.arbeidssituasjon.advarsel.ingenSituasjonValgt': 'Du må velge minst én av situasjonene over.',
 
-        'step.arbeidssituasjon.frilanser.hjelpetekst.tittel': 'Hva betyr det å være frilanser?',
-        'step.arbeidssituasjon.frilanser.hjelpetekst':
-            'Du er frilanser når du mottar lønn for enkeltstående oppdrag uten å være fast eller midlertidig ansatt hos den du utfører oppdraget for. Hvis du er usikker på om du er frilanser må du sjekke om oppdragene dine er registrert som frilansoppdrag på',
-        'step.arbeidssituasjon.frilanser.hjelpetekst.skatteetatenLenke': 'skatteetaten sine nettsider.',
+    'step.arbeidssituasjon.frilanser.hjelpetekst.tittel': 'Hva betyr det å være frilanser?',
+    'step.arbeidssituasjon.frilanser.hjelpetekst':
+        'Du er frilanser når du mottar lønn for enkeltstående oppdrag uten å være fast eller midlertidig ansatt hos den du utfører oppdraget for. Hvis du er usikker på om du er frilanser må du sjekke om oppdragene dine er registrert som frilansoppdrag på',
+    'step.arbeidssituasjon.frilanser.hjelpetekst.skatteetatenLenke': 'skatteetaten sine nettsider.',
 
-        'step.arbeidssituasjon.selvstendig.hjelpetekst.tittel': 'Hva betyr det å være selvstendig næringsdrivende?',
-        'step.arbeidssituasjon.selvstendig.hjelpetekst':
-            'Du er selvstendig næringsdrivende når du enten har et enkeltpersonforetak (ENK), et ansvarlig selskap (ANS), eller et ansvarlig selskap med delt ansvar (DA).',
-        'step.arbeidssituasjon.selvstendig.hjelpetekst.snSkatteetatenLenke':
-            'I tillegg kan du lese på skatteetatens side om andre situasjoner hvor du kan regnes som selvstendig næringsdrivende.',
+    'step.arbeidssituasjon.selvstendig.hjelpetekst.tittel': 'Hva betyr det å være selvstendig næringsdrivende?',
+    'step.arbeidssituasjon.selvstendig.hjelpetekst':
+        'Du er selvstendig næringsdrivende når du enten har et enkeltpersonforetak (ENK), et ansvarlig selskap (ANS), eller et ansvarlig selskap med delt ansvar (DA).',
+    'step.arbeidssituasjon.selvstendig.hjelpetekst.snSkatteetatenLenke':
+        'I tillegg kan du lese på skatteetatens side om andre situasjoner hvor du kan regnes som selvstendig næringsdrivende.',
 
-        'frilanser.erFrilanser.spm': 'Var du frilanser i dagene du søker utbetaling for?',
-        'frilanser.nårStartet.spm': 'Når startet du som frilanser?',
-        'frilanser.jobberFortsatt.spm': 'Jobber du fortsatt som frilanser?',
-        'frilanser.nårSluttet.spm': 'Når sluttet du som frilanser?',
+    'frilanser.erFrilanser.spm': 'Var du frilanser i dagene du søker utbetaling for?',
+    'frilanser.nårStartet.spm': 'Når startet du som frilanser?',
+    'frilanser.jobberFortsatt.spm': 'Jobber du fortsatt som frilanser?',
+    'frilanser.nårSluttet.spm': 'Når sluttet du som frilanser?',
 
-        'selvstendig.erDuSelvstendigNæringsdrivende.spm':
-            'Var du selvstendig næringsdrivende i dagene du søker utbetaling for?',
-        'selvstendig.harFlereVirksomheter.spm': 'Har du flere enn én næringsvirksomhet som er aktiv?',
-        'selvstendig.veileder.flereAktiveVirksomheter':
-            'Når du har flere aktive næringsvirksomheter skal du kun legge inn den virksomheten som er eldst av dem. Har du for eksempel en virksomhet du startet i 2012 og en annen som du startet i 2020, skal du kun legge inn virksomheten du startet i 2012.',
-        'selvstendig.infoDialog.infoTittel': 'Næringsvirksomhet som du har lagt inn:',
-        'selvstendig.infoDialog.registrerKnapp': 'Registrer virksomhet',
-        'selvstendig.infoDialog.endreKnapp': 'Endre opplysninger',
-        'selvstendig.infoDialog.fjernKnapp': 'Fjern virksomhet',
-        'selvstendig.infoDialog.tittel.en': 'Opplysninger om virksomheten din',
-        'selvstendig.infoDialog.tittel.flere': 'Opplysninger om den eldste virksomheten din',
+    'selvstendig.erDuSelvstendigNæringsdrivende.spm':
+        'Var du selvstendig næringsdrivende i dagene du søker utbetaling for?',
+    'selvstendig.harFlereVirksomheter.spm': 'Har du flere enn én næringsvirksomhet som er aktiv?',
+    'selvstendig.veileder.flereAktiveVirksomheter':
+        'Når du har flere aktive næringsvirksomheter skal du kun legge inn den virksomheten som er eldst av dem. Har du for eksempel en virksomhet du startet i 2012 og en annen som du startet i 2020, skal du kun legge inn virksomheten du startet i 2012.',
+    'selvstendig.infoDialog.infoTittel': 'Næringsvirksomhet som du har lagt inn:',
+    'selvstendig.infoDialog.registrerKnapp': 'Registrer virksomhet',
+    'selvstendig.infoDialog.endreKnapp': 'Endre opplysninger',
+    'selvstendig.infoDialog.fjernKnapp': 'Fjern virksomhet',
+    'selvstendig.infoDialog.tittel.en': 'Opplysninger om virksomheten din',
+    'selvstendig.infoDialog.tittel.flere': 'Opplysninger om den eldste virksomheten din',
 
-        'næringstype.FISKE': 'Fiske',
-        'næringstype.JORDBRUK_SKOGBRUK': 'Jordbruk',
-        'næringstype.DAGMAMMA': 'Dagmamma i eget hjem',
-        'næringstype.ANNEN': 'Annen virksomhet',
+    'næringstype.FISKE': 'Fiske',
+    'næringstype.JORDBRUK_SKOGBRUK': 'Jordbruk',
+    'næringstype.DAGMAMMA': 'Dagmamma i eget hjem',
+    'næringstype.ANNEN': 'Annen virksomhet',
 
-        'fiskerinfo.LOTT': 'Lott',
-        'fiskerinfo.HYRE': 'Hyre',
-        'fiskerinfo.BLAD_A': 'Blad A',
-        'fiskerinfo.BLAD_B': 'Blad B',
-    },
+    'fiskerinfo.LOTT': 'Lott',
+    'fiskerinfo.HYRE': 'Hyre',
+    'fiskerinfo.BLAD_A': 'Blad A',
+    'fiskerinfo.BLAD_B': 'Blad B',
 };
+
+const nn: Record<keyof typeof nb, string> = { ...nb };
+
+export const arbeidssituasjonMessages = { nb, nn };
