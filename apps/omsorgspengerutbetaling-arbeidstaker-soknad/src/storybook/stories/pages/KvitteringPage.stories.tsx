2b5f1d153e5ad8d58ec23a8e51e21309d6a9a010
Frode Hansen <frode.hansen@bekk.no>
2024-04-30 10:55:10 +0200

    Shas

---
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/storybook/stories/pages/KvitteringPage.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryFn } from '@storybook/react';
+import * as React from 'react';
+import KvitteringPage from '../../../app/pages/kvittering/KvitteringPage';
+import { withIntl } from '../../decorators/withIntl';
+import { withRouterProvider } from '../../decorators/withRouter';
+import { withAmplitudeProvider } from '../../decorators/withAmplitudeProvider';
+
+export default {
+    title: 'Pages/KvitteringPage',
+    component: KvitteringPage,
+    decorators: [withIntl, withRouterProvider, withAmplitudeProvider],
+} as Meta<typeof KvitteringPage>;
+
+const Template: StoryFn<typeof KvitteringPage> = () => <KvitteringPage søker={undefined} onUnmount={() => null} />;
+
+export const Default = Template.bind({});
+
+Default.args = {};
+
+Default.parameters = {};
