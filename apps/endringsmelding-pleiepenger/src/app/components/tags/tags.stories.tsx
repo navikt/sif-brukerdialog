cac43906c8a2d4640a22c21c45e8340a3efea5d5
Frode Hansen <frode.hansen@bekk.no>
2024-05-02 13:48:14 +0200

    ModalWrapper

--- apps/endringsmelding-pleiepenger/src/app/components/tags/tags.stories.tsx
+++ apps/endringsmelding-pleiepenger/src/app/components/tags/tags.stories.tsx
@@ -1,28 +1,28 @@
-import { Meta, StoryFn } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { withIntl } from '../../../storybook/decorators/withIntl';
+import { HStack } from '@navikt/ds-react';
 import EndretTag from './EndretTag';
-import { VStack } from '@navikt/ds-react';
 import NyTag from './NyTag';
 import FerieTag from './FerieTag';
 import KortUkeTag from './KortUkeTag';
 
-export default {
+const meta: Meta = {
     title: 'Components/Tags',
     decorators: [withIntl],
-} as Meta;
+};
 
-const Template: StoryFn = () => (
-    <div style={{ maxWidth: '10rem' }}>
-        <VStack gap="5">
+export default meta;
+
+type Story = StoryObj;
+
+export const Tags: Story = {
+    name: 'Tags',
+    render: () => (
+        <HStack gap="5">
             <EndretTag />
             <NyTag />
             <FerieTag>Ferie</FerieTag>
             <KortUkeTag />
-        </VStack>
-    </div>
-);
-
-export const Default = Template.bind({});
-
-Default.args = {};
-Default.parameters = {};
+        </HStack>
+    ),
+};
