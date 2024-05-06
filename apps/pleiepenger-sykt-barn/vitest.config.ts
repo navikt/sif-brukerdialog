d24ea0e93c60ef50165ea74e7df8c8007cd04f03
Frode Hansen <frode.hansen@bekk.no>
2024-05-06 18:07:23 +0200

    Fikse alias i test

--- apps/pleiepenger-sykt-barn/vitest.config.ts
+++ apps/pleiepenger-sykt-barn/vitest.config.ts
@@ -2,6 +2,7 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vitest/config';
+import * as path from 'path';
 
 export default defineConfig({
     test: {
@@ -9,5 +10,8 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         css: false,
+        alias: {
+            '@i18n': path.resolve(__dirname, './src/app/i18n'),
+        },
     },
 });
