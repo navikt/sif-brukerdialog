086797368cdb54581545402d45f5febecc7e631f
Frode Hansen <frode.hansen@bekk.no>
2024-05-07 10:24:03 +0200

    TidFasteDager intl fix

--- apps/pleiepenger-sykt-barn/src/app/local-sif-common-pleiepenger/components/dager-med-tid/TidFasteDager.tsx
+++ apps/pleiepenger-sykt-barn/src/app/local-sif-common-pleiepenger/components/dager-med-tid/TidFasteDager.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { ISODuration, ISODurationToDuration } from '@navikt/sif-common-utils';
 import { Time } from '../../../types/Time';
-import { AppIntlShape, useAppIntl } from '../../../i18n';
+import { AppIntlShape, AppMessageKeys, useAppIntl } from '../../../i18n';
+
+enum Dager {
+    'mandag' = 'mandag',
+    'tirsdag' = 'tirsdag',
+    'onsdag' = 'onsdag',
+    'torsdag' = 'torsdag',
+    'fredag' = 'fredag',
+}
 
 interface TidFasteDagerType {
-    mandag?: ISODuration;
-    tirsdag?: ISODuration;
-    onsdag?: ISODuration;
-    torsdag?: ISODuration;
-    fredag?: ISODuration;
+    [Dager.mandag]?: ISODuration;
+    [Dager.tirsdag]?: ISODuration;
+    [Dager.onsdag]?: ISODuration;
+    [Dager.torsdag]?: ISODuration;
+    [Dager.fredag]?: ISODuration;
 }
 
 interface Props {
@@ -21,12 +29,27 @@ const formatTime = ({ text }: AppIntlShape, time: Partial<Time>): string => {
     return text('psb.timerOgMinutter', { timer, minutter });
 };
 
+const getDagCapsIntlKey = (dag: Dager): AppMessageKeys => {
+    switch (dag) {
+        case Dager.mandag:
+            return 'mandag.caps';
+        case Dager.tirsdag:
+            return 'tirsdag.caps';
+        case Dager.onsdag:
+            return 'onsdag.caps';
+        case Dager.torsdag:
+            return 'torsdag.caps';
+        case Dager.fredag:
+            return 'fredag.caps';
+    }
+};
+
 const TidFasteDager: React.FunctionComponent<Props> = ({ fasteDager }) => {
     const appIntl = useAppIntl();
     const { text } = appIntl;
 
     if (fasteDager) {
-        const days = Object.keys(fasteDager).filter((day) => (fasteDager as any)[day] !== undefined);
+        const days = Object.keys(fasteDager).filter((day) => (fasteDager as any)[day] !== undefined) as Dager[];
         if (days.length > 0) {
             return (
                 <ul style={{ marginTop: 0 }}>
@@ -34,7 +57,7 @@ const TidFasteDager: React.FunctionComponent<Props> = ({ fasteDager }) => {
                         const time = ISODurationToDuration((fasteDager as any)[day]);
                         return (
                             <li key={idx} style={{ marginBottom: '.25rem' }}>
-                                {`${text(`${day}er.caps` as any)}: ${time ? formatTime(appIntl, time) : 0}`}
+                                {`${text(getDagCapsIntlKey(day))}: ${time ? formatTime(appIntl, time) : 0}`}
                             </li>
                         );
                     })}
