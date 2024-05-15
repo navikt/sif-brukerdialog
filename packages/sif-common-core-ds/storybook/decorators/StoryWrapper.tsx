7943ea74eed236e3b60abd4c8a6a35419fd9ee23
frodehansen2 <frode.hansen@bekk.no>
2024-05-15 10:00:59 +0200

    Erstatte FormattedMessage og intlHelper (#1837)
    * Erstatte FormattedMessage og intlHelper
    
    * Fikse storybook
    
    * Legge til story for messages
    
    * Changeset

--- packages/sif-common-core-ds/storybook/decorators/StoryWrapper.tsx
+++ packages/sif-common-core-ds/storybook/decorators/StoryWrapper.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import { IntlProvider } from 'react-intl';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { commonMessages } from '../../src/i18n/common.messages';
 import '@navikt/ds-css';
 import '@navikt/sif-common-core-ds/src/styles/sif-ds-theme.css';
 
 export interface IntlProviderProps {
     children: React.ReactNode;
-    messages?: Record<string, string>;
 }
 
-const StoryWrapper = ({ children, messages }: IntlProviderProps) => {
+const StoryWrapper = ({ children }: IntlProviderProps) => {
     React.useEffect(() => {
         window.document.body.className = window.document.body.className + ' sif-ds-theme';
     });
 
     return (
         <Router>
-            <IntlProvider messages={messages} locale="nb">
+            <IntlProvider messages={commonMessages.nb} locale="nb">
                 <div id="story-wrapper" style={{ maxWidth: '600px', fontSize: '1rem' }}>
                     <div style={{ fontSize: '1rem' }}>{children}</div>
                 </div>
