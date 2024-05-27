c7ffcd18a4ca52bfd15151242cd41bc31bc727f3
Frode Hansen <frode.hansen@bekk.no>
2024-05-27 11:40:19 +0200

    Oppdatere og legge til tekster

--- apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/søknad/steps/dine-barn/dineBarnMessages.ts
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/søknad/steps/dine-barn/dineBarnMessages.ts
@@ -13,8 +13,11 @@ const nb = {
     'step.dineBarn.annetBarnListAndDialog.listTitle': 'Har du barn som ikke er registrert her?',
     'step.dineBarn.annetBarnListAndDialog.listTitle.ingenRegistrerteBarn': 'Har du andre barn?',
     'step.dineBarn.annetBarnListAndDialog.modalTitle': 'Legg til barn',
+    'step.dineBarn.formLeggTilBarn.aldersGrenseInfo': '(du kan ikke legge til barn som er 19 år eller eldre)',
 
     'step.dineBarn.harDeltBosted.spm': 'Er det avtalt delt fast bosted for noen av barna dine?  ',
+    'validation.harDeltBosted.yesOrNoIsUnanswered':
+        'Du må svare på om det er avtalt delt fast bosted for noen av barna dine.',
     'step.dineBarn.harDeltBosted.info.tittel': 'Hva er delt fast bosted?',
     'step.dineBarn.harDeltBosted.info.tekst':
         'Hvis foreldrene til barnet ikke bor sammen, kan de inngå en avtale om delt fast bosted etter barneloven §36. Barnet bor da fast med begge sine foreldre.',
--- apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/søknad/steps/dine-barn/parts/AndreBarnPart.tsx
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/søknad/steps/dine-barn/parts/AndreBarnPart.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { AnnetBarn } from '@navikt/sif-common-forms-ds/src/forms/annet-barn/types';
-import intlHelper from '@navikt/sif-common-core-ds/src/utils/intlUtils';
 import AnnetBarnListAndDialog from '@navikt/sif-common-forms-ds/src/forms/annet-barn/AnnetBarnListAndDialog';
-import { useIntl } from 'react-intl';
 import { DineBarnFormFields } from '../DineBarnStep';
 import { dateToday } from '@navikt/sif-common-utils';
 import { nYearsAgo } from '../dineBarnStepUtils';
 import { Heading } from '@navikt/ds-react';
-import { AppText } from '../../../../i18n';
+import { AppText, useAppIntl } from '../../../../i18n';
 
 interface Props {
     søkerFnr: string;
@@ -22,7 +20,7 @@ const AndreBarnPart: React.FunctionComponent<Props> = ({
     harRegistrerteBarn,
     onAndreBarnChange,
 }) => {
-    const intl = useIntl();
+    const { text } = useAppIntl();
     const andreBarnFnr = andreBarn.map((barn) => barn.fnr);
     return (
         <div>
@@ -32,17 +30,17 @@ const AndreBarnPart: React.FunctionComponent<Props> = ({
             <AnnetBarnListAndDialog<DineBarnFormFields>
                 name={DineBarnFormFields.andreBarn}
                 labels={{
-                    addLabel: intlHelper(intl, 'step.dineBarn.annetBarnListAndDialog.addLabel'),
+                    addLabel: text('step.dineBarn.annetBarnListAndDialog.addLabel'),
                     listTitle: harRegistrerteBarn
-                        ? intlHelper(intl, 'step.dineBarn.annetBarnListAndDialog.listTitle')
-                        : intlHelper(intl, 'step.dineBarn.annetBarnListAndDialog.listTitle.ingenRegistrerteBarn'),
+                        ? text('step.dineBarn.annetBarnListAndDialog.listTitle')
+                        : text('step.dineBarn.annetBarnListAndDialog.listTitle.ingenRegistrerteBarn'),
 
-                    modalTitle: intlHelper(intl, 'step.dineBarn.annetBarnListAndDialog.modalTitle'),
+                    modalTitle: text('step.dineBarn.annetBarnListAndDialog.modalTitle'),
                 }}
                 maxDate={dateToday}
                 minDate={nYearsAgo(18)}
                 disallowedFødselsnumre={[...[søkerFnr], ...andreBarnFnr]}
-                aldersGrenseText={intlHelper(intl, 'step.dineBarn.formLeggTilBarn.aldersGrenseInfo')}
+                aldersGrenseText={text('step.dineBarn.formLeggTilBarn.aldersGrenseInfo')}
                 visBarnTypeValg={true}
                 onAfterChange={(values) => onAndreBarnChange(values)}
             />
