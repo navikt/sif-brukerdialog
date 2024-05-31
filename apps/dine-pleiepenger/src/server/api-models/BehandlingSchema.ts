182368130a6ca638b21f9c3cc3646fb0a225d4c8
frodehansen2 <frode.hansen@bekk.no>
2024-05-31 08:06:21 +0200

    Håndtere søknader===null i behandling (#1873)
    * Håndtere søknader===null i behandling
    
    * Changeset

--- apps/dine-pleiepenger/src/server/api-models/BehandlingSchema.ts
+++ apps/dine-pleiepenger/src/server/api-models/BehandlingSchema.ts
@@ -8,7 +8,7 @@ export type Behandling = z.infer<typeof BehandlingSchema>;
 
 export const BehandlingSchema = z.object({
     status: z.nativeEnum(Behandlingsstatus),
-    søknader: z.array(SøknadSchema),
+    søknader: z.array(SøknadSchema).nullable(),
     opprettetTidspunkt: z.preprocess(parseMaybeDateStringToDate, z.date()).or(z.undefined()).or(z.null()),
     avsluttetTidspunkt: z.preprocess(parseMaybeDateStringToDate, z.date()).or(z.undefined()).or(z.null()),
     aksjonspunkter: z.array(AksjonspunktSchema),
