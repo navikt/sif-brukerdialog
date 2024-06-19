392f0159bbcfe722fd9ae989c1836f6083586e8c
Frode Hansen <frode.hansen@bekk.no>
2024-06-19 13:41:46 +0200

    Oppdatere introside

--- apps/sif-ettersending/src/app/pages/velkommen-page/VelkommenPage.tsx
+++ apps/sif-ettersending/src/app/pages/velkommen-page/VelkommenPage.tsx
@@ -4,15 +4,17 @@ import Page from '@navikt/sif-common-core-ds/src/components/page/Page';
 import { SamtykkeForm, SoknadHeader } from '@navikt/sif-common-soknad-ds';
 import { useAppIntl } from '../../i18n';
 import { useSoknadContext } from '../../soknad/SoknadContext';
+import { Person } from '../../types/Person';
 import { Søknadstype } from '../../types/Søknadstype';
 import OmSøknaden from './OmSøknaden';
 import VelkommenGuide from './VelkommenGuide';
 
 interface Props {
     søknadstype: Søknadstype;
+    søker: Person;
 }
 
-const VelkommenPage: React.FC<Props> = ({ søknadstype }) => {
+const VelkommenPage: React.FC<Props> = ({ søknadstype, søker }) => {
     const { text } = useAppIntl();
     const { startSoknad } = useSoknadContext();
 
@@ -23,7 +25,7 @@ const VelkommenPage: React.FC<Props> = ({ søknadstype }) => {
             <Page
                 title={text(`application.title.${søknadstype}`)}
                 topContentRenderer={() => <SoknadHeader title={text(`application.title.${søknadstype}`)} level="2" />}>
-                <VelkommenGuide />
+                <VelkommenGuide navn={søker.fornavn} />
 
                 <OmSøknaden />
 
