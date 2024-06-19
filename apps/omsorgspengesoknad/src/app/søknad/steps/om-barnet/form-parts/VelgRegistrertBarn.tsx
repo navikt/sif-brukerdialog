8f214a656670a4e0735c693d27d78df2317bfb6c
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-06-19 11:16:28 +0200

    Hent data for alle barn med en gang, og endre tekst

--- apps/omsorgspengesoknad/src/app/søknad/steps/om-barnet/form-parts/VelgRegistrertBarn.tsx
+++ apps/omsorgspengesoknad/src/app/søknad/steps/om-barnet/form-parts/VelgRegistrertBarn.tsx
@@ -1,33 +1,27 @@
-import { Alert, Heading } from '@navikt/ds-react';
-import React, { useState } from 'react';
+import { Heading } from '@navikt/ds-react';
 import FormBlock from '@navikt/sif-common-core-ds/src/atoms/form-block/FormBlock';
 import { getTypedFormComponents } from '@navikt/sif-common-formik-ds';
 import { getRequiredFieldValidator } from '@navikt/sif-common-formik-ds/src/validation';
+import React from 'react';
+import { useAppIntl } from '../../../../i18n';
 import { RegistrertBarn } from '../../../../types/RegistrertBarn';
 import { OmBarnetFormFields, OmBarnetFormValues } from '../OmBarnetStep';
 import { mapBarnTilRadioProps } from '../omBarnetStepUtils';
-import { useAppIntl } from '../../../../i18n';
-import innvilgetVedtakEndpoint from '../../../../api/endpoints/innvilgetVedtakEndpoint';
-import { HentSisteGyldigeVedtakResponseDto } from '../../../../types/innvilgetVedtakApiData/HentSisteGyldigeVedtakResponseDto';
 
 interface Props {
     registrerteBarn: RegistrertBarn[];
     søknadenGjelderEtAnnetBarn?: boolean;
+    onAnnetBarnSelected: () => void;
 }
 
 const { RadioGroup, Checkbox } = getTypedFormComponents<OmBarnetFormFields, OmBarnetFormValues>();
 
-const VelgRegistrertBarn: React.FunctionComponent<Props> = ({ registrerteBarn, søknadenGjelderEtAnnetBarn }) => {
+const VelgRegistrertBarn: React.FunctionComponent<Props> = ({
+    registrerteBarn,
+    søknadenGjelderEtAnnetBarn,
+    onAnnetBarnSelected,
+}) => {
     const { text } = useAppIntl();
-    // use sate to store the last innvilget vedtak
-    const [sisteInnvilgetVedtak, setSisteInnvilgetVedtak] = useState<HentSisteGyldigeVedtakResponseDto | undefined>(
-        undefined,
-    );
-
-    async function harInnvilgetVedFraFør(aktørId: string) {
-        const sisteInnvilgetVedtak = await innvilgetVedtakEndpoint.send({ pleietrengendeAktørId: aktørId });
-        setSisteInnvilgetVedtak(sisteInnvilgetVedtak.data);
-    }
 
     return (
         <>
@@ -41,28 +35,19 @@ const VelgRegistrertBarn: React.FunctionComponent<Props> = ({ registrerteBarn, s
                     name={OmBarnetFormFields.barnetSøknadenGjelder}
                     radios={registrerteBarn.map((barn) => mapBarnTilRadioProps(barn, søknadenGjelderEtAnnetBarn))}
                     validate={søknadenGjelderEtAnnetBarn ? undefined : getRequiredFieldValidator()}
-                    afterOnChange={(aktørId) => {
-                        harInnvilgetVedFraFør(aktørId);
-                    }}
                 />
             </FormBlock>
             <FormBlock margin="l">
                 <Checkbox
                     label={text('steg.omBarnet.gjelderAnnetBarn')}
                     name={OmBarnetFormFields.søknadenGjelderEtAnnetBarn}
+                    afterOnChange={(isChecked) => {
+                        if (isChecked) {
+                            onAnnetBarnSelected();
+                        }
+                    }}
                 />
             </FormBlock>
-            {sisteInnvilgetVedtak && sisteInnvilgetVedtak.harInnvilgedeBehandlinger && (
-                <FormBlock margin="l">
-                    <Alert variant="warning">
-                        <Heading spacing size="small" level="3">
-                            Du trenger ikke å søke igjen.
-                        </Heading>
-                        Du har allerede et vedtak med saksnummer {sisteInnvilgetVedtak.saksnummer} som gjelder fram til
-                        barnet fyller 18 år.
-                    </Alert>
-                </FormBlock>
-            )}
         </>
     );
 };
