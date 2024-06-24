cd0b90223f42a199bdc5f6918a8de66d400c7017
frodehansen2 <frode.hansen@bekk.no>
2024-06-24 15:14:49 +0200

    Arbeidssituasjon oppsummering

--- apps/pleiepenger-i-livets-sluttfase-soknad/src/app/søknad/steps/oppsummering/arbeidssituasjon-summary/OpptjeningIUtlandetSummaryView.tsx
+++ apps/pleiepenger-i-livets-sluttfase-soknad/src/app/søknad/steps/oppsummering/arbeidssituasjon-summary/OpptjeningIUtlandetSummaryView.tsx
@@ -1,46 +1,52 @@
+import { FormSummary } from '@navikt/ds-react';
 import React from 'react';
-import { SummaryBlock } from '@navikt/sif-common-ui';
 import { ISODateToDate, prettifyDateExtended } from '@navikt/sif-common-utils';
-import { AppText, useAppIntl } from '../../../../i18n';
+import { AppText } from '../../../../i18n';
 import { OpptjeningIUtlandetApi } from '../../../../types/søknadApiData/SøknadApiData';
-
+import './opptjeningIUtlandetSummaryItem.css';
 export interface Props {
     opptjeningUtland: OpptjeningIUtlandetApi[];
 }
 
 const OpptjeningIUtlandetSummaryView: React.FC<Props> = (props) => {
     const { opptjeningUtland } = props;
-    const { text } = useAppIntl();
 
     return (
         <>
-            <SummaryBlock header={text('oppsummering.arbeidssituasjon.optjeningIUtlandet.listetittel')}>
-                {opptjeningUtland.length === 0 && <AppText id="oppsummering.arbeidssituasjon.optjeningIUtlandet.nei" />}
-                {opptjeningUtland.length > 0 && (
-                    <ul>
-                        {opptjeningUtland.map((opptjening, index) => (
-                            <li key={index}>
-                                <div className={'opptjeningIUtlandetSummaryItem'}>
-                                    <span className={'opptjeningIUtlandetSummaryItem__dates'}>
-                                        {prettifyDateExtended(ISODateToDate(opptjening.fraOgMed))} -{' '}
-                                        {prettifyDateExtended(ISODateToDate(opptjening.tilOgMed))}
-                                    </span>
-                                    <span>
-                                        <AppText
-                                            id="opptjeningIUtlandetSummaryItem.info"
-                                            values={{
-                                                landnavn: opptjening.land.landnavn,
-                                                hva: opptjening.opptjeningType.toLowerCase(),
-                                                hvor: opptjening.navn,
-                                            }}
-                                        />
-                                    </span>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </SummaryBlock>
+            <FormSummary.Answer>
+                <FormSummary.Label>
+                    <AppText id="oppsummering.arbeidssituasjon.optjeningIUtlandet.listetittel" />
+                </FormSummary.Label>
+                <FormSummary.Value>
+                    {opptjeningUtland.length === 0 && (
+                        <AppText id="oppsummering.arbeidssituasjon.optjeningIUtlandet.nei" />
+                    )}
+                    {opptjeningUtland.length > 0 && (
+                        <ul>
+                            {opptjeningUtland.map((opptjening, index) => (
+                                <li key={index}>
+                                    <div className={'opptjeningIUtlandetSummaryItem'}>
+                                        <span className={'opptjeningIUtlandetSummaryItem__dates'}>
+                                            {prettifyDateExtended(ISODateToDate(opptjening.fraOgMed))} -{' '}
+                                            {prettifyDateExtended(ISODateToDate(opptjening.tilOgMed))}
+                                        </span>
+                                        <span>
+                                            <AppText
+                                                id="opptjeningIUtlandetSummaryItem.info"
+                                                values={{
+                                                    landnavn: opptjening.land.landnavn,
+                                                    hva: opptjening.opptjeningType.toLowerCase(),
+                                                    hvor: opptjening.navn,
+                                                }}
+                                            />
+                                        </span>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </FormSummary.Value>
+            </FormSummary.Answer>
         </>
     );
 };
