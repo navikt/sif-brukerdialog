64e4f561c91909ed3c827ba26c6fac8b4069552f
frodehansen2 <frode.hansen@bekk.no>
2024-08-14 08:38:49 +0200

    Bruke List komponent fra Aksel i SummaryList

---
+++ .changeset/curly-brooms-beam.md
@@ -0,0 +1,5 @@
+---
+'@navikt/sif-common-ui': patch
+---
+
+Bytte til å bruke List komponent fra Aksel i SummaryList
--- packages/sif-common-ui/src/components/summary-list/SummaryList.tsx
+++ packages/sif-common-ui/src/components/summary-list/SummaryList.tsx
@@ -1,24 +1,17 @@
-import bemUtils from '@navikt/sif-common-core-ds/src/utils/bemUtils';
+import { List } from '@navikt/ds-react';
 import React from 'react';
-import './summaryList.scss';
-import classNames from 'classnames';
 
 interface Props {
     items: any[];
-    bullets?: boolean;
     itemRenderer: (data: any) => React.ReactNode;
 }
 
-const bem = bemUtils('summaryList');
-
-const SummaryList: React.FunctionComponent<Props> = ({ items, itemRenderer, bullets }) => (
-    <ul className={classNames(bem.block, bem.modifierConditional('bullet', bullets))}>
+const SummaryList: React.FunctionComponent<Props> = ({ items, itemRenderer }) => (
+    <List>
         {items.map((item, idx) => (
-            <li key={idx} className={bem.element('item')}>
-                {itemRenderer(item)}
-            </li>
+            <List.Item key={idx}>{itemRenderer(item)}</List.Item>
         ))}
-    </ul>
+    </List>
 );
 
 export default SummaryList;
