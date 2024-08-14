74f5b366a6086f183a6f7e52bb0310e99b30f0cd
frodehansen2 <frode.hansen@bekk.no>
2024-08-14 10:03:44 +0200

    Utvide summarylist til å støtte List

--- packages/sif-common-ui/src/components/summary-list/SummaryList.tsx
+++ packages/sif-common-ui/src/components/summary-list/SummaryList.tsx
@@ -1,24 +1,34 @@
-import bemUtils from '@navikt/sif-common-core-ds/src/utils/bemUtils';
+import { List } from '@navikt/ds-react';
 import React from 'react';
-import './summaryList.scss';
+import bemUtils from '@navikt/sif-common-core-ds/src/utils/bemUtils';
 import classNames from 'classnames';
+import './summaryList.scss';
 
 interface Props {
     items: any[];
     bullets?: boolean;
+    useAkselList?: boolean;
     itemRenderer: (data: any) => React.ReactNode;
 }
 
 const bem = bemUtils('summaryList');
 
-const SummaryList: React.FunctionComponent<Props> = ({ items, itemRenderer, bullets }) => (
-    <ul className={classNames(bem.block, bem.modifierConditional('bullet', bullets))}>
-        {items.map((item, idx) => (
-            <li key={idx} className={bem.element('item')}>
-                {itemRenderer(item)}
-            </li>
-        ))}
-    </ul>
-);
+const SummaryList = ({ items, itemRenderer, useAkselList, bullets }: Props) => {
+    return useAkselList ? (
+        <List>
+            {items.map((item, idx) => (
+                <List.Item key={idx}>{itemRenderer(item)}</List.Item>
+            ))}
+        </List>
+    ) : (
+        <ul className={classNames(bem.block, bem.modifierConditional('bullet', bullets))}>
+            {items.map((item, idx) => (
+                <li key={idx} className={bem.element('item')}>
+                    {itemRenderer(item)}
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 export default SummaryList;
