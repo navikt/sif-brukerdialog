17e45c417fea7e90d45e04e012b7db9f77ec4b4c
frodehansen2 <frode.hansen@bekk.no>
2024-08-28 15:10:47 +0200

    Cleanup

--- apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/søknad/steps/medlemskap/MedlemskapStep.tsx
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/søknad/steps/medlemskap/MedlemskapStep.tsx
@@ -1,6 +1,6 @@
 import { getTypedFormComponents } from '@navikt/sif-common-formik-ds/src/components/getTypedFormComponents';
 import { ValidationError } from '@navikt/sif-common-formik-ds/src/validation/types';
-import { MedlemskapForm, MedlemskapFormFields, MedlemskapFormValues } from '@navikt/sif-common-soknad-ds/src';
+import { MedlemskapForm, MedlemskapFormFields, MedlemskapFormValues } from '@navikt/sif-common-soknad-ds';
 import PersistStepFormValues from '../../../components/persist-step-form-values/PersistStepFormValues';
 import { useOnValidSubmit } from '../../../hooks/useOnValidSubmit';
 import { useStepNavigation } from '../../../hooks/useStepNavigation';
