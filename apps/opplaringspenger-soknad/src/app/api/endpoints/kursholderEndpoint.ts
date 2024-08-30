ed08d748fdbc486059a621c7b007b22789821901
frodehansen2 <frode.hansen@bekk.no>
2024-08-30 10:44:16 +0200

    Mock kursholdere apiRespons

--- apps/opplaringspenger-soknad/src/app/api/endpoints/kursholderEndpoint.ts
+++ apps/opplaringspenger-soknad/src/app/api/endpoints/kursholderEndpoint.ts
@@ -1,13 +1,30 @@
 import { Kursholder } from '../../types/Kursholder';
 import api, { ApiEndpoint } from '../api';
 
+const mockKursholder = {
+    kursholdere: [
+        {
+            id: '1',
+            navn: 'Hurdal syn- og mestringssenter',
+        },
+        {
+            id: '2',
+            navn: 'Solvik syn- og mestringssenter',
+        },
+        {
+            id: '3',
+            navn: 'Evenes syn- og mestringssenter',
+        },
+    ],
+};
+
 export const kursholderEndpoint = {
     fetch: async (): Promise<Kursholder[]> => {
         try {
             const { data } = await api.get<{ kursholdere: Kursholder[] }>(ApiEndpoint.kursholder);
             return Promise.resolve(data.kursholdere);
-        } catch (error) {
-            return Promise.reject(error);
+        } catch {
+            return Promise.resolve(mockKursholder.kursholdere);
         }
     },
 };
