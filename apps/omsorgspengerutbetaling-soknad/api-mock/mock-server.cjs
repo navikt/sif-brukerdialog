0b13d93c57ba9fde5821af80dea742b4692ce215
frodehansen2 <frode.hansen@bekk.no>
2024-09-10 13:40:06 +0200

    Legge til x_correlation_id i allow_headers for mock-server (#2202)

--- apps/omsorgspengerutbetaling-soknad/api-mock/mock-server.cjs
+++ apps/omsorgspengerutbetaling-soknad/api-mock/mock-server.cjs
@@ -24,7 +24,7 @@ server.use(
 server.use((req, res, next) => {
     res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.set('Access-Control-Allow-Methods', ['GET', 'POST', 'DELETE', 'PUT']);
-    res.set('Access-Control-Allow-Headers', ['content-type', 'x-brukerdialog-git-sha']);
+    res.set('Access-Control-Allow-Headers', ['content-type', 'X-Brukerdialog-Git-Sha', 'x_correlation_id']);
     res.set('Access-Control-Allow-Credentials', true);
 
     next();
