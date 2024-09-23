705022f602960506cc831da3ca163cd4ad3d7409
frodehansen2 <frode.hansen@bekk.no>
2024-09-23 09:28:27 +0200

    datainfo

--- apps/sif-demo-app/src/App.tsx
+++ apps/sif-demo-app/src/App.tsx
@@ -1,7 +1,11 @@
-import '@navikt/ds-css';
-import Page from '@navikt/sif-common-core-ds/src/components/page/Page';
-import { useInitialData } from './hooks/useInitialData';
+import { Box, Tabs } from '@navikt/ds-react';
 import LoadingSpinner from '@navikt/sif-common-core-ds/src/atoms/loading-spinner/LoadingSpinner';
+import Page from '@navikt/sif-common-core-ds/src/components/page/Page';
+import SøkerInfo from './components/SøkerInfo';
+import { useInitialData } from './hooks/useInitialData';
+import '@navikt/ds-css';
+import BarnInfo from './components/BarnInfo';
+import ArbeidsgiverInfo from './components/ArbeidsgiverInfo';
 
 const App = () => {
     const { initialData, isLoading } = useInitialData();
@@ -16,10 +20,31 @@ const App = () => {
         );
     }
 
-    const { søker, barn } = initialData;
+    const { søker, barn, arbeidsgivere } = initialData;
     return (
         <Page title="Forside">
-            Hei {søker?.fornavn}. Du har {barn.length} barn
+            <Tabs defaultValue="arbeidsgivere">
+                <Tabs.List>
+                    <Tabs.Tab value="søker" label="Søker" />
+                    <Tabs.Tab value="barn" label="Barn" />
+                    <Tabs.Tab value="arbeidsgivere" label="Arbeidsgivere" />
+                </Tabs.List>
+                <Tabs.Panel value="søker">
+                    <Box paddingBlock="4">
+                        <SøkerInfo søker={søker} />
+                    </Box>
+                </Tabs.Panel>
+                <Tabs.Panel value="barn">
+                    <Box paddingBlock="4">
+                        <BarnInfo barn={barn} />
+                    </Box>
+                </Tabs.Panel>
+                <Tabs.Panel value="arbeidsgivere">
+                    <Box paddingBlock="4">
+                        <ArbeidsgiverInfo arbeidsgivere={arbeidsgivere} />
+                    </Box>
+                </Tabs.Panel>
+            </Tabs>
         </Page>
     );
 };
--- packages/sif-common/src/api/services/arbeidsgivereService.ts
+++ packages/sif-common/src/api/services/arbeidsgivereService.ts
@@ -5,13 +5,13 @@ import { Arbeidsgivere } from '../types';
 import { arbeidsgivereResponseSchema } from '../schemas/arbeidsgivereSchema';
 
 export const fetchArbeidsgivere = async ({ from, to }: { from: Date; to: Date }): Promise<Arbeidsgivere> => {
+    const response = await k9BrukerdialogApiClient.get(`/oppslag/arbeidsgiver`, {
+        params: { fra_og_med: dateToISODate(from), til_og_med: dateToISODate(to) },
+    });
     try {
-        const response = await k9BrukerdialogApiClient.get(`/oppslag/arbeidsgiver`, {
-            params: { fra_og_med: dateToISODate(from), til_og_med: dateToISODate(to) },
-        });
         return arbeidsgivereResponseSchema.parse(response.data);
     } catch (e) {
-        console.error('Failed to fetch arbeidsgivere', e);
+        console.error('ZOD parse error', e);
         return {
             organisasjoner: [],
             privatarbeidsgiver: [],
