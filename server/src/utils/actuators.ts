40404e69db7d1d0b4a58a7bd8a6ad8c4578efe17
frodehansen2 <frode.hansen@bekk.no>
2024-09-23 20:46:00 +0200

    fix typo

--- server/src/utils/actuators.ts
+++ server/src/utils/actuators.ts
@@ -2,13 +2,13 @@ import { Express } from 'express';
 import config from './serverConfig.js';
 
 export function setupActuators(server: Express) {
-    server.get(`${config.app.port}'/internal/health/isAlive`, (_request, response) => {
+    server.get(`${config.app.publicPath}'/internal/health/isAlive`, (_request, response) => {
         return response.send({
             status: 'UP',
         });
     });
 
-    server.get(`${config.app.port}'/internal/health/isReady`, (_request, response) => {
+    server.get(`${config.app.publicPath}'/internal/health/isReady`, (_request, response) => {
         return response.send({
             status: 'UP',
         });
