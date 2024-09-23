19c48ba225cb2efc2849d8ef75d94fb5d4c5ca95
frodehansen2 <frode.hansen@bekk.no>
2024-09-23 10:47:13 +0200

    Justere tekster

--- apps/omsorgspengesoknad/src/app/i18n/all-messages.stories.tsx
+++ apps/omsorgspengesoknad/src/app/i18n/all-messages.stories.tsx
@@ -1,15 +1,15 @@
 import { StoryFn } from '@storybook/react';
 import MessagesPreview from '@navikt/sif-common-core-ds/src/dev-utils/intl/messages-preview/MessagesPreview';
-import { libMessages } from './index';
 import { withIntl } from '../../storybook/decorators/withIntl';
+import { applicationIntlMessages } from '.';
 
 export default {
-    title: 'i18N/Bibliotek tekster',
+    title: 'i18N/Alle tekster',
     decorators: [withIntl],
 };
 
 const Template: StoryFn = () => (
-    <MessagesPreview showExplanation={false} messages={libMessages} showMissingTextSummary={true} />
+    <MessagesPreview showExplanation={false} messages={applicationIntlMessages} showMissingTextSummary={true} />
 );
 export const Default = Template.bind({});
 
--- apps/omsorgspengesoknad/src/app/i18n/component-messages.stories.tsx
+++ apps/omsorgspengesoknad/src/app/i18n/component-messages.stories.tsx
@@ -1,13 +1,12 @@
-import { Meta, StoryFn } from '@storybook/react';
+import { StoryFn } from '@storybook/react';
 import MessagesPreview from '@navikt/sif-common-core-ds/src/dev-utils/intl/messages-preview/MessagesPreview';
-import KvitteringPage from '../pages/kvittering/KvitteringPage';
 import { withIntl } from '../../storybook/decorators/withIntl';
 import { commonMessages } from '@navikt/sif-common-core-ds/src/i18n/common.messages';
 
 export default {
     title: 'i18N/Komponent-tekster',
     decorators: [withIntl],
-} as Meta<typeof KvitteringPage>;
+};
 
 const Template: StoryFn = () => (
     <MessagesPreview showExplanation={false} messages={commonMessages} showMissingTextSummary={true} />
