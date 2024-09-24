f757a9ba6a7cea8a83f68299b4164d797574d7b3
frodehansen2 <frode.hansen@bekk.no>
2024-09-24 13:58:07 +0200

    Parse apiData

--- apps/ungdomsytelse-soknad/src/api/services/sendSøknadService.ts
+++ apps/ungdomsytelse-soknad/src/api/services/sendSøknadService.ts
@@ -1,8 +1,13 @@
 import { k9BrukerdialogApiClient } from '@navikt/sif-common/src/api/apiClient';
 import { SøknadApiData } from '../types';
+import { søknadApiDataSchema } from '../schemas/søknadApiDataSchema';
 
 export const sendSøknadService = {
     sendSøknad: async (data: SøknadApiData): Promise<any> => {
-        return await k9BrukerdialogApiClient.post(`/ungdomsytelse/soknad/innsending`, data);
+        const isValid = await søknadApiDataSchema.parse(data);
+        if (isValid) {
+            return await k9BrukerdialogApiClient.post(`/ungdomsytelse/soknad/innsending`, data);
+        }
+        return Promise.reject('Invalid data');
     },
 };
