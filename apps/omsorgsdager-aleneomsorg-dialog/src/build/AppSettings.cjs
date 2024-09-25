862a9eefd57971f80a35704d50a3482ba154ff8e
frodehansen2 <frode.hansen@bekk.no>
2024-09-25 12:04:31 +0200

    Justere env

--- apps/omsorgsdager-aleneomsorg-dialog/src/build/AppSettings.cjs
+++ apps/omsorgsdager-aleneomsorg-dialog/src/build/AppSettings.cjs
@@ -1,18 +1,23 @@
 require('dotenv').config();
 
+const getPublicEnvVariables = () => {
+    const publicEnv = {};
+    for (const [key, value] of Object.entries(process.env)) {
+        if (key.startsWith('SIF_PUBLIC_')) {
+            publicEnv[key] = value || '';
+        }
+    }
+    return publicEnv;
+};
+
 const getAppSettings = () => ({
     APP_VERSION: `${process.env.APP_VERSION}`,
-    APPSTATUS_DATASET: `${process.env.APPSTATUS_DATASET}`,
-    APPSTATUS_PROJECT_ID: `${process.env.APPSTATUS_PROJECT_ID}`,
-    GITHUB_REF_NAME: `${process.env.GITHUB_REF_NAME}`,
-    IMAGE: `${process.env.IMAGE}`,
-    K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH: `${process.env.K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH}`,
-    LOGIN_URL: `${process.env.LOGIN_URL}`,
-    MINSIDE_URL: `${process.env.MINSIDE_URL}`,
-    NAIS_FRONTEND_TELEMETRY_COLLECTOR_URL: `${process.env.NAIS_FRONTEND_TELEMETRY_COLLECTOR_URL}`,
-    OMS_IKKE_TILSYN_URL: `${process.env.OMS_IKKE_TILSYN_URL}`,
     PUBLIC_PATH: `${process.env.PUBLIC_PATH}`,
-    USE_FARO: `${process.env.USE_FARO}`,
+    NAIS_FRONTEND_TELEMETRY_COLLECTOR_URL: `${process.env.NAIS_FRONTEND_TELEMETRY_COLLECTOR_URL}`,
+    K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH: `${process.env.K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH}`,
+    GITHUB_REF_NAME: `${process.env.GITHUB_REF_NAME}`,
+
+    ...getPublicEnvVariables(),
 });
 
 module.exports = getAppSettings;
