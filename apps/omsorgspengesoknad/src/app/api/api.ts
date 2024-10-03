9032ed0bd11fa8b69ca65301bec1a12df4edf1e0
frodehansen2 <frode.hansen@bekk.no>
2024-10-03 11:03:20 +0200

    fix innvilget_vedtak

--- apps/omsorgspengesoknad/src/app/api/api.ts
+++ apps/omsorgspengesoknad/src/app/api/api.ts
@@ -9,7 +9,7 @@ export enum ApiEndpoint {
     'send_søknad' = 'omsorgspenger-utvidet-rett/innsending',
     'vedlegg' = 'vedlegg',
     'mellomlagring' = 'mellomlagring/OMSORGSPENGER_UTVIDET_RETT',
-    'innvilget_vedtak' = 'omsorgsdager-kronisk-sykt-barn/har-gyldig-vedtak',
+    'innvilget_vedtak' = 'k9sak/omsorgsdager-kronisk-sykt-barn/har-gyldig-vedtak',
 }
 
 const axiosConfigCommon: AxiosRequestConfig = {
