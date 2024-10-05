5e694c38c608de39d8e7fcf8c939c0b09543723b
frodehansen2 <frode.hansen@bekk.no>
2024-10-05 09:17:21 +0200

    Fjerne test for slettet kode

--- packages/sif-common-soknad-ds/src/utils/__tests__/attachmentURLUtils.test.ts
+++ packages/sif-common-soknad-ds/src/utils/__tests__/attachmentURLUtils.test.ts
@@ -1,13 +1,5 @@
-import {
-    getAttachmentId,
-    getAttachmentBackendURL,
-    getAttachmentFrontendURL,
-    attachmentURLUtils,
-} from '../attachmentURLUtils';
+import { getAttachmentId } from '../attachmentURLUtils';
 import { describe, expect, it } from 'vitest';
-
-const ATTACHMENT_BACKEND_URL = 'https://k9-brukerdialog-prosessering';
-const ATTACHMENT_FRONTEND_URL = 'https://k9-ettersending-soknad.intern.dev.nav.no/api';
 
 describe('attachmentURLUtils', () => {
     const vedleggId =
@@ -19,25 +11,5 @@ describe('attachmentURLUtils', () => {
         expect(result).toEqual(
             'eyJraWQiOiIxIiwiYWxnIjoibm9uZSIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MGVhZjM5ZC03OGJjLTQwZGEtYmEyNy1iNzAyNDA1MjQ5M2EifQ',
         );
-    });
-
-    it('getAttachmentURLFrontend - genererer riktig frontend-url for et vedlegg', () => {
-        const result = getAttachmentFrontendURL(vedleggUrlFraResponseHeader, ATTACHMENT_FRONTEND_URL);
-        expect(result).toEqual(`https://k9-ettersending-soknad.intern.dev.nav.no/api/vedlegg/${vedleggId}`);
-    });
-
-    it('getAttachmentURLFrontend - setter inn /api dersom det er gammelt format og api ikke finnes i url', () => {
-        const result = getAttachmentFrontendURL(vedleggUrlFraResponseHeader, ATTACHMENT_FRONTEND_URL);
-        expect(result).toEqual(`https://k9-ettersending-soknad.intern.dev.nav.no/api/vedlegg/${vedleggId}`);
-    });
-
-    it('getAttachmentURLBackend - genererer riktig backend-url for et vedlegg', () => {
-        const result = getAttachmentBackendURL(vedleggUrlFraResponseHeader, ATTACHMENT_BACKEND_URL);
-        expect(result).toEqual(`https://k9-brukerdialog-prosessering/vedlegg/${vedleggId}`);
-    });
-    it('fixInvalidPathInFrontendURL - korrigerer ugyldig frontent url', () => {
-        const invalidFrontendURL = `http://www.nav.no/vedlegg/${vedleggId}`;
-        const result = attachmentURLUtils.fixInvalidPathInFrontendURL(invalidFrontendURL, ATTACHMENT_FRONTEND_URL);
-        expect(result).toEqual(`https://k9-ettersending-soknad.intern.dev.nav.no/api/vedlegg/${vedleggId}`);
     });
 });
