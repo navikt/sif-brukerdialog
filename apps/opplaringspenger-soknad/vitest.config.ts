c78d675fddef336f960f9d6d18a8acdd2138ae61
frodehansen2 <frode.hansen@bekk.no>
2024-10-05 08:51:09 +0200

    exlude node_modules

--- apps/opplaringspenger-soknad/vitest.config.ts
+++ apps/opplaringspenger-soknad/vitest.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        exclude: ['./e2e/**/*', './dist/**/*', '**/*.spec.tsx', '**/*.spec.ts'],
+        exclude: ['./e2e/**/*', 'node_modules', './dist/**/*', '**/*.spec.tsx', '**/*.spec.ts'],
         globals: true,
         environment: 'jsdom',
         css: false,
