4c99c16aa43698505080c1b3fbfb64428e5b248d
frodehansen2 <frode.hansen@bekk.no>
2024-10-07 13:01:11 +0200

    Ret

--- apps/endringsmelding-pleiepenger/e2e/server/decorator.cjs
+++ apps/endringsmelding-pleiepenger/e2e/server/decorator.cjs
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const { JSDOM } = require('jsdom');
+require('dotenv').config();
 
 const extractDecoratorFragments = (html, appSettings) => {
     const { document } = new JSDOM(html).window;
@@ -27,8 +28,6 @@ const decoratorParams = new URLSearchParams({
 
 const getDecorator = (appSettings) =>
     new Promise((resolve, reject) => {
-        console.log('Fetching decorator from', process.env.SIF_PUBLIC_DEKORATOR_URL);
-        console.log('Fetching decorator from', process.env.DEKORATOR_URL);
         fetch(`${process.env.SIF_PUBLIC_DEKORATOR_URL}`, decoratorParams)
             .then((response) => response.text())
             .then((html) => {
