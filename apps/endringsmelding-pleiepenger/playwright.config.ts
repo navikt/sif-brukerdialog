1a51cccba5c322bf93b9fb681cd5a51390125498
frodehansen2 <frode.hansen@bekk.no>
2024-10-07 13:47:16 +0200

    legge alle env inn i pwconfig

--- apps/endringsmelding-pleiepenger/playwright.config.ts
+++ apps/endringsmelding-pleiepenger/playwright.config.ts
@@ -27,8 +27,29 @@ export default defineConfig({
         env: {
             SIF_PUBLIC_FEATURE_VELG_SCENARIO: 'on',
             NOW: '2023-01-01',
+            APP_VERSION: 'dev',
+            ENV: 'dev',
+            GITHUB_REF_NAME: 'some_branch_name',
+            NODE_ENV: 'development',
+            PUBLIC_PATH: '/familie/sykdom-i-familien/soknad/endringsmelding-pleiepenger',
+            SIF_PUBLIC_APPSTATUS_DATASET: 'staging',
+            SIF_PUBLIC_APPSTATUS_PROJECT_ID: 'ryujtq87',
             SIF_PUBLIC_DEKORATOR_URL:
                 'https://dekoratoren.ekstern.dev.nav.no/?simple=true&chatbot=false&urlLookupTable=false&logoutUrl=https://endringsmelding-pleiepenger.intern.dev.nav.no/oauth2/logout',
+            SIF_PUBLIC_DOMAIN_URL: 'https://endringsmelding-pleiepenger.intern.dev.nav.no',
+            SIF_PUBLIC_LOGIN_URL:
+                'http://localhost:8081/auth-mock/cookie?subject=mockSubject&redirect_location=http://localhost:8080',
+            SIF_PUBLIC_INNSYN_URL: 'https://dine-pleiepenger.intern.dev.nav.no/innsyn',
+            SIF_PUBLIC_MINSIDE_URL: 'https://www.nav.no/minside',
+            SIF_PUBLIC_FEATURE_NYNORSK: 'on',
+
+            K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH:
+                '/familie/sykdom-i-familien/soknad/endringsmelding-pleiepenger/api/k9-brukerdialog',
+            K9_BRUKERDIALOG_PROSESSERING_API_SCOPE: 'dev-gcp:dusseldorf:k9-brukerdialog-prosessering',
+            K9_BRUKERDIALOG_PROSESSERING_API_URL: 'http://k9-brukerdialog-prosessering',
+            K9_SAK_INNSYN_FRONTEND_PATH: '/familie/sykdom-i-familien/soknad/endringsmelding/api/k9-sak-innsyn',
+            K9_SAK_INNSYN_API_URL: 'http://k9-sak-innsyn-api',
+            K9_SAK_INNSYN_TOKENX_AUDIENCE: 'dev-gcp:dusseldorf:k9-sak-innsyn-api',
         },
     },
 });
