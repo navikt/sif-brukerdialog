69519253e34eb94950cb57e9f1b3e02848aa03d5
Aleksei Poroshin <aleksei.poroshin@nav.no>
2024-10-09 13:26:11 +0200

    upd STORAGE_VERSION ver

--- apps/sif-ettersending/src/app/soknad/soknadTempStorage.ts
+++ apps/sif-ettersending/src/app/soknad/soknadTempStorage.ts
@@ -10,7 +10,7 @@ import { SoknadTempStorageData } from '../types/SoknadTempStorageData';
 import { Søknadstype } from '../types/Søknadstype';
 import { StepID } from './soknadStepsConfig';
 
-export const STORAGE_VERSION = '2.3';
+export const STORAGE_VERSION = '2.4';
 
 interface UserHashInfo {
     søker: Person;
