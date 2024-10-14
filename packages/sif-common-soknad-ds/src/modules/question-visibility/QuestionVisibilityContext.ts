3088f12f072275707375bfdc979e59cb641f3376
frodehansen2 <frode.hansen@bekk.no>
2024-10-14 12:27:50 +0200

    Cleanup (#2315)
    * Slette ubrukt kode
    
    * Fjerne ref til slettet fil
    
    * Enda en ref som må fjernes

--- packages/sif-common-soknad-ds/src/modules/question-visibility/QuestionVisibilityContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-import { QuestionVisibility } from '@navikt/sif-common-formik-ds';
-
-interface VisibilityContext<FieldName> {
-    visibility: QuestionVisibility<FieldName>;
-}
-
-export const QuestionVisibilityContext = createContext<VisibilityContext<any> | undefined>(undefined);
