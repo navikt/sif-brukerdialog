481754fab7941edae84ceff22191e81f9afd4238
frodehansen2 <frode.hansen@bekk.no>
2024-10-20 09:22:20 +0200

    Oppdatere pw-config med env

--- apps/pleiepenger-i-livets-sluttfase-soknad/playwright.config.ts
+++ apps/pleiepenger-i-livets-sluttfase-soknad/playwright.config.ts
@@ -24,20 +24,23 @@ export default defineConfig({
         url: 'http://localhost:8080',
         reuseExistingServer: true,
         env: {
+            ENV: 'dev',
             APP_VERSION: 'dev',
-            NODE_ENV: 'development',
             PUBLIC_PATH: '/familie/sykdom-i-familien/soknad/pleiepenger-i-livets-sluttfase',
-            NPM_CONFIG_CACHE: '/tmp',
-
-            K9_BRUKERDIALOG_PROSESSERING_API_URL: 'http://localhost:8089',
-            K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH: '/api',
-
-            SIF_PUBLIC_USE_AMPLITUDE: 'false',
+            GITHUB_REF_NAME: 'main',
+            NAIS_FRONTEND_TELEMETRY_COLLECTOR_URL: '',
             SIF_PUBLIC_APPSTATUS_DATASET: 'staging',
             SIF_PUBLIC_APPSTATUS_PROJECT_ID: 'ryujtq87',
-            SIF_PUBLIC_DEKORATOR_URL: 'https://dekoratoren.ekstern.dev.nav.no/?simple=true&chatbot=false',
-            SIF_PUBLIC_LOGIN_URL: 'login',
+            SIF_PUBLIC_USE_AMPLITUDE: 'false',
+            SIF_PUBLIC_DEKORATOR_URL:
+                'https://dekoratoren.ekstern.dev.nav.no/?simple=true&chatbot=false&urlLookupTable=false&',
+            SIF_PUBLIC_LOGIN_URL:
+                'ttp://localhost:8089/auth-mock/cookie?subject=mockSubject&redirect_location=http://localhost:8080',
             SIF_PUBLIC_MINSIDE_URL: 'https://www.intern.dev.nav.no/minside',
+            K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH:
+                '/familie/sykdom-i-familien/soknad/pleiepenger-i-livets-sluttfase/api/k9-brukerdialog',
+            K9_BRUKERDIALOG_PROSESSERING_API_SCOPE: 'dev-gcp:dusseldorf:k9-brukerdialog-prosessering',
+            K9_BRUKERDIALOG_PROSESSERING_API_URL: 'http://k9-brukerdialog-prosessering',
         },
     },
 });
