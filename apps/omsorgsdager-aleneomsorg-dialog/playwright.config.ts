ebd8896b9a69256ae964495b0b01f32630495f16
frodehansen2 <frode.hansen@bekk.no>
2024-10-20 09:18:58 +0200

    test

--- apps/omsorgsdager-aleneomsorg-dialog/playwright.config.ts
+++ apps/omsorgsdager-aleneomsorg-dialog/playwright.config.ts
@@ -24,12 +24,22 @@ export default defineConfig({
         url: 'http://localhost:8080',
         reuseExistingServer: true,
         env: {
+            ENV: 'dev',
+            APP_VERSION: 'dev',
             PUBLIC_PATH: '/familie/sykdom-i-familien/soknad/omsorgsdager-aleneomsorg',
-            K9_BRUKERDIALOG_PROSESSERING_API_URL: 'http://localhost:8089',
-            K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH: 'http://localhost:8089',
-            SIF_PUBLIC_DEKORATOR_URL: 'https://www.nav.no/dekoratoren/?simple=true&chatbot=false',
-            SIF_PUBLIC_APPSTATUS_PROJECT_ID: 'ryujtq87',
+            GITHUB_REF_NAME: 'main',
             SIF_PUBLIC_APPSTATUS_DATASET: 'staging',
+            SIF_PUBLIC_APPSTATUS_PROJECT_ID: 'ryujtq87',
+            SIF_PUBLIC_DEKORATOR_URL:
+                'https://dekoratoren.ekstern.dev.nav.no/?simple=true&chatbot=false&urlLookupTable=false&logoutUrl=https://omsorgsdager-aleneomsorg-dialog.intern.dev.nav.no/oauth2/logout&redirectToApp=true',
+            SIF_PUBLIC_LOGIN_URL:
+                'https://omsorgsdager-aleneomsorg-dialog.intern.dev.nav.no/oauth2/login?redirect=/familie/sykdom-i-familien/soknad/omsorgsdager-aleneomsorg/soknad',
+            SIF_PUBLIC_MINSIDE_URL: 'https://www.intern.dev.nav.no/minside',
+            SIF_PUBLIC_OMS_IKKE_TILSYN_URL: 'https://ekstra-omsorgsdager-andre-forelder-ikke-tilsyn.intern.dev.nav.no/',
+            SIF_PUBLIC_USE_FARO: 'true',
+            K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH: '/api/brukerdialog',
+            K9_BRUKERDIALOG_PROSESSERING_API_SCOPE: 'dev-gcp:dusseldorf:k9-brukerdialog-prosessering',
+            K9_BRUKERDIALOG_PROSESSERING_API_URL: 'http://k9-brukerdialog-prosessering',
         },
     },
 });
