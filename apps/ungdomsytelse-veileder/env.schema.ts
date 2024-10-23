b3e23b2618527297b3fb4e7a37b5d84630f07da4
frodehansen2 <frode.hansen@bekk.no>
2024-10-23 12:13:14 +0200

    Utvide env.schema

--- apps/ungdomsytelse-veileder/env.schema.ts
+++ apps/ungdomsytelse-veileder/env.schema.ts
@@ -1,6 +1,6 @@
-import { commonEnvSchema } from '@navikt/sif-common-env';
+import { commonEnvSchema, k9SakInnsynEnvSchema, ungDeltakelseOpplyserEnvSchema } from '@navikt/sif-common-env';
 import * as z from 'zod';
 
-export const appEnvSchema = commonEnvSchema.extend({});
+export const appEnvSchema = commonEnvSchema.merge(k9SakInnsynEnvSchema).merge(ungDeltakelseOpplyserEnvSchema);
 
 export type AppEnv = z.infer<typeof appEnvSchema>;
