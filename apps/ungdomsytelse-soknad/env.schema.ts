72877aad1b9bc45a6ca75239d828aa433757436f
frodehansen2 <frode.hansen@bekk.no>
2024-10-23 12:20:49 +0200

    env.schema i ung-soknad

--- apps/ungdomsytelse-soknad/env.schema.ts
+++ apps/ungdomsytelse-soknad/env.schema.ts
@@ -1,6 +1,6 @@
-import { commonEnvSchema, k9SakInnsynEnvSchema, ungDeltakelseOpplyserEnvSchema } from '@navikt/sif-common-env';
+import { commonEnvSchema, ungDeltakelseOpplyserEnvSchema } from '@navikt/sif-common-env';
 import * as z from 'zod';
 
-export const appEnvSchema = commonEnvSchema.merge(k9SakInnsynEnvSchema).merge(ungDeltakelseOpplyserEnvSchema);
+export const appEnvSchema = commonEnvSchema.merge(ungDeltakelseOpplyserEnvSchema);
 
 export type AppEnv = z.infer<typeof appEnvSchema>;
