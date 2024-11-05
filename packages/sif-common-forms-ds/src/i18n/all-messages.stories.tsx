ee123211ba14744e08f1302108f4ab2a535930b8
frodehansen2 <frode.hansen@bekk.no>
2024-11-05 13:14:00 +0100

    Legge til story for alle tekster i sif-common-forms

---
+++ packages/sif-common-forms-ds/src/i18n/all-messages.stories.tsx
@@ -0,0 +1,18 @@
+import { StoryFn } from '@storybook/react';
+import MessagesPreview from '@navikt/sif-common-core-ds/src/dev-utils/intl/messages-preview/MessagesPreview';
+import { withIntl } from '../../storybook/decorators/withIntl';
+import { formsMessages } from './forms.messages';
+
+export default {
+    title: 'i18N/Alle tekster',
+    decorators: [withIntl],
+};
+
+const Template: StoryFn = () => (
+    <MessagesPreview showExplanation={false} messages={formsMessages} showMissingTextSummary={true} />
+);
+export const Default = Template.bind({});
+
+Default.args = {};
+
+Default.parameters = {};
