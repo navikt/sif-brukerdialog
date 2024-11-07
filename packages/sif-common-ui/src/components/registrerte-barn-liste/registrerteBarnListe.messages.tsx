76e1160e865fc4e8f94c50246fef80ef29fd0f69
frodehansen2 <frode.hansen@bekk.no>
2024-11-07 14:29:28 +0100

    Korrigere tekster

--- packages/sif-common-ui/src/components/registrerte-barn-liste/registrerteBarnListe.messages.tsx
+++ packages/sif-common-ui/src/components/registrerte-barn-liste/registrerteBarnListe.messages.tsx
@@ -2,9 +2,9 @@ import { FormattedMessage } from 'react-intl';
 
 const nb = {
     'registrerteBarnListe.ingenbarn': 'Vi fant ikke noen barn registrert på deg.',
-    'registrerteBarnListe.helpTextTooltip': 'Hvor kommer listen av barn fra?',
+    'registrerteBarnListe.helpTextTooltip': 'Hvor kommer disse opplysningene fra?',
     'registrerteBarnListe.kilde':
-        'Listen viser barn registrert på deg hos Folkeregisteret. Hvis dette ikke stemmer, kan du kontakte Skatteetaten.',
+        'Listen viser barn som er registrert på deg i Folkeregisteret. Hvis dette ikke stemmer, kan du kontakte Skatteetaten.',
     'registrertBarnListe.barn.født': 'Født {dato}',
 };
 
@@ -12,9 +12,9 @@ type MessageKeys = keyof typeof nb;
 
 const nn: Record<MessageKeys, string> = {
     'registrerteBarnListe.ingenbarn': 'Vi fann ikkje nokon barn registrert på deg.',
-    'registrerteBarnListe.helpTextTooltip': 'Kor kjem lista over barn frå?',
+    'registrerteBarnListe.helpTextTooltip': 'Kvar kjem desse opplysningane frå?',
     'registrerteBarnListe.kilde':
-        'Lista viser barn registrert på deg hos Folkeregisteret. Viss dette ikkje stemmer, kan du kontakte Skatteetaten.',
+        'Lista viser barn som er registrert på deg i Folkeregisteret. Viss dette ikkje stemmer, kan du kontakte Skatteetaten.',
     'registrertBarnListe.barn.født': 'Fødd {dato}',
 };
 
