f1e1791922717be34d75ccad7cf11b485b0a979b
frodehansen2 <frode.hansen@bekk.no>
2024-11-07 14:50:40 +0100

    Cleanup

--- packages/sif-common-api/src/api/services/innsendingService.ts
+++ packages/sif-common-api/src/api/services/innsendingService.ts
@@ -1,10 +1,27 @@
 import { k9BrukerdialogApiClient } from '../apiClient';
 import { InnsendingType } from '../types';
 
+const getBackendRouteForInnsendingType = (innsendingType: InnsendingType) => {
+    switch (innsendingType) {
+        case InnsendingType.omsorgspenger_utbetaling_arbeidstaker:
+            return 'omsorgspenger-utbetaling-arbeidstaker/innsending';
+        case InnsendingType.omsorgspenger_utbetaling_snf:
+            return 'omsorgspenger-utbetaling-snf/innsending';
+        case InnsendingType.opplaringspenger:
+            return 'opplaringspenger/innsending';
+        case InnsendingType.pleiepenger_i_livets_sluttfase:
+            return 'pleiepenger-livets-sluttfase/innsending';
+    }
+};
+
 export function getInnsendingService<ApiData>(innsendingType: InnsendingType) {
+    const backendRoute = getBackendRouteForInnsendingType(innsendingType);
+    if (!backendRoute) {
+        throw new Error(`InnsendingType ${innsendingType} is not supported`);
+    }
     return {
         send: async (apiData: ApiData) => {
-            return await k9BrukerdialogApiClient.post(innsendingType, apiData);
+            return await k9BrukerdialogApiClient.post(backendRoute, apiData);
         },
     };
 }
