1a086c372d49c2b49aad07074739392537af9695
frodehansen2 <frode.hansen@bekk.no>
2024-11-27 09:15:42 +0100

    Fallback på navn

--- apps/ungdomsytelse-veileder/src/api/schemas/deltakerSchema.ts
+++ apps/ungdomsytelse-veileder/src/api/schemas/deltakerSchema.ts
@@ -3,13 +3,16 @@ import { z } from 'zod';
 export const deltakerSchema = z.object({
     id: z.string(),
     deltakerIdent: z.string(),
-    navn: z.object({
-        fornavn: z.string(),
-        etternavn: z.string(),
-        mellomnavn: z
-            .string()
-            .optional()
-            .nullable()
-            .transform((v) => (v === null ? undefined : v)),
-    }),
+    navn: z
+        .object({
+            fornavn: z.string(),
+            etternavn: z.string(),
+            mellomnavn: z
+                .string()
+                .optional()
+                .nullable()
+                .transform((v) => (v === null ? undefined : v)),
+        })
+        .optional()
+        .default({ fornavn: 'ToDoFornavn', etternavn: 'ToDoEtternavn' }),
 });
