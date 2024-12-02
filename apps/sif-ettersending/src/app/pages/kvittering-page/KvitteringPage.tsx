cbb8a69be6332adf4b4db00bb78ceba9bc5be9f9
frodehansen2 <frode.hansen@bekk.no>
2024-12-02 09:55:28 +0100

    Fix layout kvitterinpage - ettersendelse

--- apps/sif-ettersending/src/app/pages/kvittering-page/KvitteringPage.tsx
+++ apps/sif-ettersending/src/app/pages/kvittering-page/KvitteringPage.tsx
@@ -1,22 +1,19 @@
-import { BodyLong, Heading, Link } from '@navikt/ds-react';
+import { BodyLong, Heading, Link, VStack } from '@navikt/ds-react';
 import { useEffect } from 'react';
 import Block from '@navikt/sif-common-core-ds/src/atoms/block/Block';
-import CheckmarkIcon from '@navikt/sif-common-core-ds/src/atoms/checkmark-icon/CheckmarkIcon';
 import Page from '@navikt/sif-common-core-ds/src/components/page/Page';
-import bemUtils from '@navikt/sif-common-core-ds/src/utils/bemUtils';
 import { AppText, useAppIntl } from '../../i18n';
 import getLenker from '../../lenker';
 import { DokumentType } from '../../types/DokumentType';
 import { Søknadstype } from '../../types/Søknadstype';
 import './kvitteringPage.css';
+import CheckmarkIcon from '@navikt/sif-common-core-ds/src/atoms/checkmark-icon/CheckmarkIcon';
 
 interface Props {
     søknadstype: Søknadstype;
     dokumenttype?: DokumentType;
     onUnmount?: () => void;
 }
-
-const bem = bemUtils('kvitteringPage');
 
 const KvitteringPage = ({ søknadstype, dokumenttype, onUnmount }: Props) => {
     const { text } = useAppIntl();
@@ -38,24 +35,20 @@ const KvitteringPage = ({ søknadstype, dokumenttype, onUnmount }: Props) => {
                 erLegeerklæringPSB
                     ? 'page.confirmation.psb.legeerklæring.sidetittel'
                     : 'page.confirmation.generell.sidetittel',
-            )}
-            className={bem.block}>
-            <div className={bem.element('centeredContent')}>
-                <div role="presentation" aria-hidden="true">
-                    <CheckmarkIcon />
-                </div>
-                <Block margin="xl">
-                    <Heading level="1" size="large" data-testid="søknad-mottatt">
-                        <AppText
-                            id={
-                                erLegeerklæringPSB
-                                    ? 'page.confirmation.psb.legeerklæring.tittel'
-                                    : 'page.confirmation.generell.tittel'
-                            }
-                        />
-                    </Heading>
-                </Block>
-            </div>
+            )}>
+            <VStack gap="8" align="center" justify="center" marginBlock="0 8" role="presentation" aria-hidden="true">
+                <CheckmarkIcon />
+                <Heading level="1" size="large" data-testid="søknad-mottatt">
+                    <AppText
+                        id={
+                            erLegeerklæringPSB
+                                ? 'page.confirmation.psb.legeerklæring.tittel'
+                                : 'page.confirmation.generell.tittel'
+                        }
+                    />
+                </Heading>
+            </VStack>
+
             <BodyLong as="div">
                 <Block margin="xl">
                     <ul className="checklist">
