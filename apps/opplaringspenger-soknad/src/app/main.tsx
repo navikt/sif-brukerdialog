22839cce7e6687b7291044a3d386fd48fad3d1bc
frodehansen2 <frode.hansen@bekk.no>
2024-12-09 10:47:46 +0100

    Søknad om opplæringspenger - oppdateringer og klargjøring for prod (#2554)
    Oppdatere workflows

--- apps/opplaringspenger-soknad/src/app/main.tsx
+++ apps/opplaringspenger-soknad/src/app/main.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { getMaybeEnv } from '@navikt/sif-common-env';
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+async function enableMocking() {
+    const ENV = getMaybeEnv('ENV');
+    if (ENV !== 'demo') {
+        return;
+    }
+    const { worker } = await import('../msw/browser');
+    // `worker.start()` returns a Promise that resolves
+    // once the Service Worker is up and ready to intercept requests.
+    return worker.start();
+}
+enableMocking().then(() =>
+    createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+            <App />
+        </StrictMode>,
+    ),
 );
