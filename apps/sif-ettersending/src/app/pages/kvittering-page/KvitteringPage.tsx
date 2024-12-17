753ec15159effd7d1abf7fc19329bd817cf1d153
frodehansen2 <frode.hansen@bekk.no>
2024-12-17 11:37:58 +0100

    Fix

--- apps/sif-ettersending/src/app/pages/kvittering-page/KvitteringPage.tsx
+++ apps/sif-ettersending/src/app/pages/kvittering-page/KvitteringPage.tsx
@@ -1,11 +1,11 @@
 import { Link } from '@navikt/ds-react';
-import { InfoList } from '@navikt/sif-common-core-ds/src';
 import Page from '@navikt/sif-common-core-ds/src/components/page/Page';
 import { Kvittering } from '@navikt/sif-common-soknad-ds/src';
 import { AppText, useAppIntl } from '../../i18n';
 import getLenker from '../../lenker';
 import { DokumentType } from '../../types/DokumentType';
 import { Søknadstype } from '../../types/Søknadstype';
+import { Infolist } from '@navikt/sif-common-core-ds/src';
 
 interface Props {
     søknadstype: Søknadstype;
@@ -31,7 +31,7 @@ const KvitteringPage = ({ søknadstype, dokumenttype }: Props) => {
                         ? 'page.confirmation.psb.legeerklæring.tittel'
                         : 'page.confirmation.generell.tittel',
                 )}>
-                <Infolist
+                <Infolist>
                     <li>
                         <AppText
                             id={
@@ -48,7 +48,7 @@ const KvitteringPage = ({ søknadstype, dokumenttype }: Props) => {
                             }}
                         />
                     </li>
-                </Infolist
+                </Infolist>
             </Kvittering>
         </Page>
     );
