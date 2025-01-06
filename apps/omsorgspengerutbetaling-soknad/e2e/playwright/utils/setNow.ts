93c8b8912f895c678cea023496c49e39f5ec3c8c
frodehansen2 <frode.hansen@bekk.no>
2025-01-06 10:47:36 +0100

    Korrigere alderssetting av barn i pw-test

--- apps/omsorgspengerutbetaling-soknad/e2e/playwright/utils/setNow.ts
+++ apps/omsorgspengerutbetaling-soknad/e2e/playwright/utils/setNow.ts
@@ -2,7 +2,7 @@ import MockDate from 'mockdate';
 
 import { Page } from '@playwright/test';
 
-const testDate = new Date('2024-08-01');
+export const testDate = new Date('2024-08-01');
 
 export const setNow = async (page: Page, date: Date = testDate) => {
     await page.addInitScript(() => {
