5243e2c79d614a081e645e7dafa6d358939f1937
frodehansen2 <frode.hansen@bekk.no>
2025-01-14 12:18:51 +0100

    Mer cleanup

--- apps/omsorgspengesoknad/env.schema.ts
+++ apps/omsorgspengesoknad/env.schema.ts
@@ -2,7 +2,7 @@ import { commonEnvSchema, k9SakInnsynEnvSchema } from '@navikt/sif-common-env';
 import * as z from 'zod';
 
 export const appEnvSchema = commonEnvSchema.merge(k9SakInnsynEnvSchema).extend({
-    SIF_PUBLIC_FEATURE_NYNORSK: z.enum(['on', 'off']),
+    SIF_PUBLIC_FEATURE_NYNORSK: z.enum(['on', 'off']).optional(),
 });
 
 export type AppEnv = z.infer<typeof appEnvSchema>;
