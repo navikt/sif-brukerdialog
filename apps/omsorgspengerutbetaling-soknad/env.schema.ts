076271562f62324ad61eeae3d5ee6d0c9ad82f6f
frodehansen2 <frode.hansen@bekk.no>
2025-01-14 12:31:37 +0100

    Korrigere appEnv.schema (#2642)
    Fix av env

--- apps/omsorgspengerutbetaling-soknad/env.schema.ts
+++ apps/omsorgspengerutbetaling-soknad/env.schema.ts
@@ -1,10 +1,11 @@
 import { commonEnvSchema } from '@navikt/sif-common-env';
 import * as z from 'zod';
 
-export const appEnvSchema = commonEnvSchema;
+export const appEnvSchema = commonEnvSchema.extend({
+    SIF_PUBLIC_FEATURE_NYNORSK: z.enum(['on', 'off']),
+});
 
 export const appDevEnvSchema = appEnvSchema.extend({
-    SIF_PUBLIC_FEATURE_NYNORSK: z.enum(['on', 'off']),
     MOCK_DATE: z.string().optional(),
     USE_MOCK_DATE: z.string().optional(),
 });
