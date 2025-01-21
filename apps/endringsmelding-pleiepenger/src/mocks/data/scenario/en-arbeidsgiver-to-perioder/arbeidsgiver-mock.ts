8d38dd9d7b6f5a1105e7ea74fc8d78c784c02088
frodehansen2 <frode.hansen@bekk.no>
2025-01-21 09:18:44 +0100

    stash

--- apps/endringsmelding-pleiepenger/src/mocks/data/scenario/en-arbeidsgiver-to-perioder/arbeidsgiver-mock.ts
+++ apps/endringsmelding-pleiepenger/src/mocks/data/scenario/en-arbeidsgiver-to-perioder/arbeidsgiver-mock.ts
@@ -10,13 +10,6 @@ export default {
             organisasjonsnummer: '947064649',
             navn: 'Norsk bedrift AS',
             ansattFom: '2022-12-05',
-            ansattTom: '2022-12-06',
-        },
-        {
-            organisasjonsnummer: '947064649',
-            navn: 'Norsk bedrift AS',
-            ansattFom: '2022-12-08',
-            ansattTom: null,
         },
     ],
     privateArbeidsgivere: null,
