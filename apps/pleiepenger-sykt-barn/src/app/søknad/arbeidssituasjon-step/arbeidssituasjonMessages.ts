f2beee9a48d4d4a756f6e1818c3864aa7b124447
frodehansen2 <frode.hansen@bekk.no>
2025-02-05 12:40:35 +0100

    Korrigere manglende oversetting

--- apps/pleiepenger-sykt-barn/src/app/søknad/arbeidssituasjon-step/arbeidssituasjonMessages.ts
+++ apps/pleiepenger-sykt-barn/src/app/søknad/arbeidssituasjon-step/arbeidssituasjonMessages.ts
@@ -185,68 +185,67 @@ const nn: Record<keyof typeof nb, string> = {
         'Hvis du i perioden du søkjer for er, eller var, ansatt hos en arbeidsgivar som ikkje vises her, må du be arbeidsgivaren om å sende en ny A-melding. Det gjør de enten via eget lønns- og personalsystem, eller via Altinn.',
 
     'steg.arbeidssituasjon.stønadGodtgjørelse.mottarStønadGodtgjørelse.spm':
-        'Mottar du fosterhjemsgodtgjørelse eller omsorgsstønad fra kommunen?',
-    'steg.arbeidssituasjon.stønadGodtgjørelse.mottarStønadGodtgjørelse.spm.description.tittel': 'Hva betyr dette?',
+        'Mottar du fosterheimsgodtgjersle eller omsorgsstønad frå kommunen?',
+    'steg.arbeidssituasjon.stønadGodtgjørelse.mottarStønadGodtgjørelse.spm.description.tittel': 'Kva betyr dette?',
     'steg.arbeidssituasjon.stønadGodtgjørelse.mottarStønadGodtgjørelse.spm.description':
-        'Fosterhjemsgodtgjørelse og omsorgsstønad regnes som et frilansinntekt, og kan påvirke kor mye du får i pleiepenger. ',
+        'Fosterheimsgodtgjersle og omsorgsstønad blir rekna som frilansinntekt og kan påverke kor mykje du får i pleiepengar.',
     'steg.arbeidssituasjon.stønadGodtgjørelse.mottarStønadGodtgjørelseIHelePerioden.spm':
-        'Mottar du denne stønaden eller godtgjørelsen gjennom hele perioden du søkjer om?',
+        'Mottar du denne stønaden eller godtgjersla gjennom heile perioden du søkjer om?',
     'steg.arbeidssituasjon.stønadGodtgjørelse.starterUndeveis.spm':
-        'Starter stønaden eller godtgjørelsen underveis i pleiepengeperioden di?',
+        'Startar stønaden eller godtgjersla undervegs i pleiepengeperioden din?',
     'steg.arbeidssituasjon.stønadGodtgjørelse.starterUndeveis.startdato': 'Startdato:',
     'steg.arbeidssituasjon.stønadGodtgjørelse.slutterUndeveis.spm':
-        'Stopper stønaden eller godtgjørelsen underveis i pleiepengeperioden di?',
+        'Stoppar stønaden eller godtgjersla undervegs i pleiepengeperioden din?',
     'steg.arbeidssituasjon.stønadGodtgjørelse.starterUndeveis.sluttdato': 'Sluttdato:',
+    'steg.arbeidssituasjon.frilanser.tittel': 'Frilans og oppdrag som blir rekna som frilansoppdrag',
+    'steg.arbeidssituasjon.sn.tittel': 'Sjølvstendig næringsdrivande',
 
-    'steg.arbeidssituasjon.frilanser.tittel': 'Frilans og oppdrag som regnes som frilansoppdrag',
-    'steg.arbeidssituasjon.sn.tittel': 'Selvstendig næringsdrivande',
-
-    'steg.arbeidssituasjon.opptjeningUtland.tittel': 'Jobbet i et annet EØS-land',
+    'steg.arbeidssituasjon.opptjeningUtland.tittel': 'Jobba i eit anna EØS-land',
     'steg.arbeidssituasjon.opptjeningUtland.spm':
-        'Har du jobba som arbeidstaker eller frilanser i et annet EØS-land i løpet av de 3 siste månedene før perioden du søkjer om?',
-    'steg.arbeidssituasjon.opptjeningUtland.listAndDialog.addLabel': 'Legg til jobb i et annet EØS-land',
-    'steg.arbeidssituasjon.opptjeningUtland.listAndDialog.listTitle': 'Registrert jobb i et annet EØS-land',
-    'steg.arbeidssituasjon.opptjeningUtland.listAndDialog.modalTitle': 'Jobbet i et annet EØS-land',
+        'Har du jobba som arbeidstakar eller frilanser i eit anna EØS-land i løpet av dei 3 siste månadene før perioden du søkjer om?',
+    'steg.arbeidssituasjon.opptjeningUtland.listAndDialog.addLabel': 'Legg til jobb i eit anna EØS-land',
+    'steg.arbeidssituasjon.opptjeningUtland.listAndDialog.listTitle': 'Registrert jobb i eit anna EØS-land',
+    'steg.arbeidssituasjon.opptjeningUtland.listAndDialog.modalTitle': 'Jobba i eit anna EØS-land',
 
     'steg.arbeidssituasjon.utenlandskNæring.spm':
-        'Har du jobba som selvstendig næringsdrivande i et annet EØS-land i løpet av de 3 siste årene før perioden du søkjer om?',
+        'Har du jobba som sjølvstendig næringsdrivande i eit anna EØS-land i løpet av dei 3 siste åra før perioden du søkjer om?',
 
     'steg.arbeidssituasjon.verneplikt.tittel': 'Verneplikt',
-    'steg.arbeidssituasjon.verneplikt.spm': 'Utøvde du verneplikt på tidspunktet du søkjer pleiepenger fra?',
-    'steg.arbeidssituasjon.verneplikt.info.tittel': 'Hva betyr dette?',
+    'steg.arbeidssituasjon.verneplikt.spm': 'Utøvde du verneplikt på tidspunktet du søkjer pleiepengar frå?',
+    'steg.arbeidssituasjon.verneplikt.info.tittel': 'Kva betyr dette?',
     'steg.arbeidssituasjon.verneplikt.info.tekst':
-        'Du skal svare ja på dette spørsmålet om du har utøvd verneplikt i minst 28 dagar på starttidspunktet for perioden du søkjer for, eller om perioden med verneplikt var ment å vare i minst 28 dagar.',
+        'Du skal svare ja på dette spørsmålet om du har utøvd verneplikt i minst 28 dagar på starttidspunktet for perioden du søkjer for, eller om perioden med verneplikt var meint å vare i minst 28 dagar.',
 
     'arbeidsforhold.ikkeAnsatt.info':
-        'Når du ikkje er ansatt her lenger, må du be denne arbeidsgivaren om å sende en ny A-melding med sluttdato. Dette gjør de enten via eget lønns- og personalsystem, eller via Altinn.',
+        'Når du ikkje er tilsett her lenger, må du be denne arbeidsgivaren om å sende ei ny A-melding med sluttdato. Dette gjer dei anten via eige lønns- og personalsystem, eller via Altinn.',
 
-    'arbeidsforhold.erAnsatt.spm': 'Stemmer det at du er ansatt hos {navn} i perioden du søkjer for?',
-    'arbeidsforhold.sluttetFørSøknadsperiode.spm': 'Sluttet du hos {navn} før {fraDato}?',
+    'arbeidsforhold.erAnsatt.spm': 'Stemmer det at du er tilsett hos {navn} i perioden du søkjer for?',
+    'arbeidsforhold.sluttetFørSøknadsperiode.spm': 'Slutta du hos {navn} før {fraDato}?',
     'arbeidsforhold.arbeiderNormaltTimerPerUke.snitt.spm':
-        'Hvor mange timer {jobber} du vanligvis {hvor}? Oppgi tiden i et snitt per uke:',
+        'Kor mange timar {jobber} du vanlegvis {hvor}? Oppgi tida som eit snitt per veke:',
     'arbeidsforhold.arbeiderNormaltTimerPerUke.snitt.infoStønadGodtgjørelse':
-        'Du skal ikkje inkludere timer for omsorgsstønad/fosterhjemgodtgjørelse.',
+        'Du skal ikkje inkludere timar for omsorgsstønad/fosterheimsgodtgjersle.',
 
-    'arbeidsforhold.ansatt.normalTimer.info.tittel': 'Hva om jeg jobbar turnus eller varierende?',
-    'arbeidsforhold.selvstendig.normalTimer.info.tittel': 'Hva om jeg jobbar turnus eller varierende?',
+    'arbeidsforhold.ansatt.normalTimer.info.tittel': 'Kva om eg jobbar turnus eller varierande?',
+    'arbeidsforhold.selvstendig.normalTimer.info.tittel': 'Kva om eg jobbar turnus eller varierande?',
     'arbeidsforhold.frilanser.normalTimer.frilans.omsorgsstønad':
-        'Du skal ikkje inkludere timer for fosterhjemsgodtgjørelse og omsorgsstønad.',
-    'arbeidsforhold.frilanser.normalTimer.frilans.info.tittel': 'Hvordan regner jeg et snitt?',
+        'Du skal ikkje inkludere timar for fosterheimsgodtgjersle og omsorgsstønad.',
+    'arbeidsforhold.frilanser.normalTimer.frilans.info.tittel': 'Korleis reknar eg ut eit snitt?',
     'arbeidsforhold.frilanser.normalTimer.frilans.FRILANS.info.1':
-        'Du regner ut et snitt ved å legge sammen alle timene du har jobba som frilanser de siste 12 ukene, og deler dette på 12. Da får du et snitt per uke.',
+        'Du reknar ut eit snitt ved å legge saman alle timane du har jobba som frilanser dei siste 12 vekene, og deler dette på 12. Då får du eit snitt per veke.',
     'arbeidsforhold.frilanser.normalTimer.frilans.FRILANS.info.2':
-        'Hvis de siste 12 veker ikkje er representativ for kor mye du jobbar som frilanser må du ta utgangspunkt i en så lang periode som er representativ for deg.',
+        'Viss dei siste 12 vekene ikkje er representative for kor mykje du jobbar som frilanser, må du ta utgangspunkt i ein så lang periode som er representativ for deg.',
     'arbeidsforhold.frilanser.normalTimer.frilans.HONORAR.info.1':
-        'Du regner ut et snitt ved å legge sammen alle timene du har brukt på oppdraget du mottar honorar for de siste 12 ukene, og deler dette på 12. Da får du et snitt per uke.',
+        'Du reknar ut eit snitt ved å legge saman alle timane du har brukt på oppdraget du mottar honorar for dei siste 12 vekene, og deler dette på 12. Då får du eit snitt per veke.',
     'arbeidsforhold.frilanser.normalTimer.frilans.HONORAR.info.2':
-        'Hvis de siste 12 ukene ikkje er representative i di situasjon, må du ta utgangspunkt i en så lang periode som er representativ for deg.',
+        'Viss dei siste 12 vekene ikkje er representative i di situasjon, må du ta utgangspunkt i ein så lang periode som er representativ for deg.',
     'arbeidsforhold.frilanser.normalTimer.frilans.FRILANS_HONORAR.info.1':
-        'Du regner ut et snitt ved å legge sammen antall timer du jobbar som frilanser med antall timer du brukar på det du mottar honorar for. Legg sammen timene for de siste 12 ukene, og så deler du dette på 12. Da får du et snitt per uke.',
+        'Du reknar ut eit snitt ved å legge saman talet på timar du jobbar som frilanser med talet på timar du brukar på det du mottar honorar for. Legg saman timane for dei siste 12 vekene, og så deler du dette på 12. Då får du eit snitt per veke.',
     'arbeidsforhold.frilanser.normalTimer.frilans.FRILANS_HONORAR.info.2':
-        'Hvis de siste 12 ukene ikkje er representative i di situasjon, må du ta utgangspunkt i en så lang periode som er representativ for deg.',
-    'arbeidsforhold.frilanser.normalTimer.frilans.FRILANS_HONORAR.info.3': 'Eksempel på hvordan du regner ut:',
+        'Viss dei siste 12 vekene ikkje er representative i di situasjon, må du ta utgangspunkt i ein så lang periode som er representativ for deg.',
+    'arbeidsforhold.frilanser.normalTimer.frilans.FRILANS_HONORAR.info.3': 'Eksempel på korleis du reknar ut:',
     'arbeidsforhold.frilanser.normalTimer.frilans.FRILANS_HONORAR.info.4':
-        'Du har de siste 12 ukene jobba 40 timer som frilanser, og du har brukt 20 timer på det du mottar honorar for. Da får du til sammen 60 timer, som du deler på 12 = 5. Du oppgir altså at du jobbar 5 timer i snitt per uke.',
+        'Du har dei siste 12 vekene jobba 40 timar som frilanser, og du har brukt 20 timar på det du mottar honorar for. Då får du til saman 60 timar, som du deler på 12 = 5. Du oppgir altså at du jobbar 5 timar i snitt per veke.',
 
     'arbeidsforhold.normalTimer.info.turnus':
         'Når du jobbar turnus, eller har annen varierende arbeidstid, oppgir du et snitt per uke.',
@@ -282,49 +281,48 @@ const nn: Record<keyof typeof nb, string> = {
     'arbeidsforhold.arbeidsforholdIntlValues.somFrilanser': 'som frilanser',
     'arbeidsforhold.arbeidsforholdIntlValues.somSN': 'som selvstendig næringsdrivande',
 
-    'selvstendig.harDuHattInntekt.spm': 'Er du selvstendig næringsdrivande i perioden du søkjer for?',
-    'selvstendig.harDuHattInntekt.hjelpetekst.tittel': 'Hva betyr det å være selvstendig næringsdrivande?',
+    'selvstendig.harDuHattInntekt.spm': 'Er du sjølvstendig næringsdrivande i perioden du søkjer for?',
+    'selvstendig.harDuHattInntekt.hjelpetekst.tittel': 'Kva betyr det å vere sjølvstendig næringsdrivande?',
     'selvstendig.harDuHattInntekt.hjelpetekst':
-        'Du er selvstendig næringsdrivande når du enten har et enkeltpersonforetak (ENK), et ansvarlig selskap (ANS), eller et ansvarlig selskap med delt ansvar (DA).',
+        'Du er sjølvstendig næringsdrivande når du anten har eit enkeltpersonføretak (ENK), eit ansvarleg selskap (ANS), eller eit ansvarleg selskap med delt ansvar (DA).',
     'selvstendig.harDuHattInntekt.hjelpetekst.snSkatteetatenLenke':
-        'I tillegg kan du lese på skatteetatens side om andre situasjoner kor du kan regnes som selvstendig næringsdrivande.',
-    'selvstendig.harFlereVirksomheter.spm': 'Har du flere enn én næringsvirksomhet som er aktiv?',
+        'I tillegg kan du lese på Skatteetaten si side om andre situasjonar der du kan bli rekna som sjølvstendig næringsdrivande.',
+    'selvstendig.harFlereVirksomheter.spm': 'Har du fleire enn éi næringsverksemd som er aktiv?',
     'selvstendig.veileder.flereAktiveVirksomheter':
-        'Når du har flere aktive næringsvirksomheter skal du kun legge inn den virksomheten som er eldst av dem. Har du for eksempel en virksomhet du startet i 2012 og en annen som du startet i 2020, skal du kun legge inn virksomheten du startet i 2012.',
+        'Når du har fleire aktive næringsverksemder, skal du berre legge inn den verksemda som er eldst av dei. Har du for eksempel ei verksemd du starta i 2012 og ei anna som du starta i 2020, skal du berre legge inn verksemda du starta i 2012.',
 
-    'selvstendig.infoDialog.infoTittel': 'Næringsvirksomhet som du har lagt inn:',
-    'selvstendig.infoDialog.registrerKnapp': 'Registrer virksomhet',
+    'selvstendig.infoDialog.infoTittel': 'Næringsverksemd som du har lagt inn:',
+    'selvstendig.infoDialog.registrerKnapp': 'Registrer verksemd',
     'selvstendig.infoDialog.endreKnapp': 'Endre opplysningar',
-    'selvstendig.infoDialog.fjernKnapp': 'Fjern virksomhet',
-    'selvstendig.infoDialog.tittel.en': 'Opplysninger om virksomheten di',
-    'selvstendig.infoDialog.tittel.flere': 'Opplysninger om den eldste virksomheten di',
+    'selvstendig.infoDialog.fjernKnapp': 'Fjern verksemd',
+    'selvstendig.infoDialog.tittel.en': 'Opplysningar om verksemda di',
+    'selvstendig.infoDialog.tittel.flere': 'Opplysningar om den eldste verksemda di',
 
-    'frilanser.harDuHattInntekt.spm': 'Jobber du som frilanser eller mottar du honorar?',
+    'frilanser.harDuHattInntekt.spm': 'Jobbar du som frilanser eller mottar du honorar?',
     'frilanser.harDuHattInntekt.omsorgsstønad':
-        'Hvis du kun mottar fosterhjemsgodtgjørelse eller omsorgsstønad skal du svare nei på dette spørsmålet.',
-    'frilanser.harDuHattInntekt.hvaBetyr.spm': 'Hva betyr dette?',
+        'Viss du berre mottar fosterheimsgodtgjersle eller omsorgsstønad, skal du svare nei på dette spørsmålet.',
+    'frilanser.harDuHattInntekt.hvaBetyr.spm': 'Kva betyr dette?',
     'frilanser.harDuHattInntekt.hvaBetyr.info.1':
-        'Du skal svare ja på dette spørsmålet hvis du jobbar som frilanser og/eller mottar honorar for utført oppdrag. Du kan få kompensert et eventuelt tap av denne inntekten.',
+        'Du skal svare ja på dette spørsmålet viss du jobbar som frilanser og/eller mottar honorar for utførte oppdrag. Du kan få kompensert eit eventuelt tap av denne inntekta.',
     'frilanser.harDuHattInntekt.hvaBetyr.info.2':
-        'Et honorar kan for eksempel være en utbetaling i forbindelse med et styreverv, eller som trener for et idrettslag. Honorar blir også ofte brukt av frie yrker som forfattere, fotografer og kunstnere.',
+        'Eit honorar kan for eksempel vere ei utbetaling i samband med eit styreverv eller som trenar for eit idrettslag. Honorar blir òg ofte brukt av frie yrke som forfattarar, fotografar og kunstnarar.',
 
-    'frilanser.startetFørSisteTreHeleMåneder.spm': 'Startet du som frilanser før {dato}?',
-    'frilanser.startdato.spm': 'Når startet du å jobbe som frilanser?',
+    'frilanser.startetFørSisteTreHeleMåneder.spm': 'Starta du som frilanser før {dato}?',
+    'frilanser.startdato.spm': 'Når starta du å jobbe som frilanser?',
 
-    'frilanser.erFortsattFrilanser.spm': 'Jobber du fortsatt som frilanser?',
+    'frilanser.erFortsattFrilanser.spm': 'Jobbar du framleis som frilanser?',
     'frilanser.ingenFrilans.info':
-        'Hvis det ikkje stemmer at du skal være registrert med frilansoppdrag i AA-registeret må du be de som har gjort registreringen om å oppdatere informasjonen. Du kan likevel fortsette å fylle ut og sende inn søknaden.',
+        'Viss det ikkje stemmer at du skal vere registrert med frilansoppdrag i AA-registeret, må du be dei som har gjort registreringa om å oppdatere informasjonen. Du kan likevel fortsette å fylle ut og sende inn søknaden.',
 
-    'frilanser.sluttdato.spm': 'Når sluttet du å jobbe som frilanser?',
+    'frilanser.sluttdato.spm': 'Når slutta du å jobbe som frilanser?',
     'frilanser.misterHonorar.tittle': 'Mister du honorar fordi du må pleie barnet?',
-    'frilanser.misterHonorar.description.tittel': 'Hva betyr dette?',
+    'frilanser.misterHonorar.description.tittel': 'Kva betyr dette?',
     'frilanser.misterHonorar.description':
-        'Du kan ha rett til å få kompensert tap av honorar hvis du skal pleie et sykt barn.',
+        'Du kan ha rett til å få kompensert tap av honorar viss du skal pleie eit sjukt barn.',
 
-    'frilanser.hjelpetekst.spm': 'Hva betyr det å være frilanser?',
+    'frilanser.hjelpetekst.spm': 'Kva betyr det å vere frilanser?',
     'frilanser.hjelpetekst.1':
-        'Du er frilanser når du mottar lønn for enkeltstående oppdrag uten å være fast eller midlertidig ansatt hos den du utfører oppdraget for. Hvis du er usikker på om du er frilanser må du sjekke om oppdragene dine er registrert som frilansoppdrag på skatteetaten sine nettsider.',
-
+        'Du er frilanser når du mottar lønn for enkeltståande oppdrag utan å vere fast eller mellombels tilsett hos den du utfører oppdraget for. Viss du er usikker på om du er frilanser, må du sjekke om oppdraga dine er registrerte som frilansoppdrag på Skatteetaten sine nettsider.',
     'verneplikt.summary.header': 'Verneplikt',
     'verneplikt.summary.harVærtVernepliktig': 'Utøvde verneplikt på tidspunktet det søkes pleiepenger fra',
     'verneplikt.summary.harIkkeVærtVernepliktig': 'Utøvde ikkje verneplikt på tidspunktet det søkes pleiepenger fra',
