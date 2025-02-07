ed71daeb6ae1f9beb0ee5b9ad18700d22207dc0c
frodehansen2 <frode.hansen@bekk.no>
2025-02-07 16:55:43 +0100

    Featuretoggle - Søke ett år tilbake i tid

--- apps/pleiepenger-i-livets-sluttfase-soknad/src/app/utils/getTilgjengeligSøknadsperiode.ts
+++ apps/pleiepenger-i-livets-sluttfase-soknad/src/app/utils/getTilgjengeligSøknadsperiode.ts
@@ -1,9 +1,12 @@
+import { getMaybeEnv } from '@navikt/sif-common-env';
 import { DateRange } from '@navikt/sif-common-utils';
 import dayjs from 'dayjs';
+import { appEnv } from './appEnv';
 
 export const getTilgjengeligSøknadsperiode = (): DateRange => {
+    const antallMåneder = getMaybeEnv(appEnv.SIF_PUBLIC_FEATURE_SOKE_ETT_AAR_TILBAKE) === 'on' ? 12 : 3;
     return {
-        from: dayjs().startOf('month').subtract(3, 'months').toDate(),
+        from: dayjs().startOf('month').subtract(antallMåneder, 'months').toDate(),
         to: dayjs().add(2, 'weeks').toDate(),
     };
 };
