8eb36f57271f99e22307935d5a57126a6868f51e
frodehansen2 <frode.hansen@bekk.no>
2025-02-10 10:02:17 +0100

    Korrigere env sjekk

--- apps/pleiepenger-i-livets-sluttfase-soknad/src/app/utils/getTilgjengeligSøknadsperiode.ts
+++ apps/pleiepenger-i-livets-sluttfase-soknad/src/app/utils/getTilgjengeligSøknadsperiode.ts
@@ -1,10 +1,9 @@
-import { getMaybeEnv } from '@navikt/sif-common-env';
 import { DateRange } from '@navikt/sif-common-utils';
 import dayjs from 'dayjs';
 import { appEnv } from './appEnv';
 
 export const getTilgjengeligSøknadsperiode = (): DateRange => {
-    const antallMåneder = getMaybeEnv(appEnv.SIF_PUBLIC_FEATURE_SOKE_ETT_AAR_TILBAKE) === 'on' ? 12 : 3;
+    const antallMåneder = appEnv.SIF_PUBLIC_FEATURE_SOKE_ETT_AAR_TILBAKE === 'on' ? 12 : 3;
     return {
         from: dayjs().startOf('month').subtract(antallMåneder, 'months').toDate(),
         to: dayjs().add(2, 'weeks').toDate(),
