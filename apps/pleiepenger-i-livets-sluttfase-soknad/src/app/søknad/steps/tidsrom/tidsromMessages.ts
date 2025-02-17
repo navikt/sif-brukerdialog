00b89dd33557d51e97555332726d8a978e9be625
frodehansen2 <frode.hansen@bekk.no>
2025-02-17 08:23:49 +0100

    Endre tekst

--- apps/pleiepenger-i-livets-sluttfase-soknad/src/app/søknad/steps/tidsrom/tidsromMessages.ts
+++ apps/pleiepenger-i-livets-sluttfase-soknad/src/app/søknad/steps/tidsrom/tidsromMessages.ts
@@ -3,8 +3,10 @@ const nb = {
     'step.tidsrom.stepTitle': 'Dager du må være hjemme fra jobb for å gi pleie',
     'step.tidsrom.stepIndicatorLabel': 'Periode',
     'step.tidsrom.counsellorPanel.avsnitt.1': 'Nå skal du velge de dagene du må være hjemme fra jobb for å gi pleie.',
-    'step.tidsrom.counsellorPanel.avsnitt.2':
+    'step.tidsrom.counsellorPanel.avsnitt.2.3mnd':
         'Du kan søke tre måneder tilbake i tid, og to uker frem i tid. Hvis du etter to uker fortsatt er borte fra jobb for å gi pleie, kan du sende inn en ny søknad.',
+    'step.tidsrom.counsellorPanel.avsnitt.2.3år':
+        'Du kan søke opp til tre år tilbake i tid, og to uker frem i tid. Hvis du etter to uker fortsatt er borte fra jobb for å gi pleie, kan du sende inn en ny søknad.',
     'step.tidsrom.counsellorPanel.avsnitt.3':
         'Husk at det ikke er rett til pleiepenger på dager hvor personen er innlagt på sykehus eller annen institusjon.',
 
