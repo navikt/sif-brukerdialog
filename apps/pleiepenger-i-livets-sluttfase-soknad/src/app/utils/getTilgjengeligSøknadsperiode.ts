bebcb2dc25993a50868b81d07fe9aafcd92a6c61
frodehansen2 <frode.hansen@bekk.no>
2025-02-17 09:19:04 +0100

    Korrigere periode

--- apps/pleiepenger-i-livets-sluttfase-soknad/src/app/utils/getTilgjengeligSøknadsperiode.ts
+++ apps/pleiepenger-i-livets-sluttfase-soknad/src/app/utils/getTilgjengeligSøknadsperiode.ts
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 import { appEnv } from './appEnv';
 
 export const getTilgjengeligSøknadsperiode = (): DateRange => {
-    const antallMåneder = appEnv.SIF_PUBLIC_FEATURE_SOKE_TRE_AAR_TILBAKE === 'on' ? 12 : 3;
+    const antallMåneder = appEnv.SIF_PUBLIC_FEATURE_SOKE_TRE_AAR_TILBAKE === 'on' ? 36 : 3;
     return {
         from: dayjs().startOf('month').subtract(antallMåneder, 'months').toDate(),
         to: dayjs().add(2, 'weeks').toDate(),
