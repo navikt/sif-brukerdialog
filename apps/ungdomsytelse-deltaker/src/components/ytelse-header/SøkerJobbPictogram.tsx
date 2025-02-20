3ac368dad4b9660d9c59077d926d19bbc0e9580d
frodehansen2 <frode.hansen@bekk.no>
2025-02-20 12:56:25 +0100

    Fix svg syntax

--- apps/ungdomsytelse-deltaker/src/components/ytelse-header/SøkerJobbPictogram.tsx
+++ apps/ungdomsytelse-deltaker/src/components/ytelse-header/SøkerJobbPictogram.tsx
@@ -8,36 +8,36 @@ export const SøkerJobbPictogram = ({ style }: { style?: React.CSSProperties }) 
         role="presentation"
         style={{ flexShrink: 0, ...style }}
         aria-hidden>
-        <g clip-path="url(#clip0_1_4078)">
+        <g clipPath="url(#clip0_1_4078)">
             <path
                 d="M76 48C76 32.536 63.464 20 48 20C32.536 20 20 32.536 20 48C20 63.464 32.536 76 48 76"
                 stroke="#CCE2F0"
-                stroke-width="12"
-                stroke-linecap="round"
+                strokeWidth="12"
+                strokeLinecap="round"
             />
             <path
                 d="M1.5 61C1.5 60.7239 1.72386 60.5 2 60.5H54C54.2761 60.5 54.5 60.7239 54.5 61V76C54.5 76.8284 53.8284 77.5 53 77.5H3C2.17157 77.5 1.5 76.8284 1.5 76V61Z"
                 stroke="#23262A"
-                stroke-width="3"
+                strokeWidth="3"
             />
             <path
                 d="M4.5 93V77.5H51.5V93C51.5 93.8284 50.8284 94.5 50 94.5H6C5.17157 94.5 4.5 93.8284 4.5 93Z"
                 stroke="#23262A"
-                stroke-width="3"
+                strokeWidth="3"
             />
             <path
                 d="M20.5 54C20.5 52.6193 21.6193 51.5 23 51.5H33C34.3807 51.5 35.5 52.6193 35.5 54V60.5H20.5V54Z"
                 stroke="#23262A"
-                stroke-width="3"
+                strokeWidth="3"
             />
-            <path d="M28 73L28 81" stroke="#23262A" stroke-width="3" stroke-linecap="round" />
-            <circle cx="86" cy="10" r="8.5" stroke="#23262A" stroke-width="3" />
-            <circle cx="2" cy="2" r="2" transform="matrix(1 0 0 -1 70 8)" stroke="#23262A" stroke-width="3" />
-            <circle cx="58" cy="10" r="8.5" stroke="#23262A" stroke-width="3" />
+            <path d="M28 73L28 81" stroke="#23262A" strokeWidth="3" strokeLinecap="round" />
+            <circle cx="86" cy="10" r="8.5" stroke="#23262A" strokeWidth="3" />
+            <circle cx="2" cy="2" r="2" transform="matrix(1 0 0 -1 70 8)" stroke="#23262A" strokeWidth="3" />
+            <circle cx="58" cy="10" r="8.5" stroke="#23262A" strokeWidth="3" />
             <path
                 d="M65.3643 8C67.2649 6.73631 69.5464 6 71.9998 6C74.4533 6 76.7348 6.73631 78.6354 8"
                 stroke="#23262A"
-                stroke-width="3"
+                strokeWidth="3"
             />
         </g>
         <defs>
