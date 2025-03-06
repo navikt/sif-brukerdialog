f725e1ec5ec787dbf0f55bbdb257ac1c428f7acc
frodehansen2 <frode.hansen@bekk.no>
2025-03-06 16:19:43 +0100

    Oppdatert typer -Kansellert

--- packages/ung-common/src/api/parse-utils/parseOppgaverElement.ts
+++ packages/ung-common/src/api/parse-utils/parseOppgaverElement.ts
@@ -19,6 +19,8 @@ const getOppgaveStatusEnum = (status: string): OppgaveStatus => {
             return OppgaveStatus.LØST;
         case 'ULØST':
             return OppgaveStatus.ULØST;
+        case 'KANSELLERT':
+            return OppgaveStatus.KANSELLERT;
         default:
             throw new Error(`Ukjent oppgavestatus: ${status}`);
     }
