f88855daa13076fd34c4a51c8485abbbe15fbc58
frodehansen2 <frode.hansen@bekk.no>
2025-03-19 09:13:02 +0100

    Håndtere valideringsfeil ved innsending i PSB bedre

--- packages/sif-common-api/src/api/services/validerFritekstService.ts
+++ packages/sif-common-api/src/api/services/validerFritekstService.ts
@@ -1,29 +1,30 @@
 import { k9BrukerdialogApiClient } from '../apiClient';
 import { z } from 'zod';
 
-const violationSchema = z.object({
+export const fritekstViolationSchema = z.object({
     invalidValue: z.string(),
     parameterName: z.string(),
     parameterType: z.string(),
     reason: z.string(),
 });
 
-const valideringsfeilSchema = z.object({
-    violations: z.array(violationSchema),
+export const fritekstValideringsfeilResponseSchema = z.object({
+    violations: z.array(fritekstViolationSchema),
     detail: z.string(),
     status: z.number(),
     title: z.string(),
     type: z.string(),
 });
 
-export type FritekstfeltValideringsfeil = z.infer<typeof valideringsfeilSchema>;
+export type FritekstfeltValideringsfeilResponse = z.infer<typeof fritekstValideringsfeilResponseSchema>;
+export type FritekstfeltValideringsfeil = z.infer<typeof fritekstViolationSchema>;
 
-export const validerFritekst = async (value: string): Promise<FritekstfeltValideringsfeil | undefined> => {
+export const validerFritekst = async (value: string): Promise<FritekstfeltValideringsfeilResponse | undefined> => {
     try {
         await k9BrukerdialogApiClient.post(`/valider/fritekstfelt`, { verdi: value });
     } catch (e) {
         if (e.response?.status === 400) {
-            const parseResult = valideringsfeilSchema.safeParse(e.response.data);
+            const parseResult = fritekstValideringsfeilResponseSchema.safeParse(e.response.data);
             if (parseResult.success) {
                 return parseResult.data;
             }
