077c698fa317b6038f33b4a44699a63272298e77
frodehansen2 <frode.hansen@bekk.no>
2025-04-10 15:13:02 +0200

    me json

--- server-ungdomsytelse-veileder/src/utils/frontendRoute.ts
+++ server-ungdomsytelse-veileder/src/utils/frontendRoute.ts
@@ -2,6 +2,8 @@ import { Express } from 'express';
 import path from 'node:path';
 import fs from 'fs';
 import { getToken, parseAzureUserToken } from '@navikt/oasis';
+import { appEnvSchema } from '../env.schema.js';
+import serverConfig from './serverConfig.js';
 
 export const setupAndServeHtml = async (app: Express) => {
     // When deployed, the built frontend is copied into the public directory. If running BFF locally the index.html will not exist.
@@ -9,28 +11,28 @@ export const setupAndServeHtml = async (app: Express) => {
 
     const html = fs.readFileSync(spaFilePath, 'utf-8');
 
-    // const envs = appEnvSchema.safeParse({
-    //     ENV: `${config.app.env}`,
-    //     APP_VERSION: `${config.app.version}`,
-    //     PUBLIC_PATH: `${config.app.publicPath}`,
-    //     GITHUB_REF_NAME: `${process.env.GITHUB_REF_NAME}`,
-    //     ...config.app.proxyEnvVariables,
-    //     ...config.app.publicEnvVariables,
-    // });
+    const envs = appEnvSchema.safeParse({
+        ENV: `${serverConfig.app.env}`,
+        APP_VERSION: `${serverConfig.app.version}`,
+        PUBLIC_PATH: `${serverConfig.app.publicPath}`,
+        GITHUB_REF_NAME: `${process.env.GITHUB_REF_NAME}`,
+        ...serverConfig.app.proxyEnvVariables,
+        ...serverConfig.app.publicEnvVariables,
+    });
 
-    // if (!envs.success) {
-    //     console.error('Invalid environment variables:', envs.error.format());
-    //     process.exit(1); // Exit the server if validation fails
-    // }
+    if (!envs.success) {
+        console.error('Invalid environment variables:', envs.error.format());
+        process.exit(1); // Exit the server if validation fails
+    }
 
-    // const renderedHtml = html.replaceAll('{{{APP_SETTINGS}}}', JSON.stringify(envs.data));
+    const renderedHtml = html.replaceAll('{{{APP_SETTINGS}}}', JSON.stringify(envs.data));
 
     app.get('/me', (request, response) => {
         const token = getToken(request);
         if (token) {
             const parse = parseAzureUserToken(token);
             if (parse.ok) {
-                response.send({
+                response.json({
                     name: parse.preferred_username,
                     NAVIdent: parse.NAVident,
                 });
@@ -38,7 +40,7 @@ export const setupAndServeHtml = async (app: Express) => {
                 console.error('Failed to parse Azure user token', parse.error);
             }
         }
-        response.send({});
+        response.json({});
     });
 
     app.get(/^\/(?!.*dist).*$/, async (request, response) => {
@@ -55,8 +57,6 @@ export const setupAndServeHtml = async (app: Express) => {
                 console.error('Failed to parse Azure user token', parse.error);
             }
         }
-        const renderedHtml = html.replace('{{{APP_SETTINGS}}}', JSON.stringify(userInfo));
-
         return response.send(renderedHtml);
     });
 };
