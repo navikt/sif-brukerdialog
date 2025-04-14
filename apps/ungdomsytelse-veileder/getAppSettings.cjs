c58262e06eb948aca4eef2b7897f56f8faf26aa3
frodehansen2 <frode.hansen@bekk.no>
2025-04-14 11:52:47 +0200

    Fjerne dev-veileder app

--- apps/ungdomsytelse-veileder/getAppSettings.cjs
+++ apps/ungdomsytelse-veileder/getAppSettings.cjs
@@ -7,6 +7,7 @@ const getAppSettings = () => ({
     APP_VERSION: `${process.env.APP_VERSION}`,
     GITHUB_REF_NAME: `${process.env.GITHUB_REF_NAME}`,
     PUBLIC_PATH: `${process.env.PUBLIC_PATH}`,
+    IS_LOCAL: `${process.env.IS_LOCAL}`,
 
     UNG_DELTAKELSE_OPPLYSER_API_URL: `${process.env.UNG_DELTAKELSE_OPPLYSER_API_URL}`,
     UNG_DELTAKELSE_OPPLYSER_FRONTEND_PATH: `${process.env.UNG_DELTAKELSE_OPPLYSER_FRONTEND_PATH}`,
