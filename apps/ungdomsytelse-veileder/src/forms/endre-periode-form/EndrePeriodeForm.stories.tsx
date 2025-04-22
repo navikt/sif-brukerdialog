0055d42ce45f8b38626781eabd1a12a3e239d3d1
frodehansen2 <frode.hansen@bekk.no>
2025-04-22 13:12:20 +0200

    Fjerne egne forms for start-/sluttdato

--- apps/ungdomsytelse-veileder/src/forms/endre-periode-form/EndrePeriodeForm.stories.tsx
+++ apps/ungdomsytelse-veileder/src/forms/endre-periode-form/EndrePeriodeForm.stories.tsx
@@ -2,18 +2,18 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { parsedMockDeltakelse, parsedMockDeltaker } from '../../../mock/msw/mocks/mockUtils';
 import { withIntl } from '../../../storybook/decorators/withIntl';
 import { withVeilederContext } from '../../../storybook/decorators/withVeilederContext';
-import EndreStartdatoForm from './EndreStartdatoForm';
+import EndrePeriodeForm from './EndrePeriodeForm';
 import { withModalWrapper } from '../../../storybook/decorators/withModalWrapper';
 
-const meta: Meta<typeof EndreStartdatoForm> = {
-    component: EndreStartdatoForm,
-    title: 'Forms/Endre startdato',
+const meta: Meta<typeof EndrePeriodeForm> = {
+    component: EndrePeriodeForm,
+    title: 'Forms/Endre periode form',
     parameters: {},
     decorators: [withIntl, withVeilederContext, (Story) => withModalWrapper(Story, { header: 'Endre startdato' })],
 };
 export default meta;
 
-type Story = StoryObj<typeof EndreStartdatoForm>;
+type Story = StoryObj<typeof EndrePeriodeForm>;
 
 export const Default: Story = {
     args: { deltakelse: parsedMockDeltakelse, deltaker: parsedMockDeltaker },
---
+++ apps/ungdomsytelse-veileder/src/forms/endre-periode-form/EndrePeriodeForm.tsx
@@ -0,0 +1,133 @@
+import { Alert, Button, HStack, VStack } from '@navikt/ds-react';
+import {
+    FormikDatepicker,
+    FormikYesOrNoQuestion,
+    TypedFormikForm,
+    TypedFormikWrapper,
+} from '@navikt/sif-common-formik-ds';
+import { dateToISODate } from '@navikt/sif-common-utils';
+import { Deltakelse, Deltaker, formaterNavn } from '@navikt/ung-common';
+import { GYLDIG_PERIODE } from '../../settings';
+import { max, min } from 'date-fns';
+import { usePeriodeForDeltakelse } from '../../hooks/usePeriodeForDeltakelse';
+import { EndrePeriodeVariant } from '../../types/EndrePeriodeVariant';
+import { getDateValidator, getRequiredFieldValidator } from '@navikt/sif-validation';
+
+export type EndrePeriodeFormValues = {
+    id: string;
+    fnr: string;
+    fom: string;
+    tom: string;
+};
+
+interface Props {
+    variant: EndrePeriodeVariant;
+    deltaker: Deltaker;
+    deltakelse: Deltakelse;
+    onCancel?: () => void;
+    onDeltakelseChanged: (oppdatertDeltakelse: Deltakelse) => void;
+}
+
+const EndrePeriodeForm = ({ variant, deltakelse, deltaker, onCancel, onDeltakelseChanged }: Props) => {
+    const { mutate, isPending, error } = usePeriodeForDeltakelse({
+        variant,
+        deltakelseId: deltakelse.id,
+        deltakerId: deltaker.id,
+    });
+
+    const deltakernavn = formaterNavn(deltaker.navn);
+
+    const startdatoMinMax = {
+        min: GYLDIG_PERIODE.from,
+        max: min([deltakelse.tilOgMed ? deltakelse.tilOgMed : GYLDIG_PERIODE.to, GYLDIG_PERIODE.to]),
+    };
+
+    const sluttdatoMinMax = {
+        min: max([deltakelse.fraOgMed || GYLDIG_PERIODE.from, GYLDIG_PERIODE.from]),
+        max: GYLDIG_PERIODE.to,
+    };
+
+    const handleOnSubmit = async (values: EndrePeriodeFormValues) => {
+        const dato = variant === EndrePeriodeVariant.startdato ? values.fom : values.tom;
+        if (!dato) {
+            return;
+        }
+        mutate(
+            {
+                dato,
+            },
+            {
+                onSuccess: onDeltakelseChanged,
+            },
+        );
+    };
+
+    return (
+        <TypedFormikWrapper<EndrePeriodeFormValues>
+            initialValues={{
+                fom: dateToISODate(deltakelse.fraOgMed),
+                tom: deltakelse.tilOgMed ? dateToISODate(deltakelse.tilOgMed) : undefined,
+            }}
+            onSubmit={handleOnSubmit}
+            renderForm={() => {
+                return (
+                    <VStack gap="6">
+                        <TypedFormikForm
+                            submitPending={isPending}
+                            showSubmitButton={false}
+                            submitButtonLabel="Endre"
+                            showButtonArrows={false}>
+                            <VStack gap="6">
+                                <VStack gap="8" className="rounded-xs">
+                                    {variant === EndrePeriodeVariant.startdato ? (
+                                        <FormikDatepicker
+                                            name="fom"
+                                            label="Ny startdato"
+                                            minDate={startdatoMinMax.min}
+                                            maxDate={startdatoMinMax.max}
+                                            defaultMonth={deltakelse.fraOgMed}
+                                            validate={getDateValidator({
+                                                required: true,
+                                                ...startdatoMinMax,
+                                            })}
+                                        />
+                                    ) : (
+                                        <FormikDatepicker
+                                            name="tom"
+                                            label="Ny sluttdato"
+                                            minDate={sluttdatoMinMax.min}
+                                            maxDate={sluttdatoMinMax.max}
+                                            defaultMonth={deltakelse.tilOgMed}
+                                            validate={getDateValidator({
+                                                ...sluttdatoMinMax,
+                                                required: true,
+                                            })}
+                                        />
+                                    )}
+                                    <FormikYesOrNoQuestion
+                                        name="deltakerInformert"
+                                        legend={`Er ${deltakernavn} informert om endringen?`}
+                                        validate={getRequiredFieldValidator()}
+                                    />
+                                    <HStack gap="4">
+                                        <Button type="submit" loading={isPending} variant="primary">
+                                            Lagre endring
+                                        </Button>
+                                        {onCancel ? (
+                                            <Button type="button" variant="secondary" onClick={onCancel}>
+                                                Avbryt
+                                            </Button>
+                                        ) : null}
+                                    </HStack>
+                                </VStack>
+                                {error ? <Alert variant="error">{error.error.message}</Alert> : null}
+                            </VStack>
+                        </TypedFormikForm>
+                    </VStack>
+                );
+            }}
+        />
+    );
+};
+
+export default EndrePeriodeForm;
