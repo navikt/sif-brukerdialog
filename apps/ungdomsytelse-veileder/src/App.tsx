a0aac84b4f67a934e706f96df7cd07583e6bb711
frodehansen2 <frode.hansen@bekk.no>
2025-04-25 08:16:55 +0200

    sort imports

--- apps/ungdomsytelse-veileder/src/App.tsx
+++ apps/ungdomsytelse-veileder/src/App.tsx
@@ -1,20 +1,20 @@
 import { Page, VStack } from '@navikt/ds-react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { IntlProvider } from 'react-intl';
 import { Route, Routes } from 'react-router-dom';
 import { initUngDeltakelseOpplyserApiClient } from '@navikt/ung-common';
 import AppHeader from './components/app-header/AppHeader';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
 import { VeilederProvider } from './context/VeilederContext';
+import DevFooter from './dev-components/DevFooter';
+import { ToDoProvider } from './dev-components/ToDo/ToDoContext';
 import { appMessages } from './i18n';
 import DeltakerPage from './pages/deltaker-page/DeltakerPage';
 import InfoPage from './pages/info-page/InfoPage';
-import StartPage from './pages/start-page/StartPage';
-import './app.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import NotFoundPage from './pages/page-not-found/PageNotFound';
-import DevFooter from './dev-components/DevFooter';
-import { ToDoProvider } from './dev-components/ToDo/ToDoContext';
+import StartPage from './pages/start-page/StartPage';
 import { getAppEnv } from './utils/appEnv';
-import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
+import './app.css';
 
 initUngDeltakelseOpplyserApiClient({
     onUnAuthorized: () => {
