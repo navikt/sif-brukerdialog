f2e9b8326b569aefcf02580bb075fe30324229c4
frodehansen2 <frode.hansen@bekk.no>
2025-05-06 13:03:43 +0200

    Funksjonelt vise melding om omsorgsstønad

--- apps/endringsmelding-pleiepenger/src/app/types/ArbeidstidSøknadsdata.ts
+++ apps/endringsmelding-pleiepenger/src/app/types/ArbeidstidSøknadsdata.ts
@@ -1,3 +1,4 @@
+import { YesOrNo } from '@navikt/sif-common-formik-ds';
 import { ArbeiderIPeriodenSvar } from './ArbeiderIPeriodenSvar';
 import { ArbeidstidEndringMap } from './ArbeidstidEndring';
 
@@ -5,6 +6,8 @@ export interface ArbeidstidArbeidsaktivitetMap {
     [aktivitetId: string]: {
         arbeiderIPerioden?: ArbeiderIPeriodenSvar;
         endringer: ArbeidstidEndringMap;
+        /** Gjelder kun frilans og brukes for info til bruker pga praksisendring mai 2025 */
+        mottarOmsorgsstønad?: YesOrNo;
     };
 }
 
