902c0298d1ddce6218facb380039c0ecff1288cc
frodehansen2 <frode.hansen@bekk.no>
2025-05-07 07:59:45 +0200

    ret

--- apps/ungdomsytelse-deltaker/src/main.tsx
+++ apps/ungdomsytelse-deltaker/src/main.tsx
@@ -16,6 +16,7 @@ async function enableMocking() {
     return worker.start({
         serviceWorker: {
             url: getRequiredEnv('PUBLIC_PATH') + '/mockServiceWorker.js',
+            options: { scope: getRequiredEnv('PUBLIC_PATH') },
         },
     });
 }
