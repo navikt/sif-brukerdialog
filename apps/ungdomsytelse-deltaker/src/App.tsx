2effde1b5ffc1c6c3c8c90c5fccb72dce3a49bde
frodehansen2 <frode.hansen@bekk.no>
2025-05-11 15:35:25 +0200

    Playwright setup

--- apps/ungdomsytelse-deltaker/src/App.tsx
+++ apps/ungdomsytelse-deltaker/src/App.tsx
@@ -5,16 +5,16 @@ import DeltakerInfoLoader from './components/deltaker-info-loader/DeltakerInfoLo
 import AppErrorFallback from './components/error-boundary/AppErrorFallback';
 import ErrorBoundary from './components/error-boundary/ErrorBoundary';
 import DevFooter from './dev/DevFooter';
-import { useInitApiClients } from './hooks/useInitApiClients';
+import { initApiClients } from './hooks/useInitApiClients';
 import { AppIntlMessageProvider } from './i18n/AppIntlMessageProvider';
 import '@navikt/ds-css/darkside';
 import './app.css';
 
 const queryClient = new QueryClient();
 
-function App() {
-    useInitApiClients();
+initApiClients();
 
+function App() {
     return (
         <Theme>
             <ErrorBoundary fallback={<AppErrorFallback />}>
