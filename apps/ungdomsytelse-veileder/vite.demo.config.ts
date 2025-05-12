fd6c9873900dc158737414097add373896993d36
frodehansen2 <frode.hansen@bekk.no>
2025-05-12 13:21:18 +0200

    Bruke vite for dev server

--- apps/ungdomsytelse-veileder/vite.demo.config.ts
+++ apps/ungdomsytelse-veileder/vite.demo.config.ts
@@ -1,8 +1,24 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tailwindcss from '@tailwindcss/vite';
+import { AppEnv } from './env.schema';
+
+dotenv.config();
+
+const appSettings: AppEnv = {
+    ENV: `${process.env.ENV}`,
+    APP_VERSION: `${process.env.APP_VERSION}`,
+    GITHUB_REF_NAME: `${process.env.GITHUB_REF_NAME}`,
+    PUBLIC_PATH: `${process.env.PUBLIC_PATH}`,
+    UNG_DELTAKELSE_OPPLYSER_API_URL: `${process.env.UNG_DELTAKELSE_OPPLYSER_API_URL}`,
+    UNG_DELTAKELSE_OPPLYSER_FRONTEND_PATH: `${process.env.UNG_DELTAKELSE_OPPLYSER_FRONTEND_PATH}`,
+    UNG_DELTAKELSE_OPPLYSER_API_SCOPE: `${process.env.UNG_DELTAKELSE_OPPLYSER_API_SCOPE}`,
+};
+
+const veileder = { name: 'Pål', NAVident: 'Z999999' };
 
 export default defineConfig({
     plugins: [
@@ -11,6 +27,14 @@ export default defineConfig({
             include: '**/*.{tsx}',
         }),
         checker({ typescript: true }),
+        {
+            name: 'html-transform',
+            transformIndexHtml: (html) => {
+                return html
+                    .replace('{{{APP_SETTINGS}}}', JSON.stringify(appSettings))
+                    .replace('{{{USER_INFO}}}', JSON.stringify(veileder));
+            },
+        },
     ],
     preview: {
         port: 8088,
