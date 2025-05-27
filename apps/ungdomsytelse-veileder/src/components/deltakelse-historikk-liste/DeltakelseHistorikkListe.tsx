ee89c4d84f559dfdd5663b617421b22ef12b5163
frodehansen2 <frode.hansen@bekk.no>
2025-05-27 12:39:12 +0200

    Tekstjustering

--- apps/ungdomsytelse-veileder/src/components/deltakelse-historikk-liste/DeltakelseHistorikkListe.tsx
+++ apps/ungdomsytelse-veileder/src/components/deltakelse-historikk-liste/DeltakelseHistorikkListe.tsx
@@ -1,10 +1,24 @@
 import { Table } from '@navikt/ds-react';
 import { DeltakelseHistorikkInnslag } from '../../pages/deltaker-page/parts/DeltakelseHistorikk';
 import { dateFormatter } from '@navikt/sif-common-utils';
+import { Revisjonstype } from '@navikt/ung-deltakelse-opplyser-api';
 
 interface Props {
     historikkInnslag?: DeltakelseHistorikkInnslag[];
 }
+
+const getRevisjonstypeTekst = (type: Revisjonstype): string => {
+    switch (type) {
+        case Revisjonstype.OPPRETTET:
+            return 'Deltakelse opprettet';
+        case Revisjonstype.ENDRET:
+            return 'Endret periode';
+        case Revisjonstype.SLETTET:
+            return 'Slettet';
+        default:
+            return 'Ukjent';
+    }
+};
 
 const DeltakelseHistorikkListe = ({ historikkInnslag: historikk = [] }: Props) => {
     return (
@@ -21,7 +35,7 @@ const DeltakelseHistorikkListe = ({ historikkInnslag: historikk = [] }: Props) =
                     return (
                         <Table.Row key={i}>
                             <Table.DataCell width="200">{dateFormatter.compactWithTime(tidspunkt)}</Table.DataCell>
-                            <Table.DataCell>{revisjonstype}</Table.DataCell>
+                            <Table.DataCell>{getRevisjonstypeTekst(revisjonstype)}</Table.DataCell>
                             <Table.DataCell>{utfører}</Table.DataCell>
                         </Table.Row>
                     );
