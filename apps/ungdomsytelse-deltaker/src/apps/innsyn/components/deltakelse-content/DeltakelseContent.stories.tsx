2e2352275b7811cbbe1e638ce6e97d409ec18164
frodehansen2 <frode.hansen@bekk.no>
2025-06-16 08:45:04 +0200

    Justeringer

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/components/deltakelse-content/DeltakelseContent.stories.tsx
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/components/deltakelse-content/DeltakelseContent.stories.tsx
@@ -13,7 +13,7 @@ const meta: Meta<typeof DeltakelseContent> = {
     component: DeltakelseContent,
     title: 'Innsyn/Sider/Forside',
     parameters: {},
-    decorators: [withIntl, withRouter, withDeltakerContext, (Story) => withInnsynApp(Story, new Date())],
+    decorators: [withIntl, withRouter, withDeltakerContext, (Story) => withInnsynApp(Story, new Date(), true)],
 };
 export default meta;
 
--- apps/ungdomsytelse-deltaker/storybook/decorators/withInnsynApp.tsx
+++ apps/ungdomsytelse-deltaker/storybook/decorators/withInnsynApp.tsx
@@ -1,10 +1,11 @@
 import { Theme, VStack } from '@navikt/ds-react';
 import { useEffect } from 'react';
-import ForsidePageLayout from '../../src/apps/innsyn/pages/layout/ForsidePageLayout';
 import InnsynAppHeader from '../../src/apps/innsyn/components/innsyn-app-header/InnsynAppHeader';
+import ForsidePageLayout from '../../src/apps/innsyn/pages/layout/ForsidePageLayout';
+import ForsidePageFooter from '../../src/apps/innsyn/pages/parts/ForsidePageFooter';
 import '../../src/app.css';
 
-export const withInnsynApp = (Story: any, startdato?: Date) => {
+export const withInnsynApp = (Story: any, startdato?: Date, frontpageFooter?: boolean) => {
     useEffect(() => {
         document.body.classList.add('innsynAppBody');
         return () => {
@@ -13,7 +14,7 @@ export const withInnsynApp = (Story: any, startdato?: Date) => {
     }, [location.pathname]);
     return (
         <Theme hasBackground={false}>
-            <ForsidePageLayout documentTitle="Forside">
+            <ForsidePageLayout documentTitle="Forside" footer={frontpageFooter ? <ForsidePageFooter /> : null}>
                 <VStack gap="8">
                     <InnsynAppHeader startdato={startdato || new Date()} />
                     <Story />
