1dc64cf2ff72aeee7fdcdfdd0bce8ea8eb7da811
frodehansen2 <frode.hansen@bekk.no>
2025-06-17 17:02:54 +0200

    Sende med valgt språk

--- apps/omsorgspengerutbetaling-soknad/src/app/utils/søknadsdataToApiData/getApiDataFromSøknadsdata.ts
+++ apps/omsorgspengerutbetaling-soknad/src/app/utils/søknadsdataToApiData/getApiDataFromSøknadsdata.ts
@@ -12,7 +12,7 @@ import { getUtenlansoppholdApiDataFromSøknadsdata } from './getUtenlandsopphold
 export const getApiDataFromSøknadsdata = (
     søkerNorskIdent: string,
     søknadsdata: Søknadsdata,
-    { text }: AppIntlShape,
+    { text, locale }: AppIntlShape,
 ): SøknadApiData | undefined => {
     const { id, dineBarn, fravaer, arbeidssituasjon, medlemskap, legeerklæring } = søknadsdata;
 
@@ -25,7 +25,7 @@ export const getApiDataFromSøknadsdata = (
         return undefined;
     }
 
-    const språk = 'nb';
+    const språk = locale;
 
     const yesOrNoQuestions: YesNoSpørsmålOgSvar[] = [];
 
