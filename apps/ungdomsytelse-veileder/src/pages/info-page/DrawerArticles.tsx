b68408c0222f9de857f418714620e2c3925f6129
frodehansen2 <frode.hansen@bekk.no>
2025-06-17 11:11:15 +0200

    Forslag til info

--- apps/ungdomsytelse-veileder/src/pages/info-page/DrawerArticles.tsx
+++ apps/ungdomsytelse-veileder/src/pages/info-page/DrawerArticles.tsx
@@ -1,11 +1,16 @@
-import { ExpansionCard, Heading, ReadMore, VStack } from '@navikt/ds-react';
+import { BodyLong, ExpansionCard, Heading, List, VStack } from '@navikt/ds-react';
 import { articleList } from './InfoInnhold';
 import Article from './components/Article';
+import { Process } from '../../components/process';
+import ProcessStep from '../../components/process/ProcessStep';
 
 const DrawerArticles = () => {
     return (
         <VStack gap="10">
             <VStack gap="2">
+                <Heading level="2" size="medium" spacing={true}>
+                    Om ungdomsprogramytelsen
+                </Heading>
                 {articleList.map((article) => (
                     <ExpansionCard aria-label="Demo med bare tittel" size="small" key={article.id}>
                         <ExpansionCard.Header>
@@ -17,41 +22,157 @@ const DrawerArticles = () => {
                     </ExpansionCard>
                 ))}
             </VStack>
-            {/* <VStack gap="2">
-                <Heading level="2" size="medium" className="mb-4">
-                    Om funksjoner i deltakerregistrering
-                </Heading>
-                <ReadMore header="Registrere eller finne deltaker">TODO</ReadMore>
-                <ReadMore header="Melde inn deltaker">TODO</ReadMore>
-                <ReadMore header="Endre startdato">TODO</ReadMore>
-                <ReadMore header="Melde ut deltaker eller endre sluttdato">TODO</ReadMore>
-                <ReadMore header="Slette deltaker">
-                    Veileder kan slette en deltaker frem til deltaker har søkt.
-                </ReadMore>
-            </VStack> */}
             <VStack gap="2">
-                <Heading level="2" size="medium" className="mb-4">
-                    Eksempel på en deltakerreise
+                <Heading level="2" size="medium" spacing={true}>
+                    Deltaker, deltakelse og oppgaver
                 </Heading>
-                <ReadMore header="1. Deltaker meldes inn av veileder">
-                    Veileder har en deltaker som skal registreres i ungdomsprogrammet. Veileder søker opp og melder inn
-                    deltaker.
-                </ReadMore>
-                <ReadMore header="2. Deltaker søker på ungdomsprogramytelsen">
-                    Deltaker får melding om at de må logge inn på nav.no for å søke om ungdomsprogramytelsen. Deltakeren
-                    gjør dette og sender inn søknad.
-                </ReadMore>
-                <ReadMore header="3. Deltaker får månedlig oppgave om å rapportere inntekt">
-                    Underveis i deltakelsen mottar deltaker månedlig valgfrie oppgaver om å registrere inntekt for
-                    forrige måned. Hvis deltaker ikke rapporterer noe, vil det bli registrert som at deltaker har ingen
-                    inntekt forrige periode.
-                </ReadMore>
-                <ReadMore header="4. Deltaker starter å ha inntekt">
-                    Hvis deltaker starter å få inntekt, skal de registrere dette. Nav vil også motta informasjon om
-                    inntekt og ytelser deltaker mottar , og hvis dette avviker fra det deltaker har opplyst, vil
-                    deltaker bli informert om dette.
-                </ReadMore>
-                <ReadMore header="5. Deltaker meldes ut av programmet">Veileder melder ut deltaker.</ReadMore>
+                <ExpansionCard aria-label="Demo med bare tittel" size="small">
+                    <ExpansionCard.Header>
+                        <ExpansionCard.Title size="small">Eksempel på en deltakerreise</ExpansionCard.Title>
+                    </ExpansionCard.Header>
+                    <ExpansionCard.Content>
+                        <Process>
+                            <ProcessStep completed={true} icon={<>1</>}>
+                                <Heading size="small" level="3" spacing={true}>
+                                    Deltaker meldes inn av veileder
+                                </Heading>
+                                <BodyLong>
+                                    Veileder har en kandidat som skal registreres i ungdomsprogrammet. Veileder søker
+                                    opp og melder inn deltaker.
+                                </BodyLong>
+                            </ProcessStep>
+                            <ProcessStep completed={true} icon={<>2</>}>
+                                <Heading size="small" level="3" spacing={true}>
+                                    Deltaker søker på ungdomsprogramytelsen
+                                </Heading>
+                                <BodyLong>
+                                    Deltaker får melding om at de må logge inn på nav.no for å søke om
+                                    ungdomsprogramytelsen. Deltakeren gjør dette og sender inn søknaden.
+                                </BodyLong>
+                            </ProcessStep>
+                            <ProcessStep completed={true} icon={<>3</>}>
+                                <Heading size="small" level="3" spacing={true}>
+                                    Deltaker får oppgave om å rapportere inntekt
+                                </Heading>
+                                <BodyLong>
+                                    Underveis i deltakelsen får deltaker månedlig valgfrie oppgaver om å registrere
+                                    inntekt for forrige måned. Hvis deltaker ikke rapporterer noe, vil det bli
+                                    registrert som at deltaker har ingen inntekt forrige periode.
+                                </BodyLong>
+                            </ProcessStep>
+                            <ProcessStep completed={true} icon={<>4</>}>
+                                <Heading size="small" level="3" spacing={true}>
+                                    Deltaker starter å ha inntekt
+                                </Heading>
+                                <BodyLong>
+                                    Hvis deltaker starter å ha inntekt, skal de registrere dette. Nav vil også motta
+                                    informasjon om inntekt og ytelser deltaker mottar fra registre, og hvis dette
+                                    avviker fra det deltaker har opplyst, vil deltaker bli informert om dette gjennom en
+                                    oppgave hvor de kan uttale seg om endringen.
+                                </BodyLong>
+                            </ProcessStep>
+                            <ProcessStep completed={true} icon={<>5</>}>
+                                <Heading size="small" level="3" spacing={true}>
+                                    Deltaker meldes ut av programmet
+                                </Heading>
+                                <BodyLong>Veileder melder ut deltaker.</BodyLong>
+                            </ProcessStep>
+                        </Process>
+                    </ExpansionCard.Content>
+                </ExpansionCard>
+                <ExpansionCard aria-label="Demo med bare tittel" size="small">
+                    <ExpansionCard.Header>
+                        <ExpansionCard.Title size="small">Deltakeroppgaver</ExpansionCard.Title>
+                    </ExpansionCard.Header>
+                    <ExpansionCard.Content>
+                        <VStack gap="6">
+                            <BodyLong>
+                                Deltaker vil motta ulike oppgaver underveis i programmet. Disse varsles via SMS, og
+                                leder deltakeren til Min side på nav.no. Derfra kan deltakeren gå videre til
+                                deltakerportalen, hvor hele oppgaven vises.
+                            </BodyLong>
+                            <Heading level="2" size="medium">
+                                Enkeltoppgaver
+                            </Heading>
+                            <VStack gap="2">
+                                <Heading level="3" size="small">
+                                    Send inn søknad
+                                </Heading>
+                                <BodyLong>
+                                    Når deltaker meldes inn i ungdomsprogrammet, vil hen motta en SMS med beskjed om å
+                                    sende inn søknad.
+                                </BodyLong>
+                            </VStack>
+                            <VStack gap="2">
+                                <Heading level="3" size="small">
+                                    Rapportere inntekt
+                                </Heading>
+                                <BodyLong>
+                                    Hver måned mottar deltakeren en oppgave om å rapportere inntekt for forrige måned
+                                    (gjelder ikke måneden deltakeren starter i programmet). Hvis deltakeren ikke
+                                    rapporterer noe, vil det bli registrert som at hen ikke har hatt inntekt den
+                                    måneden.
+                                </BodyLong>
+                            </VStack>
+                            <Heading level="2" size="medium">
+                                Tilbakemeldingsoppgaver
+                            </Heading>
+                            <VStack gap="2">
+                                <Heading level="3" size="small">
+                                    Endret start- eller sluttdato
+                                </Heading>
+                                <BodyLong>
+                                    Hvis veileder endrer start- eller sluttdato, vil deltakeren få en oppgave med
+                                    mulighet til å gi en tilbakemelding på endringen. Tilbakemeldingen vil ikke påvirke
+                                    selve endringen.
+                                </BodyLong>
+                            </VStack>
+                            <VStack gap="2">
+                                <Heading level="3" size="small">
+                                    Avvik i inntekt
+                                </Heading>
+                                <BodyLong>
+                                    Hvis inntekten fra registre for en periode avviker fra det deltakeren har oppgitt,
+                                    vil deltakeren få en oppgave der hen kan uttale seg om avviket. Registerført inntekt
+                                    vil alltid legges til grunn, og deltakernes tilbakemelding vil ikke endre dette.
+                                </BodyLong>
+                            </VStack>
+                            <Heading level="2" size="medium">
+                                Oppgaver som blir avbrutt eller utløper
+                            </Heading>
+                            <VStack gap="2">
+                                <Heading level="3" size="small">
+                                    Avbrutt oppgave
+                                </Heading>
+                                <BodyLong>
+                                    Noen oppgaver kan bli avbrutt hvis de blir utdaterte eller erstattet, for eksempel
+                                    ved endring av startdato. Hvis veileder endrer startdato og en oppgave opprettes,
+                                    men veileder så gjør en ny endring før deltakeren har svart på den første oppgaven,
+                                    opprettes en ny oppgave, og den første blir avbrutt.
+                                </BodyLong>
+                            </VStack>
+                            <VStack gap="2">
+                                <Heading level="3" size="small">
+                                    Utløpt oppgave
+                                </Heading>
+                                <BodyLong>
+                                    Alle oppgaver har en utløpsdato. Hvis deltakeren ikke svarer innen denne fristen,
+                                    blir oppgaven satt som utløpt.
+                                    <List>
+                                        <List.Item>
+                                            For den månedlige inntektsrapporteringen behandles dette som at deltakeren
+                                            ikke hadde inntekt den måneden.
+                                        </List.Item>
+                                        <List.Item>
+                                            For andre oppgaver betyr det at deltakeren ikke lenger har mulighet til å gi
+                                            tilbakemelding.
+                                        </List.Item>
+                                    </List>
+                                </BodyLong>
+                            </VStack>
+                        </VStack>
+                    </ExpansionCard.Content>
+                </ExpansionCard>
             </VStack>
         </VStack>
     );
