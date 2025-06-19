6077bd5bb0716224d1da2cd16dfabeb3a88ff0cb
frodehansen2 <frode.hansen@bekk.no>
2025-06-19 09:42:02 +0200

    Redirect til ny url i ung-deltaker (#3226)
    * Redirect fra hvis deltaker er på root
    
    * Fix gammel url

--- apps/ungdomsytelse-deltaker/src/App.tsx
+++ apps/ungdomsytelse-deltaker/src/App.tsx
@@ -29,6 +29,14 @@ if (getMaybeEnv('VITE') && getMaybeEnv('ENV') !== 'prod') {
 
 const queryClient = new QueryClient();
 
+if (window.location.pathname === '/') {
+    window.location.pathname = '/ungdomsprogrammet/ytelsen/';
+}
+
+if (window.location.pathname === '/ungdomsytelse-deltaker') {
+    window.location.pathname = '/ungdomsprogrammet/ytelsen/';
+}
+
 function App() {
     return (
         <ErrorBoundary fallback={<AppErrorFallback />}>
