b86372b828dfe1a73242234dc265044828c430fd
frodehansen2 <frode.hansen@bekk.no>
2025-06-23 10:54:43 +0200

    Fjerne logToConsoleOnly

--- apps/ungdomsytelse-deltaker/src/App.tsx
+++ apps/ungdomsytelse-deltaker/src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { injectDecoratorClientSide } from '@navikt/nav-dekoratoren-moduler';
 import { UngdomsytelseDeltakerApp } from '@navikt/sif-app-register';
-import { getMaybeEnv } from '@navikt/sif-common-env';
+import { EnvKey, getMaybeEnv } from '@navikt/sif-common-env';
 import DeltakerInfoLoader from './components/deltaker-info-loader/DeltakerInfoLoader';
 import AppErrorFallback from './components/error-boundary/AppErrorFallback';
 import ErrorBoundary from './components/error-boundary/ErrorBoundary';
@@ -49,8 +49,7 @@ function App() {
                 isActive={env.SIF_PUBLIC_USE_FARO}>
                 <AnalyticsProvider
                     applicationKey={UngdomsytelseDeltakerApp.key}
-                    logToConsoleOnly={true}
-                    isActive={true}>
+                    isActive={env[EnvKey.SIF_PUBLIC_USE_AMPLITUDE] === 'true'}>
                     <QueryClientProvider client={queryClient}>
                         <AppIntlMessageProvider>
                             <DeltakerInfoLoader />
