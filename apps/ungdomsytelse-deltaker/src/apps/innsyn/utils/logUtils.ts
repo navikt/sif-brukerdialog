11147ff9cf550cf52d8570767ecb842554b09f90
frodehansen2 <frode.hansen@bekk.no>
2025-06-23 10:08:19 +0200

    Antall dager mellom meldt inn og søkt

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/utils/logUtils.ts
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/utils/logUtils.ts
@@ -5,7 +5,7 @@ type DeltakelsePeriodeMeta = {
     harSøkt: boolean;
     harStartet: boolean;
     erAvsluttet: boolean;
-    antallOppgaver: number;
+    antallOppgaverTotalt: number;
     antallLøsteOppgaver: number;
     antallUløsteOppgaver: number;
     antallAvbrutteOppgaver: number;
@@ -16,19 +16,25 @@ type DeltakelsePeriodeMeta = {
     antallAvvikInntektOppgaver: number;
     antallRapporterInntektOppgaver: number;
     antallSøkYtelseOppgaver: number;
-    antallDagerIProgrammet: number;
+    antallDagerSidenStartdato: number;
+    antallDagerMellomInnmeldtOgSøknad?: number;
 };
 
 const getDeltakelsePeriodeMeta = (deltakelse: DeltakelsePeriode): DeltakelsePeriodeMeta => {
     const harSøkt = deltakelse.søktTidspunkt !== undefined;
     const harStartet = harSøkt && dayjs(deltakelse.programPeriode.from).isBefore(dayjs());
     const erAvsluttet = harSøkt && dayjs(deltakelse.programPeriode.to).isAfter(dayjs());
+
+    const søkYtelseOppgave = deltakelse.oppgaver.find((oppgave) => oppgave.oppgavetype === Oppgavetype.SØK_YTELSE);
     return {
         harSøkt,
         harStartet,
         erAvsluttet,
-        antallDagerIProgrammet: harStartet ? 0 : dayjs(deltakelse.programPeriode.from).diff(dayjs(), 'day'),
-        antallOppgaver: deltakelse.oppgaver.length,
+        antallDagerSidenStartdato: harStartet ? 0 : dayjs(deltakelse.programPeriode.from).diff(dayjs(), 'day'),
+        antallDagerMellomInnmeldtOgSøknad: søkYtelseOppgave
+            ? dayjs(deltakelse.søktTidspunkt).diff(søkYtelseOppgave.opprettetDato, 'day')
+            : undefined,
+        antallOppgaverTotalt: deltakelse.oppgaver.length,
         antallLøsteOppgaver: deltakelse.oppgaver.filter((oppgave) => oppgave.status === OppgaveStatus.LØST).length,
         antallUløsteOppgaver: deltakelse.oppgaver.filter((oppgave) => oppgave.status === OppgaveStatus.ULØST).length,
         antallAvbrutteOppgaver: deltakelse.oppgaver.filter((oppgave) => oppgave.status === OppgaveStatus.AVBRUTT)
