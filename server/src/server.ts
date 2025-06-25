90ca75d7a06a95df58404751e932770ed4cff902
frodehansen2 <frode.hansen@bekk.no>
2025-06-25 10:20:52 +0200

    debug

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,18 +12,28 @@ import { verifyToken } from './utils/tokenValidation.js';
 const server = express();
 
 setupServerDefaults(server);
+console.log('Defaults added');
+
 setupActuators(server);
+console.log('Actuators added');
 
 server.use(logger.morganMiddleware);
+console.log('Morgan added');
 server.use(express.static('./public', { index: false }));
+console.log('Static added');
 server.use(`${serverConfig.app.publicPath}/assets`, express.static(path.resolve(path.resolve('public'), 'assets')));
+console.log('Assets added');
 server.use(verifyToken);
+console.log('Token added');
 
 configureReverseProxyApi(server);
+console.log('Reverse proxy added');
 
 // Catch all route, må være sist
 setupAndServeHtml(server);
+console.log('Serve added');
 
 server.use(errorHandling);
+console.log('Error handling added');
 
 export default server;
