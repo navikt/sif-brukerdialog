6265c5892c27f56da3ee07b2deacc20525faa08a
frodehansen2 <frode.hansen@bekk.no>
2025-06-28 12:29:35 +0200

    det var da som bare

--- packages/ung-common/src/api/index.ts
+++ packages/ung-common/src/api/index.ts
@@ -2,7 +2,6 @@ import { AxiosError, HttpStatusCode } from 'axios';
 
 export * from './errorHandlers';
 export * from './k9BrukerdialogProsesseringApiClient';
-export * from './ungDeltakelseOpplyserApiClient';
 
 export const isUnauthorized = (error: AxiosError): boolean =>
     error !== undefined && error.response !== undefined && error.response.status === HttpStatusCode.Unauthorized;
