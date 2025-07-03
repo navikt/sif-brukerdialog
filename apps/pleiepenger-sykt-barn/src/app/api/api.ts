09d6211f22538c5b96aa703624587c1a276ffec7
frodehansen2 <frode.hansen@bekk.no>
2025-07-03 11:23:31 +0200

    SØKNAD_VERSJON i psb

--- apps/pleiepenger-sykt-barn/src/app/api/api.ts
+++ apps/pleiepenger-sykt-barn/src/app/api/api.ts
@@ -1,14 +1,15 @@
 import { storageParser } from '@navikt/sif-common-core-ds/src/utils/persistence/storageParser';
 import axios, { AxiosResponse } from 'axios';
 import { axiosConfigPsb } from '../config/axiosConfig';
+import { SØKNAD_VERSJON } from '../constants/SØKNAD_VERSJON';
 import { ResourceType } from '../types/ResourceType';
 import { StepID } from '../types/StepID';
 import { SøknadApiData } from '../types/søknad-api-data/SøknadApiData';
 import { SøknadFormValues } from '../types/søknad-form-values/SøknadFormValues';
-import { MELLOMLAGRING_VERSION, SøknadTempStorageData } from '../types/SøknadTempStorageData';
+import { SøknadTempStorageData } from '../types/SøknadTempStorageData';
+import { getFeatureToggles } from '../utils/featureToggleUtils';
 import { AAregArbeidsgiverRemoteData } from './getArbeidsgivereRemoteData';
 import { axiosJsonConfig, sendMultipartPostRequest } from './utils/apiUtils';
-import { getFeatureToggles } from '../utils/featureToggleUtils';
 
 export const getPersistUrl = (stepID?: StepID) =>
     stepID ? `${ResourceType.MELLOMLAGRING}?lastStepID=${encodeURI(stepID)}` : ResourceType.MELLOMLAGRING;
@@ -20,7 +21,7 @@ export const persist = ({ formValues, lastStepID }: { formValues?: SøknadFormVa
             formValues,
             metadata: {
                 lastStepID,
-                version: MELLOMLAGRING_VERSION,
+                version: SØKNAD_VERSJON,
                 updatedTimestemp: new Date().toISOString(),
                 featureToggles: getFeatureToggles(),
             },
---
+++ apps/pleiepenger-sykt-barn/src/app/constants/SØKNAD_VERSJON.ts
@@ -0,0 +1,1 @@
+export const SØKNAD_VERSJON = '14.2.0';
