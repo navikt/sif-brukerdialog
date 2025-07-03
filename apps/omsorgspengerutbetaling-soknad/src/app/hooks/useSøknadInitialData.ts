27b87f41d7dc05875de31717471071224a226c57
frodehansen2 <frode.hansen@bekk.no>
2025-07-03 11:28:10 +0200

    Standardisere MELLOMLAGRING_VERSJON

--- apps/omsorgspengerutbetaling-soknad/src/app/hooks/useSøknadInitialData.ts
+++ apps/omsorgspengerutbetaling-soknad/src/app/hooks/useSøknadInitialData.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { fetchBarn, fetchSøker, RegistrertBarn, Søker } from '@navikt/sif-common-api';
 import { isForbidden } from '@navikt/sif-common-core-ds/src/utils/apiUtils';
-import { SØKNAD_VERSJON } from '../constants/SØKNAD_VERSJON';
+import { MellomlagringData, mellomlagringService } from '../api/mellomlagringService';
+import { MELLOMLAGRING_VERSJON } from '../constants/MELLOMLAGRING_VERSJON';
 import { RequestStatus } from '../types/RequestStatus';
 import { SøknadContextState } from '../types/SøknadContextState';
 import { SøknadRoutes } from '../types/SøknadRoutes';
 import appSentryLogger from '../utils/appSentryLogger';
-import { MellomlagringData, mellomlagringService } from '../api/mellomlagringService';
 import { relocateToNoAccessPage } from '../utils/navigationUtils';
 
 export type SøknadInitialData = SøknadContextState;
@@ -51,7 +51,7 @@ const getSøknadInitialData = async (
     }
     const lagretSøknadStateToUse = isValid ? lagretSøknadState : defaultSøknadState;
     return Promise.resolve({
-        versjon: SØKNAD_VERSJON,
+        versjon: MELLOMLAGRING_VERSJON,
         søknadsdata: {},
         ...lagretSøknadStateToUse,
         søker,
