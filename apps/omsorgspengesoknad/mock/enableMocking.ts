a0d43a291ed0dd304891390006b458fabc0acc03
frodehansen2 <frode.hansen@bekk.no>
2025-07-06 11:32:51 +0200

    Forenkle enableMocking

--- apps/omsorgspengesoknad/mock/enableMocking.ts
+++ apps/omsorgspengesoknad/mock/enableMocking.ts
@@ -1,7 +1,5 @@
-import { getMaybeEnv } from '@navikt/sif-common-env';
-
 export async function enableMocking() {
-    if (getMaybeEnv('ENV') !== 'development' || import.meta.env.MODE !== 'msw') {
+    if (import.meta.env.MODE !== 'msw') {
         return;
     }
     const { worker } = await import('./msw/browser');
