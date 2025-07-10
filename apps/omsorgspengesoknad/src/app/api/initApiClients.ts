0fdbf13afc4926226650cb262423bba9bc4389e2
frodehansen2 <frode.hansen@bekk.no>
2025-07-10 17:29:34 +0200

    init query clients

--- apps/omsorgspengesoknad/src/app/api/initApiClients.ts
+++ apps/omsorgspengesoknad/src/app/api/initApiClients.ts
@@ -1,11 +1,14 @@
 import { initK9BrukerdialogProsesseringApiClients } from '@navikt/k9-brukerdialog-prosessering-api';
 import { initK9SakInnsynApiClients } from '@navikt/k9-sak-innsyn-api';
 import { getCommonEnv, getRequiredEnv } from '@navikt/sif-common-env';
+import { initSifComonQueryClients } from '@navikt/sif-common-query';
 
 export const initApiClients = () => {
-    initK9SakInnsynApiClients(getRequiredEnv('K9_SAK_INNSYN_FRONTEND_PATH'), getCommonEnv().SIF_PUBLIC_LOGIN_URL);
-    initK9BrukerdialogProsesseringApiClients(
-        getRequiredEnv('K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH'),
-        getCommonEnv().SIF_PUBLIC_LOGIN_URL,
-    );
+    const innsynFrontendPath = getRequiredEnv('K9_SAK_INNSYN_FRONTEND_PATH');
+    const brukerdialogFrontendPath = getRequiredEnv('K9_BRUKERDIALOG_PROSESSERING_FRONTEND_PATH');
+    const sifPublicLoginUrl = getCommonEnv().SIF_PUBLIC_LOGIN_URL;
+
+    initK9SakInnsynApiClients(innsynFrontendPath, sifPublicLoginUrl);
+    initK9BrukerdialogProsesseringApiClients(brukerdialogFrontendPath, sifPublicLoginUrl);
+    initSifComonQueryClients(brukerdialogFrontendPath, sifPublicLoginUrl);
 };
