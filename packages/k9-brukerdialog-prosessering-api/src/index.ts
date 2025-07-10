041ea727634d271c925d52b544a630e90ea84d1a
frodehansen2 <frode.hansen@bekk.no>
2025-07-10 11:13:48 +0200

    Stash

--- packages/k9-brukerdialog-prosessering-api/src/index.ts
+++ packages/k9-brukerdialog-prosessering-api/src/index.ts
@@ -1,3 +1,13 @@
+// Omsorgspenger API
+export * as omsorgspenger from './generated/omsorgspenger';
+
+// Pleiepenger API
+export * as pleiepenger from './generated/pleiepenger';
+
+// Ungdomsytelse API
+export * as ungdomsytelse from './generated/ungdomsytelse';
+
+// Legacy exports (kan fjernes etter migrasjon)
 export * from './client';
 export * from './client/sdk.gen';
 export * from './client/types.gen';
