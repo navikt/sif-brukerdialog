d2db941815dd174acb21ce0d84dc90cf0d6362a1
frodehansen2 <frode.hansen@bekk.no>
2025-07-11 08:53:40 +0200

    Oppdaterte versjoner av pakker under refactoring (#3316)
    * Kopiere inn oppdaterte pakker fra refactor-branch
    
    * Fjerne ubrukt fil

--- packages/k9-brukerdialog-prosessering-api/src/index.ts
+++ packages/k9-brukerdialog-prosessering-api/src/index.ts
@@ -1,12 +1,14 @@
+// Legacy export (for bakoverkompatibilitet)
+export * from './generated/client';
+export * from './generated/client/sdk.gen';
+export * from './generated/client/types.gen';
+export * from './generated/client/zod.gen';
+export * from './generated/client/client.gen';
+
 // Omsorgspenger API
 export * as omsorgspenger from './generated/omsorgspenger';
 
 // Ungdomsytelse API
 export * as ungdomsytelse from './generated/ungdomsytelse';
 
-// Legacy exports (kan fjernes etter migrasjon)
-export * from './client';
-export * from './client/sdk.gen';
-export * from './client/types.gen';
-export * from './client/zod.gen';
-export * from './client/client.gen';
+export * from './initK9BrukerdialogProsesseringApiClients';
--- packages/sif-common-query/src/api-clients/index.ts
+++ packages/sif-common-query/src/api-clients/index.ts
@@ -1,3 +1,2 @@
 export * from './errorHandlers';
-export * from './k9BrukerdialogProsesseringApiClient';
-export * from './k9SakInnsynApiClient';
+export * from './initSifComonQueryClients';
