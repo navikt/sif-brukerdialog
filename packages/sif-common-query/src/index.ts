c20230e409b6c73069468eb09d9d9fad52e91a30
frodehansen2 <frode.hansen@bekk.no>
2025-07-15 08:36:49 +0200

    Fjerne unødvendig eksport

--- packages/sif-common-query/src/index.ts
+++ packages/sif-common-query/src/index.ts
@@ -1,16 +1,13 @@
 // Query keys
 export { sifCommonQueryKeys } from './queryKeys';
 
-// All types
+// Alle typer som er hensiktsmessig å eksponere
 export * from './types/arbeidsgivere';
 export * from './types/barn';
 export * from './types/invalidParameterProblemDetail';
-export * from './types/mellomlagring';
 export * from './types/søker';
-export * from './types/validerFritekst';
-export * from './types/vedlegg';
 
-// All hooks
+// Alle hooks
 export * from './hooks/useArbeidsgivere';
 export * from './hooks/useBarn';
 export * from './hooks/useYtelseMellomlagring';
@@ -18,15 +15,14 @@ export * from './hooks/useSøker';
 export * from './hooks/useValiderFritekst';
 export * from './hooks/useVedlegg';
 
-// All API calls (for non-React usage)
+// Alle API kall (for non-React usage)
 export * from './api';
 
-// All utils
+// Alle utils
 export * from './utils/createYtelseMellomlagringUtils';
 export * from './utils/dateUtils';
 export * from './utils/jsonResponseParser';
 
 // API client initialization and error types
 export * from './api-clients';
-
 export * from './api-clients/initSifComonQueryClients';
