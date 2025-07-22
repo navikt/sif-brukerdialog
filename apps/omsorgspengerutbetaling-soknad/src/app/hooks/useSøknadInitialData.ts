e96fac9c8a0ae115c28faecddbaa0c322aa66ed8
frodehansen2 <frode.hansen@bekk.no>
2025-07-22 09:40:01 +0200

    Sort imports

--- apps/omsorgspengerutbetaling-soknad/src/app/hooks/useSøknadInitialData.ts
+++ apps/omsorgspengerutbetaling-soknad/src/app/hooks/useSøknadInitialData.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { fetchBarn, fetchSøker, RegistrertBarn, Søker } from '@navikt/sif-common-api';
 import { isForbidden } from '@navikt/sif-common-core-ds/src/utils/apiUtils';
+import { MellomlagringData, mellomlagringService } from '../api/mellomlagringService';
 import { MELLOMLAGRING_VERSJON } from '../constants/MELLOMLAGRING_VERSJON';
 import { RequestStatus } from '../types/RequestStatus';
 import { SøknadContextState } from '../types/SøknadContextState';
 import { SøknadRoutes } from '../types/SøknadRoutes';
 import appSentryLogger from '../utils/appSentryLogger';
-import { MellomlagringData, mellomlagringService } from '../api/mellomlagringService';
 import { relocateToNoAccessPage } from '../utils/navigationUtils';
 
 export type SøknadInitialData = SøknadContextState;
