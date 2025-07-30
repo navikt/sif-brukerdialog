90d4b7362cf522a96986a6aa9280dd30d5fe53d2
frodehansen2 <frode.hansen@bekk.no>
2025-07-30 10:46:31 +0200

    Flytte inject decorator for dev (#3386)

--- apps/opplaringspenger-soknad/src/app/main.tsx
+++ apps/opplaringspenger-soknad/src/app/main.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { injectDecoratorClientSide } from '@navikt/nav-dekoratoren-moduler';
+import { getMaybeEnv } from '@navikt/sif-common-env';
 import { enableMocking } from '../../mock/msw/enableMocking';
+import App from './App';
+
+if (getMaybeEnv('INJECT_DECORATOR') === 'true') {
+    injectDecoratorClientSide({
+        env: 'dev',
+        params: {
+            simple: true,
+            chatbot: false,
+        },
+    });
+}
 
 enableMocking().then(() => {
     createRoot(document.getElementById('root')!).render(
