beed36d677b6db152812be79ca521e54d8368258
frodehansen2 <frode.hansen@bekk.no>
2025-08-04 10:49:45 +0200

    Alias-fix

--- apps/pleiepenger-sykt-barn/vitest.config.ts
+++ apps/pleiepenger-sykt-barn/vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import * as path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -9,5 +10,8 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         css: false,
+        alias: {
+            '@i18n': path.resolve(__dirname, './src/app/i18n'),
+        },
     },
 });
