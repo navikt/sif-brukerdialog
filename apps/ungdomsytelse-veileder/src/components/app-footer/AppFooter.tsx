be0771b3ceb22261dd523eed2af4e62f2d41e16e
frodehansen2 <frode.hansen@bekk.no>
2025-08-07 10:36:35 +0200

    Ung veileder - legge til spørsmål om vedtaksbrev fra gosys (#3435)
    * Legge til spørsmål om vedtaksbrev
    
    * Tekstoppdatering

--- apps/ungdomsytelse-veileder/src/components/app-footer/AppFooter.tsx
+++ apps/ungdomsytelse-veileder/src/components/app-footer/AppFooter.tsx
@@ -8,7 +8,9 @@ const AppFooter = () => {
                     <VStack gap="6" className="pt-6 pb-6">
                         <BodyShort>
                             Hvis du opplever feil med løsningen, kan du melde fra om dette{' '}
-                            <Link href="https://jira.adeo.no/plugins/servlet/desk" target="_blank">
+                            <Link
+                                href="https://jira.adeo.no/plugins/servlet/desk/portal/541/create/6034"
+                                target="_blank">
                                 i Porten
                             </Link>
                             .
