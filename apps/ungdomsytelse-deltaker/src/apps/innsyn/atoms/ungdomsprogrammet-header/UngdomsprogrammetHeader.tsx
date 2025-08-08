4314222af8e44c3ed5c9ce32d0e8948287bfe488
frodehansen2 <frode.hansen@bekk.no>
2025-08-08 12:32:42 +0200

    Update

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/atoms/ungdomsprogrammet-header/UngdomsprogrammetHeader.tsx
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/atoms/ungdomsprogrammet-header/UngdomsprogrammetHeader.tsx
@@ -1,4 +1,4 @@
-import { Bleed, Box, Heading, Hide, Show, VStack } from '@navikt/ds-react';
+import { Bleed, Box, Heading, Hide, Show } from '@navikt/ds-react';
 import MannSvg from '../../atoms/svg/Mann';
 
 const UngdomsprogrammetHeader = () => {
@@ -10,39 +10,35 @@ const UngdomsprogrammetHeader = () => {
                 </Heading>
             </Hide>
             <Show above="sm">
-                <VStack
-                    style={{
-                        color: '#002060',
-                        fontWeight: 'bold',
-                        lineHeight: '1.125rem',
-                        fontSize: '3.5rem',
-                        paddingBottom: '1rem',
-                    }}
-                    gap="2">
-                    <div role="heading" aria-level={1} aria-label="Ungdomsprogrammet ">
-                        <div aria-hidden="true">
-                            <Box>
-                                <span
-                                    className="inline-block rounded-xl p-3 pt-6 pb-6 relative z-1 "
-                                    style={{ backgroundColor: '#D7E6F0', fontWeight: '900' }}>
-                                    Ungdoms-
-                                    <span
-                                        style={{ position: 'absolute', left: '100%', top: '-1rem' }}
-                                        aria-hidden="true">
-                                        <MannSvg />
-                                    </span>
+                <h2 aria-label="Ungdomsprogrammet">
+                    <div
+                        aria-hidden="true"
+                        style={{
+                            color: '#002060',
+                            fontWeight: 'bold',
+                            lineHeight: '1.125rem',
+                            fontSize: '3.5rem',
+                            paddingBottom: '1rem',
+                        }}>
+                        <Box>
+                            <span
+                                className="inline-block rounded-xl p-3 pt-6 pb-6 relative z-1 "
+                                style={{ backgroundColor: '#D7E6F0', fontWeight: '900' }}>
+                                Ungdoms-{/* */}
+                                <span style={{ position: 'absolute', left: '100%', top: '-1rem' }} aria-hidden="true">
+                                    <MannSvg />
                                 </span>
-                            </Box>
-                            <Bleed marginBlock="3">
-                                <span
-                                    className="inline-block rounded-xl p-3 pb-6 pt-6"
-                                    style={{ backgroundColor: '#D7E6F0', paddingRight: '1.5rem' }}>
-                                    programmet
-                                </span>
-                            </Bleed>
-                        </div>
+                            </span>
+                        </Box>
+                        <Bleed marginBlock="3">
+                            <span
+                                className="inline-block rounded-xl p-3 pb-6 pt-6"
+                                style={{ backgroundColor: '#D7E6F0', paddingRight: '1.5rem' }}>
+                                programmet
+                            </span>
+                        </Bleed>
                     </div>
-                </VStack>
+                </h2>
             </Show>
         </>
     );
