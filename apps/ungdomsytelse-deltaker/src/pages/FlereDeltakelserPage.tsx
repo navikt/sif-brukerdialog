0b01210f67ad715ed68da6323b754f0992ed130e
frodehansen2 <frode.hansen@bekk.no>
2025-08-08 13:19:08 +0200

    Mer fix

--- apps/ungdomsytelse-deltaker/src/pages/FlereDeltakelserPage.tsx
+++ apps/ungdomsytelse-deltaker/src/pages/FlereDeltakelserPage.tsx
@@ -1,27 +1,21 @@
 import { BodyLong, Box, Heading } from '@navikt/ds-react';
 import SifGuidePanel from '@navikt/sif-common-core-ds/src/components/sif-guide-panel/SifGuidePanel';
-import { ErrorPage } from '@navikt/sif-common-soknad-ds/src';
+import Feilside from './Feilside';
 
 const FlereDeltakelserPage = () => (
-    <ErrorPage
-        pageTitle="Flere deltakelser"
-        bannerTitle="Ungdomsprogramytelsen"
-        contentRenderer={() => {
-            return (
-                <Box paddingBlock="0 24">
-                    <SifGuidePanel mood="uncertain">
-                        <Heading level="2" size="medium" spacing>
-                            Oops, det oppstod en feil
-                        </Heading>
-                        <BodyLong>
-                            Vi har registrert feilen og undersøker saken. Hvis problemet vedvarer, kan du kontakte
-                            veilederen din.
-                        </BodyLong>
-                    </SifGuidePanel>
-                </Box>
-            );
-        }}
-    />
+    <Feilside pageTitle="Flere deltakelser" bannerTitle="Ungdomsprogramytelsen">
+        <Box paddingBlock="0 24">
+            <SifGuidePanel mood="uncertain">
+                <Heading level="2" size="medium" spacing>
+                    Oops, det oppstod en feil
+                </Heading>
+                <BodyLong>
+                    Vi har registrert feilen og undersøker saken. Hvis problemet vedvarer, kan du kontakte veilederen
+                    din.
+                </BodyLong>
+            </SifGuidePanel>
+        </Box>
+    </Feilside>
 );
 
 export default FlereDeltakelserPage;
--- apps/ungdomsytelse-deltaker/src/pages/IngenSendSøknadOppgave.tsx
+++ apps/ungdomsytelse-deltaker/src/pages/IngenSendSøknadOppgave.tsx
@@ -1,28 +1,21 @@
 import { Alert, BodyShort, Heading } from '@navikt/ds-react';
 import SifGuidePanel from '@navikt/sif-common-core-ds/src/components/sif-guide-panel/SifGuidePanel';
-import { ErrorPage } from '@navikt/sif-common-soknad-ds/src';
+import Feilside from './Feilside';
 
 const IngenSendSøknadOppgave = () => {
     return (
-        <ErrorPage
-            pageTitle="Det oppstod en feil"
-            bannerTitle="Søknad om ungdomsprogramytelse"
-            contentRenderer={() => {
-                return (
-                    <SifGuidePanel mood="uncertain">
-                        <Heading level="2" size="medium" spacing>
-                            Deltaker har ikke en aktiv sendSøknadOppgave
-                        </Heading>
-                        <Alert variant="error" inline>
-                            <BodyShort>
-                                Dette er på grunn av gamle testdata, og skal ikke oppstå for nye deltakere som meldes
-                                inn.
-                            </BodyShort>
-                        </Alert>
-                    </SifGuidePanel>
-                );
-            }}
-        />
+        <Feilside pageTitle="Det oppstod en feil" bannerTitle="Søknad om ungdomsprogramytelse">
+            <SifGuidePanel mood="uncertain">
+                <Heading level="2" size="medium" spacing>
+                    Deltaker har ikke en aktiv sendSøknadOppgave
+                </Heading>
+                <Alert variant="error" inline>
+                    <BodyShort>
+                        Dette er på grunn av gamle testdata, og skal ikke oppstå for nye deltakere som meldes inn.
+                    </BodyShort>
+                </Alert>
+            </SifGuidePanel>
+        </Feilside>
     );
 };
 
