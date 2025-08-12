4c6cfad7c11463f5779748b0c92879745d9de00b
frodehansen2 <frode.hansen@bekk.no>
2025-08-12 15:22:06 +0200

    Revert endring

--- apps/ungdomsytelse-veileder/src/types/UtvidetRevisjonstype.ts
+++ apps/ungdomsytelse-veileder/src/types/UtvidetRevisjonstype.ts
@@ -1,13 +1,9 @@
 import { Revisjonstype } from '@navikt/ung-deltakelse-opplyser-api-veileder';
 
-enum RevisjonstypeSøknadInnsendt {
-    'SØKNAD_INNSENDT' = 'SØKNAD_INNSENDT',
-}
-
 export enum UtvidetRevisjonstype {
     'OPPRETTET' = Revisjonstype.OPPRETTET,
     'ENDRET' = Revisjonstype.ENDRET,
     'SLETTET' = Revisjonstype.SLETTET,
     'UKJENT' = Revisjonstype.UKJENT,
-    'SØKNAD_INNSENDT' = RevisjonstypeSøknadInnsendt.SØKNAD_INNSENDT,
+    'SØKNAD_INNSENDT' = 'SØKNAD_INNSENDT',
 }
