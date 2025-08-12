2914b50f830b115f2620241eef2e04552bafcd0f
frodehansen2 <frode.hansen@bekk.no>
2025-08-12 15:13:01 +0200

    Fix sonar enum feil

--- apps/ungdomsytelse-veileder/src/types/UtvidetRevisjonstype.ts
+++ apps/ungdomsytelse-veileder/src/types/UtvidetRevisjonstype.ts
@@ -1,9 +1,13 @@
 import { Revisjonstype } from '@navikt/ung-deltakelse-opplyser-api-veileder';
 
+enum RevisjonstypeSøknadInnsendt {
+    'SØKNAD_INNSENDT' = 'SØKNAD_INNSENDT',
+}
+
 export enum UtvidetRevisjonstype {
     'OPPRETTET' = Revisjonstype.OPPRETTET,
     'ENDRET' = Revisjonstype.ENDRET,
     'SLETTET' = Revisjonstype.SLETTET,
     'UKJENT' = Revisjonstype.UKJENT,
-    'SØKNAD_INNSENDT' = 'SØKNAD_INNSENDT',
+    'SØKNAD_INNSENDT' = RevisjonstypeSøknadInnsendt.SØKNAD_INNSENDT,
 }
