badb2897e5893b3ef3a66933e99e8a4240b9cb4e
frodehansen2 <frode.hansen@bekk.no>
2025-08-25 11:22:09 +0200

    Fjerne tekst om arbeidsgiver på kvitteringsside

--- apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/pages/kvittering/KvitteringPage.tsx
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/pages/kvittering/KvitteringPage.tsx
@@ -1,6 +1,4 @@
-import { Alert, Button, VStack } from '@navikt/ds-react';
-import { Søker } from '@navikt/sif-common-api';
-import Block from '@navikt/sif-common-core-ds/src/atoms/block/Block';
+import { VStack } from '@navikt/ds-react';
 import Infolist from '@navikt/sif-common-core-ds/src/components/lists/infolist/Infolist';
 import Page from '@navikt/sif-common-core-ds/src/components/page/Page';
 import { useEffectOnce } from '@navikt/sif-common-hooks';
@@ -8,15 +6,9 @@ import { Kvittering } from '@navikt/sif-common-soknad-ds/src';
 import { AppText, useAppIntl } from '../../i18n';
 import actionsCreator from '../../søknad/context/action/actionCreator';
 import { useSøknadContext } from '../../søknad/context/hooks/useSøknadContext';
-import { ArbeidsgiverDetaljer } from '../../types/søknadApiData/SøknadApiData';
-import TilArbeidsgiverDokumentListe from './components/TilArbeidsgiverDokumentListe';
 import './kvitteringPage.css';
 
-interface Props {
-    søker: Søker | undefined;
-    kvitteringInfo?: ArbeidsgiverDetaljer[];
-}
-const KvitteringPage = ({ søker, kvitteringInfo }: Props) => {
+const KvitteringPage = () => {
     const { text } = useAppIntl();
     const { dispatch } = useSøknadContext();
 
@@ -27,46 +19,16 @@ const KvitteringPage = ({ søker, kvitteringInfo }: Props) => {
     return (
         <Page title={text('page.confirmation.sidetittel')}>
             <Kvittering tittel={text('page.confirmation.tittel')}>
-                <Alert variant="warning">
-                    <AppText id="page.conformation.alert.infoForsvinner" />
-                    <Block margin="l">
-                        <AppText id="page.conformation.alert.infoPrint" />
-                    </Block>
-                </Alert>
                 <VStack gap="8">
                     <Infolist heading={text('page.confirmation.undertittel')}>
                         <li>
                             <AppText id="page.conformation.alert.info.1" />
                         </li>
                         <li>
-                            <AppText id="page.conformation.alert.info.2" />
-                        </li>
-                        <li>
                             <AppText id="page.conformation.alert.info.3" />
                         </li>
-                        <li>
-                            <AppText id="page.conformation.alert.info.4" />
-                        </li>
                     </Infolist>
-
-                    <VStack align="center" className="kvittering-print-button">
-                        <Button
-                            type="button"
-                            variant="secondary"
-                            onClick={(): boolean => {
-                                window.print();
-                                return false;
-                            }}>
-                            <AppText id="page.conformation.skrivUtKnapp" />
-                        </Button>
-                    </VStack>
-
-                    <AppText id="page.conformation.skrivUt.info" />
                 </VStack>
-
-                {søker && kvitteringInfo && (
-                    <TilArbeidsgiverDokumentListe søker={søker} arbeidsgivere={kvitteringInfo} />
-                )}
             </Kvittering>
         </Page>
     );
--- apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/pages/kvittering/i18n/nn.ts
+++ apps/omsorgspengerutbetaling-arbeidstaker-soknad/src/app/pages/kvittering/i18n/nn.ts
@@ -4,35 +4,7 @@ export const kvitteringMessages_nn: Record<keyof typeof kvitteringMessages_nb, s
     'page.confirmation.sidetittel': 'Me har mottatt søknaden din',
     'page.confirmation.tittel': 'Me har mottatt søknad om utbetaling av omsorgspengar',
     'page.confirmation.undertittel': 'Kva skjer vidare no?',
-    'page.conformation.alert.infoForsvinner':
-        'Obs! Denne informasjonen forsvinn når du lukkar den. Det er derfor viktig at du les gjennom før du går vidare.',
-    'page.conformation.alert.infoPrint':
-        'Du kan skrive ut denne informasjonssida og gje utskrifta til arbeidsgjevaren din.',
     'page.conformation.alert.info.1':
         'Dette er ei stadfesting på at me har mottatt søknaden din. Når søknaden din er handsama, får du eit svar frå oss.',
-    'page.conformation.alert.info.2':
-        'Du må be arbeidsgjevar om å sende inntektsmelding til oss. Det er viktig at arbeidsgjevar kryssar av for at inntektsmeldinga gjeld omsorgspengar.',
     'page.conformation.alert.info.3': 'Me kontaktar deg dersom me treng fleire opplysningar.',
-    'page.conformation.alert.info.4':
-        'Du kan skrive ut denne informasjonssida og gje utskrifta til arbeidsgjevaren din. Dersom du er registrert med fleire arbeidsgjevarar, får du ein utskrift til kvar av dei.',
-    'page.conformation.skrivUtKnapp': 'Skriv ut denne sida no',
-    'page.conformation.skrivUt.info':
-        'Dersom du ikkje kan skrive ut denne informasjonssida, kan du ta bilete av den. Hugs også å ta bilete av informasjonen som kjem under, som du kan gje til arbeidsgjevar.',
-    'page.conformation.tilArbeidsgiverDokument.tittel': 'Nav har mottatt følgjande opplysningar:',
-    'page.conformation.tilArbeidsgiverDokument.1': '{søkersNavn} er tilsett hos {arbeidsgiversNavn}',
-    'page.conformation.tilArbeidsgiverDokument.2': '{søkersNavn} søkjer om {søknadNavn} for periodane:',
-    'page.conformation.tilArbeidsgiverDokument.alert.1':
-        'For at arbeidstakar skal få raskt svar på søknaden sin, ber me om at inntektsmeldinga blir sendt til oss så snart som mogleg.',
-    'page.conformation.tilArbeidsgiverDokument.alert.2':
-        'Det er viktig at du kryssar av for at inntektsmeldinga gjeld {søknadNavn}.',
-    'page.conformation.tilArbeidsgiverDokument.alert.3':
-        'Dersom inntektsmeldinga allereie er sendt, kan du sjå bort frå denne meldinga.',
-    'page.conformation.tilArbeidsgiverDokument.info.tittel': 'Slik sender du inntektsmeldinga',
-    'page.conformation.tilArbeidsgiverDokument.info.1':
-        'Inntektsmeldinga blir sendt frå arbeidsgjevar sitt eige løns- og personalsystem eller frå altinn.no. Meldinga inneheld inntektsopplysningar og anna informasjon Nav må ha for å handsame søknaden arbeidstakar har sendt. Hugs å velje riktig inntektsmelding.',
-    'page.conformation.tilArbeidsgiverDokument.info.2':
-        'Fyll inn alle dagar og/eller periodar som samsvarar med arbeidstakar sin søknad. Dersom søknaden ikkje stemmer med kva de har avtalt, må de avklare dette dykk imellom før du sender inntektsmeldinga.',
-    'page.conformation.tilArbeidsgiverDokument.info.3': 'Du finn meir informasjon om inntektsmeldinga på',
-
-    'page.conformation.tilArbeidsgiverDokument.info.4.lenkeTekst': 'nav.no/inntektsmeldingen',
 };
