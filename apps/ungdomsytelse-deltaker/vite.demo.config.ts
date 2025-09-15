6626934e45df3ce78b7ce879d101c22ebc117efb
frodehansen2 <frode.hansen@bekk.no>
2025-09-15 10:09:27 +0200

    msw update

--- apps/ungdomsytelse-deltaker/vite.demo.config.ts
+++ apps/ungdomsytelse-deltaker/vite.demo.config.ts
@@ -8,5 +8,27 @@ export default defineConfig({
     plugins: mockPlugins,
     define: commonDefines.demo,
     ...mockBaseConfig,
+    // Overskriv base kun for demo-build (gh-pages)
     base: '/sif-brukerdialog/ungdomsytelse-deltaker/',
+    // Overskriv MSW-proxy kun for demo-build
+    server: {
+        ...mockBaseConfig.server,
+        proxy: {
+            ...mockBaseConfig.server.proxy,
+            '/mockServiceWorker.js': {
+                target: 'http://localhost:8080',
+                rewrite: () => '/sif-brukerdialog/ungdomsytelse-deltaker/mockServiceWorker.js',
+            },
+        },
+    },
+    preview: {
+        ...mockBaseConfig.preview,
+        proxy: {
+            ...mockBaseConfig.preview.proxy,
+            '/mockServiceWorker.js': {
+                target: 'http://localhost:8080',
+                rewrite: () => '/sif-brukerdialog/ungdomsytelse-deltaker/mockServiceWorker.js',
+            },
+        },
+    },
 });
