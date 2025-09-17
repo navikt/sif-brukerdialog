5fa4e1847e143660f8f44fbb0e5a2e72daf61ec3
frodehansen2 <frode.hansen@bekk.no>
2025-09-17 10:07:48 +0200

    Update

--- packages/sif-common-ui/src/components/summary-answers/TextareaSvar.tsx
+++ packages/sif-common-ui/src/components/summary-answers/TextareaSvar.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const TextareaSvar = ({ text, spacing = true }: Props) => {
     if (text && text.trim().length > 0) {
-        const cleanedText = DOMPurify.sanitize(text);
+        const cleanedText = DOMPurify.sanitize(text, { USE_PROFILES: { html: true } });
         return (
             <div style={spacing ? { marginTop: '0.5rem', marginBottom: '1rem', whiteSpace: 'pre-wrap' } : undefined}>
                 {cleanedText}
