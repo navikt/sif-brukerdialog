55c918df0ead0823eb942b0562832d12b32c4377
frodehansen2 <frode.hansen@bekk.no>
2025-09-18 10:00:33 +0200

    Oppgavetittel [gh-pages]

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/i18n/messages/nb.ts
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/i18n/messages/nb.ts
@@ -37,9 +37,8 @@ export const innsynMessages_nb = {
     'oppgavetype.BEKREFT_ENDRET_SLUTTDATO.tilbakemeldingFritekstLabel': 'Tilbakemelding',
     'oppgavetype.BEKREFT_ENDRET_SLUTTDATO.kvitteringTekst': 'Du vil om kort tid motta et oppdatert vedtak.',
 
-    'oppgavetype.BEKREFT_AVVIK_REGISTERINNTEKT.sidetittel':
-        'Lønn fra {antallArbeidsgivere, plural, one {arbeidsgiver} other {arbeidsgivere}} utbetalt {måned}',
-    'oppgavetype.BEKREFT_AVVIK_REGISTERINNTEKT.oppgavetittel': 'Se og gi tilbakemelding på lønn for i {måned}',
+    'oppgavetype.BEKREFT_AVVIK_REGISTERINNTEKT.sidetittel': 'Tilbakemelding på lønn {måned}',
+    'oppgavetype.BEKREFT_AVVIK_REGISTERINNTEKT.oppgavetittel': 'Se og gi tilbakemelding på lønn i {måned}',
     'oppgavetype.BEKREFT_AVVIK_REGISTERINNTEKT.info':
         'Sjekk at opplysningene våre om lønnen du fikk i {måned} er riktige. Send oss en tilbakemelding når du har sjekket.',
     'oppgavetype.BEKREFT_AVVIK_REGISTERINNTEKT.harTilbakemeldingSpørsmål': 'Har du en tilbakemelding?',
