5cbe55edd4767951137b78f4995e32622400d688
frodehansen2 <frode.hansen@bekk.no>
2025-09-21 10:19:32 +0200

    ret

--- apps/ungdomsytelse-deltaker/playwright/utils/testAccessibility.ts
+++ apps/ungdomsytelse-deltaker/playwright/utils/testAccessibility.ts
@@ -1,4 +1,5 @@
 import AxeBuilder from '@axe-core/playwright';
+import { expect } from '@playwright/test';
 
 export const testAccessibility = async (page) => {
     const accessibilityScanResults = await new AxeBuilder({ page }).disableRules('color-contrast').analyze();
