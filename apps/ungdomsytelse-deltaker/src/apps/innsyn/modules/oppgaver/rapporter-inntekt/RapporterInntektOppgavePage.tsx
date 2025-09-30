fc6a5a8729548f52c2fe7dcf383877d434186142
frodehansen2 <frode.hansen@bekk.no>
2025-09-30 19:26:47 +0200

    Fjerne midlertidig testplassering

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/modules/oppgaver/rapporter-inntekt/RapporterInntektOppgavePage.tsx
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/modules/oppgaver/rapporter-inntekt/RapporterInntektOppgavePage.tsx
@@ -7,7 +7,6 @@ import { RapporterInntektOppgave } from '@shared/types/Oppgave';
 import { useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
 
-import Skyra from '../../../../../components/skyra/Skyra';
 import RapporterInntektOppgavePart from './parts/RapporterInntektOppgavePart';
 
 export interface RapporterInntektOppgaveProps {
@@ -39,9 +38,6 @@ const RapporterInntektOppgavePage = (props: RapporterInntektOppgaveProps) => {
     return (
         <DefaultPageLayout documentTitle={getOppgaveDokumentTittel(props.oppgave, appIntl)}>
             <RapporterInntektOppgavePart {...props} />
-            <div>
-                <Skyra slug="arbeids-og-velferdsetaten-nav/ungdomsprorgramytelsen-rapportere-inntekt" />
-            </div>
         </DefaultPageLayout>
     );
 };
