1a0675bb86edbd628437e251957d084389a54b1c
frodehansen2 <frode.hansen@bekk.no>
2025-10-02 11:10:44 +0200

    Utvide Skyra test med enum. + Riktig slug i prod

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/pages/SkyraTestPage.tsx
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/pages/SkyraTestPage.tsx
@@ -1,6 +1,6 @@
 import { Alert, BodyShort, Heading, VStack } from '@navikt/ds-react';
 
-import Skyra from '../../../components/skyra/Skyra';
+import Skyra, { Slug } from '../../../components/skyra/Skyra';
 import { useSkyraReloader } from '../hooks/useSkyraReloader';
 import DefaultPageLayout from './layout/DefaultPageLayout';
 
@@ -8,28 +8,30 @@ const SkyraTestPage = () => {
     useSkyraReloader();
     return (
         <DefaultPageLayout documentTitle="Skyra testside">
-            <VStack gap="4">
-                <Heading size="large" level="1">
-                    Skyra testside
-                </Heading>
-                <Alert variant="error">Side som tester om skyra-undersøkelse vises</Alert>
-            </VStack>
-
             <VStack gap="10">
-                <div>
-                    <Heading size="medium" level="2">
-                        Test
+                <VStack gap="4">
+                    <Heading size="large" level="1">
+                        Skyra testside
                     </Heading>
-                    <BodyShort>Slug: arbeids-og-velferdsetaten-nav/test-rapportering-av-inntekt</BodyShort>
-                    <Skyra slug="arbeids-og-velferdsetaten-nav/test-rapportering-av-inntekt" />
-                </div>
-                <div>
-                    <Heading size="medium" level="2">
-                        Prod
-                    </Heading>
-                    <BodyShort>Slug: arbeids-og-velferdsetaten-nav/ungdomsprorgramytelsen-rapportere-inntekt</BodyShort>
-                    <Skyra slug="arbeids-og-velferdsetaten-nav/ungdomsprorgramytelsen-rapportere-inntekt" />
-                </div>
+                    <Alert variant="error">Side som tester om skyra-undersøkelse vises</Alert>
+                </VStack>
+
+                <VStack gap="10">
+                    <div>
+                        <Heading size="medium" level="2">
+                            Test
+                        </Heading>
+                        <BodyShort>Slug: {Slug.test_rapporterInntekt}</BodyShort>
+                        <Skyra slug={Slug.test_rapporterInntekt} />
+                    </div>
+                    <div>
+                        <Heading size="medium" level="2">
+                            Prod
+                        </Heading>
+                        <BodyShort>Slug: {Slug.prod_rapporterInntekt}</BodyShort>
+                        <Skyra slug={Slug.prod_rapporterInntekt} />
+                    </div>
+                </VStack>
             </VStack>
         </DefaultPageLayout>
     );
