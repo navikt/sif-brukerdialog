5e54769ca5214e5bf226cc0d755d8c942d4d8fdd
frodehansen2 <frode.hansen@bekk.no>
2025-10-06 14:58:01 +0200

    retry

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/ux-signals/UXRapportertInntekt.tsx
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/ux-signals/UXRapportertInntekt.tsx
@@ -1,6 +1,9 @@
 import { UxSignalsPanel } from '@navikt/sif-common-core-ds';
+import useUxSignals from '@navikt/sif-common-core-ds/src/hooks/useUxSignals';
 
 const UXRapportertInntekt = () => {
+    useUxSignals(true);
+
     return <UxSignalsPanel panelId="1bmhfberhs" mode="demo" />;
 };
 
