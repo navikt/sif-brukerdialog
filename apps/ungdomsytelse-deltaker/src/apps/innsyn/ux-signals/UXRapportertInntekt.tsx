b7997343449e298b94c4097f24d5ea6c05d5578d
frodehansen2 <frode.hansen@bekk.no>
2025-10-07 14:12:49 +0200

    Droppe demo mode på uxsignals

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/ux-signals/UXRapportertInntekt.tsx
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/ux-signals/UXRapportertInntekt.tsx
@@ -3,7 +3,7 @@ import { useUxSignalsLoader, UxSignalsPanel } from '@navikt/sif-common-core-ds';
 const UXRapportertInntekt = () => {
     useUxSignalsLoader(true);
 
-    return <UxSignalsPanel panelId="1bmhfberhs" mode="demo" />;
+    return <UxSignalsPanel panelId="1bmhfberhs" />;
 };
 
 export default UXRapportertInntekt;
