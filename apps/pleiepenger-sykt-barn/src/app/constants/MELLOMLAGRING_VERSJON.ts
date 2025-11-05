bf9033e74d5c7867e615abfa97521f2f0c9b89a3
frodehansen2 <frode.hansen@bekk.no>
2025-11-05 15:35:41 +0100

    Justering etter CR

--- apps/pleiepenger-sykt-barn/src/app/constants/MELLOMLAGRING_VERSJON.ts
+++ apps/pleiepenger-sykt-barn/src/app/constants/MELLOMLAGRING_VERSJON.ts
@@ -1,5 +1,2 @@
 /** Normalarbeidstid for frilansere med oppdrag, men som ikke er frilansere */
 export const MELLOMLAGRING_VERSJON = '14.4.0';
-
-/** Ta i bruk velg barn input komponent */
-// export const MELLOMLAGRING_VERSJON = '14.3.0';
