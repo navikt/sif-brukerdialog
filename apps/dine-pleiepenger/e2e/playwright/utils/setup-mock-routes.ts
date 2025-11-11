d38b8828d60361c766e9aa16372e87daae11735a
frodehansen2 <frode.hansen@bekk.no>
2025-11-11 11:45:04 +0100

    Mer mock for tester

--- apps/dine-pleiepenger/e2e/playwright/utils/setup-mock-routes.ts
+++ apps/dine-pleiepenger/e2e/playwright/utils/setup-mock-routes.ts
@@ -1,7 +1,21 @@
 import { Page } from '@playwright/test';
+import dayjs from 'dayjs';
 
+import { Innsynsdata } from '../../../src/types/InnsynData';
+import { sakerMetadata, sakerMock } from '../mockdata/saker.mock';
+import { søkerMockData } from '../mockdata/søker.mock';
 // import { søknaderMockData } from '../mockdata/søknader.mock';
 import { setConsentCookie } from './setup-test-context';
+
+const sak = sakerMock[0].sak;
+
+const getSakResponseMedUtledetStatus = (status: any) => ({
+    sak: {
+        ...sak,
+        utledetStatus: { ...sak.utledetStatus, ...status },
+    },
+    inntektsmeldinger: [],
+});
 
 export const setupMockRoutes = async (page: Page) => {
     // Sett consent-cookie for å unngå samtykke-banner
@@ -9,5 +23,20 @@ export const setupMockRoutes = async (page: Page) => {
 
     await page.route('*', async (route: any) => {
         await route.fulfill({ status: 200 });
+    });
+
+    await page.route('**/innsynsdata', async (route: any) => {
+        const response: Innsynsdata = {
+            saker: [],
+            sakerMetadata: sakerMetadata,
+            harSak: true,
+            søker: søkerMockData as any,
+        };
+        await route.fulfill({ status: 200, body: JSON.stringify(response) });
+    });
+
+    await page.route('**/api/sak/*', async (route) => {
+        const response = getSakResponseMedUtledetStatus({ saksbehandlingsFrist: dayjs().add(1, 'day').toDate() });
+        await route.fulfill({ status: 200, body: JSON.stringify(response) });
     });
 };
