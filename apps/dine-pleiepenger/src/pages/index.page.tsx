a18dce62daec01c9ff0770114a35edbf96c13fe3
frodehansen2 <frode.hansen@bekk.no>
2025-11-19 09:58:07 +0100

    Fix 2

--- apps/dine-pleiepenger/src/pages/index.page.tsx
+++ apps/dine-pleiepenger/src/pages/index.page.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
+import { withAuthenticatedPage } from '../auth/withAuthentication';
 import PageLoading from '../components/page-layout/page-loading/PageLoading';
 import SøknaderEllerIngenSakFalback from '../components/søknader-eller-ingen-sak-fallback/SøknaderEllerIngenSakFalback';
 import VelgSakPage from '../components/velg-sak-page/VelgSakPage';
@@ -29,6 +30,6 @@ function DinePleiepengerPage() {
     return <SøknaderEllerIngenSakFalback />;
 }
 
-// export const getServerSideProps = withAuthenticatedPage();
+export const getServerSideProps = withAuthenticatedPage();
 
 export default DinePleiepengerPage;
