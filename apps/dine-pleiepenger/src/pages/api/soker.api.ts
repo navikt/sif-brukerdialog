c431bc22171fd4ec9d4760f414df57f2ce74a216
frodehansen2 <frode.hansen@bekk.no>
2025-11-19 14:45:54 +0100

    Legge til prepApiError - etter cr

--- apps/dine-pleiepenger/src/pages/api/soker.api.ts
+++ apps/dine-pleiepenger/src/pages/api/soker.api.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { withAuthenticatedApi } from '../../auth/withAuthentication';
 import { fetchSøker } from '../../server/fetchers/fetchSøker';
+import { prepApiError } from '../../utils/apiUtils';
 import { getLogger } from '../../utils/getLogCorrelationID';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -9,7 +10,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
         const unparsed = req.query.unparsed === 'true';
         res.send(await fetchSøker(req, unparsed));
     } catch (err) {
-        getLogger(req).error(`Hent søker feilet: ${err}`);
+        getLogger(req).error(`Hent søker feilet`, prepApiError(err));
         res.status(500).json({ error: 'Kunne ikke hente søker' });
     }
 }
