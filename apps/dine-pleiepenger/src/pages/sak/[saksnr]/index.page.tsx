634d709121b6f366d14302a35e19be215b16f1f4
frodehansen2 <frode.hansen@bekk.no>
2025-11-19 10:51:10 +0100

    Redirect hvis saksnr ikke er i metadata

--- apps/dine-pleiepenger/src/pages/sak/[saksnr]/index.page.tsx
+++ apps/dine-pleiepenger/src/pages/sak/[saksnr]/index.page.tsx
@@ -1,13 +1,26 @@
+import { useRouter } from 'next/router';
+
 import { withAuthenticatedPage } from '../../../auth/withAuthentication';
+import PageLoading from '../../../components/page-layout/page-loading/PageLoading';
 import SakIkkeFunnetPage from '../../../components/sak-pages/SakIkkeFunnetPage';
 import SakPage from '../../../components/sak-pages/SakPage';
+import { useInnsynsdataContext } from '../../../hooks/useInnsynsdataContext';
 import { usePleietrengendeMedSakFromRoute } from '../../../hooks/usePleietrengendeMedSakFromRoute';
 
 export default function SakRoutePage() {
+    const router = useRouter();
     const { pleietrengendeMedSak, saksnr, isLoading, error } = usePleietrengendeMedSakFromRoute();
+    const {
+        innsynsdata: { sakerMetadata },
+    } = useInnsynsdataContext();
 
     if (!saksnr) {
         return <SakIkkeFunnetPage saksnr={saksnr} />;
+    }
+
+    if (sakerMetadata && !sakerMetadata.find((s) => s.saksnummer === saksnr)) {
+        router.push('/');
+        return <PageLoading />;
     }
 
     return (
