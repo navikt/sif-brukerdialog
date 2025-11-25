1dfc5aec522ae17b644db1c6be25424b1cd29465
frodehansen2 <frode.hansen@bekk.no>
2025-11-25 15:38:57 +0100

    Fjerne prometheus metrics - er ikke aktivt brukt

--- server-ungdomsytelse-veileder/src/utils/metrics.ts
+++
@@ -1,7 +0,0 @@
-import promClient from 'prom-client';
-
-// Samle default metrics (memory, CPU, event loop, etc.)
-promClient.collectDefaultMetrics();
-
-// Eksporter register
-export const register = promClient.register;
--- server/src/utils/actuators.ts
+++ server/src/utils/actuators.ts
@@ -1,5 +1,4 @@
 import { Express } from 'express';
-import { register } from './metrics.js';
 
 export function setupActuators(server: Express) {
     server.get(`/internal/health/isAlive`, (_request, response) => {
@@ -12,11 +11,5 @@ export function setupActuators(server: Express) {
         response.send({
             status: 'UP',
         });
-    });
-
-    // Prometheus metrics endpoint
-    server.get('/metrics', async (_request, response) => {
-        response.set('Content-Type', register.contentType);
-        response.send(await register.metrics());
     });
 }
