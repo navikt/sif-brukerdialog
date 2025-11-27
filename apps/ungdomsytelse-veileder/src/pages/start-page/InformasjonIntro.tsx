f072228d547f8423452e4323b2f35ebb09951041
frodehansen2 <frode.hansen@bekk.no>
2025-11-27 11:59:33 +0100

    InfoCard

--- apps/ungdomsytelse-veileder/src/pages/start-page/InformasjonIntro.tsx
+++ apps/ungdomsytelse-veileder/src/pages/start-page/InformasjonIntro.tsx
@@ -1,25 +1,31 @@
 import { InformationSquareIcon } from '@navikt/aksel-icons';
-import { Alert, BodyLong } from '@navikt/ds-react';
+import { BodyLong, InfoCard } from '@navikt/ds-react';
 
 const InformasjonIntro = () => {
     return (
-        <Alert variant="info" className="w-full">
-            <BodyLong>
-                Du finner mer informasjon om denne løsningen, ungdomsprogrammet og ungdomsprogramytelsen ved å klikke på{' '}
-                <strong style={{ whiteSpace: 'nowrap' }}>
-                    <span>
-                        <InformationSquareIcon
-                            fontSize="1.6rem"
-                            display="inline"
-                            className="inline"
-                            aria-label="Informasjonikon"
-                        />
-                    </span>{' '}
-                    Hjelpeartikler
-                </strong>{' '}
-                i menyen oppe til høyre.
-            </BodyLong>
-        </Alert>
+        <InfoCard data-color="info">
+            <InfoCard.Header>
+                <InfoCard.Title>Hjelpeartikler</InfoCard.Title>
+            </InfoCard.Header>
+            <InfoCard.Content>
+                <BodyLong>
+                    Du finner mer informasjon om denne løsningen, ungdomsprogrammet og ungdomsprogramytelsen ved å
+                    klikke på{' '}
+                    <strong style={{ whiteSpace: 'nowrap' }}>
+                        <span>
+                            <InformationSquareIcon
+                                fontSize="1.6rem"
+                                display="inline"
+                                className="inline"
+                                aria-label="Informasjonikon"
+                            />
+                        </span>{' '}
+                        Hjelpeartikler
+                    </strong>{' '}
+                    i menyen oppe til høyre.
+                </BodyLong>
+            </InfoCard.Content>
+        </InfoCard>
     );
 };
 
