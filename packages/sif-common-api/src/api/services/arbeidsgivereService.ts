621b04c1d482461d7988058eb0e5ad41fa7a445f
frodehansen2 <frode.hansen@bekk.no>
2025-12-02 12:24:50 +0100

    Endre funksjonsinterface

--- packages/sif-common-api/src/api/services/arbeidsgivereService.ts
+++ packages/sif-common-api/src/api/services/arbeidsgivereService.ts
@@ -1,19 +1,18 @@
 import getSentryLoggerForApp from '@navikt/sif-common-sentry';
-import { dateToISODate } from '@navikt/sif-common-utils';
+import { DateRange, dateToISODate } from '@navikt/sif-common-utils';
 
 import { k9BrukerdialogApiClient } from '../apiClient';
 import { arbeidsgivereResponseSchema } from '../schemas/arbeidsgivereSchema';
 import { Arbeidsgivere } from '../types';
 
-export const fetchArbeidsgivere = async ({
-    from,
-    to,
-    frilansoppdrag,
-}: {
-    from: Date;
-    to: Date;
+interface FetchArbeidsgivereParams {
+    periode: DateRange;
     frilansoppdrag?: boolean;
-}): Promise<Arbeidsgivere> => {
+}
+export const fetchArbeidsgivere = async ({
+    periode: { from, to },
+    frilansoppdrag = true,
+}: FetchArbeidsgivereParams): Promise<Arbeidsgivere> => {
     const response = await k9BrukerdialogApiClient.get(`/oppslag/arbeidsgiver`, {
         params: { fra_og_med: dateToISODate(from), til_og_med: dateToISODate(to), frilansoppdrag },
     });
