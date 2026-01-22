737249ccfa58d40c1f8e91cfaa8003fc2dbe457d
frodehansen2 <frode.hansen@bekk.no>
2026-01-22 11:30:04 +0100

    Oppdatere etter aksel-8

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/modules/oppgaver/fjernet-periode/FjernetPeriodeOppgavePage.stories.tsx
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/modules/oppgaver/fjernet-periode/FjernetPeriodeOppgavePage.stories.tsx
@@ -40,14 +40,14 @@ const besvartOppgave: FjernetPeriodeOppgave = {
 export const OppgavePanel: Story = {
     name: 'Oppgavevisning på forside',
     render: () => (
-        <VStack gap="10">
-            <VStack gap="4">
+        <VStack gap="space-40">
+            <VStack gap="space-16">
                 <Heading level="2" size="medium">
                     Uløst oppgave
                 </Heading>
                 <OppgaverList oppgaver={[oppgave]} />
             </VStack>
-            <VStack gap="4">
+            <VStack gap="space-16">
                 <Heading level="2" size="medium">
                     Løste oppgaver
                 </Heading>
