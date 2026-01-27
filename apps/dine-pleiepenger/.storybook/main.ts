262d38d32d51203b069b868f14ef6fc857b3dfd7
frodehansen2 <frode.hansen@bekk.no>
2026-01-27 14:40:23 +0100

    Fix 2 - storybook

--- apps/dine-pleiepenger/.storybook/main.ts
+++ apps/dine-pleiepenger/.storybook/main.ts
@@ -24,12 +24,47 @@ const config = {
         options: {},
     },
 
+    env: (config) => ({
+        ...config,
+        NEXT_PUBLIC_BASE_PATH: '/innsyn',
+        NEXT_PUBLIC_LOGIN_URL: '/login',
+        NEXT_PUBLIC_RUNTIME_ENVIRONMENT: 'storybook',
+        NEXT_PUBLIC_API_URL_K9_SAK_INNSYN: 'http://localhost:1234',
+        NEXT_PUBLIC_API_URL_INNSYN: 'http://localhost:1234',
+        NEXT_PUBLIC_API_URL_BRUKERDIALOG: 'http://localhost:1234',
+        NEXT_PUBLIC_TELEMETRY_URL: '#',
+        NEXT_PUBLIC_ANALYTICS_KEY: 'default',
+        NEXT_PUBLIC_APPSTATUS_PROJECT_ID: 'ryujtq87',
+        NEXT_PUBLIC_APPSTATUS_DATASET: 'staging',
+        NEXT_PUBLIC_DINE_UTBETALINGER_URL: '#',
+        NEXT_PUBLIC_SKJEMA_ENDRINGSMELDING_URL: '#',
+        NEXT_PUBLIC_SKJEMA_ETTERSENDELSE_URL: '#',
+        NEXT_PUBLIC_SKJEMA_PLEIEPENGER_URL: '#',
+        NEXT_PUBLIC_ENRINGER_DU_MA_GI_BESKJED_OM_INFO_URL: '#',
+        NEXT_PUBLIC_KLAGE_INFO_URL: '#',
+        NEXT_PUBLIC_MIN_SIDE_URL: '#',
+        NEXT_PUBLIC_NAV_URL: '#',
+        NEXT_PUBLIC_INNBOKS_URL: '#',
+        NEXT_PUBLIC_PLEIEPENGER_INFO_URL: '#',
+        NEXT_PUBLIC_REGELVERK_INFO_URL: '#',
+        NEXT_PUBLIC_SAKSBEHANDLINGSTID_INFO_URL: '#',
+        NEXT_PUBLIC_BESKJED_URL: '#',
+        NEXT_PUBLIC_SKRIV_TIL_OSS_URL: '#',
+        NEXT_PUBLIC_SYKDOM_I_FAMILIEN_INFO_URL: '#',
+        NEXT_PUBLIC_ARBEIDSGIVER_PLEIEPENGER_URL: '#',
+        NEXT_PUBLIC_MINSIDE_DOKUMENTOVERSIKT_URL: '#',
+        NEXT_PUBLIC_UTBETALINGSOVERSIKT_URL: '#',
+        NEXT_PUBLIC_FEATURE_APPSTATUS: 'off',
+        NEXT_PUBLIC_FEATURE_FARO: 'off',
+        NEXT_PUBLIC_FEATURE_INNTEKTSMELDING: 'on',
+    }),
+
     webpackFinal: async (config) => {
         config.resolve = config.resolve || {};
         config.resolve.alias = {
             ...config.resolve.alias,
-            // Mock withAuthentication for å unngå Node.js-moduler fra @navikt/oasis
-            '../auth/withAuthentication': resolve(__dirname, '../src/storybook/mocks/withAuthentication.mock.ts'),
+            // Mock @navikt/oasis for å unngå Node.js-moduler (prom-client, cluster, v8, etc.)
+            '@navikt/oasis': resolve(__dirname, '../src/storybook/mocks/oasis.mock.ts'),
         };
         return config;
     },
