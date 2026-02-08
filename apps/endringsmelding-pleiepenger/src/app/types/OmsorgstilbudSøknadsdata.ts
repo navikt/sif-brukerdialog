9f9bd35a3e8553da3cd23544e6662f5460966bb4
frodehansen2 <frode.hansen@bekk.no>
2026-02-08 09:59:46 +0100

    stash

--- apps/endringsmelding-pleiepenger/src/app/types/OmsorgstilbudSøknadsdata.ts
+++ apps/endringsmelding-pleiepenger/src/app/types/OmsorgstilbudSøknadsdata.ts
@@ -1,5 +1,19 @@
-import { DateDurationMap } from '@navikt/sif-common-utils';
+import { DateDurationMap, ISODate } from '@navikt/sif-common-utils';
+import { Duration } from 'date-fns';
+
+export type Omsorgsdag = {
+    opprinneligTid?: Duration;
+    nyTid?: Duration;
+    erEndret: boolean;
+};
+
+export type OmsorgsdagerMap = {
+    [isoDate: ISODate]: Omsorgsdag;
+};
 
 export interface OmsorgstilbudSøknadsdata {
     enkeltdager: DateDurationMap;
+    // opprinneligEnkeltdager: DateDurationMap;
+    // /** Alle ukedager hvor en har søkt pleiepenger - innenfor perioden(e) en har søkt for */
+    // omsorgsdagerMap: OmsorgsdagerMap;
 }
