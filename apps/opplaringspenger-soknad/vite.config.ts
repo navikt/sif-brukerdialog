78407dcd831edf5e3c2740e753e265b9e090dbb0
frodehansen2 <frode.hansen@bekk.no>
2026-02-16 12:19:45 +0100

    Enable ux i prod

--- apps/opplaringspenger-soknad/vite.config.ts
+++ apps/opplaringspenger-soknad/vite.config.ts
@@ -33,6 +33,10 @@ export default defineConfig({
             },
         },
     },
+    define: {
+        'import.meta.env.ENABLED_UXSIGNALS': true,
+    },
+
     css: {
         preprocessorOptions: {
             scss: {
