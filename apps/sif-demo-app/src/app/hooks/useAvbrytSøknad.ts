fc472208fe1329948e192097585d2fcfe12ab9ce
frodehansen2 <frode.hansen@bekk.no>
2026-03-05 15:50:04 +0100

    fix

--- apps/sif-demo-app/src/app/hooks/useAvbrytSøknad.ts
+++ apps/sif-demo-app/src/app/hooks/useAvbrytSøknad.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { useStepFormValues } from '../../rammeverk/state/StepFormValuesContext';
 import { useSøknadMellomlagring } from './useSøknadMellomlagring';
 import { useSøknadStore } from './useSøknadStore';
 
@@ -8,10 +9,12 @@ export const useAvbrytSøknad = () => {
     const navigate = useNavigate();
     const resetSøknad = useSøknadStore((s) => s.resetSøknad);
     const { slettMellomlagring } = useSøknadMellomlagring();
+    const { clearAllStepFormValues } = useStepFormValues();
 
     return useCallback(() => {
         resetSøknad();
+        clearAllStepFormValues();
         slettMellomlagring().catch(() => {});
         navigate('/');
-    }, [resetSøknad, slettMellomlagring, navigate]);
+    }, [resetSøknad, clearAllStepFormValues, slettMellomlagring, navigate]);
 };
