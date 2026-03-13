c01b02cc9ceeba3964441ef6b7a3f3fb04255998
frodehansen2 <frode.hansen@bekk.no>
2026-03-13 12:43:31 +0100

    codegen prod

--- packages/k9-brukerdialog-prosessering-api/src/generated/omsorgspenger/zod.gen.ts
+++ packages/k9-brukerdialog-prosessering-api/src/generated/omsorgspenger/zod.gen.ts
@@ -148,7 +148,7 @@ export const zInnsendingOmsorgspengerKroniskSyktBarnSøknadData = z.object({
 export const zSlettVedleggData = z.object({
     body: z.never().optional(),
     path: z.object({
-        vedleggId: z.string(),
+        vedleggId: z.string().min(1),
     }),
     query: z.never().optional(),
 });
@@ -161,7 +161,7 @@ export const zSlettVedleggResponse = z.void();
 export const zHentVedleggData = z.object({
     body: z.never().optional(),
     path: z.object({
-        vedleggId: z.string(),
+        vedleggId: z.string().min(1),
     }),
     query: z.never().optional(),
 });
