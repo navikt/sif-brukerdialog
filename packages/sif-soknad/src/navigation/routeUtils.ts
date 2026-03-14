0e56dfc43276265a61bf93dde591cf78eb64a930
frodehansen2 <frode.hansen@bekk.no>
2026-03-14 11:36:02 +0100

    upd

--- packages/sif-soknad/src/navigation/routeUtils.ts
+++ packages/sif-soknad/src/navigation/routeUtils.ts
@@ -1,9 +1,25 @@
+const trimLeadingSlashes = (value: string): string => {
+    let index = 0;
+    while (index < value.length && value[index] === '/') {
+        index += 1;
+    }
+    return value.slice(index);
+};
+
+const trimTrailingSlashes = (value: string): string => {
+    let end = value.length;
+    while (end > 0 && value[end - 1] === '/') {
+        end -= 1;
+    }
+    return value.slice(0, end);
+};
+
 export const normalizeRouteSegment = (route: string): string => {
-    return route.replace(/^\/+|\/+$/g, '');
+    return trimTrailingSlashes(trimLeadingSlashes(route));
 };
 
 const normalizeBasePath = (basePath: string): string => {
-    const trimmed = basePath.replace(/\/+$/g, '');
+    const trimmed = trimTrailingSlashes(basePath);
     return trimmed.length > 0 ? trimmed : '/';
 };
 
