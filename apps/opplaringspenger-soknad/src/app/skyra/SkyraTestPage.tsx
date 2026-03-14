b82dc7cc55dc0f306dedcc88f71e269df7d4d798
frodehansen2 <frode.hansen@bekk.no>
2026-03-14 18:30:42 +0100

    debugside

--- apps/opplaringspenger-soknad/src/app/skyra/SkyraTestPage.tsx
+++ apps/opplaringspenger-soknad/src/app/skyra/SkyraTestPage.tsx
@@ -1,10 +1,12 @@
-import { Alert, BodyShort, Heading, VStack } from '@navikt/ds-react';
+import { Alert, BodyShort, Button, Heading, VStack } from '@navikt/ds-react';
 import Page from '@navikt/sif-common-core-ds/src/components/page/Page';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import Skyra, { Slug } from './Skyra';
 import { useSkyraReloader } from './useSkyraReloader';
 
-const SkyraTestPage = () => {
+const Page2 = () => {
+    const navigate = useNavigate();
     useSkyraReloader();
     return (
         <Page title="Skyra testside">
@@ -14,8 +16,10 @@ const SkyraTestPage = () => {
                         Skyra testside
                     </Heading>
                     <Alert variant="error">Side som tester om skyra-undersøkelse vises</Alert>
+                    <Button type="button" variant="primary" onClick={() => navigate('/soknad/skyra/test/page1')}>
+                        Gå til side 1
+                    </Button>
                 </VStack>
-
                 <VStack gap="space-40">
                     <div>
                         <Heading size="medium" level="2">
@@ -27,6 +31,35 @@ const SkyraTestPage = () => {
                 </VStack>
             </VStack>
         </Page>
+    );
+};
+const Page1 = () => {
+    const navigate = useNavigate();
+    return (
+        <Page title="Skyra testside">
+            <VStack gap="space-40">
+                <VStack gap="space-16">
+                    <Heading size="large" level="1">
+                        Skyra testside - uten reloader
+                    </Heading>
+                    <Alert variant="error">Side som tester om skyra-undersøkelse vises</Alert>
+                </VStack>
+
+                <Button type="button" variant="primary" onClick={() => navigate('/soknad/skyra/test/page2')}>
+                    Gå til side 2
+                </Button>
+            </VStack>
+        </Page>
+    );
+};
+
+const SkyraTestPage = () => {
+    return (
+        <Routes>
+            <Route path="/skyra/test" element={<Page1 />} />
+            <Route path="/skyra/test/page1" element={<Page1 />} />
+            <Route path="/skyra/test/page2" element={<Page2 />} />
+        </Routes>
     );
 };
 
