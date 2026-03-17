04d349406d6e61a0a3cfb59c641b3cf92bea5ebd
frodehansen2 <frode.hansen@bekk.no>
2026-03-17 11:27:14 +0100

    Init storybook

--- apps/sif-demo-app/eslint.config.js
+++ apps/sif-demo-app/eslint.config.js
@@ -1,4 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import commonConfig from '@sif/eslint-config';
+import storybook from 'eslint-plugin-storybook';
 
 export default [
     ...commonConfig,
@@ -9,4 +11,5 @@ export default [
             },
         },
     },
+    ...storybook.configs['flat/recommended'],
 ];
