840ce3ed7203ff43cf0b2f04bc9575d7e69645a1
frodehansen2 <frode.hansen@bekk.no>
2026-03-19 14:35:24 +0100

    .

--- apps/aktivitetspenger-soknad-v2/src/app/setup/hooks/useSøknadStore.ts
+++ apps/aktivitetspenger-soknad-v2/src/app/setup/hooks/useSøknadStore.ts
@@ -1,10 +1,10 @@
-import { createSøknadStore as store } from '@sif/soknad/store';
+import { createSøknadStore } from '@sif/soknad/store';
 
 import { Søknadsdata } from '../../types/Søknadsdata';
 import { SøknadState } from '../../types/SøknadState';
 import { søknadStepConfig, søknadStepOrder } from '../config/søknadStepConfig';
 
-export const useSøknadStore = store<SøknadState, Søknadsdata>({
+export const useSøknadStore = createSøknadStore<SøknadState, Søknadsdata>({
     stepOrder: søknadStepOrder,
     stepConfig: søknadStepConfig,
 });
