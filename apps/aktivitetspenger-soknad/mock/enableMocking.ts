c600f5f1b39d184e7128128b18ea6c3ef8718cbf
frodehansen2 <frode.hansen@bekk.no>
2026-03-20 15:21:37 +0100

    fix mocking

--- apps/aktivitetspenger-soknad/mock/enableMocking.ts
+++ apps/aktivitetspenger-soknad/mock/enableMocking.ts
@@ -1,7 +1,25 @@
+import { getMaybeEnv, getRequiredEnv } from '@navikt/sif-common-env';
+
 export async function enableMocking() {
+    const ENV = getMaybeEnv('ENV');
+
+    if (ENV !== 'development' || import.meta.env.MODE !== 'msw') {
+        return;
+    }
+
     const { worker } = await import('./msw/browser');
 
-    return worker.start({
-        onUnhandledRequest: 'warn',
-    });
+    if (document.location.pathname === '/') {
+        document.location.replace(getRequiredEnv('PUBLIC_PATH'));
+    }
+
+    try {
+        return worker.start({
+            onUnhandledRequest: 'warn',
+        });
+    } catch {
+        // do nothing
+    }
+
+    return worker.start();
 }
