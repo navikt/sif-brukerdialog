87867dc1b2986f9b951e5c1a29ccff25fb5a604a
frodehansen2 <frode.hansen@bekk.no>
2026-03-23 11:45:46 +0100

    Mer fix

--- packages/ung-brukerdialog-api/src/ung-brukerdialog-api.ts
+++ packages/ung-brukerdialog-api/src/ung-brukerdialog-api.ts
@@ -1,5 +1,3 @@
 export * from './ung-brukerdialog-api/client';
 export * from './ung-brukerdialog-api/client/client.gen';
-export * from './ung-brukerdialog-api/client/sdk.gen';
-export * from './ung-brukerdialog-api/client/types.gen';
 export * from './ung-brukerdialog-api/client/zod.gen';
