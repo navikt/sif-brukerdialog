d7a6c6de59d9a752a68eaa771f0fc764d3887336
frodehansen2 <frode.hansen@bekk.no>
2026-03-23 08:15:24 +0100

    Pakkeoppdateringer + vite.config rollup endringer (#4223)
    * Pakkeoppdatering - minor/patch
    * react-intl 10
    * vite 8

--- apps/endringsmelding-pleiepenger/vite.config.ts
+++ apps/endringsmelding-pleiepenger/vite.config.ts
@@ -38,7 +38,18 @@ export default defineConfig({
         },
     },
     build: {
+        chunkSizeWarningLimit: 2000,
         sourcemap: true,
+        target: 'esnext',
+        codeSplitting: {
+            groups: [
+                { name: 'vendor', test: /\/node_modules\/(react|react-dom|react-intl)\//, priority: 5 },
+                { name: 'router', test: /\/node_modules\/react-router-dom\//, priority: 4 },
+                { name: 'forms', test: /\/node_modules\/formik\//, priority: 3 },
+                { name: 'utils', test: /\/node_modules\/(lodash|date-fns|dayjs|axios|uuid|zod)\//, priority: 2 },
+                { name: 'navikt', test: /\/node_modules\/@navikt\/(ds-react|aksel-icons)\//, priority: 1 },
+            ],
+        },
     },
     css: {
         preprocessorOptions: {
