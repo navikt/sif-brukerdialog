64a4cbf02cace2f601edbab5b8d7c54041875e08
frodehansen2 <frode.hansen@bekk.no>
2026-03-24 10:42:04 +0100

    lint

--- apps/aktivitetspenger-soknad/src/app/steps/index.ts
+++ apps/aktivitetspenger-soknad/src/app/steps/index.ts
@@ -1,6 +1,6 @@
 export { BarnSteg } from './barn/BarnSteg';
-export { StartdatoOgAndreYtelserSteg } from './startdato-og-andre-ytelser/StartdatoOgAndreYtelserSteg';
 export { BostedSteg } from './bosted/BostedSteg';
 export { BostedUtlandSteg } from './bosted-utland/BostedUtlandSteg';
 export { KontonummerSteg } from './kontonummer/KontonummerSteg';
 export { OppsummeringSteg } from './oppsummering/OppsummeringSteg';
+export { StartdatoOgAndreYtelserSteg } from './startdato-og-andre-ytelser/StartdatoOgAndreYtelserSteg';
--- apps/aktivitetspenger-soknad/src/app/utils/formValuesToSoknadsdata.ts
+++ apps/aktivitetspenger-soknad/src/app/utils/formValuesToSoknadsdata.ts
@@ -3,14 +3,14 @@ import { StepFormValues, StepSøknadsdata } from '@sif/soknad/types';
 
 import { toBarnSøknadsdata } from '../steps/barn/barnStegUtils';
 import { BarnFormValues } from '../steps/barn/types';
-import { toStartdatoOgAndreYtelserSøknadsdata } from '../steps/startdato-og-andre-ytelser/startdatoOgAndreYtelserStegUtils';
-import { StartdatoOgAndreYtelserFormValues } from '../steps/startdato-og-andre-ytelser/types';
 import { toBostedSøknadsdata } from '../steps/bosted/bostedStegUtils';
 import { BostedFormValues } from '../steps/bosted/types';
 import { toBostedUtlandStegSøknadsdata } from '../steps/bosted-utland/bostedUtlandStegUtils';
 import { BostedUtlandFormValues } from '../steps/bosted-utland/types';
 import { toKontonummerSøknadsdata } from '../steps/kontonummer/kontonummerStegUtils';
 import { KontonummerFormValues } from '../steps/kontonummer/types';
+import { toStartdatoOgAndreYtelserSøknadsdata } from '../steps/startdato-og-andre-ytelser/startdatoOgAndreYtelserStegUtils';
+import { StartdatoOgAndreYtelserFormValues } from '../steps/startdato-og-andre-ytelser/types';
 
 export const formValuesToSøknadsdata = (stepId: string, formValues: StepFormValues): StepSøknadsdata | undefined => {
     switch (stepId) {
