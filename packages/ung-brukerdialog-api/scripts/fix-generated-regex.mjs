efdbbbe9ecc7e07756b3f6a4efee852a98ee7c4d
frodehansen2 <frode.hansen@bekk.no>
2026-03-24 18:36:27 +0100

    Tilbakestille ekstra replacement i url for ung-brukerdialog-api

--- packages/ung-brukerdialog-api/scripts/fix-generated-regex.mjs
+++ packages/ung-brukerdialog-api/scripts/fix-generated-regex.mjs
@@ -2,12 +2,4 @@
 
 import { fixAndFormatGeneratedCode } from '../../../codegenUtils.js';
 
-fixAndFormatGeneratedCode('src/ung-brukerdialog-api', {
-    patterns: {
-        prefixEksternApiUrls: {
-            pattern: /url:\s*'\/ekstern\/api/g,
-            replacement: "url: '/ung/brukerdialog/ekstern/api",
-            description: 'SDK urls prefixed with /ung/brukerdialog',
-        },
-    },
-});
+fixAndFormatGeneratedCode('src/ung-brukerdialog-api');
