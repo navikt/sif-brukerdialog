08b36118a33b597eb7f60f18346a39e6bd8aaf2c
frodehansen2 <frode.hansen@bekk.no>
2026-03-25 12:52:32 +0100

    Fjerne ref til ung-deltakelse-opplyser

--- apps/aktivitetspenger-innsyn/src/app/api/initApiClients.ts
+++ apps/aktivitetspenger-innsyn/src/app/api/initApiClients.ts
@@ -1,16 +1,9 @@
 import { initK9BrukerdialogProsesseringApiClients } from '@navikt/k9-brukerdialog-prosessering-api';
 import { EnvKey, getRequiredEnv } from '@navikt/sif-common-env';
 import { initUngBrukerdialogApiClient } from '@navikt/ung-brukerdialog-api';
-import { initUngDeltakelseOpplyserApiDeltakerClient } from '@navikt/ung-deltakelse-opplyser-api-deltaker';
 
 export const initApiClients = () => {
     initUngBrukerdialogApiClient({
-        onUnAuthorized: () => {
-            globalThis.location.reload();
-        },
-    });
-
-    initUngDeltakelseOpplyserApiDeltakerClient({
         onUnAuthorized: () => {
             globalThis.location.reload();
         },
