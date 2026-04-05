78c2e0bce12ba02b01a17dee264f760c3e76d394
frodehansen2 <frode.hansen@bekk.no>
2026-04-05 19:54:20 +0200

    Fix validering av skjulte felter

--- apps/omsorgspengesoknad-v2/src/app/steps/om-barnet/omBarnetStegUtils.ts
+++ apps/omsorgspengesoknad-v2/src/app/steps/om-barnet/omBarnetStegUtils.ts
@@ -2,6 +2,7 @@ import { BarnSammeAdresse } from '@app/types/BarnSammeAdresse';
 import { OmBarnetSøknadsdata } from '@app/types/Soknadsdata';
 import { RegistrertBarn } from '@sif/api/k9-prosessering';
 import { YesOrNo } from '@sif/rhf';
+import dayjs from 'dayjs';
 
 import { ANNET_BARN, OmBarnetFormFields, OmBarnetFormValues } from './types';
 
@@ -90,3 +91,22 @@ export const getOmBarnetDefaultValues = (): Partial<OmBarnetFormValues> => ({
     [OmBarnetFormFields.barnetsFødselsnummer]: '',
     [OmBarnetFormFields.barnetsNavn]: '',
 });
+
+export const getMinDatoForBarnetsFødselsdato = (): Date => {
+    // April 1 dette år
+    const today = dayjs();
+    const frist = dayjs(today).set('month', 3).set('date', 1);
+
+    return today.isBefore(frist)
+        ? today.subtract(19, 'year').startOf('year').toDate()
+        : today.subtract(18, 'year').startOf('year').toDate();
+};
+
+export const isBarnOver18år = (fødselsdato: Date | string): boolean => {
+    const dato18år = dayjs(fødselsdato).add(18, 'year');
+
+    // Siden det kan gis ekstra dager opp til 3 måneder tilbake i tid fra søknadsdato brukes det 1. april året etter det kalenderåret barnet fylte 18 år som frist.
+    const frist = dato18år.add(1, 'year').set('month', 3).set('date', 1);
+
+    return dayjs().isSame(frist) || dayjs().isAfter(frist);
+};
--- apps/omsorgspengesoknad/src/app/søknad/steps/om-barnet/spørsmål/AnnetBarnFødselsdatoSpørsmål.tsx
+++ apps/omsorgspengesoknad/src/app/søknad/steps/om-barnet/spørsmål/AnnetBarnFødselsdatoSpørsmål.tsx
@@ -1,5 +1,6 @@
-import { getDateValidator, ValidateDateError, ValidateDateErrorKeys } from '@navikt/sif-validation';
 import { dateFormatter, getDateToday } from '@navikt/sif-common-utils';
+import { getDateValidator, ValidateDateError, ValidateDateErrorKeys } from '@navikt/sif-validation';
+
 import { useAppIntl } from '../../../../i18n';
 import { omBarnetFormComponents } from '../omBarnetFormComponents';
 import { OmBarnetFormFields } from '../OmBarnetStep';
