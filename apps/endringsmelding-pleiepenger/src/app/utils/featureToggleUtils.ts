3fa361befbcbe4f5fd95d3c2219ed2f993160065
frodehansen2 <frode.hansen@bekk.no>
2026-04-08 09:32:16 +0200

    Featuretoggle - skjul tid i omsorgstilbud (#4282)

--- apps/endringsmelding-pleiepenger/src/app/utils/featureToggleUtils.ts
+++ apps/endringsmelding-pleiepenger/src/app/utils/featureToggleUtils.ts
@@ -2,6 +2,12 @@ import { getMaybeEnv } from '@navikt/sif-common-env';
 
 export enum Feature {
     SIF_PUBLIC_ENDRE_OMSORGSTILBUD = 'SIF_PUBLIC_ENDRE_OMSORGSTILBUD',
+
+    /** Skjul registrert tid i tilsynsordning.
+     * - Vi viser bare tid som innlogget bruker har sendt inn, ikke tid fra annen part.
+     * - Bruker skal alltid kunne sende inn ny tid, også når den er lik sist innsending.
+     */
+    SIF_PUBLIC_SKJUL_TID_I_OMSORGSTILBUD = 'SIF_PUBLIC_SKJUL_TID_I_OMSORGSTILBUD',
 }
 
 export const isFeatureEnabled = (feature: Feature) => {
