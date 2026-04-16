335e74da3f967b2c24ad3f4585e6b7b6ebc00921
frodehansen2 <frode.hansen@bekk.no>
2026-04-16 10:12:50 +0200

    Slette fil

--- apps/endringsmelding-pleiepenger/src/app/søknad/steps/tilsynsordning/i18n/nn.ts
+++
@@ -1,5 +0,0 @@
-import { tilsynsordningMessages_nb } from './nb';
-
-export const tilsynsordningMessages_nn: Record<keyof typeof tilsynsordningMessages_nb, string> = {
-    ...tilsynsordningMessages_nb,
-};
