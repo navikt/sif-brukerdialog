5bfe022e17a24be4af053bd7f75503fce069c9d7
frodehansen2 <frode.hansen@bekk.no>
2026-04-20 08:31:12 +0200

    Skyra-inline + testside

--- apps/endringsmelding-pleiepenger/src/app/pages/kvittering/KvitteringPage.tsx
+++ apps/endringsmelding-pleiepenger/src/app/pages/kvittering/KvitteringPage.tsx
@@ -1,9 +1,10 @@
 import { AppText, useAppIntl } from '@app/i18n';
-import { BodyShort, Link } from '@navikt/ds-react';
+import { BodyShort, Link, VStack } from '@navikt/ds-react';
 import Page from '@navikt/sif-common-core-ds/src/components/page/Page';
 import { Kvittering } from '@navikt/sif-common-soknad-ds';
 import { useEffect } from 'react';
 
+import Skyra, { Slug } from '../../skyra/Skyra';
 import { appEnv } from '../../utils/appEnv';
 
 interface Props {
@@ -19,16 +20,19 @@ const KvitteringPage = ({ onUnmount }: Props) => {
     });
     return (
         <Page title={text('kvitteringPage.pageTitle')}>
-            <Kvittering tittel={text('kvitteringPage.title')}>
-                <BodyShort>
-                    <AppText
-                        id="kvitteringPage.info.1"
-                        values={{
-                            Lenke: (children) => <Link href={appEnv.SIF_PUBLIC_INNSYN_URL}>{children}</Link>,
-                        }}
-                    />
-                </BodyShort>
-            </Kvittering>
+            <VStack gap="space-24">
+                <Kvittering tittel={text('kvitteringPage.title')}>
+                    <BodyShort>
+                        <AppText
+                            id="kvitteringPage.info.1"
+                            values={{
+                                Lenke: (children) => <Link href={appEnv.SIF_PUBLIC_INNSYN_URL}>{children}</Link>,
+                            }}
+                        />
+                    </BodyShort>
+                </Kvittering>
+                <Skyra slug={Slug.kvittering_inline} />
+            </VStack>
         </Page>
     );
 };
