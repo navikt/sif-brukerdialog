5f8867a002f23a7d9e99174a596aef4dbba4fc3f
frodehansen2 <frode.hansen@bekk.no>
2026-04-22 11:50:31 +0200

    Endre sentry-oppsett

--- apps/endringsmelding-pleiepenger/src/app/utils/appSentryLogger.ts
+++ apps/endringsmelding-pleiepenger/src/app/utils/appSentryLogger.ts
@@ -1,3 +1,21 @@
-import getSentryLoggerForApp from '@navikt/sif-common-sentry';
+import * as Sentry from '@sentry/react';
+import { AxiosError } from 'axios';
 
-export const appSentryLogger = getSentryLoggerForApp('endringsmelding-pleiepenger', ['endringsmelding-pleiepenger']);
+export const appSentryLogger = {
+    logInfo: (message: string, payload?: string) =>
+        Sentry.captureMessage(message, { level: 'info', extra: payload ? { info: payload } : undefined }),
+    logError: (message: string, payload?: string) =>
+        Sentry.captureMessage(message, { level: 'error', extra: payload ? { info: payload } : undefined }),
+    logApiError: (error: AxiosError, context?: string) => {
+        if (error.response?.status === 401 || error.response?.status === 0 || error.code === 'ERR_NETWORK') {
+            return;
+        }
+        Sentry.captureException(error, {
+            extra: {
+                context,
+                xRequestId: error.response?.headers?.['x-request-id'],
+                xCorrelationId: error.response?.headers?.['x-correlation-id'],
+            },
+        });
+    },
+};
