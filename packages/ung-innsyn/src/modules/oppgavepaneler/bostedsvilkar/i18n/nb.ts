f53b2350a0ebf3a48c0b10db030be4c62b8caac2
frodehansen2 <frode.hansen@bekk.no>
2026-04-23 12:47:47 +0200

    Oppdatere tekst [gh-pages]

--- packages/ung-innsyn/src/modules/oppgavepaneler/bostedsvilkar/i18n/nb.ts
+++ packages/ung-innsyn/src/modules/oppgavepaneler/bostedsvilkar/i18n/nb.ts
@@ -1,12 +1,13 @@
 export const bostedVilkårMessages_nb = {
-    '@ungInnsyn.bostedVilkårOppgave.tekst.1': 'Veilederen mener du ikke lenger bor i Trondheim.',
+    '@ungInnsyn.bostedVilkårOppgave.tekst.1':
+        'Det er registrert informasjon hos oss om at du ikke lenger bor i Trondheim.',
     '@ungInnsyn.bostedVilkårOppgave.tekst.2':
         'Du får denne meldingen slik at du kan komme med en tilbakemelding på dette.',
     '@ungInnsyn.bostedVilkårOppgave.tekst.3':
         'Ingen tilbakemelding? Kryss av på "Nei" med en gang og send inn svaret ditt. Jo fortere du svarer, jo fortere får vi behandlet saken din.',
     '@ungInnsyn.bostedVilkårOppgave.tekst.4':
-        'Har du en tilbakemelding? Ta kontakt med veilederen din først. Når dere har snakket sammen, sender du inn svaret ditt her.',
+        'Har du en tilbakemelding? Ta kontakt med [veileder|saksbehandler|nks] din først. Når dere har snakket sammen, sender du inn svaret ditt her.',
     '@ungInnsyn.bostedVilkårOppgave.tekst.5': 'Fristen for å svare er senest {formatertFrist}.',
     '@ungInnsyn.bostedVilkårOppgave.tekst.6': 'Hvis vi ikke hører fra deg innen svarfristen har gått ut, vil ...',
-    '@ungInnsyn.bostedVilkårOppgave.oppsummering': 'Veileder har registrert at du ikke bor i Trondheim lenger.',
+    '@ungInnsyn.bostedVilkårOppgave.oppsummering': 'Saksbehandler har registrert at du ikke bor i Trondheim lenger.',
 };
