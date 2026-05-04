a4ba3213d9d8aedb29234f2f8defa9f7312a7e2c
frodehansen2 <frode.hansen@bekk.no>
2026-05-04 07:52:28 +0200

    Korrigere tekster

--- apps/omsorgspengesoknad-v2/src/app/steps/om-barnet/TrengerIkkeSøkeForBarnAlert.tsx
+++ apps/omsorgspengesoknad-v2/src/app/steps/om-barnet/TrengerIkkeSøkeForBarnAlert.tsx
@@ -18,10 +18,17 @@ export const TrengerIkkeSøkeForBarnAlert = ({ barnetsFornavn }: Props) => {
                     {text('omBarnetSteg.alert.trengerIkkeSøke.tittel', { barnetsFornavn })}
                 </Heading>
                 <BodyLong>
-                    <AppText id="omBarnetSteg.alert.trengerIkkeSøke.tekst" values={{ barnetsFornavn }} />{' '}
-                    <Link href={lenker.navMinSide} target="_blank" rel="noopener noreferrer">
-                        <AppText id="omBarnetSteg.alert.trengerIkkeSøke.minsideLenke" />
-                    </Link>
+                    <AppText
+                        id="omBarnetSteg.alert.trengerIkkeSøke.tekst"
+                        values={{
+                            barnetsFornavn,
+                            Lenke: (chunks: React.ReactNode) => (
+                                <Link href={lenker.navMinSide} target="_blank" rel="noopener noreferrer">
+                                    {chunks}
+                                </Link>
+                            ),
+                        }}
+                    />
                 </BodyLong>
             </VStack>
         </SifInfoCard>
