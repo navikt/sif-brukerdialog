33672bc93b59b2ae8779a753bf0b4c2d0e8b6089
frodehansen2 <frode.hansen@bekk.no>
2026-05-12 12:52:24 +0200

    Cleanup scanning-guide

--- packages/sif-soknad-ui/src/components/picture-scanning-guide/PictureScanningGuide.tsx
+++ packages/sif-soknad-ui/src/components/picture-scanning-guide/PictureScanningGuide.tsx
@@ -1,7 +1,7 @@
 import './pictureScanningGuide.css';
 
-import { ExternalLinkIcon } from '@navikt/aksel-icons';
-import { BodyLong, Box, Heading, HStack, Link, List, ReadMore, VStack } from '@navikt/ds-react';
+import { CheckmarkCircleFillIcon, ExternalLinkIcon, XMarkOctagonFillIcon } from '@navikt/aksel-icons';
+import { BodyLong, Box, Heading, HGrid, HStack, Link, List, ReadMore, VStack } from '@navikt/ds-react';
 
 import { useSifSoknadUiIntl } from '../../i18n';
 import ScanningIcon from './ScanningIcon';
@@ -34,7 +34,13 @@ const ScanningExample = ({
         <Box marginBlock="space-0 space-16">{image}</Box>
         <Heading size="xsmall" level={headingLevel} spacing={true}>
             <HStack gap="space-4" align="center">
-                <span aria-hidden="true">{isGood ? '✓' : '✗'}</span>
+                <span aria-hidden="true">
+                    {isGood ? (
+                        <CheckmarkCircleFillIcon style={{ color: 'var(--ax-bg-success-strong)' }} />
+                    ) : (
+                        <XMarkOctagonFillIcon style={{ color: 'var(--ax-bg-danger-strong)' }} />
+                    )}
+                </span>
                 <span>{statusText}</span>
             </HStack>
         </Heading>
@@ -50,7 +56,7 @@ export const PictureScanningGuide = ({ headingLevel = '2' }: Props) => {
 
     return (
         <ReadMore header={text('@sifSoknadUi.psg.expandable.tittel')}>
-            <div className="pictureScanningGuide">
+            <div>
                 <BodyLong as="div">
                     <VStack gap="space-24">
                         <Heading level={headingLevel} size="medium">
@@ -90,44 +96,36 @@ export const PictureScanningGuide = ({ headingLevel = '2' }: Props) => {
                             <Heading level={childLevel} size="small" spacing={true}>
                                 {text('@sifSoknadUi.psg.icon.heading')}
                             </Heading>
-                            <div className="pictureScanningGuide__body">
-                                <div className="pictureScanningGuide__cell">
-                                    <ScanningExample
-                                        image={<ScanningIcon status="good" height={svgIconHeight} />}
-                                        isGood={true}
-                                        statusText={text('@sifSoknadUi.psg.good')}
-                                        description={text('@sifSoknadUi.psg.icon.label.good')}
-                                        headingLevel={exampleLevel}
-                                    />
-                                </div>
-                                <div className="pictureScanningGuide__cell">
-                                    <ScanningExample
-                                        image={<ScanningIcon status="keystone" height={svgIconHeight} />}
-                                        isGood={false}
-                                        statusText={text('@sifSoknadUi.psg.bad')}
-                                        description={text('@sifSoknadUi.psg.icon.label.keystone')}
-                                        headingLevel={exampleLevel}
-                                    />
-                                </div>
-                                <div className="pictureScanningGuide__cell">
-                                    <ScanningExample
-                                        image={<ScanningIcon status="horizontal" height={svgIconHeight} />}
-                                        isGood={false}
-                                        statusText={text('@sifSoknadUi.psg.bad')}
-                                        description={text('@sifSoknadUi.psg.icon.label.horizontal')}
-                                        headingLevel={exampleLevel}
-                                    />
-                                </div>
-                                <div className="pictureScanningGuide__cell">
-                                    <ScanningExample
-                                        image={<ScanningIcon status="shadow" height={svgIconHeight} />}
-                                        isGood={false}
-                                        statusText={text('@sifSoknadUi.psg.bad')}
-                                        description={text('@sifSoknadUi.psg.icon.label.shadow')}
-                                        headingLevel={exampleLevel}
-                                    />
-                                </div>
-                            </div>
+                            <HGrid columns={{ xs: 1, sm: 2, md: 4 }} gap="space-16">
+                                <ScanningExample
+                                    image={<ScanningIcon status="good" height={svgIconHeight} />}
+                                    isGood={true}
+                                    statusText={text('@sifSoknadUi.psg.good')}
+                                    description={text('@sifSoknadUi.psg.icon.label.good')}
+                                    headingLevel={exampleLevel}
+                                />
+                                <ScanningExample
+                                    image={<ScanningIcon status="keystone" height={svgIconHeight} />}
+                                    isGood={false}
+                                    statusText={text('@sifSoknadUi.psg.bad')}
+                                    description={text('@sifSoknadUi.psg.icon.label.keystone')}
+                                    headingLevel={exampleLevel}
+                                />
+                                <ScanningExample
+                                    image={<ScanningIcon status="horizontal" height={svgIconHeight} />}
+                                    isGood={false}
+                                    statusText={text('@sifSoknadUi.psg.bad')}
+                                    description={text('@sifSoknadUi.psg.icon.label.horizontal')}
+                                    headingLevel={exampleLevel}
+                                />
+                                <ScanningExample
+                                    image={<ScanningIcon status="shadow" height={svgIconHeight} />}
+                                    isGood={false}
+                                    statusText={text('@sifSoknadUi.psg.bad')}
+                                    description={text('@sifSoknadUi.psg.icon.label.shadow')}
+                                    headingLevel={exampleLevel}
+                                />
+                            </HGrid>{' '}
                             <Link
                                 target="_blank"
                                 rel="noopener noreferrer"
