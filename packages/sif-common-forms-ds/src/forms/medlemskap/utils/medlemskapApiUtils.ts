fbdcdec6384a08a591c1d2dd166194b8863cea7c
frodehansen2 <frode.hansen@bekk.no>
2026-05-15 16:30:28 +0200

    fix: fiks sirkulære imports og oppdater yarn-referanser i kode
    Fiks sirkulære imports i sif-common-forms-ds og sif-common-soknad-ds:
    - Erstatt interne imports fra '@navikt/sif-common-forms-ds' med relative stier
    - Fiks import av SoknadHeader til relativ sti i ErrorBoundary
    - Fiks import av ApplicationPictogram til relativ sti i VelkommenPageHeader
    
    Oppdater HTML-template-referanser fra yarn til pnpm

--- packages/sif-common-forms-ds/src/forms/medlemskap/utils/medlemskapApiUtils.ts
+++ packages/sif-common-forms-ds/src/forms/medlemskap/utils/medlemskapApiUtils.ts
@@ -1,7 +1,7 @@
 import { getCountryName } from '@navikt/sif-common-formik-ds';
-import { BostedUtland } from '@navikt/sif-common-forms-ds';
 import { dateToISODate } from '@navikt/sif-common-utils';
 
+import type { BostedUtland } from '../../bosted-utland/types';
 import { BostedUtlandApiData, MedlemskapApiData } from '../types';
 import { MedlemskapSøknadsdata } from '../types/MedlemskapSøknadsdata';
 
