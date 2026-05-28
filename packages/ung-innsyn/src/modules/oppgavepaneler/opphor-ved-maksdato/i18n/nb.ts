5254325666b59dc3b9a36e00bbd919da9be005f7
frodehansen2 <frode.hansen@bekk.no>
2026-05-28 12:58:56 +0200

    Tekstjustering

--- packages/ung-innsyn/src/modules/oppgavepaneler/opphor-ved-maksdato/i18n/nb.ts
+++ packages/ung-innsyn/src/modules/oppgavepaneler/opphor-ved-maksdato/i18n/nb.ts
@@ -1,7 +1,8 @@
 export const automatiskOpphorMessages_nb = {
     '@ungInnsyn.oppgavetype.BEKREFT_OPPHOR_VED_MAKSDATO.paneltittel': 'Se og gi tilbakemelding på sluttdato',
     '@ungInnsyn.oppgavetype.BEKREFT_OPPHOR_VED_MAKSDATO.oppgavetittel': 'Tilbakemelding på sluttdato',
-    '@ungInnsyn.oppgavetype.BEKREFT_OPPHOR_VED_MAKSDATO.info': 'Perioden din med ungdomsprogramytelsen er snart over.',
+    '@ungInnsyn.oppgavetype.BEKREFT_OPPHOR_VED_MAKSDATO.info':
+        'Sjekk datoen for din siste dag med ungdomsprogramytelsen.',
     '@ungInnsyn.oppgavetype.BEKREFT_OPPHOR_VED_MAKSDATO.harTilbakemeldingSpørsmål':
         'Har du en tilbakemelding på sluttdatoen?',
     '@ungInnsyn.oppgavetype.BEKREFT_OPPHOR_VED_MAKSDATO.harIkkeUttalelseLabel': 'Nei',
@@ -15,7 +16,7 @@ export const automatiskOpphorMessages_nb = {
     '@ungInnsyn.automatiskOpphor.oppgavetekst.2':
         'Du får denne meldingen slik at du kan komme med en tilbakemelding på datoen.',
     '@ungInnsyn.automatiskOpphor.oppgavetekst.3':
-        'Ingen tilbakemelding? Kryss av på “Nei” med en gang og send inn svaret ditt. Jo fortere du svarer, jo fortere får vi behandlet saken din.',
+        'Ingen tilbakemelding? Kryss av på “Nei” med en gang og send inn svaret ditt.',
     '@ungInnsyn.automatiskOpphor.oppgavetekst.svarfrist': 'Fristen for å svare er senest {formatertFrist}.',
     '@ungInnsyn.automatiskOpphor.oppgavetekst.5':
         'Hvis vi ikke hører fra deg innen svarfristen har gått ut, bruker vi {formatertDato} som siste dag med ytelsen når vi behandler saken din.',
