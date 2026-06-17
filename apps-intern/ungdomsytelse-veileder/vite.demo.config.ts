fdba0a8340ae3760a3f6f6351381aa000072a4af
frodehansen2 <frode.hansen@bekk.no>
2026-06-17 10:00:38 +0200

    Skru på forleng feature i demo

--- apps-intern/ungdomsytelse-veileder/vite.demo.config.ts
+++ apps-intern/ungdomsytelse-veileder/vite.demo.config.ts
@@ -11,7 +11,7 @@ const appSettings: AppEnv = {
     PUBLIC_PATH: '/sif-brukerdialog/ungdomsytelse-veileder',
     GITHUB_REF_NAME: 'local',
 
-    SIF_PUBLIC_FEATURE_FORLENGE_PERIODE: 'off',
+    SIF_PUBLIC_FEATURE_FORLENGE_PERIODE: 'on',
     SIF_PUBLIC_FEATURE_SLETT_AKTIV_DELTAKELSE: 'on',
     SIF_PUBLIC_TILLAT_TIDLIG_INNMELDING: 'on',
 
