5fb81df83635412d8ec0cccedf9a3c8dbb967ceb
frodehansen2 <frode.hansen@bekk.no>
2026-06-20 22:41:36 +0200

    Oppdatere setNow i pw. Droppe MockDate

--- apps/endringsmelding-pleiepenger/playwright/utils/setNow.ts
+++ apps/endringsmelding-pleiepenger/playwright/utils/setNow.ts
@@ -1,10 +1,24 @@
 import { Page } from '@playwright/test';
-import MockDate from 'mockdate';
 
 const testDate = new Date('2023-02-15T12:00:00');
 
+const getSetDateScript = (timestamp: number) => `
+{
+    Date = class extends Date {
+      constructor(...args) {
+        if (args.length === 0) {
+          super(${timestamp});
+        } else {
+          super(...args);
+        }
+      }
+    }
+    const __DateNowOffset = ${timestamp} - Date.now();
+    const __DateNow = Date.now;
+    Date.now = () => __DateNow() + __DateNowOffset;
+  }
+`;
+
 export const setNow = async (page: Page, date: Date = testDate) => {
-    await page.addInitScript((timestamp: number) => {
-        MockDate.set(new Date(timestamp));
-    }, date.valueOf());
+    await page.addInitScript(getSetDateScript(date.valueOf()));
 };
--- apps/ungdomsytelse-deltaker/playwright/utils/setNow.ts
+++ apps/ungdomsytelse-deltaker/playwright/utils/setNow.ts
@@ -1,12 +1,26 @@
 import { Page } from '@playwright/test';
-import MockDate from 'mockdate';
 
 import { getMockToday } from '../../mock/utils/mockDate';
 
 const testDate = new Date(getMockToday());
 
+const getSetDateScript = (timestamp: number) => `
+{
+    Date = class extends Date {
+      constructor(...args) {
+        if (args.length === 0) {
+          super(${timestamp});
+        } else {
+          super(...args);
+        }
+      }
+    }
+    const __DateNowOffset = ${timestamp} - Date.now();
+    const __DateNow = Date.now;
+    Date.now = () => __DateNow() + __DateNowOffset;
+  }
+`;
+
 export const setNow = async (page: Page, date: Date = testDate) => {
-    await page.addInitScript((dateString: string) => {
-        MockDate.set(new Date(dateString));
-    }, date.toISOString());
+    await page.addInitScript(getSetDateScript(date.valueOf()));
 };
