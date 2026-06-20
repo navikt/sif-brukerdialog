dd2326f1e704f73e993588564dbb1867a3595b25
frodehansen2 <frode.hansen@bekk.no>
2026-06-20 22:47:51 +0200

    Oppdatere setNow i pw. Droppe MockDate

--- apps/endringsmelding-pleiepenger/playwright/utils/setNow.ts
+++ apps/endringsmelding-pleiepenger/playwright/utils/setNow.ts
@@ -1,10 +1,7 @@
 import { Page } from '@playwright/test';
-import MockDate from 'mockdate';
 
 const testDate = new Date('2023-02-15T12:00:00');
 
 export const setNow = async (page: Page, date: Date = testDate) => {
-    await page.addInitScript((timestamp: number) => {
-        MockDate.set(new Date(timestamp));
-    }, date.valueOf());
+    await page.clock.setFixedTime(date);
 };
