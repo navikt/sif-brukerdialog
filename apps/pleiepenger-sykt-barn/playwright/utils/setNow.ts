d5ffb47a519230a328728d53cf3e9cd02b0add04
frodehansen2 <frode.hansen@bekk.no>
2026-06-20 22:49:15 +0200

    Bruke playwrights clock.setFixedTime

--- apps/pleiepenger-sykt-barn/playwright/utils/setNow.ts
+++ apps/pleiepenger-sykt-barn/playwright/utils/setNow.ts
@@ -2,23 +2,6 @@ import { Page } from '@playwright/test';
 
 const testDate = new Date('2023-01-01T12:00:00');
 
-const getSetDateScript = (timestamp: number) => `
-{
-    Date = class extends Date {
-      constructor(...args) {
-        if (args.length === 0) {
-          super(${timestamp});
-        } else {
-          super(...args);
-        }
-      }
-    }
-    const __DateNowOffset = ${timestamp} - Date.now();
-    const __DateNow = Date.now;
-    Date.now = () => __DateNow() + __DateNowOffset;
-  }
-`;
-
 export const setNow = async (page: Page, date: Date = testDate) => {
-    await page.addInitScript(getSetDateScript(date.valueOf()));
+    await page.clock.setFixedTime(date);
 };
--- apps/ungdomsytelse-deltaker/playwright/utils/setNow.ts
+++ apps/ungdomsytelse-deltaker/playwright/utils/setNow.ts
@@ -4,23 +4,6 @@ import { getMockToday } from '../../mock/utils/mockDate';
 
 const testDate = new Date(getMockToday());
 
-const getSetDateScript = (timestamp: number) => `
-{
-    Date = class extends Date {
-      constructor(...args) {
-        if (args.length === 0) {
-          super(${timestamp});
-        } else {
-          super(...args);
-        }
-      }
-    }
-    const __DateNowOffset = ${timestamp} - Date.now();
-    const __DateNow = Date.now;
-    Date.now = () => __DateNow() + __DateNowOffset;
-  }
-`;
-
 export const setNow = async (page: Page, date: Date = testDate) => {
-    await page.addInitScript(getSetDateScript(date.valueOf()));
+    await page.clock.setFixedTime(date);
 };
