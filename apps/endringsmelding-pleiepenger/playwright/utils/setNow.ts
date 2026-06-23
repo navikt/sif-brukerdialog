f217587c4961590f093aeae57eb8aa31b771b556
frodehansen2 <frode.hansen@bekk.no>
2026-06-23 14:56:29 +0200

    console.log tidssone i pw-test

--- apps/endringsmelding-pleiepenger/playwright/utils/setNow.ts
+++ apps/endringsmelding-pleiepenger/playwright/utils/setNow.ts
@@ -6,4 +6,8 @@ const testDate = new Date('2023-02-15T12:00:00Z');
 
 export const setNow = async (page: Page, date: Date = testDate) => {
     await page.clock.setFixedTime(date);
+
+    const browserTz = await page.evaluate(() => Intl.DateTimeFormat().resolvedOptions().timeZone);
+    // eslint-disable-next-line no-console
+    console.log(`[setNow] Nettlesertidssone: ${browserTz}`);
 };
