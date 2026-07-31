aa755e984a5ed2177bba4f21efacb20d2f7f2203
frodehansen2 <frode.hansen@bekk.no>
2026-07-31 08:00:29 +0200

    nn update

--- packages/sif-soknad-ui/src/pages/error-page/i18n/nn.ts
+++ packages/sif-soknad-ui/src/pages/error-page/i18n/nn.ts
@@ -4,7 +4,7 @@ export const errorPageMessages_nn: Record<keyof typeof errorPageMessages_nb, str
     '@sifSoknadUi.errorPage.documentTitle': 'Noko gjekk gale',
     '@sifSoknadUi.initialDataError.title': 'Noko gjekk gale',
     '@sifSoknadUi.initialDataError.description':
-        'Vi klarte dessverre ikkje å hente informasjonen vi treng for å starte søknaden. Prøv å laste sida på nytt. Dersom feilen held fram, prøv igjen litt seinare.',
+        'Vi klarte dessverre ikkje å hente informasjonen me treng for å starte søknaden. Prøv å laste sida på nytt. Dersom feilen held fram, prøv igjen litt seinare.',
     '@sifSoknadUi.applicationUnavailable.title': 'Søknaden er dessverre ikkje tilgjengeleg',
-    '@sifSoknadUi.applicationUnavailable.description': 'Vi jobber så raskt vi kan med å få den tilgjengelig.',
+    '@sifSoknadUi.applicationUnavailable.description': 'Me jobbar så raskt me kan med å få den tilgjengeleg.',
 };
