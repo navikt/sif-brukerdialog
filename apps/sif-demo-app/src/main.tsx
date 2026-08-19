7d3059dc9b5500bdb1b00e02c2ce7072260c98c5
frodehansen2 <frode.hansen@bekk.no>
2026-08-19 15:54:57 +0200

    demo init

--- apps/sif-demo-app/src/main.tsx
+++ apps/sif-demo-app/src/main.tsx
@@ -1,8 +1,12 @@
+import { init } from '@nais/apm';
+import { getMaybeEnv } from '@navikt/sif-common-env';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { enableMocking } from '../mock/enableMocking';
 import { App } from './App';
+
+void init({ app: 'sif-demo-app', namespace: 'dusseldorf', version: getMaybeEnv('APP_VERSION') });
 
 enableMocking().then(() => {
     createRoot(document.getElementById('root')!).render(
