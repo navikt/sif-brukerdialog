8f6cbb70ddb6f32f2cc550bd60d4cb805fbf57d0
frodehansen2 <frode.hansen@bekk.no>
2026-08-25 08:15:45 +0200

    Justere filter på dekoratøren

--- packages/sif-apm/src/initApm.ts
+++ packages/sif-apm/src/initApm.ts
@@ -8,13 +8,16 @@ import { init, type InitOptions } from '@nais/apm';
  */
 export const initApm = (options: InitOptions): void => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    init({ ...options, beforeSend: (item: any) => {
-        if (item?.type === 'exception') {
-            const stacktrace = item.payload?.errors?.[0]?.stacktrace ?? '';
-            if (stacktrace.includes('cdn.nav.no/personbruker/nav-dekoratoren')) {
-                return null;
+    init({
+        ...options,
+        beforeSend: (item: any) => {
+            if (item?.type === 'exception') {
+                const stacktrace = item.payload?.errors?.[0]?.stacktrace ?? '';
+                if (stacktrace.includes('personbruker/nav-dekoratoren')) {
+                    return null;
+                }
             }
-        }
-        return item;
-    }});
+            return item;
+        },
+    });
 };
