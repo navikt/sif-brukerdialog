b1bc32d2539abcdbf24c02706788306c541a2b91
frodehansen2 <frode.hansen@bekk.no>
2026-08-26 12:28:33 +0200

    Layout for Todo

--- apps/aktivitetspenger-soknad/src/app/components/Todo.tsx
+++ apps/aktivitetspenger-soknad/src/app/components/Todo.tsx
@@ -1,4 +1,4 @@
-import { Box, InfoCard, VStack } from '@navikt/ds-react';
+import { Bleed, Box, InfoCard, Tag } from '@navikt/ds-react';
 import { ExclamationmarkTriangleFillIcon } from '@navikt/aksel-icons';
 
 interface Props {
@@ -8,10 +8,17 @@ interface Props {
 
 export const Todo = ({ children, spacing = true }: Props) => (
     <Box marginBlock={spacing ? 'space-0 space-24' : 'space-0'}>
-        <VStack gap="space-16">
+        <Bleed marginInline={'space-8'}>
+            <div style={{ zIndex: 2, position: 'relative' }}>
+                <Tag variant="strong" size="xsmall" data-color="danger" style={{ rotate: '-12deg' }}>
+                    Todo
+                </Tag>
+            </div>
+        </Bleed>
+        <Bleed marginBlock={'space-8'}>
             <InfoCard data-color="danger" size="small">
                 <InfoCard.Message icon={<ExclamationmarkTriangleFillIcon aria-hidden />}>{children}</InfoCard.Message>
             </InfoCard>
-        </VStack>
+        </Bleed>
     </Box>
 );
