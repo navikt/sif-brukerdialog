904d31e4290a300d93ae764ac27253e510b95bb5
frodehansen2 <frode.hansen@bekk.no>
2026-08-26 14:27:58 +0200

    StepFormValues = object fix

--- packages/sif-soknad-app/src/types.ts
+++ packages/sif-soknad-app/src/types.ts
@@ -2,11 +2,10 @@ import { ReactNode } from 'react';
 
 /**
  * Basetype for skjema-verdier i ett steg.
- * Bruker {} i stedet for Record<string, unknown> så stegspesifikke interfaces
+ * Bruker object i stedet for Record<string, unknown> så stegspesifikke interfaces
  * ikke trenger en index-signatur.
  */
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export type StepFormValues = {};
+export type StepFormValues = object;
 
 /**
  * Mellomlagring-blob som rammeverket lagrer og henter som opak struktur.
