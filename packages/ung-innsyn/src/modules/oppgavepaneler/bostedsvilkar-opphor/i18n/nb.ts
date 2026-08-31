dd309e873a5077d6e16d21616c433089a912fc00
frodehansen2 <frode.hansen@bekk.no>
2026-08-31 17:38:47 +0200

    Tekstjusteringer [gh-pages]

--- packages/ung-innsyn/src/modules/oppgavepaneler/bostedsvilkar-opphor/i18n/nb.ts
+++ packages/ung-innsyn/src/modules/oppgavepaneler/bostedsvilkar-opphor/i18n/nb.ts
@@ -17,9 +17,9 @@ export const bostedVilkårOpphørMessages_nb = {
         felles['@ungInnsyn.oppgavebekreftelse.felles.kvitteringTekst'],
 
     '@ungInnsyn.bostedVilkårOpphørOppgave.IKKE_BOSATTADRESSE_I_TRONDHEIM':
-        'Vi har fått opplysninger om at du fra {fom} ikke lenger bor (har bostedsadresse) i Trondheim kommune. Du må ha bostedsadresse i Trondheim kommune for å få aktivitetspenger.',
+        'Vi har fått opplysninger om at du fra {fom} ikke lenger bor i Trondheim kommune. Du må ha bostedsadresse i Trondheim kommune for å få aktivitetspenger',
     '@ungInnsyn.bostedVilkårOpphørOppgave.IKKE_BOSTEDSADRESSE_OG_IKKE_FOLKEREGISTRERT_I_TRONDHEIM':
-        'Vi har fått opplysninger om at du fra {fom} ikke lenger bor (har bostedsadresse) i Trondheim kommune, og at du heller ikke er folkeregistrert der. Du må ha bostedsadresse i Trondheim kommune for å få aktivitetspenger.',
+        'Vi har fått opplysninger om at du fra {fom} ikke lenger bor i Trondheim kommune, og at du heller ikke er folkeregistrert der. Du må ha bostedsadresse i Trondheim kommune for å få aktivitetspenger.',
     '@ungInnsyn.bostedVilkårOpphørOppgave.STUDIE_ELLER_ARBEIDSSTED_UTENFOR_TRONDHEIM':
         'Vi har fått opplysninger om at du fra {fom} ikke lenger har studie- eller arbeidssted i Trondheim kommune. Du må bo i Trondheim kommune for å få aktivitetspenger.',
     '@ungInnsyn.bostedVilkårOpphørOppgave.ANNET':
