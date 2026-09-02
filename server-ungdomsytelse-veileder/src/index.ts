aedb018cde6a0a984f1be166cab936f2ef157ed0
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-09-02 13:02:32 +0200

    Bytter til chainguard image i de andre appene også

--- server-ungdomsytelse-veileder/src/index.ts
+++ server-ungdomsytelse-veileder/src/index.ts
@@ -1,8 +1,24 @@
 import server from './server.js';
 import config from './utils/serverConfig.js';
+import log from './utils/log.js';
 
 const port = config.app.port;
 
-server.listen(port, () => {
+const runningServer = server.listen(port, () => {
     console.log(`Starting server at ${port}`);
+});
+
+process.on('SIGTERM', () => {
+    log.info('SIGTERM received.');
+    // Vent litt før stopp starter, så Kubernetes load balancer får tid til å rute nye requests til andre pods
+    setTimeout(() => {
+        runningServer.close(error => {
+            if (error) {
+                log.warning('SIGTERM received on non-open server.');
+            } else {
+                log.info('SIGTERM stopped server.');
+            }
+            process.exit(0);
+        });
+    }, 2_000);
 });
