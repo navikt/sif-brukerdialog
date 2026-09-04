149a230bcf702b5e828c54c6291855dd6ac0b7cb
frodehansen2 <frode.hansen@bekk.no>
2026-09-04 09:29:54 +0200

    Fix import etter renaming av named export

--- apps/ungdomsytelse-deltaker/src/apps/innsyn/i18n/index.ts
+++ apps/ungdomsytelse-deltaker/src/apps/innsyn/i18n/index.ts
@@ -1,15 +1,16 @@
-import { ungUiMessages } from '@sif/ung-innsyn/i18n';
+import { ungInnsynMessages } from '@sif/ung-innsyn/i18n';
 
 import { innsynMessages_nb } from './messages/nb';
 import { innsynMessages_nn } from './messages/nn';
 
 const nb = {
     ...innsynMessages_nb,
-    ...ungUiMessages.nb,
+    ...ungInnsynMessages.nb,
 };
 
 const nn: Record<keyof typeof nb, string> = {
     ...nb,
+    ...ungInnsynMessages.nn,
     ...innsynMessages_nn,
 };
 export const innsynMessages = {
