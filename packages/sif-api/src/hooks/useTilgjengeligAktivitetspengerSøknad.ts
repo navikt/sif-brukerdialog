dbb1a88ca460e8261d936d81b464ca15621a30fd
frodehansen2 <frode.hansen@bekk.no>
2026-09-08 16:59:02 +0200

    Fix

--- packages/sif-api/src/hooks/useTilgjengeligAktivitetspengerSøknad.ts
+++ packages/sif-api/src/hooks/useTilgjengeligAktivitetspengerSøknad.ts
@@ -10,15 +10,14 @@ import { TilgjengeligSøknadResponse } from '@navikt/ung-brukerdialog-api';
  *
  * Denne informasjonen endrer seg når en søknad er sendt inn, ellers sjelden.
  * - query invalideres ved innsending, ellers brukes cached data inntil staleTime er nådd
- * - staleTime: 1000 * 30 * 20 - Data er alltid fresh i 20 minutter
- * - Ingen refetch på focus/mount/reconnect
+ * - staleTime: 1000 * 30 * 20 - Data er alltid fresh i 10 minutter
  */
 export const useTilgjengeligAktivitetspengerSøknad = (enabled = true) => {
     return useQuery<TilgjengeligSøknadResponse, ApiError>({
         queryKey: sifApiQueryKeys.aktivitetspengerTilgjengeligSøknad,
         queryFn: tilgjengeligAktivitetspengerSoknad,
         enabled,
-        staleTime: 1000 * 30 * 20, // 20 minutter
+        staleTime: 1000 * 30 * 20, // 10 minutter
         retry: 1,
     });
 };
