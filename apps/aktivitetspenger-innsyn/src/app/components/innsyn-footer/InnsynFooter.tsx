ec6ff2441f31ac9441c6d4c1380261db81717051
frodehansen2 <frode.hansen@bekk.no>
2026-09-14 12:45:04 +0200

    Egen footer [gh-pages]

--- apps/aktivitetspenger-innsyn/src/app/components/innsyn-footer/InnsynFooter.tsx
+++ apps/aktivitetspenger-innsyn/src/app/components/innsyn-footer/InnsynFooter.tsx
@@ -1,27 +1,38 @@
-import { BodyLong, Heading, Link, VStack } from '@navikt/ds-react';
+import { Bleed, BodyLong, Box, Heading, Link, VStack } from '@navikt/ds-react';
 import { ReactNode } from 'react';
 import { AppText } from '../../i18n';
 import getLenker from '../../lenker';
+import { InnsynPageBoundary } from '@sif/ung-innsyn/components';
 
 export const InnsynFooter = () => {
     return (
-        <div>
-            <Heading level="2" size="medium" spacing>
-                <AppText id="page.forside.om.tittel" />
-            </Heading>
-            <VStack gap="space-16">
-                <BodyLong>
-                    <AppText id="page.forside.om.tekst.1" />
-                </BodyLong>
-                <BodyLong>
-                    <AppText
-                        id="page.forside.om.tekst.2"
-                        values={{
-                            Lenke: (chunks: ReactNode) => <Link href={getLenker().aktivitetspenger}>{chunks}</Link>,
-                        }}
-                    />
-                </BodyLong>
-            </VStack>
-        </div>
+        <footer>
+            <Bleed marginInline="full" asChild>
+                <Box background="brand-blue-moderateA" paddingBlock="space-24 space-48">
+                    <InnsynPageBoundary>
+                        <Box className="mx-auto">
+                            <Heading level="2" size="medium" spacing>
+                                <AppText id="page.forside.om.tittel" />
+                            </Heading>
+                            <VStack gap="space-16">
+                                <BodyLong>
+                                    <AppText id="page.forside.om.tekst.1" />
+                                </BodyLong>
+                                <BodyLong>
+                                    <AppText
+                                        id="page.forside.om.tekst.2"
+                                        values={{
+                                            Lenke: (chunks: ReactNode) => (
+                                                <Link href={getLenker().aktivitetspenger}>{chunks}</Link>
+                                            ),
+                                        }}
+                                    />
+                                </BodyLong>
+                            </VStack>
+                        </Box>
+                    </InnsynPageBoundary>
+                </Box>
+            </Bleed>
+        </footer>
     );
 };
