1233b6d65ddcc04c0d36d23ca29e7e96e4f48960
frodehansen2 <frode.hansen@bekk.no>
2026-09-15 10:29:45 +0200

    Ugyldig parameter mock respons

--- apps/aktivitetspenger-soknad/mock/msw/handlers.ts
+++ apps/aktivitetspenger-soknad/mock/msw/handlers.ts
@@ -24,7 +24,7 @@ export const handlers = [
         return HttpResponse.json(store.get().kontonummer);
     }),
 
-    http.post('**/aktivitetspenger/soknad/innsending', async () => {
+    http.post('**/aktivitetspenger/soknad/innsending-', async () => {
         await delay(300);
         return HttpResponse.json({}, { status: 200 });
     }),
@@ -34,26 +34,46 @@ export const handlers = [
         return HttpResponse.json({}, { status: 500 });
     }),
 
-    http.post('**/aktivitetspenger/soknad/innsending-parameterfeil', async () => {
+    http.post('**/aktivitetspenger/soknad/innsending', async () => {
         await delay(300);
-        return HttpResponse.json(
-            {
-                type: 'https://k9-brukerdialog-prosessering/problem-details/invalid-request-parameters',
-                title: 'invalid-request-parameters',
-                status: 400,
-                detail: 'Forespørselen inneholder valideringsfeil',
-                instance: '/aktivitetspenger/soknad/innsending',
-                invalid_parameters: [
-                    {
-                        invalidValue: '',
-                        parameterName: 'startdato',
-                        parameterType: 'ENTITY',
-                        reason: 'Startdato er ugyldig',
-                    },
-                ],
-            },
-            { status: 400, headers: { 'Content-Type': 'application/problem+json' } },
-        );
+        const errorDetails = {
+            violations: [
+                {
+                    invalidValue: null,
+                    parameterName: 'Aktivitetspengersøknad.medlemskap',
+                    parameterType: 'ENTITY',
+                    reason: 'Aktivitetspengersøknad.medlemskap er påkrevd, men var ikke satt',
+                },
+                {
+                    invalidValue: null,
+                    parameterName: 'MedlemskapAktivitetspenger.utenlandsopphold',
+                    parameterType: 'ENTITY',
+                    reason: 'MedlemskapAktivitetspenger.utenlandsopphold er påkrevd, men var ikke satt',
+                },
+                {
+                    invalidValue: null,
+                    parameterName: 'UtenlandsoppholdAktivitetspenger.land',
+                    parameterType: 'ENTITY',
+                    reason: 'UtenlandsoppholdAktivitetspenger.land er påkrevd, men var ikke satt',
+                },
+                {
+                    invalidValue: null,
+                    parameterName: '[].null',
+                    parameterType: 'ENTITY',
+                    reason: '[].null er påkrevd, men var ikke satt',
+                },
+            ],
+            detail: 'Forespørselen inneholder valideringsfeil',
+            instance: 'https://aktivitetspenger-soknad.intern.dev.nav.no/aktivitetspenger/soknad/innsending',
+            properties: null,
+            status: 400,
+            title: 'invalid-request-parameters',
+            type: '/problem-details/invalid-request-parameters',
+        };
+        return HttpResponse.json(errorDetails, {
+            status: 400,
+            headers: { 'Content-Type': 'application/problem+json' },
+        });
     }),
 
     http.get(`**/aktivitetspenger/soknad/tilgjengelig`, () => HttpResponse.json(store.get().tilgjengeligSøknad)),
